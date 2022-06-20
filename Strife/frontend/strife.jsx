ed960635c82b6f0c253cc9c6bf357a0dc380a8bd
miker704 <miker704@gmail.com>
2022-06-19 20:17:30 -0400

    updates to setup

--- Strife/frontend/strife.jsx
+++ Strife/frontend/strife.jsx
@@ -1,3 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+
+document.addEventListener("DOMContentLoaded", ()=>{
+	ReactDom.render(
+		<h1>Hello world</h1>,
+		document.getElementById('root')
+	);
+})
