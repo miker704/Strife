c63b01e28e30cda440e6afe6a0998e017b635f66
miker704 <miker704@gmail.com>
2022-06-20 04:56:19 -0400

    updates

---
+++ Strife/db/migrate/20220620083144_create_users.rb
@@ -0,0 +1,22 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.citext :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.boolean :online, default: false, null: false
+      t.string :profile_pic_url
+      t.string :id_tag, null: false
+      t.date :birthday, null: false
+      t.timestamps
+    end
+    add_index :users, :username
+    add_index :users, :email, :unique =>  true
+    add_index :users, :session_token, :unique => true
+    add_index :users, [:username, :id_tag], :unique =>  true
+
+
+    #Ex:- add_index("admin_users", "username")
+  end
+end
