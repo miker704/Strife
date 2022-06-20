15cd89354ee9a3ee08588b019e9ba35f3c395a47
miker704 <miker704@gmail.com>
2022-06-20 14:36:05 -0400

    updates

--- Strife/config/routes.rb
+++ Strife/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
 
+    resources :users
      resource :session, only: [:create, :destroy]
-     resources :users
   end
 
 
--- Strife/frontend/utils/route_util.jsx
+++ Strife/frontend/utils/route_util.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Redirect, Route, withRouter } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { Redirect, Route, withRouter } from "react-router-dom";
 
-const mapStateToProps = state => ({
-  loggedIn: Boolean(state.session.currentUser),
-});
+const mSTP = state => ({
+    loggedIn: Boolean(state.session.currentUser)
+})
 
-const Auth = ({ component: Component, path, loggedIn }) => (
-  <Route
-    path={path}
-    render={props => (
-    loggedIn ? <Redirect to="/" /> : <Component {...props} />
-    )}
-  />
+// redirect users (functional component) 
+// AuthRoute path = "" component = {}/>
+const Auth = ({loggedIn,path, component: Component}) => (
+    <Route
+        path={path}
+        render={props => (
+            loggedIn ? <Redirect to={`/`}/> : <Component {...props}/>
+        )}
+    />
 );
 
-const Protected = ({ component: Component, path, loggedIn }) => (
-  <Route
+const Protected = ({loggedIn,path, component: Component}) => (
+    <Route
     path={path}
     render={props => (
-    loggedIn ? <Component {...props} /> : <Redirect to="/signup" />
+        loggedIn ?  <Component {...props}/> : <Redirect to={`/signup`}/> 
     )}
-  />
+    />
 );
 
-export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
-export const ProtectedRoute = withRouter(connect(mapStateToProps, undefined)(Protected));
+
+
+ export const AuthRoute = withRouter(connect(mSTP)(Auth)) ;
+ export const ProtectedRoute = withRouter(connect(mSTP)(Protected)) ;
