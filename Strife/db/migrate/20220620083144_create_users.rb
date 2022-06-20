09de7b31cd542a314f049dbcc1d7c12c86404eeb
miker704 <miker704@gmail.com>
2022-06-20 05:00:11 -0400

    updates

--- Strife/db/migrate/20220620083144_create_users.rb
+++ Strife/db/migrate/20220620083144_create_users.rb
@@ -2,12 +2,13 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :username, null: false
-      t.citext :email, null: false
+      t.string :email, null: false
       t.string :password_digest, null: false
       t.string :session_token, null: false
       t.boolean :online, default: false, null: false
       t.string :profile_pic_url
       t.string :id_tag, null: false
+      t.integer :color_id, null: false
       t.date :birthday, null: false
       t.timestamps
     end
