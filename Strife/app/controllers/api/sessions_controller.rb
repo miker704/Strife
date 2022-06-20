f1446f7e56992720b7aebfe8e3939dce56c3d416
miker704 <miker704@gmail.com>
2022-06-20 06:18:43 -0400

    updates

--- Strife/app/controllers/api/sessions_controller.rb
+++ Strife/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,21 @@
 class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if @user.nil?
+            render json: ["Invalid Credentials"], status: 401
+        else
+            login(@user)
+            render 'api/users/show'
+        end
+    end
+
+    def destroy
+        if current_user.nil?
+            render json: ['Login Required!'], status: 404
+        else
+            logout
+            render json: {}
+        end
+    end
+
 end
--- Strife/app/controllers/api/users_controller.rb
+++ Strife/app/controllers/api/users_controller.rb
@@ -1,2 +1,37 @@
 class Api::UsersController < ApplicationController
+
+
+    def index 
+
+    end
+
+
+    def show
+
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            render  "api/users/show"
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+
+    end
+
+
+
+
+
+    private
+    def user_params
+        return params.require(:user).permit(:email,:username,:password)
+    end
+
+    
 end
