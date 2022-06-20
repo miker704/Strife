e2a6d4bcba77b2030c0bfb62944667886b328b52
miker704 <miker704@gmail.com>
2022-06-20 06:26:39 -0400

    updates

--- Strife/config/routes.rb
+++ Strife/config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+
+     resource :session, only: [:create, :destroy]
+     resources :users
+  end
+
+
+ 
+
   root to: 'static_pages#root'
 
 end
