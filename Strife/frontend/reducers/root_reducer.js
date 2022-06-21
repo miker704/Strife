233ae0d00f839e03790950c6df58eee198685ee7
miker704 <miker704@gmail.com>
2022-06-21 09:22:54 -0400

    adding reducers and actions for users, entities , errors

--- Strife/frontend/reducers/root_reducer.js
+++ Strife/frontend/reducers/root_reducer.js
@@ -1,8 +1,12 @@
 
 import { combineReducers } from "redux";
 import sessionReducer from "./session_reducer";
+import EntitiesReducer from "./entities_reducer";
+
+
 const rootReducer = combineReducers({
-    session : sessionReducer
+    entities: EntitiesReducer,
+    session: sessionReducer
 })
 
 export default rootReducer;
