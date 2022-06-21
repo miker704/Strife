2010a814fa371e763d93fcb91ac72d375ee1d7d2
miker704 <miker704@gmail.com>
2022-06-21 13:14:52 -0400

    adding reducers and actions for users, entities , errors

--- Strife/frontend/actions/session_actions.js
+++ Strife/frontend/actions/session_actions.js
@@ -3,9 +3,10 @@ import * as SessionAPIUtil from "../utils/session_api_util.js"
 
  export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
  export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+ export const REMOVE_CURRENT_USER  = "REMOVE_CURRENT_USER";
  export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
  export const REMOVE_SESSION_ERRORS = "REMOVE_SESSION_ERRORS";
- 
+
 
  export const receiveCurrentUser = user => {
     return{
@@ -19,6 +20,14 @@ import * as SessionAPIUtil from "../utils/session_api_util.js"
         type: LOGOUT_CURRENT_USER,
     }
  }
+
+
+export const removeCurrentUser = (userId) => {
+    return {
+            type: REMOVE_CURRENT_USER,
+            userId
+    }
+}
 
 
 
