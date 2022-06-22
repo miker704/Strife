2c67b6f68e43c60af9082f39eca953a056bb30c7
miker704 <miker704@gmail.com>
2022-06-22 03:00:24 -0400

    adding reducers and actions for users, entities , errors- updated session_actions for removal of user and error receive and remove

--- Strife/frontend/reducers/errors_reducer.js
+++ Strife/frontend/reducers/errors_reducer.js
@@ -1,3 +1,12 @@
 //reducer for handling errors across the entire app
 
+import { combineReducers } from "redux";
+import sessionErrorsReducer from "./session_errors_reducer";
 
+
+const errorsReducer = combineReducers({
+    sessionError: sessionErrorsReducer
+})
+
+
+export default errorsReducer;
