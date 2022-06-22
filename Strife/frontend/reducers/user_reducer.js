8b4913147df68944ac7adfd985fab723eafedc21
miker704 <miker704@gmail.com>
2022-06-22 12:57:36 -0400

    adding reducers and actions for users, entities , errors- updated session_actions for removal of user and error receive and remove

---
+++ Strife/frontend/reducers/user_reducer.js
@@ -0,0 +1,21 @@
+import { LOGOUT_CURRENT_USER,RECEIVE_CURRENT_USER, REMOVE_CURRENT_USER } from "../actions/session_actions";
+
+const userReducer = (state ={},action) => {
+
+    Object.freeze(state);
+
+    switch(action.type){
+        case RECEIVE_CURRENT_USER:
+        return {currentUser : action.user};
+        case REMOVE_CURRENT_USER:
+            return {};
+        case LOGOUT_CURRENT_USER:
+            return {} ;
+        default:
+            return state;
+    }
+
+}
+
+
+export default userReducer;
