134f67bd949d2d9edf3b436e82a32821eaec033b
miker704 <miker704@gmail.com>
2022-06-24 05:19:55 -0400

    adding reducers and actions for users, entities , errors- updated session_actions for removal of user and error receive and remove

--- Strife/db/migrate/20220623145552_create_messages.rb
+++ Strife/db/migrate/20220623145552_create_messages.rb
@@ -3,6 +3,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
       t.integer :channel_id, null: false
       t.integer :author_id, null: false
+      t.integer :parent_message_id
+      t.integer :replier_id
       t.text  :body, null: false
 
       t.timestamps
@@ -10,7 +12,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
 
     add_index :messages, :channel_id
     add_index :messages, :author_id
-
+    add_index :messages, :parent_message_id
+    #Ex:- add_index("admin_users", "username")
     #Ex:- add_index("admin_users", "username")
   end
 end
