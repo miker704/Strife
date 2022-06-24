84cf66e717a2b79c75b15285aa17b03e13d2d0ea
miker704 <miker704@gmail.com>
2022-06-24 06:11:04 -0400

    adding reducers and actions for users, entities , errors- updated session_actions for removal of user and error receive and remove

--- Strife/db/migrate/20220624092053_create_dm_members.rb
+++ Strife/db/migrate/20220624092053_create_dm_members.rb
@@ -5,9 +5,9 @@ class CreateDmMembers < ActiveRecord::Migration[5.2]
       t.integer :dm_member_id, null: false
 
       t.timestamps
-      add_index :dm_members, [:dm_server_id,:dm_member_id], :unique => true
-      add_index :dm_members, :dm_server_id
-      add_index :dm_members, :dm_member_id
     end
+    add_index :dm_members, [:dm_server_id,:dm_member_id], :unique => true
+    add_index :dm_members, :dm_server_id
+    add_index :dm_members, :dm_member_id
   end
 end
