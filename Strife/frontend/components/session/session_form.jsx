f8ec9d05509dc8234ccf4a4ef2b822f2cbc7be77
miker704 <miker704@gmail.com>
2022-06-26 15:08:27 -0400

    changed stat.errors.sessionerrors in connects for session form to state.error.session as it wss changed a while ago to when error reducer was changed

--- Strife/frontend/components/session/session_form.jsx
+++ Strife/frontend/components/session/session_form.jsx
@@ -14,7 +14,7 @@ class SessionForm extends React.Component {
         this.handleInput = this.handleInput.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.errors = this.errors.bind(this);
-        this.userNameErrors=this.userNameErrors.bind(this);
+        this.userNameErrors = this.userNameErrors.bind(this);
         this.emailErrors = this.emailErrors.bind(this);
         this.passwordErrors = this.passwordErrors.bind(this);
         this.birthdayErrors = this.birthdayErrors.bind(this);
@@ -65,7 +65,7 @@ class SessionForm extends React.Component {
         )
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         this.props.removeSessionErrors();
     }
 
@@ -86,77 +86,77 @@ class SessionForm extends React.Component {
     }
 
 
-    userNameErrors(){
+    userNameErrors() {
 
-        let USERNAME_ERRORS = ['Username Must be between 2 and 32 in length',"Username can't be blank"]
-            if(this.props.formType==="Sign Up"){
-                if(this.props.errors.includes('Username Must be between 2 and 32 in length')){
-                  
-                    return ' - Must be between 2 and 32 in length';
-                }
-                else if(this.props.errors.includes("Username can't be blank")){
-                    return "Username can't be blank";
-                }
+        let USERNAME_ERRORS = ['Username Must be between 2 and 32 in length', "Username can't be blank"]
+        if (this.props.formType === "Sign Up") {
+            if (this.props.errors.includes('Username Must be between 2 and 32 in length')) {
+
+                return ' - Must be between 2 and 32 in length';
             }
-            return "";
+            else if (this.props.errors.includes("Username can't be blank")) {
+                return "Username can't be blank";
+            }
+        }
+        return "";
     }
 
-    emailErrors(){
-        const EMAIL_ERRORS =["Not well formed email address", "Must be 320 or fewer in Length", "Email can't be blank","Email has already been taken"];
+    emailErrors() {
+        const EMAIL_ERRORS = ["Not well formed email address", "Must be 320 or fewer in Length", "Email can't be blank", "Email has already been taken"];
         // if(this.props.formType === "Sign In"){
         //     if()
         // }
-    
-        if (this.props.formType === "Sign Up"){
-            if(this.props.errors.includes(EMAIL_ERRORS[0])){
-                  
+
+        if (this.props.formType === "Sign Up") {
+            if (this.props.errors.includes(EMAIL_ERRORS[0])) {
+
                 return "Not well formed email address";
             }
-            else if(this.props.errors.includes(EMAIL_ERRORS[1])){
+            else if (this.props.errors.includes(EMAIL_ERRORS[1])) {
                 return "Must be 320 or fewer in Length";
             }
-             if(this.props.errors.includes("Email can't be blank")){
+            if (this.props.errors.includes("Email can't be blank")) {
                 return "Email can't be blank";
             }
-            else if(this.props.errors.includes("Email has already been taken")){
+            else if (this.props.errors.includes("Email has already been taken")) {
                 return "Email has already been taken";
             }
-            
-            
-            
+
+
+
         }
-            
-         
-        
+
+
+
         return "";
     }
 
-    passwordErrors(){
-        const PASSWORD_ERRORS =["PASSWORD - Must be 72 or fewer in length", "Password is too short (minimum is 6 characters)", "Password can't be blank"];
+    passwordErrors() {
+        const PASSWORD_ERRORS = ["PASSWORD - Must be 72 or fewer in length", "Password is too short (minimum is 6 characters)", "Password can't be blank"];
         // if(this.props.formType === "Sign In"){
         //     if()
         // }
-        if (this.props.formType === "Sign Up"){
-                for(let i= 0; i<=PASSWORD_ERRORS.length;i++){
-                    if(this.props.errors.includes(PASSWORD_ERRORS[i])){
-                        return PASSWORD_ERRORS[i];
-                    }
+        if (this.props.formType === "Sign Up") {
+            for (let i = 0; i <= PASSWORD_ERRORS.length; i++) {
+                if (this.props.errors.includes(PASSWORD_ERRORS[i])) {
+                    return PASSWORD_ERRORS[i];
                 }
+            }
         }
         return "";
     }
 
-    birthdayErrors(){
-        const BIRTHDAY_ERRORS =["Birthday can't be blank"]
+    birthdayErrors() {
+        const BIRTHDAY_ERRORS = ["Birthday can't be blank"]
         // if(this.props.formType === "Sign In"){
         //     if()
         // }
-        if (this.props.formType === "Sign Up"){
-             
-                    if(this.props.errors.includes(BIRTHDAY_ERRORS[0])){
-                        return BIRTHDAY_ERRORS[0];
-                    }
-                
+        if (this.props.formType === "Sign Up") {
+
+            if (this.props.errors.includes(BIRTHDAY_ERRORS[0])) {
+                return BIRTHDAY_ERRORS[0];
+            }
+
         }
         return "";
     }
@@ -166,20 +166,20 @@ class SessionForm extends React.Component {
 
         //assign variables to change classname tags depending if an error has occcured or not
 
-        let emailErrorTag= "";
-        let passwordErrorTag= "";
+        let emailErrorTag = "";
+        let passwordErrorTag = "";
         let birthdayErrorTag = "";
         let usernameErrorTag = "";
-        if(this.props.formType === "Sign In" ){
+        if (this.props.formType === "Sign In") {
             emailErrorTag = this.emailErrors() === "" ? "" : "login-error";
             // passwordErrorTag= this.passwordErrors() === "" ? "" : "login-error";
         }
-        else if (this.props.formType === "Sign Up" ){
+        else if (this.props.formType === "Sign Up") {
             emailErrorTag = this.props.errors.includes(this.emailErrors()) ? "login-error" : "";
             // passwordErrorTag= this.passwordErrors() === "" ? "" : "login-error";
             //  birthdayErrorTag = this.birthdayErrors()=== "" ? "" : "login-error";
-             usernameErrorTag = this.props.errors.includes('Username Must be between 2 and 32 in length')||
-             this.props.errors.includes("Username can't be blank") ? "login-error" : ""
+            usernameErrorTag = this.props.errors.includes('Username Must be between 2 and 32 in length') ||
+                this.props.errors.includes("Username can't be blank") ? "login-error" : ""
         }
 
         //assign variables that will display content bassed if the form is sigin vs sign out
@@ -190,50 +190,58 @@ class SessionForm extends React.Component {
         //     </div>
         // );
         let password;
-        
+
         let birthday;
-        let userName = this.props.formType === "Sign In" ? (""):(
+        let userName = this.props.formType === "Sign In" ? ("") : (
             <div className="field">
                 <label id="username-label" className={usernameErrorTag}>USERNAME{this.userNameErrors()}</label>
-                <input id="username" type="text"value={this.state.username} onChange={this.handleInput('username')}/>
+                <input id="username" type="text" value={this.state.username} onChange={this.handleInput('username')} />
             </div>
         );
 
-        const submitButtonMessage = this.props.formType === "Sign In" ?  "Login" : "Continue"
-        const headerMessage = this.props.formType === "Sign In" ? (<h2 className="welcome-message">Welcome Back!</h2>):
+        const submitButtonMessage = this.props.formType === "Sign In" ? "Login" : "Continue"
+        const headerMessage = this.props.formType === "Sign In" ? (<h2 className="welcome-message">Welcome Back!</h2>) :
             (<h2 className="signup-header">Create an account</h2>);
-        const subHeaderMessage = this.props.formType === "Sign In" ? ("We're so excited to see you again!"):("");
-             
+        const subHeaderMessage = this.props.formType === "Sign In" ? ("We're so excited to see you again!") : ("");
+
         return (
             <div className="session-signup-form">
+                <div className="login-box">
 
-            <h2>Create an account</h2>
-            <form onSubmit={this.handleSubmit}>
-                  
-                    {/* <input type="text"value={this.state.username} onChange={this.handleInput('username')}/> */}
-                    {/* {email} */}
-                    <div className="email-field">
-                        <label id="email-field" className={emailErrorTag}>
-                            EMAIL{this.emailErrors()}
-                        </label>
-                        <input type="email" className={emailErrorTag} id="email"value={this.state.email} onChange={this.handleInput('email')}/>
-                    </div>
-                    {/* {email}
-                    <input type="email"value={this.state.email} onChange={this.handleInput('email')} /> */}
-                  
-                    {userName}
+                    {headerMessage}
 
-                  
-                    {/* {birthday} */}
-                    <input type="date"value={this.state.birthday} onChange={this.handleInput('birthday')} />
-                    {/* {password} */}
+                    <h3>{subHeaderMessage}</h3>
                     
+                    <form onSubmit={this.handleSubmit}>
 
-               
-                    <input type="password" value={this.state.password} onChange={this.handleInput('password')}/>
-                    <button type="submit">Create Account!</button>
-            </form>
-</div>
+                        {/* <input type="text"value={this.state.username} onChange={this.handleInput('username')}/> */}
+                        {/* {email} */}
+                        <div className="email-field">
+                            <label id="email-field" className={emailErrorTag}>
+                                EMAIL{this.emailErrors()}
+                            </label>
+                            <input type="email" className={emailErrorTag} id="email" value={this.state.email} onChange={this.handleInput('email')} />
+                        </div>
+                        {/* {email}
+                    <input type="email"value={this.state.email} onChange={this.handleInput('email')} /> */}
+
+                        {userName}
+
+
+                        {/* {birthday} */}
+                        <input type="date" value={this.state.birthday} onChange={this.handleInput('birthday')} />
+                        {/* {password} */}
+
+
+
+                        <input type="password" value={this.state.password} onChange={this.handleInput('password')} />
+
+
+                        <div className="field"><button type="submit">{submitButtonMessage}</button></div>
+
+                    </form>
+                </div>
+            </div>
         )
     }
 
