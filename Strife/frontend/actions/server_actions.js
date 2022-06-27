878d6f24b3e35f5222aeef152e990377a820b6c2
miker704 <miker704@gmail.com>
2022-06-27 09:53:39 -0400

    started server actions done

--- Strife/frontend/actions/server_actions.js
+++ Strife/frontend/actions/server_actions.js
@@ -10,32 +10,38 @@ export const REMOVE_SERVER_ERRORS = "REMOVE_SERVER_ERRORS";
 
 export const receiveServer = (server) => {
     return{
-
+            type: RECEIVE_SERVER,
+            server
     }
 }
 
 export const receiveServers = (servers) => {
     return{
-
+            type: RECEIVE_SERVERS,
+            servers
     }
 }
 
 export const removeServer = (serverId) => {
     return{
-
+            type: REMOVE_SERVER,
+            serverId
 }
 }
 
 export const receiveServerErrors = (errors) => {
     return{
-
+        type: RECEIVE_SERVER_ERRORS,
+        errors
     }}
 
 
 export const removeServerErrors = () => {
     return{
-        
+        type: REMOVE_SERVER_ERRORS,
+   
     }
 }
 
-
+export const fetchServers = (user) => (dispatch) =>
+ServerAPI.fetchServers(user).then((servers) => {dispatch(receiveServers(servers))},(err)=>{dispatch(receiveServerErrors(err.responseJSON))}) 
