92f20af748012f65fd0cc3259104d8f76e0eab22
miker704 <miker704@gmail.com>
2022-06-27 11:12:45 -0400

    imported servererrorsreducer to errors reducers

--- Strife/frontend/reducers/errors_reducer.js
+++ Strife/frontend/reducers/errors_reducer.js
@@ -1,11 +1,13 @@
 //reducer for handling errors across the entire app
 
 import { combineReducers } from "redux";
+import serverErrorsReducer from "./server_error_reducer";
 import sessionErrorsReducer from "./session_errors_reducer";
 
 
 const errorsReducer = combineReducers({
-    session: sessionErrorsReducer
+    session: sessionErrorsReducer,
+    server: serverErrorsReducer
 })
 
 
---
+++ Strife/frontend/reducers/server_error_reducer.js
@@ -0,0 +1,12 @@
+import { RECEIVE_SERVER_ERRORS, REMOVE_SERVER_ERRORS } from "../actions/server_actions";
+
+
+const serverErrorsReducer = (state=[], action) => {
+
+
+
+
+
+}
+
+export default serverErrorsReducer;
