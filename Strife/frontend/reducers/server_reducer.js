734b410c5f0271172a88cdc9b6002a0976188673
miker704 <miker704@gmail.com>
2022-06-27 11:38:03 -0400

    finished server__reducer

--- Strife/frontend/reducers/server_reducer.js
+++ Strife/frontend/reducers/server_reducer.js
@@ -1,4 +1,35 @@
-import {RECEIVE_SERVER, RECEIVE_SERVERS, REMOVE_SERVER} from "../actions/server_actions.js"
+import { RECEIVE_SERVER, RECEIVE_SERVERS, REMOVE_SERVER } from "../actions/server_actions.js"
+import { RECEIVE_SERVER_MEMBERSHIP, REMOVE_SERVER_MEMBERSHIP } from "../actions/server_membership_actions.js"
 import { LOGOUT_CURRENT_USER } from "../actions/session_actions"
 
 
+
+const serverReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let nextState = Object.assign({}, state);
+    switch (action.type) {
+        case RECEIVE_SERVER:
+            nextState[action.server.server.id] = action.server.server
+            return nextState;
+        case RECEIVE_SERVERS:
+            return action.servers;
+        case REMOVE_SERVER:
+            delete nextState[action.serverId]
+            return nextState
+
+        case RECEIVE_SERVER_MEMBERSHIP:
+
+        nextState[action.servermembership.server.id]= action.servermembership.server;
+        return nextState;
+        case REMOVE_SERVER_MEMBERSHIP:
+
+            delete nextState[action.membershiphash.server.id];
+            return nextState;
+        case LOGOUT_CURRENT_USER:
+            return {};
+        default:
+            return state;
+    }
+
+}
+export default serverReducer;
