c7f88130b20b1d1c1ff427e0972c0d16171b9975
miker704 <miker704@gmail.com>
2022-06-27 09:44:55 -0400

    started server actions done

---
+++ Strife/frontend/actions/server_actions.js
@@ -0,0 +1,15 @@
+import * as ServerAPI from "../utils/server_api_util.js"
+
+
+export const RECEIVE_SERVER = "RECEIVE_SERVER";
+export const RECEIVE_SERVERS = "RECEIVE_SERVERS";
+export const REMOVE_SERVER = "REMOVE_SERVER";
+export const RECEIVE_SERVER_ERRORS = "RECEIVE_SERVER_ERRORS";
+export const REMOVE_SERVER_ERRORS = "REMOVE_SERVER_ERRORS";
+
+
+const receiveServer = (server) => {return{}}
+const receiveServers = (servers) => {return{}}
+const removeServer = (serverId) => {return{}}
+const receiveServerErrors = (errors) => {return{}}
+const removveServerErrors = () => {return{}}
