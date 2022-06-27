d1333a7c03d5b79c5822b9a961f69cdc86c94c33
miker704 <miker704@gmail.com>
2022-06-27 09:31:44 -0400

    server api util done

--- Strife/frontend/utils/server_api_util.js
+++ Strife/frontend/utils/server_api_util.js
@@ -3,3 +3,10 @@
 export const fetchServers = (userId) =>  $.ajax({ url: "/api/servers/", method: "GET", data: { user: userId } });
 
 export const fetchServer = (serverId) =>  $.ajax({ url: `/api/servers/${serverId}`, method: "GET" });
+
+export const createServer = (server) =>  $.ajax({ url: `/api/servers/`, method: "POST", data:{server:server} });
+
+
+export const updateServer = (server) =>  $.ajax({ url: `/api/servers/${server.id}`, method: "PATCH", data:{server:server} });
+
+export const deleteServer = (serverId) =>  $.ajax({ url: `/api/servers/${serverId}`, method: "DELETE" });
