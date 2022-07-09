bd12097a6a9de51864f451486bb64f98d3442000
miker704 <miker704@gmail.com>
2022-07-09 16:47:35 -0400

    Revert "updates"
    This reverts commit 2ad69a8a149082a963c9844ecfbd483a3f2a0db5.
    reverting changes to save project

--- Strife/frontend/components/channels/channel_forms/channel_form.jsx
+++ Strife/frontend/components/channels/channel_forms/channel_form.jsx
@@ -81,8 +81,81 @@ class ChannelForm extends React.Component {
 
     
     render(){
+        // Placeholder based on form
+        const placeholder = this.props.formType === "Edit Channel" ? this.props.name : "new-channel"
+        // Form Message based on creating or editing Channel and if it is the general channel.
+        let formMessage = this.props.formType === "Update Channel" && this.props?.channelName === "general" 
+          ? <div id ="channel-form-header"> 
+              <h5>Edit (Disabled for general)</h5>
+              <p> In {this.props.serverName}</p>
+            </div> 
+          : this.props.formType === "Update Channel"  
+          ? <div id ="channel-form-header"> 
+              <h5>Edit Channel</h5>
+              <p> In {this.props.serverName}</p>
+            </div> 
+          : <div id ="channel-form-header"> 
+            <h5>Create A Channel</h5>
+            <p> In {this.props.serverName}</p>
+          </div>
     
-      
+        // Delete Button if Update Channel"
+        let deleteButton = this.props.formType === "Update Channel" && this.props?.channelName === "general" 
+          ?  <button id="channel-delete-button" type="button"> Delete Channel </button>
+          : this.props.formType === "Update Channel" 
+          ? <button id="channel-delete-button" type="button"
+              onClick={() => this.handleDelete(this.props.channelId)}> 
+              Delete Channel </button>
+          : null;
+    
+        // Channel Name is format based on if there are errors
+        let channelName = this.props.errors.includes("Name can't be blank") ? 
+              <p id="channel-error-name"> CHANNEL NAME: CANNOT BE BLANK</p> :
+              <p id="channel-form-name"> CHANNEL NAME</p>
+    
+        // Change channelName  if channel name is already taken
+        if(this.props.errors.includes("Name has already been taken")){
+          channelName = <p id="channel-error-name"> CHANNEL NAME: NAME ALREADY IN USE</p>}
+    
+        // Submit Button (Disabled for general) 
+        let submitButton = this.props?.channelName === "general" 
+          ? <button id="channel-edit-submit" type="button">Update Channel </button>
+          : <button id="channel-edit-submit" type="submit">{this.props.formType}</button>
+    
+        // Cancel Button
+        let cancelButton = <button id="cancel-form" 
+          type = "submit"
+          ref={el=>this.cancelButton=el} 
+          onClick={() => this.cancel = true}
+          >Cancel</button>
+    
+        return (
+        <div id="channel-form"> 
+          <form autoComplete="off" onSubmit={this.handleSubmit}>
+            <div id="channel-form-top"> 
+              {formMessage}
+                {channelName}
+                <div id ="channel-form-name-input-container">
+                  <span className="server-message-input-padding">"</span>
+                  <i className="fa-solid fa-hashtag fa-sm"></i>
+                  <input 
+                  autoFocus
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.handleName("name")}
+                    id ="channel-form-name-input"
+                    placeholder={placeholder}
+                  />
+                </div>
+            </div>
+              <div id="channel-form-bottom"> 
+              {/* normal submit button to handle enter properly */}
+                {submitButton}
+                {deleteButton}
+                {cancelButton}
+              </div>
+          </form>
+        </div>)
     }
 
 }
