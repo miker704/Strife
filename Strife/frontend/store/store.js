00d5a87e42f4d97d057b2e1d031f5dbbbdd963b6
miker704 <miker704@gmail.com>
2022-07-12 12:09:48 -0400

    modified store to add redux devtools wrapping apply middleware for better redux state analysis

--- Strife/frontend/store/store.js
+++ Strife/frontend/store/store.js
@@ -1,10 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from 'redux-thunk';
 import rootReducer from "../reducers/root_reducer";
 import logger from "redux-logger";
+import {composeWithDevTools} from "@redux-devtools/extension";
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 
 const configureStore = (preLoadedState = {}) => {
-    return createStore(rootReducer, preLoadedState, applyMiddleware(thunk,logger));
+    // return createStore(rootReducer, preLoadedState, composeEnhancers(applyMiddleware(thunk, logger)));
+    return createStore(rootReducer, preLoadedState, composeWithDevTools(applyMiddleware(thunk, logger)));
+
 }
 
 export default configureStore;
