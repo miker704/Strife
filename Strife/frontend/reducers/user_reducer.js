57c219d0b9e072c216efffaa2bfc669ccf225497
miker704 <miker704@gmail.com>
2022-07-12 16:22:27 -0400

    edited user reducer

--- Strife/frontend/reducers/user_reducer.js
+++ Strife/frontend/reducers/user_reducer.js
@@ -1,16 +1,37 @@
-import { LOGOUT_CURRENT_USER,RECEIVE_CURRENT_USER, REMOVE_CURRENT_USER } from "../actions/session_actions";
+import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER, REMOVE_CURRENT_USER } from "../actions/session_actions.js";
+import { RECEIVE_DM_MEMBER } from "../actions/dm_member_actions.js";
+import { RECEIVE_DM_SERVERS } from '../actions/dm_server_actions.js';
+import { RECEIVE_SERVER } from '../actions/server_actions.js';
+import { RECEIVE_SERVER_MEMBERSHIP } from '../actions/server_membership_actions.js';
 
-const userReducer = (state={},action) => {
+
+
+const userReducer = (state = {}, action) => {
 
     Object.freeze(state);
 
-    switch(action.type){
+    switch (action.type) {
         case RECEIVE_CURRENT_USER:
-        return {currentUser : action.user};
+            // return {currentUser : action.user};
+            // return Object.assign({}, state, {[action.currentUser.id]: action.currentUser});
+            return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+
+
+
+        case RECEIVE_SERVER:
+            return action.servers.users;
+        case RECEIVE_SERVER_MEMBERSHIP:
+            return action.servermembership.users;
+        case RECEIVE_DM_MEMBER:
+            return Object.assign({}, state, { [action.dm_member.user.id]: action.dm_member.user });
+        case RECEIVE_DM_SERVERS:
+            return action.dmServers.users;
+
+
         case REMOVE_CURRENT_USER:
             return {};
         case LOGOUT_CURRENT_USER:
-            return {} ;
+            return {};
         default:
             return state;
     }
