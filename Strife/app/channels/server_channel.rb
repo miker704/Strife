3b922cbdd3ae1bc088577d10ff7c33417cc8d2f9
miker704 <miker704@gmail.com>
2022-07-12 17:52:59 -0400

    action cable

---
+++ Strife/app/channels/server_channel.rb
@@ -0,0 +1,13 @@
+class ServerChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    server_channel = Channel.find_by(id: params[:id])
+    stream_for server_channel
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
+
+  
