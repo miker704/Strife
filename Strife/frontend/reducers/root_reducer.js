fc0582bbe6b26a91b9173c0ddd4e6ac51fafcc57
miker704 <miker704@gmail.com>
2022-07-12 16:19:23 -0400

    edited root reducer added ui reducer

--- Strife/frontend/reducers/root_reducer.js
+++ Strife/frontend/reducers/root_reducer.js
@@ -3,11 +3,12 @@ import { combineReducers } from "redux";
 import sessionReducer from "./session_reducer";
 import EntitiesReducer from "./entities_reducer";
 import errorsReducer from "./errors_reducer";
-
+import uiReducer from "./ui_reducer.js";
 const rootReducer = combineReducers({
     entities: EntitiesReducer,
     session: sessionReducer,
-    errors: errorsReducer
+    errors: errorsReducer,
+    ui: uiReducer
 })
 
 export default rootReducer;
