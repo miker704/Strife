c62a2dfc8f05c267d52fa690758ca816c2adcf70
miker704 <miker704@gmail.com>
2022-07-13 14:10:10 -0400

    server nav bar being built

--- Strife/frontend/components/server/server_nav_bar_/server_nav_bar.jsx
+++ Strife/frontend/components/server/server_nav_bar_/server_nav_bar.jsx
@@ -1,50 +1,63 @@
 import React from "react";
 
-class ServerNavBar extends React.Component{
-        constructor(props){
-            super(props);
-     
+class ServerNavBar extends React.Component {
+    constructor(props) {
+        super(props);
 
-        }
 
-        componentDidMount(){
-            this.props.fetchUserServers(this.props.currentUser.id);
-            // this.props.fetchAllServers(); 
-        }
+    }
 
-        render(){
-       
-         
-            console.log(this.props.servers);
-           let userServer = this.props.servers.map((server,serverIndex) =>{
-                return (
-                    <li className="server-bubbles" key={serverIndex}>
-                    
-                        <span>
-                        <p>{serverIndex}{": {{ server id : "} {server.id} {"} "}
-                        {"{ server_owner_id : "} {server.server_owner_id} {"} "}
-                        {"{ server name : "} {server.server_name} {"} "}
-                        {"{ server icon : "} {server.server_icon} {"} "}
-                        {"{ server public status : "} {server.public} {"} "}
-                        {"{ server invite code : "} {server.invite_code} {"}} "}
-                        
-                        </p>
-                     
-                        </span>
-                    </li>
-                )
-           }
-           )
+    componentDidMount() {
+        this.props.fetchUserServers(this.props.currentUser.id);
+        // this.props.fetchAllServers(); 
+    }
 
+    render() {
+
+
+        console.log(this.props.servers);
+        console.log(this.props.serverId);
+
+        let userServer = this.props.servers.map((server, serverIndex) => {
             return (
-                <div className="server-nav-bar">
-                        {/* <p>{currentuserprops}</p> */}
-                        {/* <p>{serverprops}</p> */}
-                        {userServer}
+                <li className="server-bubbles" key={serverIndex}>
 
-                </div>
+                    <span>
+                        <p>{serverIndex}{": {{ server id : "} {server.id} {"} "}
+                            {"{ server_owner_id : "} {server.server_owner_id} {"} "}
+                            {"{ server name : "} {server.server_name} {"} "}
+                            {"{ server icon : "} {server.server_icon} {"} "}
+                            {"{ server public status : "} {server.public} {"} "}
+                            {"{ server invite code : "} {server.invite_code} {"}} "}
+
+                        </p>
+
+                    </span>
+                </li>
             )
         }
+        )
+
+        let homeButton = this.props.serverId === "@me" ? "selected-Server" : "unselected-Server";
+
+
+
+        return (
+            <div className="server-nav-bar">
+                <ul className="server-nav-bar-list">  
+                <li key="homeBubbles" className="server-bubbles">
+                    <div>
+                        
+                    </div>
+                
+                </li>            
+                    {userServer}
+                
+                </ul>
+              
+            </div>
+        )
+    }
 }
 
 export default ServerNavBar;
