fa18825cf3d330ef7d83d63a8908b876a3f757fa
miker704 <miker704@gmail.com>
2022-07-14 22:25:55 -0400

    updated server nav bar container

--- Strife/frontend/components/server/server_nav_bar/server_nav_bar_container.js
+++ Strife/frontend/components/server/server_nav_bar/server_nav_bar_container.js
@@ -1,25 +1,31 @@
 import { connect } from "react-redux";
-import { fetchServers, fetchServer, removeServerErrors } from "../../../actions/server_actions.js";
-import { withRouter } from "react-router";
 import ServerNavBar from "./server_nav_bar.jsx";
+import {  withRouter } from "react-router";
 
+import { fetchServer, fetchServers } from "../../../actions/server_actions";
 
 
 const mSTP = (state, ownProps) => {
+    
     return {
         currentUser: state.entities.users[state.session.id],
         servers: Object.values(state.entities.servers),
-        errors: state.errors.server,
-        serverId: ownProps.match.params.serverId
+        serverId: ownProps.match.params.serverId,
+        errors: state.errors.servers,
+       
+
     }
 }
 
 const mDTP = (dispatch) => {
+    
     return {
-        fetchServers: (userId) => { dispatch(fetchServers(userId)) },
-        fetchAllServers: () => { dispatch(fetchServers()) },
-        fetchServer: (serverId) => dispatch(fetchServer(serverId)),
+        fetchUserServers: (userId) => dispatch(fetchServers(userId)),
+        fetchAllServers : () => dispatch(fetchServers()),
+        fetchServer : (serverId) => dispatch(fetchServer(serverId)),
     }
 }
+
+
 const ServerNavBarContainer = withRouter(connect(mSTP,mDTP)(ServerNavBar));
 export default ServerNavBarContainer;
