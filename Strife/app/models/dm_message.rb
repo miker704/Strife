71b5840c51cb083cee870302ff32e7af2c0a2de1
miker704 <miker704@gmail.com>
2022-07-16 12:47:46 -0400

    removed bug

--- Strife/app/models/dm_message.rb
+++ Strife/app/models/dm_message.rb
@@ -17,7 +17,8 @@
 #  index_dm_messages_on_sender_id          (sender_id)
 #
 class DmMessage < ApplicationRecord
-    validates :dm_server_id, :sender_id, :body, :parent_message_id, presence: true
+    # validates :dm_server_id, :sender_id, :body, :parent_message_id, presence: true
+    validates :dm_server_id, :sender_id, :body, presence: true
     validates :body, length: {in: 1..2000, too_long: "Please Subscribe to Strife Nitro to send messages larger than 2000 characters"}
     belongs_to :message_creator, class_name: "User", foreign_key: "sender_id"
     belongs_to :dm_server, class_name: "DmServer", foreign_key: "dm_server_id"
