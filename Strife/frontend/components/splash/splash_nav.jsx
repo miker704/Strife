857a31f86d064df1b96898e8cdaca517d7cb9f99
miker704 <miker704@gmail.com>
2022-07-17 18:17:19 -0400

    edited changes due too reset sheet

--- Strife/frontend/components/splash/splash_nav.jsx
+++ Strife/frontend/components/splash/splash_nav.jsx
@@ -10,15 +10,23 @@ class SplashNav extends React.Component {
 
     render() {
         return (
-            <div id="splash-nav">
-          
+
+            <div className="splash-nav-flex-grid">
+            <div className="splash-nav-grid">
+                <div id="splash-nav">
+                    <div className="website-banner-flex">
                     <div id="website-banner">
-                        <i className="fa-brands fa-discord fa-xl" />
-                        <span id="website-name"> STRIFE </span>
+                        <span id="change-down-icon">
+                        <i className="fa-brands fa-discord fa-xl logo-banner">{"    "}</i>
+                        </span>
+                        <span id="website-name-banner"> STRIFE </span>
                     </div>
+                    </div>
+
+
                     <div id="splash-nav-links">
-                        
-                        <a className="link" href="app/assets/images/discord_Strife_logo.png" download = "STRIFE.EXE"
+
+                        <a className="link" href="app/assets/images/discord_Strife_logo.png" download="STRIFE.EXE"
                             target="_blank"> Download </a>
                         <a className="link" href="https://github.com/miker704/Strife"
                             target="_blank"> STRIFE </a>
@@ -27,17 +35,20 @@ class SplashNav extends React.Component {
                         <a className="link" href="https://www.linkedin.com/in/michael-ramoutar/"
                             target="_blank"> LinkedIn  </a>
                         <a className="link" href="https://www.appacademy.io/"
-                            target="_blank"> App/acc </a>
+                            target="_blank"> App/ac </a>
                         <a className="link" href="https://discord.com/"
                             target="_blank"> Discord  </a>
                     </div>
+                    <div className="login-container">
                     <div id="login">
                         <Link to="/login">Login</Link>
                     </div>
-
+                    </div>
 
                 </div>
-                )
+            </div>
+            </div>
+        )
     }
 }
 
