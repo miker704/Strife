677566425d57fc087017aadbc11ec38f24a3ffd5
miker704 <miker704@gmail.com>
2022-07-18 09:47:36 -0400

    edit server form container modal

---
+++ Strife/frontend/components/server/server_forms/edit_server_forms/edit_server_form.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+
+
+class EditServerForm extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+
+
+            </div>
+        )
+    }
+}
+
+
+export default EditServerForm;
