d41a39b330cfba9c21bb64cd674b5e838fd38353
miker704 <miker704@gmail.com>
2022-07-18 10:00:24 -0400

    edit server form container updates

--- Strife/frontend/components/server/server_forms/edit_server_forms/edit_server_form_container.js
+++ Strife/frontend/components/server/server_forms/edit_server_forms/edit_server_form_container.js
@@ -1,16 +1,18 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
+import { removeServerErrors, updateServer } from "../../../../actions/server_actions";
 import EditServerForm from "./edit_server_form";
 
 const mSTP = (state) => {
     return {
-
+        errors : state.errors.server
     }
 }
 
 const mDTP = (dispatch) => {
     return {
-
+        updateServerInfo: (server) => dispatch(updateServer(server)),
+        removeServerErrors: () => dispatch(removeServerErrors())
     }
 }
 
