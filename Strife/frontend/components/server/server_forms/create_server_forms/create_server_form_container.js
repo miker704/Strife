dbb853a7a9319359737ea424c77b23c12c400f9f
miker704 <miker704@gmail.com>
2022-07-18 09:08:26 -0400

    import mstp and mdtp for create server modal

--- Strife/frontend/components/server/server_forms/create_server_forms/create_server_form_container.js
+++ Strife/frontend/components/server/server_forms/create_server_forms/create_server_form_container.js
@@ -1,16 +1,26 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
+import { createServer, removeServerErrors } from "../../../../actions/server_actions";
 import CreateServerForm from "./create_server_form";
 
 
 
 const mSTP = (state) => {
     return {
-       
+       currentUser: state.entities.users[state.session.id],
+       server: {
+        server_owner_id: state.session.id,
+        public : true,
+        server_name: "",
+       },
+       errors : state.errors.server
     }
 }
 const mDTP = (dispatch) => {
     return {
+        
+        createServer: (server) => dispatch(createServer(server)),
+        removeServerErrors: () => dispatch(removeServerErrors()),
         closeModal: ()=> dispatch(closeModal())
     }
 }
