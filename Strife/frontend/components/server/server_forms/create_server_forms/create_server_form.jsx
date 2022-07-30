8bc472ab84d55ca31597880fd6fa80bd6a129aa2
miker704 <miker704@gmail.com>
2022-07-30 16:59:31 -0400

    building

--- Strife/frontend/components/server/server_forms/create_server_forms/create_server_form.jsx
+++ Strife/frontend/components/server/server_forms/create_server_forms/create_server_form.jsx
@@ -4,15 +4,50 @@ import React from "react";
 class CreateServerForm extends React.Component{
     constructor(props){
         super(props);
+        this.state={
+            server_owner_id: this.props.currentUser.id,
+            server_name: "",
+            public: true, //true by default
+            server_icon: "", // empty by default until aws functionality is implemented
+            form_number:1,
+            invite_code: "",
+        }
+        console.log("calling create server modal");
     }
 
+
+    submitFormPrivate(){
+        let submitServer={
+            server_owner_id: this.props.currentUser.id,
+            server_name: "",
+            public: false, //true by default
+            server_icon: "", // empty by default until aws functionality is implemented,
+            invite_code: "",
+        }
+        return this.props.createServer(submitServer);
+    }
+
+    submitFormPublic(){
+        let submitServer={
+            server_owner_id: this.props.currentUser.id,
+            server_name: "",
+            public: true, //true by default
+            server_icon: "", // empty by default until aws functionality is implemented,
+            invite_code: "",
+        }
+        return this.props.createServer(submitServer);
+
+    }
+
+
     render(){
+        console.log("call");
         return(
-            <div>
+            <div className="create-A-Server">
+
+                <p>HELLO</p>
 
 
-
-                
             </div>
         )
     }
