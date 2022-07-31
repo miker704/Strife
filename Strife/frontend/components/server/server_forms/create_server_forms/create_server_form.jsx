d4e5ae1c4ef86d7320e8863175d6afcddc7b5d0d
miker704 <miker704@gmail.com>
2022-07-31 18:29:16 -0400

    creating this modal

--- Strife/frontend/components/server/server_forms/create_server_forms/create_server_form.jsx
+++ Strife/frontend/components/server/server_forms/create_server_forms/create_server_form.jsx
@@ -1,23 +1,60 @@
 import React from "react";
 
 
-class CreateServerForm extends React.Component{
-    constructor(props){
+class CreateServerForm extends React.Component {
+    constructor (props) {
         super(props);
-        this.state={
+        this.state = {
             server_owner_id: this.props.currentUser.id,
             server_name: "",
             public: true, //true by default
             server_icon: "", // empty by default until aws functionality is implemented
-            form_number:1,
+            form_number: 1,
+            foward: false,
+            backward: false,
             invite_code: "",
         }
         console.log("calling create server modal");
+        let serverNamefiller = `${this.props.currentUser.username}'s server`;
+        console.log(serverNamefiller);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInput = this.handleInput.bind(this);
     }
 
 
-    submitFormPrivate(){
-        let submitServer={
+    handleSubmit () {
+        let serverSubmission = {}
+
+
+        if (this.state.public === true) {
+            serverSubmission = {
+                server_owner_id: this.props.currentUser.id,
+                server_name: "",
+                public: true, //true by default
+                server_icon: "", // empty by default until aws functionality is implemented,
+                invite_code: "",
+            }
+        }
+        else {
+            serverSubmission = {
+                server_owner_id: this.props.currentUser.id,
+                server_name: "",
+                public: false, //true by default
+                server_icon: "", // empty by default until aws functionality is implemented,
+                invite_code: "",
+            }
+        }
+        console.log("serverSubmission Form: ", serverSubmission);
+    }
+
+    handleInput (e) {
+
+        return (e) => this.setState({ server_name: e.currentTarget.value });
+
+    }
+
+    submitFormPrivate () {
+        let submitServer = {
             server_owner_id: this.props.currentUser.id,
             server_name: "",
             public: false, //true by default
@@ -27,8 +64,8 @@ class CreateServerForm extends React.Component{
         return this.props.createServer(submitServer);
     }
 
-    submitFormPublic(){
-        let submitServer={
+    submitFormPublic () {
+        let submitServer = {
             server_owner_id: this.props.currentUser.id,
             server_name: "",
             public: true, //true by default
@@ -40,12 +77,53 @@ class CreateServerForm extends React.Component{
     }
 
 
-    render(){
+    render () {
         console.log("call");
-        return(
-            <div className="create-A-Server">
 
-                <p>HELLO</p>
+
+        let currentModalState;
+        let form_state;
+
+
+        if (this.state.foward === false) {
+
+        }
+
+        let slide1;
+        let slide2;
+        let slide3;
+
+
+
+
+
+
+
+
+        return (
+            <div className="create-A-Server-Wrapper">
+
+                <div className="x-To-Close">
+
+                    <svg
+                        // className="close-button"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        onClick={() => this.props.closeModal()}
+                    ><path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"></path>
+                    </svg>
+
+                </div>
+
+
+
+                <div className="create-A-Server-Modal">
+                    <p>HELLO</p>
+                    <button onClick={() => this.handleSubmit()}>hello submit</button>
+
+
+                </div>
 
 
             </div>
