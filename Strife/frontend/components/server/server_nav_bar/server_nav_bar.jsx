f809c9dd997d4443a3f354792b6c8d367d7bc0ff
miker704 <miker704@gmail.com>
2022-07-31 18:27:39 -0400

    added download apps button

--- Strife/frontend/components/server/server_nav_bar/server_nav_bar.jsx
+++ Strife/frontend/components/server/server_nav_bar/server_nav_bar.jsx
@@ -5,12 +5,20 @@ class ServerNavBar extends React.Component {
         super(props);
 
         this.splitServerName = this.splitServerName.bind(this);
+        this.renderModal = this.renderModal.bind(this);
     }
 
     componentDidMount () {
         this.props.fetchUserServers(this.props.currentUser.id);
         // this.props.fetchAllServers(); 
     }
+
+
+    renderModal (modalName) {
+        console.log("can this work goddamit");
+        this.props.openModal(modalName);
+    }
+
 
     splitServerName (serverName, serverIcon) {
         //if the server owner has given a server icon to there server set the servericon url to fill the 
@@ -78,7 +86,9 @@ class ServerNavBar extends React.Component {
 
                     <li className="server-bubbles" key="createServer">
                         <div className="server-nav-bar-a">
-                            <button id="create-server" onClick={() => this.props.openModal("createServerForm" )}>
+                            {/* <button id="create-server" onClick={() => this.props.openModal('createServerForm')}> */}
+                            <button id="create-server" onClick={() => this.renderModal('createServerForm')}>
+
                                 <i className="fa-solid fa-plus" />
                             </button>
                         </div>
@@ -86,11 +96,38 @@ class ServerNavBar extends React.Component {
                     </li>
                     <li className="server-bubbles" key="serverSearch">
                         <div className="server-nav-bar-a">
-                            <button id="search-servers" onClick={() => this.props.openModal({modal:"serverSearch"})}>
+                            <button id="search-servers" onClick={() => this.props.openModal('serverSearch')}>
                                 <i className="fa-solid fa-compass" />
                             </button>
                         </div>
                         <div className="server-nav-bar-tool-kit">Explore Public Servers</div>
+                    </li>
+
+                    {/* <li> */}
+                        <div className="bottom-server-bubble-seperator-container">
+                            <div className="bottom-server-bubble-seperator"></div>
+                        </div>
+                    {/* </li> */}
+
+
+                    <li className="server-bubbles" key="downloadApps">
+                        {/* <div className="bottom-server-bubble-seperator-container">
+                            <div className="server-bubble-seperator"></div>
+                        </div> */}
+
+                        <div className="server-nav-bar-a">
+                            <button id="download-apps" onClick={() => this.props.openModal('downloadApps')}>
+
+                                <svg fill="currentColor" aria-hidden="false" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M16.293 9.293L17.707 10.707L12 16.414L6.29297 10.707L7.70697 9.293L11 
+                                         12.586V2H13V12.586L16.293 9.293ZM18 20V18H20V20C20 21.102 19.104 22 18 22H6C4.896 22 4 21.102 4 
+                                         20V18H6V20H18Z">
+                                    </path>
+                                </svg>
+
+                            </button>
+                        </div>
+                        <div className="server-nav-bar-tool-kit">Download Apps</div>
                     </li>
 
                 </ul>
