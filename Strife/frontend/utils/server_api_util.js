5b897e45d3aeafd92361db7acc55943903086f78
miker704 <miker704@gmail.com>
2022-08-06 15:53:19 -0400

    + api function backend to join server via invite

--- Strife/frontend/utils/server_api_util.js
+++ Strife/frontend/utils/server_api_util.js
@@ -10,3 +10,5 @@ export const createServer = (server) =>  $.ajax({ url: `/api/servers/`, method: 
 export const updateServer = (server) =>  $.ajax({ url: `/api/servers/${server.id}`, method: "PATCH", data:{server:server} });
 
 export const deleteServer = (serverId) =>  $.ajax({ url: `/api/servers/${serverId}`, method: "DELETE" });
+
+export const joinServer = (inviteCode) => $.ajax({url: `/api/servers/join`, method: "POST", data:{inviteCode}});
