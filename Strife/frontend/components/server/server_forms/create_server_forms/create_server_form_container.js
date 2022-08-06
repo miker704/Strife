5ff002be6a7fabbd06e1363e806d290250f9c3fa
miker704 <miker704@gmail.com>
2022-08-06 15:55:08 -0400

    formatting and added joinserver as mdtp

--- Strife/frontend/components/server/server_forms/create_server_forms/create_server_form_container.js
+++ Strife/frontend/components/server/server_forms/create_server_forms/create_server_form_container.js
@@ -1,33 +1,34 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import { openModal,closeModal } from "../../../../actions/modal_actions";
-import { createServer, removeServerErrors } from "../../../../actions/server_actions";
+import { openModal, closeModal } from "../../../../actions/modal_actions";
+import { createServer, removeServerErrors, joinServer } from "../../../../actions/server_actions";
 import CreateServerForm from "./create_server_form.jsx";
 
 
 
 const mSTP = (state) => {
     return {
-       currentUser: state.entities.users[state.session.id],
-       server: {
-        server_owner_id: state.session.id,
-        public : true,
-        server_name: "",
-       },
-       errors : state.errors.server
+        currentUser: state.entities.users[state.session.id],
+        server: {
+            server_owner_id: state.session.id,
+            public: true,
+            server_name: "",
+        },
+        errors: state.errors.server
     }
 }
 const mDTP = (dispatch) => {
     return {
-        
+
         createServer: (server) => dispatch(createServer(server)),
         removeServerErrors: () => dispatch(removeServerErrors()),
         openModal: (modal) => dispatch(openModal(modal)),
-        closeModal: ()=> dispatch(closeModal())
+        closeModal: () => dispatch(closeModal()),
+        joinServer: (inviteCode) => dispatch(joinServer(inviteCode))
     }
 }
 
 // const CreateServerFormContainer = connect(mSTP,mDTP)(withRouter(CreateServerForm));
-const CreateServerFormContainer = connect(mSTP,mDTP)(CreateServerForm);
-// const CreateServerFormContainer =withRouter(connect(mSTP,mDTP)(CreateServerForm));
+// const CreateServerFormContainer = connect(mSTP, mDTP)(CreateServerForm);
+const CreateServerFormContainer =withRouter(connect(mSTP,mDTP)(CreateServerForm));
 export default CreateServerFormContainer;
