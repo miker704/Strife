2d538ab281b8f13ce4fa860b9dc5aa6130454975
miker704 <miker704@gmail.com>
2022-08-07 01:35:48 -0400

    added unique index to channel memberships

--- Strife/test/models/channel_membership_test.rb
+++ Strife/test/models/channel_membership_test.rb
@@ -8,6 +8,10 @@
 #  channel_id  :integer          not null
 #  receiver_id :integer          not null
 #
+# Indexes
+#
+#  index_channel_memberships_on_channel_id_and_receiver_id  (channel_id,receiver_id) UNIQUE
+#
 require 'test_helper'
 
 class ChannelMembershipTest < ActiveSupport::TestCase
