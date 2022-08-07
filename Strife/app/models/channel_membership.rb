5d11c5ed8adb2aff3eb6fc09f1e15cbd1d0d2430
miker704 <miker704@gmail.com>
2022-08-07 01:35:58 -0400

    added unique index to channel memberships

--- Strife/app/models/channel_membership.rb
+++ Strife/app/models/channel_membership.rb
@@ -8,8 +8,13 @@
 #  channel_id  :integer          not null
 #  receiver_id :integer          not null
 #
+# Indexes
+#
+#  index_channel_memberships_on_channel_id_and_receiver_id  (channel_id,receiver_id) UNIQUE
+#
 class ChannelMembership < ApplicationRecord
     validates :channel_id, :receiver_id, presence: true
+    validates :receiver_id, uniqueness: {scope: :channel_id}
     belongs_to :channel, class_name: "Channel", foreign_key: "channel_id"
     belongs_to :member, class_name: "User", foreign_key: "receiver_id"
 end
