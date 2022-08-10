d20da909d4ecd76aca888125a9b44fd10b4766d4
miker704 <miker704@gmail.com>
2022-08-10 13:01:00 -0400

    edits

--- Strife/frontend/components/users/user_nav/user_nav_bar.jsx
+++ Strife/frontend/components/users/user_nav/user_nav_bar.jsx
@@ -9,8 +9,8 @@ class UserNavBar extends React.Component {
       micMute: false,
       deafen: false
     }
-    console.log("usernav bar current user : ",this.props.currentUser)
-    console.log("usernav bar props : ",this.props)
+    console.log("usernav bar current user : ", this.props.currentUser)
+    console.log("usernav bar props : ", this.props)
 
     this.clickToMute = this.clickToMute.bind(this);
     this.clickToDeafen = this.clickToDeafen.bind(this);
@@ -36,6 +36,8 @@ class UserNavBar extends React.Component {
 
 
   render () {
+    let mic_component_className = this.state.micMute === false ? "user-settings-toggle" : "user-settings-toggle-muted";
+    let headphone_component_className = this.state.deafen === false ? "" : "";
 
     let mic_component = this.state.micMute === false ?
       (<svg onClick={() => this.setState({ micMute: true })} className="svg-microphone" width="20" height="20" viewBox="0 0 20 20">
@@ -108,8 +110,7 @@ class UserNavBar extends React.Component {
 
         <div id="user-settings-icons">
 
-          {/* <i className="fa-solid fa-microphone"> </i> */}
-          {/* <i className="fa-solid fa-microphone-slash"> </i> */}
+        
 
           <div className="contain-icon">
             {mic_component}
@@ -120,25 +121,25 @@ class UserNavBar extends React.Component {
             {headphone_component}
             <div className="user-settings-toggle2">{this.clickToDeafen()}</div>
           </div>
-         
 
 
-       
-            {/* redirect link */}
-            <Link to={`/users/${this.props.currentUser.id}`}>
-              {/* <i className="fa-solid fa-gear" /> */}
-              <svg className="fa-gear" width="20" height="20" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M19.738 10H22V14H19.739C19.498 14.931 19.1 15.798 18.565 16.564L20 
+
+
+          {/* redirect link */}
+          <Link to={`/users/${this.props.currentUser.id}`}>
+            {/* <i className="fa-solid fa-gear" /> */}
+            <svg className="fa-gear" width="20" height="20" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M19.738 10H22V14H19.739C19.498 14.931 19.1 15.798 18.565 16.564L20 
                 18L18 20L16.565 18.564C15.797 19.099 14.932 19.498 14 19.738V22H10V19.738C9.069 19.498 8.203 
                 19.099 7.436 18.564L6 20L4 18L5.436 16.564C4.901 15.799 4.502 14.932 4.262 14H2V10H4.262C4.502 
                 9.068 4.9 8.202 5.436 7.436L4 6L6 4L7.436 5.436C8.202 4.9 9.068 4.502 10 4.262V2H14V4.261C14.932
                  4.502 15.797 4.9 16.565 5.435L18 3.999L20 5.999L18.564 7.436C19.099 8.202 19.498 9.069 19.738 10ZM12 
                  16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 
                  16 12 16Z"></path>
-                 </svg>
-              <div className="user-settings-toggle1">User Settings</div>
-            </Link>
-          
+            </svg>
+            <div className="user-settings-toggle1">User Settings</div>
+          </Link>
+
 
 
 
