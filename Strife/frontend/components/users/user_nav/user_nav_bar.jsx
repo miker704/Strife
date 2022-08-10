18d2a32d800e309b4e0f890a9d395d4e8ce224d9
miker704 <miker704@gmail.com>
2022-08-10 10:51:41 -0400

    testing modal call instead of route call

--- Strife/frontend/components/users/user_nav/user_nav_bar.jsx
+++ Strife/frontend/components/users/user_nav/user_nav_bar.jsx
@@ -10,6 +10,8 @@ class UserNavBar extends React.Component {
       deafen: false
     }
     console.log("usernav bar current user : ",this.props.currentUser)
+    console.log("usernav bar props : ",this.props)
+
     this.clickToMute = this.clickToMute.bind(this);
     this.clickToDeafen = this.clickToDeafen.bind(this);
   }
