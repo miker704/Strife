a338c26ca3accd94d0c8b49ab30789254bfe8794
miker704 <miker704@gmail.com>
2022-08-10 09:41:37 -0400

    finihsed inital sturcutre moving to user settings

--- Strife/frontend/components/users/user_nav/user_nav_bar.jsx
+++ Strife/frontend/components/users/user_nav/user_nav_bar.jsx
@@ -24,10 +24,10 @@ class UserNavBar extends React.Component {
   }
 
   clickToDeafen () {
-    if (this.state.clickToDeafen === false) {
+    if (this.state.deafen === false) {
       return "Deafen";
     }
-    else if (this.state.clickToDeafen === true) {
+    else if (this.state.deafen === true) {
       return "Undeafen";
     }
   }
@@ -35,13 +35,6 @@ class UserNavBar extends React.Component {
 
   render () {
 
-    let microphone_Icon = this.clickToMute() === true ? ("") : ("");
-    let headphone_Icon = this.clickToDeafen() === true ? ("") : ("");
-    let microphone_Tag;
-    let headphone_Tag;
-
-    // let mic_component2 = 
-    let muted_mic_component;
     let mic_component = this.state.micMute === false ?
       (<svg onClick={() => this.setState({ micMute: true })} className="svg-microphone" width="20" height="20" viewBox="0 0 20 20">
         <path d="M14.99 11C14.99 12.66 13.66 14 12 14C10.34 
@@ -72,8 +65,29 @@ class UserNavBar extends React.Component {
         </path>
       </svg>);
 
-    let headphone_component;
-    let deafen_headphone_component;
+    let headphone_component = this.state.deafen === false ?
+      (<svg onClick={() => this.setState({ deafen: true })} className="svg-headphone" width="20" height="20" viewBox="0 0 24 24">
+        <svg width="24" height="24" viewBox="0 0 24 24"><path d="M12 2.00305C6.486 2.00305 2 6.48805 2 12.0031V20.0031C2 
+    21.1071 2.895 22.0031 4 22.0031H6C7.104 22.0031 8 21.1071 8 20.0031V17.0031C8 15.8991 7.104 15.0031 6 15.0031H4V12.0031C4 
+    7.59105 7.589 4.00305 12 4.00305C16.411 4.00305 20 7.59105 20 12.0031V15.0031H18C16.896 15.0031 16 15.8991 16 
+    17.0031V20.0031C16 21.1071 16.896 22.0031 18 22.0031H20C21.104 22.0031 22 21.1071 22 20.0031V12.0031C22 6.48805 
+    17.514 2.00305 12 2.00305Z" fill="currentColor">
+        </path>
+        </svg>
+      </svg>) :
+      (<svg onClick={() => this.setState({ deafen: false })} className="svg-muted-headphone" width="20" height="20" viewBox="0 0 24 24">
+        <path d="M6.16204 15.0065C6.10859 15.0022 6.05455 15 6 15H4V12C4 7.588 7.589 4 12 4C13.4809 4 14.8691 4.40439 
+      16.0599 5.10859L17.5102 3.65835C15.9292 2.61064 14.0346 2 12 2C6.486 2 2 6.485 2 12V19.1685L6.16204 15.0065Z"
+          fill="currentColor"></path>
+        <path d="M19.725 9.91686C19.9043 10.5813 20 11.2796 20 12V15H18C16.896 15 16 15.896 
+      16 17V20C16 21.104 16.896 22 18 22H20C21.105 22 22 21.104 22 20V12C22 10.7075 21.7536 9.47149 21.3053 8.33658L19.725 9.91686Z"
+          fill="currentColor">
+        </path>
+        <path d="M3.20101 23.6243L1.7868 22.2101L21.5858 2.41113L23 3.82535L3.20101 23.6243Z"
+          fill="red">
+        </path>
+      </svg>);
+
 
 
 
@@ -100,19 +114,31 @@ class UserNavBar extends React.Component {
             <div className="user-settings-toggle">{this.clickToMute()}</div>
           </div>
 
-
-          <i className="fa-solid fa-headphones" title="user"></i>
-
-          {/* redirect link */}
-          <Link to={`/users/${this.props.currentUser.id}`}>
-
-
-            <i className="fa-solid fa-gear" />
+          <div className="contain-icon">
+            {headphone_component}
+            <div className="user-settings-toggle">{this.clickToDeafen()}</div>
+          </div>
+          {/* <i className="fa-solid fa-headphones" title="user"></i> */}
 
 
-            <div className="user-settings-toggle">User Settings</div>
+          {/* <div className="contain-icon"> */}
+            {/* redirect link */}
+            <Link to={`/users/${this.props.currentUser.id}`}>
+              {/* <i className="fa-solid fa-gear" /> */}
+              <svg className="fa-gear" width="20" height="20" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M19.738 10H22V14H19.739C19.498 14.931 19.1 15.798 18.565 16.564L20 
+                18L18 20L16.565 18.564C15.797 19.099 14.932 19.498 14 19.738V22H10V19.738C9.069 19.498 8.203 
+                19.099 7.436 18.564L6 20L4 18L5.436 16.564C4.901 15.799 4.502 14.932 4.262 14H2V10H4.262C4.502 
+                9.068 4.9 8.202 5.436 7.436L4 6L6 4L7.436 5.436C8.202 4.9 9.068 4.502 10 4.262V2H14V4.261C14.932
+                 4.502 15.797 4.9 16.565 5.435L18 3.999L20 5.999L18.564 7.436C19.099 8.202 19.498 9.069 19.738 10ZM12 
+                 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 
+                 16 12 16Z"></path>
+                 </svg>
+              <div className="user-settings-toggle">User Settings</div>
+            </Link>
+          {/* </div> */}
 
-          </Link>
+
 
         </div>
       </div>
