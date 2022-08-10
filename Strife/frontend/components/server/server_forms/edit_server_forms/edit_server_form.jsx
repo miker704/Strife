dbab64aeb5b3d85e9ba14feb3a7169e19cfea9fd
miker704 <miker704@gmail.com>
2022-08-10 10:34:23 -0400

    started dev on edit server form modal

--- Strife/frontend/components/server/server_forms/edit_server_forms/edit_server_form.jsx
+++ Strife/frontend/components/server/server_forms/edit_server_forms/edit_server_form.jsx
@@ -6,6 +6,8 @@ import React from "react";
 class EditServerForm extends React.Component{
     constructor(props){
         super(props);
+
+        console.log("this.props.editServerform : ", this.props);
     }
 
     render(){
