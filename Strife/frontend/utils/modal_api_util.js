f466a6422984cd319db98b86b26ed15d176403d7
miker704 <miker704@gmail.com>
2022-08-12 12:41:59 -0400

    added escape lister event

---
+++ Strife/frontend/utils/modal_api_util.js
@@ -0,0 +1,16 @@
+import { closeModal } from "../actions/modal_actions";
+
+export const handleKeyUp = (e) => {
+    console.log("in esc function");
+
+    const keys = {
+        27: () => {
+            // e.preventDefault();
+            dispatch(closeModal());
+            window.removeEventListener('keyup', handleKeyUp, false);
+        },
+    };
+    if (keys[e.keyCode]) {
+        keys[e.keyCode]();
+    }
+}
