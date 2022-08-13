cc57008d97ca58288cdba02faefa74cb25112ddd
miker704 <miker704@gmail.com>
2022-08-13 11:02:19 -0400

    updates

--- Strife/frontend/components/users/user_edit_username_form/user_edit_username_form.jsx
+++ Strife/frontend/components/users/user_edit_username_form/user_edit_username_form.jsx
@@ -10,8 +10,19 @@ class EditUsernameForm extends React.Component {
             username: this.props.currentUser.username
         }
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInput = this.handleInput.bind(this);
         this.handleCloseModal = this.handleCloseModal.bind(this);
+        this.userNameErrors = this.userNameErrors.bind(this);
+
     }
+    componentWillUnmount () {
+        this.props.removeSessionErrors()
+    }
+
+    handleInput (field) {
+        return (e) => { this.setState({ [field]: e.currentTarget.value }) }
+    }
+
 
 
 }
