dbc00275775d644ccd21fbdc3369b9364ec28dad
miker704 <miker704@gmail.com>
2022-08-13 10:42:31 -0400

    import funct

--- Strife/frontend/components/users/user_edit_password_form/user_edit_password_container.js
+++ Strife/frontend/components/users/user_edit_password_form/user_edit_password_container.js
@@ -2,8 +2,8 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { handleKeyUp } from "../../../utils/modal_api_util";
 import EditUserPasswordForm from "./user_edit_password_form";
-
-
+import { openModal, closeModal } from "../../../actions/modal_actions";
+import { updateUserInfo, removeSessionErrors } from "../../../actions/session_actions";
 
 const mSTP = (state) => {
     return {
