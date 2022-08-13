d4593d6c47cbb8856c1576aa629d1d648984746e
miker704 <miker704@gmail.com>
2022-08-13 10:52:01 -0400

    updates

--- Strife/frontend/components/users/user_edit_profile_pic_form/user_edit_pfp_form.jsx
+++ Strife/frontend/components/users/user_edit_profile_pic_form/user_edit_pfp_form.jsx
@@ -3,7 +3,15 @@ import { Link, Redirect } from 'react-router-dom'
 
 
 class EditUserPFP extends React.Component{
-
+    constructor (props) {
+        super(props)
+        this.state = {
+            user: this.props.currentUser,
+            photo: this.props.currentUser.photo
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
 }
 
 export default EditUserPFP;
