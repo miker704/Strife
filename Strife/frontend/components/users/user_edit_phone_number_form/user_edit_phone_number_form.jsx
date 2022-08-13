e95e2ba2d1f4471ae861cbe5f4e5d80a93e807ad
miker704 <miker704@gmail.com>
2022-08-13 10:51:55 -0400

    updates

--- Strife/frontend/components/users/user_edit_phone_number_form/user_edit_phone_number_form.jsx
+++ Strife/frontend/components/users/user_edit_phone_number_form/user_edit_phone_number_form.jsx
@@ -3,7 +3,15 @@ import { Link, Redirect } from 'react-router-dom'
 
 
 class EditUserPhoneNumberForm extends React.Component{
-
+    constructor (props) {
+        super(props)
+        this.state = {
+            user: this.props.currentUser,
+            phone_number: this.props.currentUser.phone_number
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
 }
 
 export default EditUserPhoneNumberForm;
