5eb82005fb21f6cdd543ca8e867a222cad000c88
miker704 <miker704@gmail.com>
2022-08-13 10:51:52 -0400

    updates

--- Strife/frontend/components/users/user_edit_password_form/user_edit_password_form.jsx
+++ Strife/frontend/components/users/user_edit_password_form/user_edit_password_form.jsx
@@ -3,7 +3,16 @@ import { Link, Redirect } from 'react-router-dom'
 
 
 class EditUserPasswordForm extends React.Component{
-
+    constructor (props) {
+        super(props)
+        this.state = {
+            user: this.props.currentUser,
+            password: "",
+            newPassword: ""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
 }
 
 export default EditUserPasswordForm;
