497f36defc04c027d61c30b3407dce823c97f3cb
miker704 <miker704@gmail.com>
2022-08-13 10:24:11 -0400

    started file structre

---
+++ Strife/frontend/components/users/user_edit_phone_number_form/user_edit_phone_number_form.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Link, Redirect } from 'react-router-dom'
+
+
+class EditUserPhoneNumberForm extends React.Component{
+
+}
+
+export default EditUserPhoneNumberForm;
