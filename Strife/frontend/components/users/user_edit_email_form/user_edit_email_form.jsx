b369f8776387b1ff57d0b2832eef08c586bf2e1c
miker704 <miker704@gmail.com>
2022-08-13 10:51:48 -0400

    updates

--- Strife/frontend/components/users/user_edit_email_form/user_edit_email_form.jsx
+++ Strife/frontend/components/users/user_edit_email_form/user_edit_email_form.jsx
@@ -3,7 +3,15 @@ import { Link, Redirect } from 'react-router-dom'
 
 
 class EditUserEmailForm extends React.Component{
-
+    constructor (props) {
+        super(props)
+        this.state = {
+            user: this.props.currentUser,
+            email: this.props.currentUser.email
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
 }
 
 export default EditUserEmailForm;
