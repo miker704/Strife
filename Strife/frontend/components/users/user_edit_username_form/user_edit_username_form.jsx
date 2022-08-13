a362ce9001d2b5002366c003b62ae2b7273f09aa
miker704 <miker704@gmail.com>
2022-08-13 10:52:50 -0400

    updates

--- Strife/frontend/components/users/user_edit_username_form/user_edit_username_form.jsx
+++ Strife/frontend/components/users/user_edit_username_form/user_edit_username_form.jsx
@@ -2,7 +2,17 @@ import React from "react"
 import { Link, Redirect } from 'react-router-dom'
 
 
-class EditUsernameForm extends React.Component{
+class EditUsernameForm extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            user: this.props.currentUser,
+            username: this.props.currentUser.username
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+
 
 }
 
