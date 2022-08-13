423b170b9fa49ceb3cb591f07558715bda17df80
miker704 <miker704@gmail.com>
2022-08-13 10:25:02 -0400

    file structure

--- Strife/frontend/components/users/user_edit_password_form/user_edit_password_container.js
+++ Strife/frontend/components/users/user_edit_password_form/user_edit_password_container.js
@@ -1,2 +1,21 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
+
+
+
+
+
+const mSTP = (state) => {
+    return {
+
+    }
+};
+
+const mDTP = (dispatch) => {
+    return {
+
+    }
+};
+
+const EditUserPhoneNumberContainer = withRouter(connect(mSTP,mDTP)(EditUserPhoneNumberForm))
+export default EditUserPhoneNumberContainer;
