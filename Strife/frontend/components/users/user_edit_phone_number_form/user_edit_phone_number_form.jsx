4994aad30e7f0b03bd83cc8eeb5a0740d8a4dd09
miker704 <miker704@gmail.com>
2022-08-14 14:03:22 -0400

    started

--- Strife/frontend/components/users/user_edit_phone_number_form/user_edit_phone_number_form.jsx
+++ Strife/frontend/components/users/user_edit_phone_number_form/user_edit_phone_number_form.jsx
@@ -5,15 +5,29 @@ import { Link, Redirect } from 'react-router-dom'
 class EditUserPhoneNumberForm extends React.Component {
     constructor (props) {
         super(props)
-        this.state = {
-            user: this.props.currentUser,
-            phone_number: this.props.currentUser.phone_number
-        }
+       
+        this.state = this.props.currentUser;
+        this.cancel = false;
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleCloseModal = this.handleCloseModal.bind(this);
         this.handleInput = this.handleInput.bind(this);
+        this.passwordErrors = this.passwordErrors.bind(this);
 
     }
+
+
+    passwordErrors () {
+        console.log("hello password errors");
+        if (this.props.errors.includes('Login or password is invalid')) {
+            console.log("no match in password")
+            return " - Password does not match.";
+        }
+        else if (this.props.errors.includes("Incorrect Password !")){
+            console.log("password is incorrect")
+            return " - Password does not match.";
+        }
+        return "";
+    }
+
 
     componentWillUnmount () {
         this.props.removeSessionErrors()
@@ -30,6 +44,29 @@ class EditUserPhoneNumberForm extends React.Component {
         }
         this.props.updateUserInfo(submissionState);
       }
+
+
+
+      render (){
+        // let usernameErrorTag = this.props.errors.length > 0 ? "field-error" : "";
+        let passwordErrorTag = this.props.errors.length > 0 ? "field-error" : "";
+
+        return (
+        
+        <div>
+
+
+
+        </div>
+        
+        )
+      }
+
+
+
+
+
+
 }
 
 export default EditUserPhoneNumberForm;
