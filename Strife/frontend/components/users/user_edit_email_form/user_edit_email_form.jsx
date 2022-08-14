b6c9802d20eca65c6f101694f30cd5272e0e91fe
miker704 <miker704@gmail.com>
2022-08-14 14:03:15 -0400

    started

--- Strife/frontend/components/users/user_edit_email_form/user_edit_email_form.jsx
+++ Strife/frontend/components/users/user_edit_email_form/user_edit_email_form.jsx
@@ -12,7 +12,51 @@ class EditUserEmailForm extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleCloseModal = this.handleCloseModal.bind(this);
         this.handleInput = this.handleInput.bind(this);
+        this.userNameErrors = this.userNameErrors.bind(this);
+        this.passwordErrors = this.passwordErrors.bind(this);
 
+    }
+
+    
+    passwordErrors () {
+        if (this.props.errors.includes('Login or password is invalid')) {
+            return " - Password does not match.";
+        }
+        else if (this.props.errors.includes("Error Incorrect Password !")){
+            return " - Password does not match.";
+        }
+        return "";
+    }
+
+
+
+    emailErrors() {
+        const EMAIL_ERRORS = ['Email Not well formed email address', "Must be 320 or fewer in Length", "Email can't be blank", "Email has already been taken", "Login or password is invalid"];
+
+        if (this.props.formType === "Sign In") {
+            if (this.props.errors.includes('Login or password is invalid')) {
+                return " - Login or password is invalid";
+            }
+        }
+
+        if (this.props.formType === "Sign Up") {
+            if (this.props.errors.includes("Email can't be blank")) {
+                return " - can't be blank";
+            }
+            else if (this.props.errors.includes("Email Not well formed email address")) {
+
+                return " - Not well formed email address";
+            }
+            else if (this.props.errors.includes("Email Must be 320 or fewer in Length")) {
+                return " - Must be 320 or fewer in Length";
+            }
+            else if (this.props.errors.includes("Email has already been taken")) {
+                return " - Email is already registered ";
+            }
+
+        }
+
+        return "";
     }
 
 
@@ -31,6 +75,23 @@ class EditUserEmailForm extends React.Component {
         }
         this.props.updateUserInfo(submissionState);
       }
+
+
+
+      render (){
+
+        let emailErrorTag = this.props.errors.length > 0 ? "field-error" : "";
+        let passwordErrorTag = this.props.errors.length > 0 ? "field-error" : "";
+
+        return (
+            <div>
+
+            </div>
+        )
+      }
+
+
+
 }
 
 export default EditUserEmailForm;
