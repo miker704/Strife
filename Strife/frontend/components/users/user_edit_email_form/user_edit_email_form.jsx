2dab1674e283bd1b5810fbf61a60ada451408e02
miker704 <miker704@gmail.com>
2022-08-14 15:02:14 -0400

    styles

--- Strife/frontend/components/users/user_edit_email_form/user_edit_email_form.jsx
+++ Strife/frontend/components/users/user_edit_email_form/user_edit_email_form.jsx
@@ -90,10 +90,10 @@ class EditUserEmailForm extends React.Component {
             <div id="edit-userInfo-model" className="edit-userInfo-model" >
             <div className="edit-username-header-section">
                 <div className="edit-username-header">
-                    Change your username
+                    Enter an email address
                 </div>
-                <div className="edit-username-header-info">
-                    Enter a new username and your existing password
+                <div className="form-email-header-info">
+                    Enter a new email address and your existing password
                 </div>
             </div>
             <form  onSubmit={this.handleSubmit}>
@@ -102,33 +102,10 @@ class EditUserEmailForm extends React.Component {
                     <div className="form-username-sec">
                         <h5 className="form-username-header"><label className={emailErrorTag}>Email{this.emailErrors()}</label></h5>
                         <div className="username-form-input-sec">
-                            <div className="username-input-wrapper1">
-                                <input placeholder={this.props.currentUser.email} value={this.state.email} onChange={this.handleInput("email")} className="input-1" type="email" />
+                            <div className="email-input-wrapper">
+                                <input placeholder={this.props.currentUser.email} value={this.state.email} onChange={this.handleInput("email")} className="input-4-email" type="email" />
                             </div>
-                            <div className="username-input-wrapper2">
-                                <span className="quarter-hash-tag">#</span>
-                                <input className="input-2" type="text" placeholder={this.props.currentUser.strife_id_tag} disabled />
-                            </div>
-
-                            <span className="form-question-mark">
-                                <svg aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 24 24">
-                                    <g fill="currentColor" fillRule="evenodd" transform="translate(7 4)">
-                                        <path d="M0 4.3258427C0 5.06741573.616438356 5.68539326 1.35616438 5.68539326 2.09589041
-                                     5.68539326 2.71232877 5.06741573 2.71232877 4.3258427 2.71232877 2.84269663 4.31506849
-                                      2.78089888 4.5 2.78089888 4.68493151 2.78089888 6.28767123 2.84269663 6.28767123
-                                       4.3258427L6.28767123 4.63483146C6.28767123 5.25280899 5.97945205 5.74719101 5.42465753
-                                        6.05617978L4.19178082 6.73595506C3.51369863 7.10674157 3.14383562 7.78651685 3.14383562
-                                         8.52808989L3.14383562 9.64044944C3.14383562 10.3820225 3.76027397 11 4.5 11 5.23972603
-                                          11 5.85616438 10.3820225 5.85616438 9.64044944L5.85616438 8.96067416 6.71917808 8.52808989C8.1369863
-                                           7.78651685 9 6.30337079 9 4.69662921L9 4.3258427C9 1.48314607 6.71917808 0 4.5 0 2.21917808 0 0 
-                                           1.48314607 0 4.3258427zM4.5 12C2.5 12 2.5 15 4.5 15 6.5 15 6.5 12 4.5 12L4.5 12z">
-                                        </path>
-                                    </g>
-                                </svg>
-                                <div className="user-settings-toggle4">Want to customize your tag? Get Nitro!</div>
-
-                            </span>
-
+                         
                         </div>
                     </div>
                     <div className="password-section">
