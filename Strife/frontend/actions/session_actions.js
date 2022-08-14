9e5b4dba152ba6e34e3c6a940287e2a54119adda
miker704 <miker704@gmail.com>
2022-08-14 16:04:21 -0400

    fixed a error in dev tools that causes logout err

--- Strife/frontend/actions/session_actions.js
+++ Strife/frontend/actions/session_actions.js
@@ -2,35 +2,35 @@
 import * as SessionAPIUtil from "../utils/session_api_util.js"
 
 
- export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
- export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
- export const REMOVE_CURRENT_USER  = "REMOVE_CURRENT_USER";
- export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
- export const REMOVE_SESSION_ERRORS = "REMOVE_SESSION_ERRORS";
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const REMOVE_CURRENT_USER = "REMOVE_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+export const REMOVE_SESSION_ERRORS = "REMOVE_SESSION_ERRORS";
 
 
- export const receiveCurrentUser = currentUser => {
-    return{
+export const receiveCurrentUser = currentUser => {
+    return {
         type: RECEIVE_CURRENT_USER,
         currentUser
     }
- }
+}
 
- export const logoutCurrentUser  = () => {
-    return{
+export const logoutCurrentUser = () => {
+    return {
         type: LOGOUT_CURRENT_USER,
     }
- }
+}
 
 
 export const removeCurrentUser = (userId) => {
     return {
-            type: REMOVE_CURRENT_USER,
-            userId
+        type: REMOVE_CURRENT_USER,
+        userId
     }
 }
 
-export const receiveSessionErrors =  (errors) => {
+export const receiveSessionErrors = (errors) => {
     return {
         type: RECEIVE_SESSION_ERRORS,
         errors
@@ -46,26 +46,26 @@ export const removeSessionErrors = () => {
 
 
 
- //ajax dispatch
+//ajax dispatch
 
- export const signUpUser = user => (dispatch) =>
- SessionAPIUtil.signUp(user).then( user => (dispatch(receiveCurrentUser(user))), err => (dispatch(receiveSessionErrors(err.responseJSON))));
+export const signUpUser = user => (dispatch) =>
+    SessionAPIUtil.signUp(user).then(user => (dispatch(receiveCurrentUser(user))), err => (dispatch(receiveSessionErrors(err.responseJSON))));
 
- export const signInUser = user => (dispatch) =>
- SessionAPIUtil.signIn(user).then( (user) =>  dispatch(receiveCurrentUser(user)), (err) => dispatch(receiveSessionErrors(err.responseJSON)));
+export const signInUser = user => (dispatch) =>
+    SessionAPIUtil.signIn(user).then((user) => dispatch(receiveCurrentUser(user)), (err) => dispatch(receiveSessionErrors(err.responseJSON)));
 
- export const logoutUser = ()=> (dispatch) =>
- SessionAPIUtil.signOut().then( () => dispatch(logoutCurrentUser()));
+export const logoutUser = () => (dispatch) =>{
+   return SessionAPIUtil.signOut().then(() => dispatch(logoutCurrentUser()));}
 
 
 // for updating and removing a user 
-export const updateUserInfo = user => (dispatch) => 
-SessionAPIUtil.updateUser(user).then((user) => (dispatch(receiveCurrentUser(user))),(err) => dispatch(receiveSessionErrors(err.responseJSON)));
+export const updateUserInfo = user => (dispatch) =>
+    SessionAPIUtil.updateUser(user).then((user) => (dispatch(receiveCurrentUser(user))), (err) => dispatch(receiveSessionErrors(err.responseJSON)));
 
 //delete / ban a user from Strife or a channel/server
 
-export const removeUserAccount = userId => (dispatch) => 
-SessionAPIUtil.removeUser(userId).then(() => {dispatch(logoutCurrentUser()),dispatch(removeCurrentUser(userId))}, (err) => dispatch(receiveSessionErrors(err.responseJSON)));
+export const removeUserAccount = userId => (dispatch) =>
+    SessionAPIUtil.removeUser(userId).then(() => { dispatch(logoutCurrentUser()), dispatch(removeCurrentUser(userId)) }, (err) => dispatch(receiveSessionErrors(err.responseJSON)));
 
 //search up a user 
 // export const searchUpUser = userId => (dispatch) =>
