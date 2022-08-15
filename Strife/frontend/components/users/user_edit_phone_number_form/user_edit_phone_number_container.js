823480bda9d800e651d77abaee6588b4fcba60d2
miker704 <miker704@gmail.com>
2022-08-15 08:23:05 -0400

    updtes

--- Strife/frontend/components/users/user_edit_phone_number_form/user_edit_phone_number_container.js
+++ Strife/frontend/components/users/user_edit_phone_number_form/user_edit_phone_number_container.js
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { handleKeyUp } from "../../../utils/modal_api_util";
 import EditUserPhoneNumberForm from "./user_edit_phone_number_form";
-import { updateUserInfo, removeSessionErrors, receiveSessionErrors } from "../../../actions/session_actions";
+import { updateUserInfo, removeSessionErrors } from "../../../actions/session_actions";
 import { openModal, closeModal } from "../../../actions/modal_actions";
 
 
