f94249e33b247a6315e9b3884bb6b34c3916c855
miker704 <miker704@gmail.com>
2022-08-15 12:44:19 -0400

    render remove phone form

--- Strife/frontend/components/users/user_profile/user_profile.jsx
+++ Strife/frontend/components/users/user_profile/user_profile.jsx
@@ -74,6 +74,32 @@ class UserProfile extends React.Component {
     if (this.state.removePhoneNumber === true) {
       window.removeEventListener('keyup', this.props.handleESC, false);
       window.addEventListener('keyup', this.handleESC, false);
+
+      return(
+        <div className="edit-userInfo-modal-wrapper" onClick={() => this.closeModal("changePhone")} >
+          <div className="edit-user-flex-box">
+            <div id="edit-userInfo-model" className="edit-userInfo-model" onClick={e => e.stopPropagation()}>
+              <div className="edit-user-info-exit-button" >
+
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  onClick={() => this.handleSubModalClose("changePhone")}
+                ><path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"></path>
+                </svg>
+
+              </div>
+              <div onSubmit={() => this.handleSubmit("changePhone")}>
+                <EditUserPhoneNumberContainer />
+              </div>
+
+            </div>
+          </div>
+        </div>
+      )
+
+
     }
   }
 
@@ -481,6 +507,7 @@ class UserProfile extends React.Component {
 
     return (
       <div className="user-profile-wrapper" onClick={e => e.stopPropagation()}>
+        {this.renderRemovePhoneNumber()}
         {this.renderChangeEmail()}
         {this.renderChangePhone()}
         {this.renderEditUserNameModal()}
