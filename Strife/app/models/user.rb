fd69109595c9763899f853296df602882c604ccc
miker704 <miker704@gmail.com>
2022-08-15 11:08:15 -0400

    edit validations

--- Strife/app/models/user.rb
+++ Strife/app/models/user.rb
@@ -39,7 +39,7 @@ class User < ApplicationRecord
     validates :email, format: {with: /\A[\w+-.]+@[a-z\d-]+(.[a-z\d-]+)*.[a-z]+\z/i, message: "Not well formed email address"}, 
     length: {maximum:320, too_long: "Must be 320 or fewer in Length"},uniqueness: {case_sensitive: false}
     validates :phone_number, format:{with: /\+\d{1}-\d{3}-\d{3}-\d{4}/, message: "is invalid, please confirm that it is correct."},
-    length: {is: 15, message: "Phone Number is not of 9 digits long" }, uniqueness: true, allow_nil: true
+    length: {is: 15, message: "is not of 9 digits long" }, uniqueness: true, allow_nil: true
     validates :online, inclusion: {in: [true,false]}
 
     # has_one_attached :profile_pic_url 
