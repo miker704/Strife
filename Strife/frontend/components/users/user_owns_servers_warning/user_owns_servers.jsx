2d2c5ffa1eac5a6241df35bc8c7db47ec7aac646
miker704 <miker704@gmail.com>
2022-08-16 11:24:52 -0400

    updates

---
+++ Strife/frontend/components/users/user_owns_servers_warning/user_owns_servers.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+class UserOwnsServers extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        
+    }
+
+
+}
+
+export default UserOwnsServers;
