6314f0cc8abd9ae5e504032051f6b884bf6f6f43
miker704 <miker704@gmail.com>
2022-08-16 10:06:58 -0400

    updates

--- Strife/frontend/components/users/user_disable_account_form/user_disable_account_container.js
+++ Strife/frontend/components/users/user_disable_account_form/user_disable_account_container.js
@@ -8,3 +8,18 @@ const mSTP = (state) => {
         errors: state.errors.session
     }
 };
+
+
+
+const mDTP = (dispatch) => {
+    return {
+        updateUserInfo : (user) => {dispatch(updateUserInfo(user))},
+        openModal: (modal) => dispatch(openModal(modal)),
+        closeModal: () => dispatch(closeModal()),
+        removeSessionErrors: () => dispatch(removeSessionErrors()),
+        handleESC : (e) => handleKeyUp(e)
+    }
+};
+
+const EditUserEmailContainer = withRouter(connect(mSTP,mDTP)(EditUserEmailForm))
+export default EditUserEmailContainer;
