b790c8b1a083b59f5718594053873a5865767e1e
miker704 <miker704@gmail.com>
2022-08-16 15:48:02 -0400

    updates

--- Strife/frontend/components/users/user_disable_account_form/user_disable_account_form.jsx
+++ Strife/frontend/components/users/user_disable_account_form/user_disable_account_form.jsx
@@ -8,7 +8,8 @@ class DisableUserAccountForm extends React.Component {
 
         this.state = {
             username: this.props.currentUser.username,
-            password: ""
+            password: "",
+            hasServers: false
         }
 
         this.hasServers = false;
@@ -53,49 +54,93 @@ class DisableUserAccountForm extends React.Component {
         }
 
 
-        this.props.updateUserInfo(subState);
+        // this.props.updateUserInfo(subState);
+        this.props.disableUserAccout(subState);
+    }
+    componentDidMount () {
+        if (this.props.currentUser.ownedServers.length > 0) {
+            // this.hasServers = true;
+            this.setState({ hasServers: true });
+        }
+
     }
 
-
-
+    componentWillUnmount () {
+        this.props.removeSessionErrors();
+    }
 
     render () {
+
+
+        let hasServers = this.state.hasServers === true ? (<div id="edit-userInfo-model" className="edit-userInfo-model" >
+            <div className="remove-phone-form-header-wrapper">
+                <div className="remove-phone-header">
+                    You Own Servers!
+                </div>
+            </div>
+            <div className="edit-username-header-info">
+                In order to delete or disable your account you must first transfer ownership of all servers that you own.
+            </div>
+            <div className="username-edit-sep"></div>
+
+            <form onSubmit={this.handleSubmit}>
+
+                <div className="username-edit-button-sec">
+                    <button type="submit" onClick={() => this.cancel = true} className="username-edit-submit-button">Okay</button>
+                </div>
+
+            </form>
+        </div>) : ("");
+
+
+        let disAcc = this.state.hasServers === false ? (<div id="edit-userInfo-model" className="edit-userInfo-model" >
+            <div className="remove-phone-form-header-wrapper">
+                <div className="remove-phone-header">
+                    Disable Account
+                </div>
+            </div>
+            <div className="disable-or-delete-container-warning">
+                Are You sure that you want to disable your account? This will immediately
+                log you out and make your account inaccessible to anyone.
+            </div>
+            <div className="username-edit-sep"></div>
+
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-container1">
+
+
+                    <div className="password-section">
+                        <h5 className="password-header1">
+                            <label className={passwordErrorTag}>Current Password{this.passwordErrors()}</label>
+                        </h5>
+                        <div className="input-3-password-wrapper">
+                            <input value={this.state.password} onChange={this.handleInput("password")} type="password" className="input-3-password" />
+                        </div>
+                    </div>
+                    <div className="username-edit-sep"></div>
+                </div>
+                <div className="username-edit-button-sec">
+                    <button type="submit" className="username-edit-submit-button">Disable Account</button>
+                    <button type="submit" onClick={() => this.cancel = true} className="username-edit-cancel-button">Cancel</button>
+                </div>
+
+            </form>
+        </div>
+
+
+        ) : ("");
+
+
+
+
+
         let passwordErrorTag = this.props.errors.length > 0 ? "field-error" : "";
 
         return (
             <div id="edit-userInfo-model" className="edit-userInfo-model" >
-                <div className="remove-phone-form-header-wrapper">
-                    <div className="remove-phone-header">
-                        Disable Account
-                    </div>
-                </div>
-                <div className="disable-or-delete-container-warning">
-                        
-                </div>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-container1">
-
-
-                        <div className="password-section">
-                            <h5 className="password-header1">
-                                <label className={passwordErrorTag}>Current Password{this.passwordErrors()}</label>
-                            </h5>
-                            <div className="input-3-password-wrapper">
-                                <input value={this.state.password} onChange={this.handleInput("password")} type="password" className="input-3-password" />
-                            </div>
-                        </div>
-                        <div className="username-edit-sep"></div>
-                    </div>
-                    <div className="username-edit-button-sec">
-                        <button type="submit" className="username-edit-submit-button">Done</button>
-                        <button type="submit" onClick={() => this.cancel = true} className="username-edit-cancel-button">Cancel</button>
-                    </div>
-
-
-
-                </form>
+                {hasServers}
+                {disAcc}
             </div>
-
         )
 
     }
