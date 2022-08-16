feced6f4a4ae2eff75259305dcda55789a3a3b30
miker704 <miker704@gmail.com>
2022-08-16 10:07:20 -0400

    updates

--- Strife/frontend/components/users/user_delete_account_form/user_delete_account_container.js
+++ Strife/frontend/components/users/user_delete_account_form/user_delete_account_container.js
@@ -1,2 +1,9 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
+
+const mSTP = (state) => {
+    return {
+        currentUser: state.entities.users[state.session.id],
+        errors: state.errors.session
+    }
+};
