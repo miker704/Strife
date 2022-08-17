3dbbc239e23e5a14a7e9a29e5c8b795c382d8eed
miker704 <miker704@gmail.com>
2022-08-17 10:19:36 -0400

    added x out to pfpf form

--- Strife/frontend/components/users/user_profile/user_profile.jsx
+++ Strife/frontend/components/users/user_profile/user_profile.jsx
@@ -254,9 +254,19 @@ class UserProfile extends React.Component {
         <div className="edit-userInfo-modal-wrapper" onClick={() => this.closeModal("changePFP")} >
           <div className="edit-user-flex-box">
             <div id="edit-userInfo-model" className="edit-userInfo-model" onClick={e => e.stopPropagation()}>
+              <div className="edit-user-info-exit-button" >
 
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  onClick={() => this.handleSubModalClose("changePFP")}
+                ><path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"></path>
+                </svg>
+
+              </div>
               <div onSubmit={() => this.handleSubmit("changePFP")}>
-                <EditUserPFPContainer/>
+                <EditUserPFPContainer />
 
               </div>
 
