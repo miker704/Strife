6427a0586094deafc5d60dbde9c2c59ee3b7e768
miker704 <miker704@gmail.com>
2022-08-16 20:53:10 -0400

    updates

--- Strife/frontend/components/users/user_disable_account_form/user_disable_account_container.js
+++ Strife/frontend/components/users/user_disable_account_form/user_disable_account_container.js
@@ -1,12 +1,14 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import DisableUserAccountForm from "./user_disable_account_form.jsx";
-import { removeSessionErrors, disableUserAccout } from "../../../actions/session_actions";
+import { removeSessionErrors, disableUserAccout, logoutUser } from "../../../actions/session_actions";
+import { closeModal } from "../../../actions/modal_actions.js";
 
 const mSTP = (state) => {
     return {
         currentUser: state.entities.users[state.session.id],
-        errors: state.errors.session
+        errors: state.errors.session,
+
     }
 };
 
@@ -14,8 +16,10 @@ const mSTP = (state) => {
 
 const mDTP = (dispatch) => {
     return {
-        disableUserAccout : (user) => dispatch(disableUserAccout(user)),
+        disableUserAccout: (user) => dispatch(disableUserAccout(user)),
+        logoutUser: () => dispatch(logoutUser()),
         removeSessionErrors: () => dispatch(removeSessionErrors()),
+        closeModal:() => dispatch(closeModal())
     }
 };
 
