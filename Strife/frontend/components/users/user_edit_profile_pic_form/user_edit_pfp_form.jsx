2b7a382ab19c5c2676ea9aafac94193212605d07
miker704 <miker704@gmail.com>
2022-08-17 10:08:50 -0400

    updates testeing form design

--- Strife/frontend/components/users/user_edit_profile_pic_form/user_edit_pfp_form.jsx
+++ Strife/frontend/components/users/user_edit_profile_pic_form/user_edit_pfp_form.jsx
@@ -9,13 +9,28 @@ class EditUserPFP extends React.Component {
             user: this.props.currentUser,
             photo: this.props.currentUser.photo,
             newPhoto: this.props.currentUser.photo,
-            
+
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInput = this.handleInput.bind(this);
-        this.handleCloseModal = this.handleCloseModal.bind(this);
         this.handleFileInput = this.handleFileInput.bind(this);
+        this.fileProcessingErrors = this.fileProcessingErrors.bind(this);
     }
+
+
+    fileProcessingErrors(){
+        if(this.props.errors.includes('cannot process file')){
+            return " - file is bad format/failed to process";
+        }
+        return "";
+    }
+
+
+    handleFileInput(e){
+
+    }
+
+
 
     componentWillUnmount () {
         this.props.removeSessionErrors()
@@ -25,24 +40,59 @@ class EditUserPFP extends React.Component {
         return (e) => { this.setState({ [field]: e.currentTarget.value }) }
     }
 
-    handleSubmit(e){
+    handleSubmit (e) {
         e.preventDefault();
-    
+
         let submissionState = {
             photo: this.state.newPhoto
         }
         this.props.updateUserInfo(submissionState);
-      }
+    }
 
-      render(){
+    render () {
+
+        //checking to see what is what 
+        console.log("user photo : ", this.props.currentUser.photo);
+        let fileErrorTag = this.props.errors.length > 0 ? "field-error" : "";
 
 
-        return(
-            <div></div>
+        return (
+
+
+            <div id="edit-userInfo-model" className="edit-userInfo-model" >
+                <div className="remove-phone-form-header-wrapper">
+                    <div className="remove-phone-header">
+                        Remove Phone Number
+                    </div>
+                </div>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-container1">
+
+
+                        <div className="password-section">
+                            <h5 className="password-header1">
+                                <label className={fileErrorTag}>{this.fileProcessingErrors()}</label>
+                            </h5>
+                            <div className="input-3-password-wrapper">
+                                <input value={this.state.newPhoto} onChange={this.handleFileInput("file")} type="password" className="input-3-password" />
+                            </div>
+                        </div>
+                        <div className="username-edit-sep"></div>
+                    </div>
+                    <div className="username-edit-button-sec">
+                        <button type="submit" className="username-edit-submit-button">Done</button>
+                        <button type="submit" onClick={() => this.cancel = true} className="username-edit-cancel-button">Cancel</button>
+                    </div>
+
+
+
+                </form>
+            </div>
+
         )
 
 
-      }
+    }
 
 
 
