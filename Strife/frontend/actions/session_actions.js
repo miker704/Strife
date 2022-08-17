7f79790953d95bff69ca73f8cc2f1b7db374b36c
miker704 <miker704@gmail.com>
2022-08-17 08:45:30 -0400

    fixed sytax error

--- Strife/frontend/actions/session_actions.js
+++ Strife/frontend/actions/session_actions.js
@@ -84,7 +84,7 @@ export const changeUserPFP = user => (dispatch) =>
     SessionAPIUtil.changeUserPFP(user).then((user) => (dispatch(receiveCurrentUser(user))), (err) => (dispatch(receiveSessionErrors(err.responseJSON))));
 
 
-export const disableUserAccout = user => (dispatch) =>
+export const disableUserAccount = user => (dispatch) =>
     SessionAPIUtil.disableAccount(user).then((user) => (
         dispatch(receiveCurrentUser(user))
         ), 
