ebe695857a83ff2590dfd37bc4c3edfbc389fd54
miker704 <miker704@gmail.com>
2022-08-19 14:40:34 -0400

    updates

--- Strife/frontend/components/channels/channel_nav_bar/channel_nav_bar_container.js
+++ Strife/frontend/components/channels/channel_nav_bar/channel_nav_bar_container.js
@@ -1,6 +1,6 @@
 import { withRouter } from "react-router";
 import { connect } from "react-redux";
-
+import ChannelNavBar from "./channel_nav_bar";
 
 const mSTP = (state) => {
 
@@ -8,3 +8,15 @@ const mSTP = (state) => {
 
     }
 };
+
+
+const mDTP = (dispatch) => {
+    return {
+        fetchChannel: (channelId) => { dispatch(fetchChannel(channelId)) },
+    }
+}
+
+
+const ChannelNavBarContainer = withRouter(connect(mSTP, mDTP)(ChannelNavBar));
+
+export default ChannelNavBarContainer;
