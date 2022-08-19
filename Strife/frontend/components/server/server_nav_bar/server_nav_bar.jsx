7f1a10c5eef6844635dcb637ba29d1164765bc33
miker704 <miker704@gmail.com>
2022-08-19 12:10:03 -0400

    removed dead state

--- Strife/frontend/components/server/server_nav_bar/server_nav_bar.jsx
+++ Strife/frontend/components/server/server_nav_bar/server_nav_bar.jsx
@@ -121,8 +121,8 @@ class ServerNavBar extends React.Component {
                         </div> */}
 
                         <div className="server-nav-bar-a">
-                            <button id="download-apps" onClick={() => this.props.openModal('downloadApps')}>
-
+                            <button id="download-apps" >
+                                 {/* onClick={() => this.props.openModal('downloadApps')} */}
                                 <svg fill="currentColor" aria-hidden="false" width="24" height="24" viewBox="0 0 24 24">
                                     <path d="M16.293 9.293L17.707 10.707L12 16.414L6.29297 10.707L7.70697 9.293L11 
                                          12.586V2H13V12.586L16.293 9.293ZM18 20V18H20V20C20 21.102 19.104 22 18 22H6C4.896 22 4 21.102 4 
