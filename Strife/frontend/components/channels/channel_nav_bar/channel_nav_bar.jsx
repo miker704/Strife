4e8b6006fc3d20dbec81b98bff1ffdb6212f307b
miker704 <miker704@gmail.com>
2022-08-20 01:00:36 -0400

    started

--- Strife/frontend/components/channels/channel_nav_bar/channel_nav_bar.jsx
+++ Strife/frontend/components/channels/channel_nav_bar/channel_nav_bar.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 
-class ChannelNavBar extends React.Component{
+class ChannelNavBar extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state
+
+    }
+
+
+    render () {
+    let default_profile_pic = this.props.currentUser.photo === undefined ? "https://strife-seeds.s3.amazonaws.com/defaultProfilePic.png" : this.props.currentUser.photo;
+
+
+        if (this.props.server) {
+            return (
+                <div id="channel-nav-bar">
+                    <div id="channel-nav-sep">
+
+                        <div id="channel-nav-bar-server-name">
+                            <h5>{this.props.server.server_name}</h5>
+
+                        </div>
+
+
+
+                    </div>
+                </div>
+            )
+        }
+        else {
+            return null;
+        }
+    }
 
 }
 
