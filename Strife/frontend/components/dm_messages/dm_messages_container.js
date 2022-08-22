f73d9ca6aa8c6f47b359e92e5732295a70334aa4
miker704 <miker704@gmail.com>
2022-08-22 11:26:14 -0400

    imports

--- Strife/frontend/components/dm_messages/dm_messages_container.js
+++ Strife/frontend/components/dm_messages/dm_messages_container.js
@@ -1,3 +1,4 @@
 import { Connect } from "react-redux";
 import { withRouter } from "react-router";
 import { fetchDmServer } from "../../actions/dm_server_actions.js";
+import { createDmMessage } from "../../actions/dm_messages_actions.js";
