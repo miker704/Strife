68f31800a0e3c4ab27ebfe727ce4103bc437178a
miker704 <miker704@gmail.com>
2022-08-22 11:33:28 -0400

    started  class setup

---
+++ Strife/frontend/components/dm_messages/dm_messages.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+class DmMessages extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default DmMessages;
