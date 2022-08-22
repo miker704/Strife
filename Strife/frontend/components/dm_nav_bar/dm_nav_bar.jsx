21e31380ebe673a14cec60f57b5abb7452cf9930
miker704 <miker704@gmail.com>
2022-08-22 16:47:27 -0400

    testing

--- Strife/frontend/components/dm_nav_bar/dm_nav_bar.jsx
+++ Strife/frontend/components/dm_nav_bar/dm_nav_bar.jsx
@@ -45,7 +45,6 @@ class DmNavBar extends React.Component {
         console.log("dmserver props: ", this.props);
         console.log("dmusers", this.props.dmUsers);
         console.log("dmServers: ", this.props.dmServers);
-        console.log("dmServers2: ", this.props.dmServers2);
 
 
         return (
