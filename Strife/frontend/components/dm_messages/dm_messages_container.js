b9e7b9211d7930102c510736c5862832e9886218
miker704 <miker704@gmail.com>
2022-08-22 11:34:26 -0400

    started container for dm messages

--- Strife/frontend/components/dm_messages/dm_messages_container.js
+++ Strife/frontend/components/dm_messages/dm_messages_container.js
@@ -2,3 +2,21 @@ import { Connect } from "react-redux";
 import { withRouter } from "react-router";
 import { fetchDmServer } from "../../actions/dm_server_actions.js";
 import { createDmMessage } from "../../actions/dm_messages_actions.js";
+
+
+const mSTP = (state, ownProps) => {
+    return {
+
+    }
+}
+
+
+const mDTP = (dispatch, ownProps) => {
+    return {
+
+    }
+}
+
+
+const DmMessagesContainer = withRouter(connect(mSTP, mDTP)(DmMessages));
+export default DmMessagesContainer;
