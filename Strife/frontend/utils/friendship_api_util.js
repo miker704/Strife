c6a409138b0219b4a527e07f5b792ec31553e899
miker704 <miker704@gmail.com>
2022-08-21 22:34:41 -0400

    updates

--- Strife/frontend/utils/friendship_api_util.js
+++ Strife/frontend/utils/friendship_api_util.js
@@ -1,5 +1,20 @@
-export const requestFriendships = () => $.ajax({ url: `/api/friendships`, method: 'GET' });
-export const createFriendship = (account_ids) => $.ajax({ url: `/api/friendships`, method: 'POST', data: { friendship: account_ids } })
+export const requestFriendships = () => $.ajax({ url: `/api/friendships/`, method: 'GET' });
+export const createFriendship = (account_ids) => $.ajax({ url: `/api/friendships/`, method: 'POST', data: { friendship: account_ids } })
 
+
+export const updateFriendships = (account_ids) => 
+$.ajax({
+    url: `/api/friendships/`,
+    method: "PATCH",
+    data: {friendship: account_ids}
+});
+
+
+export const deleteFriendships = (account_ids) => 
+$.ajax({
+    url: `/api/friendships/`,
+    method: "DELETE",
+    data: {friendship: account_ids}
+});
 
 //i am pausing dev on friendships rn as its not as important right now
