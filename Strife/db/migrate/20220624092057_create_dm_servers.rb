cf808054db08c2b930cecd82f388e60991b9d08f
miker704 <miker704@gmail.com>
2022-08-23 09:58:23 -0400

    updates added optional dm_server_name

--- Strife/db/migrate/20220624092057_create_dm_servers.rb
+++ Strife/db/migrate/20220624092057_create_dm_servers.rb
@@ -2,6 +2,7 @@ class CreateDmServers < ActiveRecord::Migration[5.2]
   def change
     create_table :dm_servers do |t|
       t.integer :owner_id, null: false
+      t.string :dm_server_name
       t.timestamps
     end
     add_index :dm_servers, :owner_id
