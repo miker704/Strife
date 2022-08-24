2dfb5f3702cc767a7b21de762ea741ab1c908d7a
miker704 <miker704@gmail.com>
2022-08-24 17:43:30 -0400

    updattes

--- Strife/frontend/components/friends/friends_page_main/friends_home_page.jsx
+++ Strife/frontend/components/friends/friends_page_main/friends_home_page.jsx
@@ -5,6 +5,20 @@ class FriendsPage extends React.Component{
     constructor(props){
         super(props);
     }
+
+
+
+    render(){
+                    return(
+                        <div className="home-nav-bar-container">
+                                <div className="home-nav-bar">
+                                    <div className="home-nav-bar-inner"></div>
+                                </div>
+                        </div>
+                    )
+    }
+
+
 }
 
 export default FriendsPage;
