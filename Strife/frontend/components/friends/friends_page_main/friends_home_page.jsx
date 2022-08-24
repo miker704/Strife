c9e71a880b68d5b25b81657d5d3618e4d5dbba65
miker704 <miker704@gmail.com>
2022-08-24 18:40:40 -0400

    update

--- Strife/frontend/components/friends/friends_page_main/friends_home_page.jsx
+++ Strife/frontend/components/friends/friends_page_main/friends_home_page.jsx
@@ -29,23 +29,32 @@ class FriendsHomePageContainer extends React.Component {
                                 </g>
                             </svg>
                         </div>
-                            <h3 className="home-nav-bar-friends-text">Friends</h3>
-                            <div className="home-friend-divider"></div>
-                            <div className="friends-status-nav-bar">
-                                <div className="online-tab">Online</div>
-                                <div className="all-tab">All</div>
-                                <div className="pending-tab">Pending</div>
-                                <div className="blocked-tab">Blocked</div>
-                                <div className="add-friend-tab"><span>Add Friend</span></div>
+                        <h3 className="home-nav-bar-friends-text">Friends</h3>
+                        <div className="home-friend-divider"></div>
+                        <div className="friends-status-nav-bar">
+                            <div className="online-tab">Online</div>
+                            <div className="all-tab">All</div>
+                            <div className="pending-tab">Pending</div>
+                            <div className="blocked-tab">Blocked</div>
+                            <div className="add-friend-tab"><span>Add Friend</span></div>
+
+                        </div>
+                        <div className="home-nav-tool-bar">
+                            <div className="invite-tool-bar">
+                                <div className="invite-tool-bar-group-dm">
+                                    <svg x="0" y="0" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                        <path fill="currentColor" fillRule="evenodd" clipRule="evenodd" d="M20.998 0V3H23.998V5H20.998V8H18.998V5H15.998V3H18.998V0H20.998ZM2.99805
+                                         20V24L8.33205 20H14.998C16.102 20 16.998 19.103 16.998 18V9C16.998 7.896 16.102 7 14.998 7H1.99805C0.894047 7 -0.00195312 7.896 
+                                         -0.00195312 9V18C-0.00195312 19.103 0.894047 20 1.99805 20H2.99805Z">
+                                        </path>
+                                    </svg>
+                                </div>
+                                <div className="home-friend-divider"></div>
 
                             </div>
-                            <div className="home-nav-tool-bar">
-                                <div className="invite-tool-bar">
-                                    <div className="invite-tool-bar-group-dm"></div>
-                                </div>
-                                <div className="inbox-tool-bar"></div>
-                                <div className="help-tool-bar"></div>
-                            </div>
+                            <div className="inbox-tool-bar"></div>
+                            <div className="help-tool-bar"></div>
+                        </div>
                     </div>
 
 
