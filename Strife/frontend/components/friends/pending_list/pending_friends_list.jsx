f52ae46d44348c8e04fb03070ef09d26c2028bd1
miker704 <miker704@gmail.com>
2022-08-25 07:44:05 -0400

    updates

--- Strife/frontend/components/friends/pending_list/pending_friends_list.jsx
+++ Strife/frontend/components/friends/pending_list/pending_friends_list.jsx
@@ -1,16 +1,85 @@
 import React from "react";
 
-class PendingFriendList extends React.Component{
-    constructor(props){
+class PendingFriendList extends React.Component {
+    constructor (props) {
         super(props);
     }
 
-    render(){
-        return (
-            <div> 
-                
-            </div>
-        )
+    componentDidMount () {
+        this.props.requestFriendships();
+    }
+
+
+    render () {
+        let outgoing_requests = this.props.ongoing;
+        let incoming_requests = this.props.incoming;
+        let default_Photo = "https://strife-seeds.s3.amazonaws.com/defaultProfilePic.png";
+
+
+        if (outgoing_requests.length === 0 && incoming_requests.length === 0) {
+            return (
+                <div className="friend-index-container">
+                    <div className="empty-state-container">
+                        <div className="blocked-users-empty">
+                            <div className="blocked-users-flex">
+                                <img className="cant-block-wumpus" alt="img" />
+                                <div className="block-wumpus-text">You can't unblock the Wumpus.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+
+        else {
+            return (
+                <div className="friend-index-container">
+                    <div className="all-friends">
+                        {`ONLINE - ${allFriends.length}`}
+                    </div>
+                    <div className="friend-index">
+                        <ul className="friend-index-item-wrapper">
+                            {
+                                allFriends.map((friend, friendIdx) => {
+                                    return (
+                                        <li className="friend-index-item" key={friendIdx}>
+
+                                            <div className="friend-index-item-wrapper-inner">
+                                                <div className="friend-account-info-wrapper-super">
+                                                    <div className="friend-info">
+                                                        <img src={`${friend.photo === undefined ? default_Photo : friend.photo}`} alt="pfp" />
+                                                    </div>
+                                                    <div className="friend-account-info-wrapper">
+                                                        <div className="friend-account-info">
+                                                            <div className="friend-tag">
+                                                                {friend.username}
+                                                                <span>#{friend.strife_id_tag}</span>
+                                                            </div>
+                                                        </div>
+                                                        <div className="subtext">
+                                                            <div className="subtext-inner">
+                                                                {`${friend.online ? "online" : "offline"}`}
+                                                                <div className={`${friend.online ? "circle-online" : "circle-offline"}`}></div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+
+
+
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                </div>
+
+
+            )
+        }
     }
 }
 
