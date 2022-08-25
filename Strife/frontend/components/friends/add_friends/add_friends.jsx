6e4fbd18a38d7fc0811bd7ff45c5d6626e70d5b3
miker704 <miker704@gmail.com>
2022-08-25 09:22:10 -0400

    updates

--- Strife/frontend/components/friends/add_friends/add_friends.jsx
+++ Strife/frontend/components/friends/add_friends/add_friends.jsx
@@ -7,8 +7,25 @@ class AddFriends extends React.Component{
     }
     render(){
         return (
-            <div>
-
+            <div className="friend-index-container">
+                    <div className="add-friend-header-wrapper-1">
+                        <h2 className="add-friend-header-1">
+                            Add Friend
+                        </h2>
+                        <form autoComplete="off">
+                            <div className="add-friend-subtitle">You can add a friend with their STRIFE Tag. It's cAsE sEnSitIvE!</div>
+                            <div className="add-friend-input-search-wrapper">
+                                <div className="add-fr"iend-input-search-inner-wrapper">
+                                    <input className="type="text" />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="add-friend-header-wrapper-2">
+                        <h2 className="add-friend-header-2">
+                            Other places to find friends
+                        </h2>
+                    </div>
             </div>
         )
     }
