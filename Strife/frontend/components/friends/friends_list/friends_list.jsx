a2e5432f21434c739683e69cb18a1f3000410e3c
miker704 <miker704@gmail.com>
2022-08-25 11:02:55 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list.jsx
+++ Strife/frontend/components/friends/friends_list/friends_list.jsx
@@ -79,7 +79,17 @@ class FriendShipIndex extends React.Component {
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        <div><p>hellowoerld</p></div>
+                                                        <div className="friend-msg-actions">
+                                                            <div className="friend-msg-button">
+                                                                <svg className="icon-1WV" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                                    <path fill="currentColor" d="M4.79805 3C3.80445 3 2.99805 3.8055 2.99805 
+                                                                4.8V15.6C2.99805 16.5936 3.80445 17.4 4.79805 17.4H7.49805V21L11.098 
+                                                                17.4H19.198C20.1925 17.4 20.998 16.5936 20.998 15.6V4.8C20.998 3.8055 
+                                                                20.1925 3 19.198 3H4.79805Z">
+                                                                    </path>
+                                                                </svg>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
 
@@ -98,7 +108,7 @@ class FriendShipIndex extends React.Component {
             )
         }
 
-        else{
+        else {
             return (
                 <div className="friend-index-container">
                     <div className="empty-state-container">
