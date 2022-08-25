5eccd90f8a32a8bdbcf3be82da56520d2ed41a6d
miker704 <miker704@gmail.com>
2022-08-25 17:01:50 -0400

    updates

--- Strife/frontend/components/friends/pending_list/pending_friends_list.jsx
+++ Strife/frontend/components/friends/pending_list/pending_friends_list.jsx
@@ -16,7 +16,7 @@ class PendingFriendList extends React.Component {
     }
 
     deleteFriendShip(){
-        
+
     }
 
 
@@ -66,7 +66,7 @@ class PendingFriendList extends React.Component {
                                             <div className="pending-request-actions">
 
                                                 <div className="pending-check-icon">
-                                                    <svg stroke="currentColor" fill="currentColor" strokeWidth="0"
+                                                    <svg onClick={() => this.updateFriendShip()} stroke="currentColor" fill="currentColor" strokeWidth="0"
                                                         viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998
                                                      26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 
