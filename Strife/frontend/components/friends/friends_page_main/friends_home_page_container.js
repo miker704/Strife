997959d3e8e8b26ab7d25c8b56b76dae4b22ce17
miker704 <miker704@gmail.com>
2022-08-24 20:14:10 -0400

    finished contaiener

--- Strife/frontend/components/friends/friends_page_main/friends_home_page_container.js
+++ Strife/frontend/components/friends/friends_page_main/friends_home_page_container.js
@@ -1,6 +1,15 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { requestFriendships } from '../../../actions/friendship_actions';
 import FriendsHomePageContainer from './friends_home_page';
 
 
 
+const mDTP  = (dispatch) => {
+    return {
+        requestFriendships: () => dispatch(requestFriendships())
+    };
+};
+
+const HomePageContainer = withRouter(connect(null,mDTP)(FriendsHomePageContainer));
+export default HomePageContainer;
