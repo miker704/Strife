b0be169407c8c5e27d3f4e5c643dc3dffded0f62
miker704 <miker704@gmail.com>
2022-08-25 08:32:48 -0400

    updates

---
+++ Strife/frontend/components/friends/add_friends/add_friends_container.js
@@ -0,0 +1,28 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { selectFriendStatus } from '../../../utils/selectors_api_util';
+import { requestFriendships, updateFriendship, deleteFriendship } from '../../../actions/friendship_actions';
+import AddFriends from './add_friends';
+
+const mSTP = (state) => {
+    return {
+        currentUser: state.entities.users[state.session.id],
+        ongoing: selectFriendStatus(state,1),
+        incoming: selectFriendStatus(state,2)
+    }
+};
+
+
+const mDTP = (dispatch) => {
+    return {
+        requestFriendships: () => dispatch(requestFriendships()),
+        updateFriendship: (ids) => dispatch(updateFriendship(ids)),
+        deleteFriendship: (ids) => dispatch(deleteFriendship(ids))
+
+    }
+};
+
+const AddFriendsContainer = withRouter(connect(mSTP, mDTP)(AddFriends));
+export default AddFriendsContainer;
+
+
