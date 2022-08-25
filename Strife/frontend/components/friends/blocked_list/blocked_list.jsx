fd5acb171c7d105c441f6a039be679bc6ee09edb
miker704 <miker704@gmail.com>
2022-08-25 06:51:26 -0400

    updates

---
+++ Strife/frontend/components/friends/blocked_list/blocked_list.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+class BlockedList extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default BlockedList;
