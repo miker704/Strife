b7d8d6eca1fbaa91f00643f795a34c95f8ec4ad9
miker704 <miker704@gmail.com>
2022-08-25 00:06:57 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list_container.js
+++ Strife/frontend/components/friends/friends_list/friends_list_container.js
@@ -1,19 +1,22 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import FriendShipIndex from './friends_list';
+import { requestFriendships } from '../../../actions/friendship_actions';
 
 const mSTP = (state) => {
     return {
-
+        currentUser: state.entities.users[state.session.id],
+        friends: Object.values(state.entities.users)
     }
 };
 
 
 const mDTP = (dispatch) => {
     return {
+        requestFriendships: () => dispatch(requestFriendships())
 
     }
 };
 
-const FriendShipIndexContainer = withRouter(connect(mSTP,mDTP)(FriendShipIndex));
+const FriendShipIndexContainer = withRouter(connect(mSTP, mDTP)(FriendShipIndex));
 export default FriendShipIndexContainer;
