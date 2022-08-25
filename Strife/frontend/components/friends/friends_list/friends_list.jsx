7bde24a8264b804b527b6009dcdc9e22c3aa6d91
miker704 <miker704@gmail.com>
2022-08-25 00:07:00 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list.jsx
+++ Strife/frontend/components/friends/friends_list/friends_list.jsx
@@ -7,11 +7,35 @@ class FriendShipIndex extends React.Component{
         super(props)
     }
 
-    render(){
-        return (
-            <div>
+    componentDidMount(){
+        this.props.requestFriendships();
+    }
 
+
+    render(){
+        // let allFriends = this.props.requestFriendships();
+        let allFriends = [1];
+        // console.log("all friends : ", allFriends);
+        console.log("this.props.friends-index : ",this.props);
+        return (
+            // <div className="friend-index-item-wrapper">
+            //     <div className="friend-index-item">
+            //         <div className="friend-info">
+            //                 <p className="this-is-a-test">HELLOOOOOOOOOO</p>
+            //         </div>
+            //     </div>
+            // </div>
+
+            <div className="friend-index-container">
+                <div className="friend-index">
+                    <div className="all-friends">
+                        {`ALL FRIENDS - ${allFriends.length}`}
+                    </div>
+                   
+                </div>
             </div>
+
+
         )
     }
 }
