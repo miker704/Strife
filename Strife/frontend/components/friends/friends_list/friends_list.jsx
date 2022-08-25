ddb4195f1744adfd10991aebfd42fc20886031a6
miker704 <miker704@gmail.com>
2022-08-24 20:20:09 -0400

    updates

---
+++ Strife/frontend/components/friends/friends_list/friends_list.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+class FriendShipIndex extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default FriendShipIndex;
