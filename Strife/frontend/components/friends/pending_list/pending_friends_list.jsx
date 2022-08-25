129cc2551e78a3efd10e6fc4ec4b846fa033bdc8
miker704 <miker704@gmail.com>
2022-08-25 16:36:03 -0400

    updates

--- Strife/frontend/components/friends/pending_list/pending_friends_list.jsx
+++ Strife/frontend/components/friends/pending_list/pending_friends_list.jsx
@@ -56,12 +56,21 @@ class PendingFriendList extends React.Component {
                                             <div className="pending-request-actions">
 
                                                 <div className="pending-check-icon">
-                                                    
+                                                    <svg stroke="currentColor" fill="currentColor" strokeWidth="0"
+                                                        viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998
+                                                     26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 
+                                                36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 
+                                                294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                                                        </path>
+                                                    </svg>
+                                                    <div className="pending-request-actions-tool-tip">Accept</div>
+                                                    <div className="pending-request-actions-tool-tip-triangle"></div>
                                                 </div>
 
 
                                                 <div className="pending-deny-icon">
-                                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                                                    <svg stroke="currentColor" fill="currentColor" strokeWidth="0"
                                                         viewBox="0 0 352 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19
                                                      0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 
@@ -73,6 +82,8 @@ class PendingFriendList extends React.Component {
                                                           12.28-32.19 0-44.48L242.72 256z">
                                                         </path>
                                                     </svg>
+                                                    <div className="pending-request-actions-tool-tip">Ignore</div>
+                                                    <div className="pending-request-actions-tool-tip-triangle"></div>
                                                 </div>
                                             </div>
 
@@ -109,7 +120,7 @@ class PendingFriendList extends React.Component {
 
                                             <div className="pending-request-actions">
                                                 <div className="pending-deny-icon">
-                                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                                                    <svg stroke="currentColor" fill="currentColor" strokeWidth="0"
                                                         viewBox="0 0 352 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19
                                                      0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 
@@ -121,6 +132,8 @@ class PendingFriendList extends React.Component {
                                                           12.28-32.19 0-44.48L242.72 256z">
                                                         </path>
                                                     </svg>
+                                                    <div className="pending-request-actions-tool-tip">Ignore</div>
+                                                    <div className="pending-request-actions-tool-tip-triangle"></div>
                                                 </div>
                                             </div>
 
