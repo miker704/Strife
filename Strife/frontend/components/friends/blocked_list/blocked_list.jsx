e7866ee4d59bff7e5f98adc5a4449fdc330cc9ef
miker704 <miker704@gmail.com>
2022-08-25 07:50:49 -0400

    reversed render

--- Strife/frontend/components/friends/blocked_list/blocked_list.jsx
+++ Strife/frontend/components/friends/blocked_list/blocked_list.jsx
@@ -18,21 +18,21 @@ class BlockedList extends React.Component {
         let allBlockedUsers = this.props.blockedUsers;
         let default_Photo = "https://strife-seeds.s3.amazonaws.com/defaultProfilePic.png";
 
-        if (allBlockedUsers.length === 0) {
-            return (
-                <div className="friend-index-container">
-                        <div className="empty-state-container">
-                            <div className="blocked-users-empty">
-                                <div className="blocked-users-flex">
-                                    <img className="cant-block-wumpus" alt="img" />
-                                    <div className="block-wumpus-text">You can't unblock the Wumpus.</div>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-            )
-        }
-        else {
+        // if (allBlockedUsers.length === 0) {
+        //     return (
+        //         <div className="friend-index-container">
+        //                 <div className="empty-state-container">
+        //                     <div className="blocked-users-empty">
+        //                         <div className="blocked-users-flex">
+        //                             <img className="cant-block-wumpus" alt="img" />
+        //                             <div className="block-wumpus-text">You can't unblock the Wumpus.</div>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //         </div>
+        //     )
+        // }
+        if(allBlockedUsers.length > 0){
 
             return (
 
@@ -84,6 +84,22 @@ class BlockedList extends React.Component {
 
             )
 
+        }
+        else{
+            // if (allBlockedUsers.length === 0) {
+                return (
+                    <div className="friend-index-container">
+                            <div className="empty-state-container">
+                                <div className="blocked-users-empty">
+                                    <div className="blocked-users-flex">
+                                        <img className="cant-block-wumpus" alt="img" />
+                                        <div className="block-wumpus-text">You can't unblock the Wumpus.</div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                )
+            // }
         }
 
 
