2e427f4c9e4d4f8e922b288b168411e37f493726
miker704 <miker704@gmail.com>
2022-08-25 06:55:14 -0400

    started

--- Strife/frontend/components/friends/blocked_list/blocked_list.jsx
+++ Strife/frontend/components/friends/blocked_list/blocked_list.jsx
@@ -6,11 +6,65 @@ class BlockedList extends React.Component{
         super(props);
     }
 
-    render(){
-        return (
-            <div>
 
+
+    componentDidMount () {
+        this.props.requestFriendships();
+    }
+
+    
+    render(){
+
+        let allFriends = this.props.friends;
+        let default_Photo = "https://strife-seeds.s3.amazonaws.com/defaultProfilePic.png";
+
+        return (
+    
+            <div className="friend-index-container">
+                    <div className="all-friends">
+                        {`ONLINE - ${allFriends.length}`}
+                    </div>
+                <div className="friend-index">
+                    <ul className="friend-index-item-wrapper">
+                        {
+                            allFriends.map((friend, friendIdx) => {
+                                return (
+                                    <li className="friend-index-item" key={friendIdx}>
+
+                                        <div className="friend-index-item-wrapper-inner">
+                                            <div className="friend-account-info-wrapper-super">
+                                                <div className="friend-info">
+                                                    <img src={`${friend.photo === undefined ? default_Photo : friend.photo}`} alt="pfp" />
+                                                </div>
+                                                <div className="friend-account-info-wrapper">
+                                                    <div className="friend-account-info">
+                                                        <div className="friend-tag">
+                                                            {friend.username}
+                                                            <span>#{friend.strife_id_tag}</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className="subtext">
+                                                        <div className="subtext-inner">
+                                                            {`${friend.online ? "online" : "offline"}`}
+                                                            <div className={`${friend.online ? "circle-online" : "circle-offline"}`}></div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+
+
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
+
+
         )
     }
 }
