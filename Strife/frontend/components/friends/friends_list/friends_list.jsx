6921a700c27baee22f40df29024330ed2e508753
miker704 <miker704@gmail.com>
2022-08-25 11:13:59 -0400

    add message buttons

--- Strife/frontend/components/friends/friends_list/friends_list.jsx
+++ Strife/frontend/components/friends/friends_list/friends_list.jsx
@@ -79,29 +79,29 @@ class FriendShipIndex extends React.Component {
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        <div className="friend-msg-actions">
-                                                            <div className="friend-msg-button">
-                                                                <svg className="icon-1WV" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                                                    <path fill="currentColor" d="M4.79805 3C3.80445 3 2.99805 3.8055 2.99805 
+                                                    </div>
+                                                </div>
+                                                <div className="friend-msg-actions">
+                                                    <div className="friend-msg-button">
+                                                        <svg className="icon-1WV" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                            <path fill="currentColor" d="M4.79805 3C3.80445 3 2.99805 3.8055 2.99805 
                                                                 4.8V15.6C2.99805 16.5936 3.80445 17.4 4.79805 17.4H7.49805V21L11.098 
                                                                 17.4H19.198C20.1925 17.4 20.998 16.5936 20.998 15.6V4.8C20.998 3.8055 
                                                                 20.1925 3 19.198 3H4.79805Z">
-                                                                    </path>
-                                                                </svg>
-                                                            </div>
-                                                            <div className="friend-options-button">
-                                                                <svg className="icon-1WVg" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
-                                                                    <g fill="none" fillRule="evenodd">
-                                                                        <path d="M24 0v24H0V0z">
-                                                                        </path>
-                                                                        <path fill="currentColor" d="M12 16c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2
+                                                            </path>
+                                                        </svg>
+                                                    </div>
+                                                    <div className="friend-options-button">
+                                                        <svg className="icon-1WVg" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                                            <g fill="none" fillRule="evenodd">
+                                                                <path d="M24 0v24H0V0z">
+                                                                </path>
+                                                                <path fill="currentColor" d="M12 16c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2
                                                                      .8954305-2 2-2zm0-6c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2 .8954305-2
                                                                       2-2zm0-6c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2 .8954305-2 2-2z">
-                                                                        </path>
-                                                                    </g>
-                                                                </svg>
-                                                            </div>
-                                                        </div>
+                                                                </path>
+                                                            </g>
+                                                        </svg>
                                                     </div>
                                                 </div>
 
