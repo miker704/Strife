d1e483ac42abec3a414a2d7efa3df09ee7a8a922
miker704 <miker704@gmail.com>
2022-08-25 11:13:55 -0400

    add message buttons

--- Strife/frontend/components/friends/friends_list_online/friends_list_online.jsx
+++ Strife/frontend/components/friends/friends_list_online/friends_list_online.jsx
@@ -17,21 +17,21 @@ class FriendShipIndexOnline extends React.Component {
         let default_Photo = "https://strife-seeds.s3.amazonaws.com/defaultProfilePic.png";
 
 
-       
-        if(allFriends.length > 0){
+
+        if (allFriends.length > 0) {
             return (
-    
+
                 <div className="friend-index-container">
-                        <div className="all-friends">
-                            {`ONLINE - ${allFriends.length}`}
-                        </div>
+                    <div className="all-friends">
+                        {`ONLINE - ${allFriends.length}`}
+                    </div>
                     <div className="friend-index">
                         <ul className="friend-index-item-wrapper">
                             {
                                 allFriends.map((friend, friendIdx) => {
                                     return (
                                         <li className="friend-index-item" key={friendIdx}>
-    
+
                                             <div className="friend-index-item-wrapper-inner">
                                                 <div className="friend-account-info-wrapper-super">
                                                     <div className="friend-info">
@@ -51,12 +51,35 @@ class FriendShipIndexOnline extends React.Component {
                                                             </div>
                                                         </div>
                                                     </div>
-    
+
                                                 </div>
                                             </div>
-    
-    
-    
+
+                                            <div className="friend-msg-actions">
+                                                <div className="friend-msg-button">
+                                                    <svg className="icon-1WV" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                        <path fill="currentColor" d="M4.79805 3C3.80445 3 2.99805 3.8055 2.99805 
+                                                                4.8V15.6C2.99805 16.5936 3.80445 17.4 4.79805 17.4H7.49805V21L11.098 
+                                                                17.4H19.198C20.1925 17.4 20.998 16.5936 20.998 15.6V4.8C20.998 3.8055 
+                                                                20.1925 3 19.198 3H4.79805Z">
+                                                        </path>
+                                                    </svg>
+                                                </div>
+                                                <div className="friend-options-button">
+                                                    <svg className="icon-1WVg" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                                        <g fill="none" fillRule="evenodd">
+                                                            <path d="M24 0v24H0V0z">
+                                                            </path>
+                                                            <path fill="currentColor" d="M12 16c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2
+                                                                     .8954305-2 2-2zm0-6c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2 .8954305-2
+                                                                      2-2zm0-6c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2 .8954305-2 2-2z">
+                                                            </path>
+                                                        </g>
+                                                    </svg>
+                                                </div>
+                                            </div>
+
+
                                         </li>
                                     )
                                 })
@@ -64,11 +87,11 @@ class FriendShipIndexOnline extends React.Component {
                         </ul>
                     </div>
                 </div>
-    
-    
+
+
             )
         }
-        else{
+        else {
             return (
                 <div className="friend-index-container">
                     <div className="empty-state-container">
@@ -82,7 +105,7 @@ class FriendShipIndexOnline extends React.Component {
                 </div>
             )
         }
-        
+
 
 
 
