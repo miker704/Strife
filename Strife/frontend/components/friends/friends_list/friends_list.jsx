9e011d92e6a072ed99d67d3783da3abb201157cc
miker704 <miker704@gmail.com>
2022-08-25 11:06:51 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list.jsx
+++ Strife/frontend/components/friends/friends_list/friends_list.jsx
@@ -90,7 +90,16 @@ class FriendShipIndex extends React.Component {
                                                                 </svg>
                                                             </div>
                                                             <div className="friend-options-button">
-                                                                
+                                                                <svg className="icon-1WVg" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                                                    <g fill="none" fillRule="evenodd">
+                                                                        <path d="M24 0v24H0V0z">
+                                                                        </path>
+                                                                        <path fill="currentColor" d="M12 16c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2
+                                                                     .8954305-2 2-2zm0-6c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2 .8954305-2
+                                                                      2-2zm0-6c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2 .8954305-2 2-2z">
+                                                                        </path>
+                                                                    </g>
+                                                                </svg>
                                                             </div>
                                                         </div>
                                                     </div>
