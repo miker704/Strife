56b268b5f0f95a35c0ebafab227d04db998b2f62
miker704 <miker704@gmail.com>
2022-08-25 06:53:15 -0400

    finished

---
+++ Strife/frontend/components/friends/blocked_list/blocked_list_container.js
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { selectFriendStatus } from '../../../utils/selectors_api_util';
+import { requestFriendships } from '../../../actions/friendship_actions';
+import BlockedList from './blocked_list';
+
+const mSTP = (state) => {
+    return {
+        currentUser: state.entities.users[state.session.id],
+        friends: selectFriendStatus(state,3)
+    }
+};
+
+
+const mDTP = (dispatch) => {
+    return {
+        requestFriendships: () => dispatch(requestFriendships())
+
+    }
+};
+
+const BlockedListContainer = withRouter(connect(mSTP, mDTP)(BlockedList));
+export default BlockedListContainer;
+
+
