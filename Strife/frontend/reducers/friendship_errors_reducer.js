efe6c82f52d76b133c2e07aa603348b23e7c343b
miker704 <miker704@gmail.com>
2022-08-26 09:55:29 -0400

    err

--- Strife/frontend/reducers/friendship_errors_reducer.js
+++ Strife/frontend/reducers/friendship_errors_reducer.js
@@ -1,2 +1,7 @@
 import { RECEIVE_FRIENDSHIP_ERRORS,REMOVE_FRIENDSHIP_ERRORS } from "../actions/friendship_actions";
 
+const friendshipErrorsReducer = (state = [], actions) => {
+
+}
+
+export default friendshipErrorsReducer;
