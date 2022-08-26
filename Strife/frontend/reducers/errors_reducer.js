da2c4ee837033fb34f98d750bdda106d7a808e6b
miker704 <miker704@gmail.com>
2022-08-26 09:59:43 -0400

    updates

--- Strife/frontend/reducers/errors_reducer.js
+++ Strife/frontend/reducers/errors_reducer.js
@@ -7,6 +7,7 @@ import channelErrorsReducer from "./channel_errors_reducer";
 import messageErrorsReducer from "./message_errors_reducer";
 import dm_MessageErrorsReducer from "./dm_message_errors_reducer";
 import dmServerErrorsReducer from "./dm_server_errors_reducer";
+import friendshipErrorsReducer from "./friendship_errors_reducer";
 
 const errorsReducer = combineReducers({
     session: sessionErrorsReducer,
