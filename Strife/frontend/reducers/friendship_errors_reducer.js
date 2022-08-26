0d4615cb7fef0802ea8dc9fd178a8b547ef37c63
miker704 <miker704@gmail.com>
2022-08-26 09:57:27 -0400

    updates

--- Strife/frontend/reducers/friendship_errors_reducer.js
+++ Strife/frontend/reducers/friendship_errors_reducer.js
@@ -1,7 +1,16 @@
 import { RECEIVE_FRIENDSHIP_ERRORS,REMOVE_FRIENDSHIP_ERRORS } from "../actions/friendship_actions";
 
-const friendshipErrorsReducer = (state = [], actions) => {
+const friendshipErrorsReducer = (state = [], action) => {
         Object.freeze(state);
+
+        switch(action){
+            case RECEIVE_FRIENDSHIP_ERRORS:
+                return action.errors
+            case REMOVE_FRIENDSHIP_ERRORS:
+                return [];
+            default:
+                return state;
+        }
 }
 
 export default friendshipErrorsReducer;
