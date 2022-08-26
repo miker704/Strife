5323cd1648869bd04911ec5a6c28f0aa03fd600c
miker704 <miker704@gmail.com>
2022-08-26 10:16:32 -0400

    updates

--- Strife/frontend/components/friends/pending_list/pending_friends_list_container.js
+++ Strife/frontend/components/friends/pending_list/pending_friends_list_container.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { selectFriendStatus } from '../../../utils/selectors_api_util';
-import { requestFriendships, updateFriendship, deleteFriendship } from '../../../actions/friendship_actions';
+import { requestFriendships, updateFriendship, deleteFriendship, removeFriendshipErrors } from '../../../actions/friendship_actions';
 import PendingFriendList from './pending_friends_list';
 
 const mSTP = (state) => {
