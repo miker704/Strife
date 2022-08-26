b7efecb2cff09bf4de8f8d6e283e8fd12a199f21
miker704 <miker704@gmail.com>
2022-08-26 09:52:22 -0400

    updates

--- Strife/frontend/actions/friendship_actions.js
+++ Strife/frontend/actions/friendship_actions.js
@@ -1,12 +1,12 @@
 import * as FRIENDSHIP_API_UTIL from "../utils/friendship_api_util.js";
 import { receiveUsers } from "./session_actions.js";
 
-
-
 export const RECEIVE_FRIENDSHIP = 'UPDATE_FRIENDSHIP';
 export const REMOVE_FRIENDSHIP = 'REMOVE_FRIENDSHIP';
 export const RECEIVE_FRIENDSHIP_ERRORS = "RECEIVE_FRIENDSHIP_ERRORS";
 export const REMOVE_FRIENDSHIP_ERRORS = "REMOVE_FRIENDSHIP_ERRORS";
+
+
 export const receiveFriendship = (friendship) => {
     return {
         type: RECEIVE_FRIENDSHIP,
