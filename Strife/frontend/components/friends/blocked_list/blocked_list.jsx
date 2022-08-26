e588a71061cb7d73becb05eae6c7fb5957c563d5
miker704 <miker704@gmail.com>
2022-08-26 11:16:29 -0400

    rm frienderrors

--- Strife/frontend/components/friends/blocked_list/blocked_list.jsx
+++ Strife/frontend/components/friends/blocked_list/blocked_list.jsx
@@ -22,9 +22,9 @@ class BlockedList extends React.Component {
         let substate = {
             user_id: this.props.currentUser.id,
             friend_id: blockedUser.id,
-            friend_request_status: blockedUser.friend_request_status
+            // friend_request_status: blockedUser.friend_request_status
         }
-        console.log("friendship delete on substate: ", substate);
+        // console.log("friendship delete on substate: ", substate);
         this.props.removeBlockedUser(substate);
     }
 
