7f4e6546808a8c4cdc31e754b0b871eb70fcdc12
miker704 <miker704@gmail.com>
2022-08-26 20:39:08 -0400

    updates

--- Strife/frontend/components/friends/pending_list/pending_friends_list.jsx
+++ Strife/frontend/components/friends/pending_list/pending_friends_list.jsx
@@ -168,7 +168,11 @@ class PendingFriendList extends React.Component {
 
                                                 <div className="pending-request-actions">
                                                     <div className="pending-deny-icon">
-                                                       
+                                                        <svg onClick={() => this.deleteFriendShip(friend)} className="icon-1WV" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                                            <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 
+                                                        18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
+                                                            </path>
+                                                        </svg>
                                                         <div className="pending-request-actions-tool-tip">Cancel</div>
                                                         <div className="pending-request-actions-tool-tip-triangle"></div>
                                                     </div>
