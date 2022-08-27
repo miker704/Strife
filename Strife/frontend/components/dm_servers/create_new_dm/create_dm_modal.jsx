13021129783e04182fef5ae13292f32cd0bd3c7d
miker704 <miker704@gmail.com>
2022-08-27 14:08:50 -0400

    updates

--- Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
+++ Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
@@ -44,6 +44,8 @@ const CreateDmModal = ({
                     return;
             }
         }
+        console.log("dmServer trying to created : ",dmMembersArray);
+
     }
 
 
@@ -149,7 +151,7 @@ const CreateDmModal = ({
                     </div>
                     <div className="create-dm-footer"></div>
                     <div className="create-dm-button-sec">
-                        <button className="create-dm-button" type="submit" disabled={count > 9 || count === 0}>
+                        <button className="create-dm-button" onSubmit={handleDmServerCreation}  type="submit" disabled={count > 9 || count === 0}>
                             <div className="create-dm-button-text">Create Group DM</div>
                         </button>
                     </div>
