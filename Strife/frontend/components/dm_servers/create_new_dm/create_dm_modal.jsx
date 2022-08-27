0d0831e1ce6b92b1dfd9a114a2d626fbaa14467f
miker704 <miker704@gmail.com>
2022-08-26 22:30:21 -0400

    updates

--- Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
+++ Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
@@ -7,7 +7,20 @@ class createDmModal extends React.Component{
     render(){
         return (
             <div className="create-dm-modal-popup">
-                    <div className="create-dm-"></div>
+                    <div className="create-dm-modal-focus-lock">
+                        <div className="create-dm-modal">
+                            <div className="create-dm-header">
+
+                            </div>
+                            <div className="create-dm-scroller">
+
+                            </div>
+                            <div className="create-dm-footer"></div>
+                            <div className="create-dm-button-sec">
+                                
+                            </div>
+                        </div>
+                    </div>
             </div>
         )
     }
