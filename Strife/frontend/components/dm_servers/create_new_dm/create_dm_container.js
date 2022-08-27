39e3586dbb5af072853ae477ee8abc3e4c698c2b
miker704 <miker704@gmail.com>
2022-08-27 15:08:36 -0400

    updates

--- Strife/frontend/components/dm_servers/create_new_dm/create_dm_container.js
+++ Strife/frontend/components/dm_servers/create_new_dm/create_dm_container.js
@@ -4,6 +4,7 @@ import CreateDmModal from './create_dm_modal';
 import { selectFriendStatus } from '../../../utils/selectors_api_util';
 import { requestFriendships } from '../../../actions/friendship_actions'; 
 import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
+import { createDmMember } from '../../../actions/dm_member_actions';
 
 const mSTP = (state) => {
     return {
@@ -18,6 +19,7 @@ const mDTP = (dispatch) => {
     return {
         requestFriendships: () => dispatch(requestFriendships()),
         createDmServer: (dmserver) => dispatch(createDmServer(dmserver)),
+        createDmMember: (dm_member) => dispatch(createDmMember(dm_member)),
         removeDmServerErrors: () => dispatch(removeDmServerErrors())
     }
 }
