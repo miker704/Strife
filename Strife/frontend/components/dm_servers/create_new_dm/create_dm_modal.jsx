1d3d9810e6b84cfbe8160acc1ee6a81d00d31e70
miker704 <miker704@gmail.com>
2022-08-27 09:40:55 -0400

    updates

--- Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
+++ Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
@@ -54,7 +54,11 @@ const CreateDmModal = ({
                                                     </div>
                                                     <span className="create-dm-check-box-wrapper">
                                                         <div className="create-dm-check-box">
-                                                        <svg aria-hidden="true" role="img" width="18" height="18" viewBox="0 0 24 24"><path fill="transparent" fill-rule="evenodd" clip-rule="evenodd" d="M8.99991 16.17L4.82991 12L3.40991 13.41L8.99991 19L20.9999 7.00003L19.5899 5.59003L8.99991 16.17Z"></path></svg>
+                                                            <svg aria-hidden="true" role="img" width="18" height="18" viewBox="0 0 24 24">
+                                                                <path fill="transparent" fillRule="evenodd" clipRule="evenodd" d="M8.99991 16.17L4.82991 
+                                                            12L3.40991 13.41L8.99991 19L20.9999 7.00003L19.5899 5.59003L8.99991 16.17Z">
+                                                                </path>
+                                                            </svg>
                                                         </div>
                                                     </span>
                                                 </div>
