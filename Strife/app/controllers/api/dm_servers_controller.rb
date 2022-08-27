a00ceb26f66306332fcbc48dcab2b3ebee68be40
miker704 <miker704@gmail.com>
2022-08-27 18:11:17 -0400

    updates

--- Strife/app/controllers/api/dm_servers_controller.rb
+++ Strife/app/controllers/api/dm_servers_controller.rb
@@ -10,13 +10,7 @@ class Api::DmServersController < ApplicationController
     end
     
     def create 
-        # @dm_server = DmServer.new()
-        # @current_user = current_user
-        # if @dm_server.save
-        #     render :create
-        # else
-        #     render json: @dm_servers.errors.full_messages, status: 400
-        # end
+      
 
         found_existing_dm_server = current_user.dm_servers.includes(:members).select{|dms| dms.member_ids.sort === dm_server_params[:dm_member_ids].map(&:to_i)}
 
@@ -34,24 +28,10 @@ class Api::DmServersController < ApplicationController
         @dm_server.member_ids = dm_server_params[:dm_member_ids]
 
         if @dm_server.save
-
-            # d1_members = params[:dm_server][:dm_member_ids]
-            # puts 'd1_members'
-            # puts d1_members
-            # d1_members.each do |dm_member|
-            #     DmMember.create!(dm_server_id: @dm_server.id, dm_member_id: dm_member)
-            # end
-
-            # puts 'dm_server members'
-            # puts @dm_server.members
-            # puts 'dm_server_dm_members'
-            # puts @dm_server.dm_members
-
             render :show
         else
                 render json: @dm_server.errors.full_messages, status: 400
         end
-
 
 
     end
