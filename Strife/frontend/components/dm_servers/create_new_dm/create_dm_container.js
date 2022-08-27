b8c0629ffe1d4e829f8a6f21f6a88fec9467daec
miker704 <miker704@gmail.com>
2022-08-27 08:08:13 -0400

    updates

--- Strife/frontend/components/dm_servers/create_new_dm/create_dm_container.js
+++ Strife/frontend/components/dm_servers/create_new_dm/create_dm_container.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import CreateDmModal from './create_dm_modal';
-
+import { selectFriendStatus } from '../../../utils/selectors_api_util';
 
 const mSTP = (state) =>{
     return {
