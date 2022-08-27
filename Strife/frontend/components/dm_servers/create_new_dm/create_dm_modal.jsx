311417ebc723ce2eee65039afc5906fa9eec35c2
miker704 <miker704@gmail.com>
2022-08-27 11:51:30 -0400

    updates

--- Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
+++ Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
@@ -54,7 +54,7 @@ const CreateDmModal = ({
                                                 return (
                                                     <div className="mini-box" key={friend.id}>
                                                         {friend.username}
-                                                        <svg className="close-3-lUJg" aria-label="Remove" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                                        <svg className="close-3-icon" aria-label="Remove" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
                                                             <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 
                                                             12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
                                                             </path>
