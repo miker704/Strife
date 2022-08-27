45ea27669e2eb6f6d98603fcaba492d9a92e8126
miker704 <miker704@gmail.com>
2022-08-26 22:41:59 -0400

    updates

--- Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
+++ Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
@@ -11,6 +11,7 @@ class createDmModal extends React.Component{
                         <div className="create-dm-modal">
                             <div className="create-dm-header-sec">
                                     <h2 className="create-dm-header-h2">Select Friends</h2>
+                                    <div className="num-of-dm-members-selected">You can add x more friends.</div>
                             </div>
                             <div className="create-dm-scroller">
 
