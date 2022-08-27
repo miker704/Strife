3293e901b5fca2c4fd2eb85a956e6b94c59aa266
miker704 <miker704@gmail.com>
2022-08-27 12:13:27 -0400

    updates

--- Strife/frontend/utils/close_hook_modals_api_utils.js
+++ Strife/frontend/utils/close_hook_modals_api_utils.js
@@ -1,2 +1,16 @@
 import { useEffect } from "react";
 
+export const closeHookModalOnESC = (closeHookModal) => {
+    const checkforESCPress = (e) => {
+        if (e.keyCode === 27) {
+            e.stopImmediatePropagation();
+            closeModal();
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("keydown", checkforESCPress, true);
+        return () => document.removeEventListener("keydown", checkforESCPress, true);
+    }, []);
+
+}
