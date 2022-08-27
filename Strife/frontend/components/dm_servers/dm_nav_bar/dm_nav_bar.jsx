35fc558fea5d862cc600080f9512387bd1672146
miker704 <miker704@gmail.com>
2022-08-27 13:52:18 -0400

    updates

--- Strife/frontend/components/dm_servers/dm_nav_bar/dm_nav_bar.jsx
+++ Strife/frontend/components/dm_servers/dm_nav_bar/dm_nav_bar.jsx
@@ -14,6 +14,7 @@ class DmNavBar extends React.Component {
         this.generateDmServerName = this.generateDmServerName.bind(this);
         this.renderDmServerPFP = this.renderDmServerPFP.bind(this);
         this.handleESC = this.handleESC.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
 
