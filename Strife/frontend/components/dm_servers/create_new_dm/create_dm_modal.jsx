24cf1f860bc54ac3bc4e2ff9e83d1efd88ec94e0
miker704 <miker704@gmail.com>
2022-08-27 18:17:16 -0400

    updates

--- Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
+++ Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
@@ -193,43 +193,4 @@ const CreateDmModal = ({
     )
 }
 
-
-// class CreateDmModal extends React.Component {
-//     constructor (props) {
-//         super(props);
-//     }
-//     render () {
-//         return (
-//             <div className="clear-modal-wrapper">
-//                 <div className="create-dm-modal-popup">
-//                     <div className="create-dm-modal-focus-lock">
-//                         <div className="create-dm-modal">
-//                             <div className="create-dm-header-sec">
-//                                 <h2 className="create-dm-header-h2">Select Friends</h2>
-//                                 <div className="num-of-dm-members-selected">You can add x more friends.</div>
-//                                 <div className="create-dm-search-bar-wrapper">
-//                                     <div className="create-dm-search-bar-outer-wrapper">
-//                                         <div className="create-dm-search-bar-inner-wrapper">
-//                                             <input className="create-dm-search-bar" type="text" name="" id="" />
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                             <div className="create-dm-scroller">
-
-//                             </div>
-//                             <div className="create-dm-footer"></div>
-//                             <div className="create-dm-button-sec">
-//                                 <button className="create-dm-button">
-//                                     <div className="create-dm-button-text">Create Group DM</div>
-//                                 </button>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
 export default CreateDmModal;
