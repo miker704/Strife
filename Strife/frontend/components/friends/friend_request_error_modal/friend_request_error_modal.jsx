a539d5fa076fd2d2574a4e16f9d7ca741991d002
miker704 <miker704@gmail.com>
2022-08-27 23:23:46 -0400

    updates

---
+++ Strife/frontend/components/friends/friend_request_error_modal/friend_request_error_modal.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class FriendRequestErrorModal extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default FriendRequestErrorModal;
