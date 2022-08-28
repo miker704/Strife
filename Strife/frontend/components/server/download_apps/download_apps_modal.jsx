435d172ada845d1f1a5de7c971fe18f6577e44a4
miker704 <miker704@gmail.com>
2022-08-28 07:05:38 -0400

    updates

---
+++ Strife/frontend/components/server/download_apps/download_apps_modal.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class DownloadApps extends React.Component {
+
+    constructor (props) {
+        super(props);
+    }
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default DownloadApps;
