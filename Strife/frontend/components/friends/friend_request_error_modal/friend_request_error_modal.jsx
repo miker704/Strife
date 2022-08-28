7e1d256583ecab804eb997ffe79deaeaa9d1aa44
miker704 <miker704@gmail.com>
2022-08-27 23:34:14 -0400

    updates

--- Strife/frontend/components/friends/friend_request_error_modal/friend_request_error_modal.jsx
+++ Strife/frontend/components/friends/friend_request_error_modal/friend_request_error_modal.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 
-class FriendRequestErrorModal extends React.Component{
-    constructor(props){
+class FriendRequestErrorModal extends React.Component {
+    constructor (props) {
         super(props);
     }
-    render(){
+    render () {
         return (
             <div className="frfm-backdrop">
-                    <div className="frfm-wrapper">
-                        <div className="frfm">
-                            <div className="frfm-inner">
-                                <form className="form-class-200">
-                                    <div className="form-content-1">
-                                        <div className="form-content-1-inner">
-                                            <h2 className="form-content-1-header">
-                                                Friend Request Failed
-                                            </h2>
+                <div className="frfm-wrapper">
+                    <div className="frfm">
+                        <div className="frfm-inner">
+                            <form className="form-class-200">
+                                <div className="form-content-1">
+                                    <div className="form-content-1-inner">
+                                        <h2 className="form-content-1-header">
+                                            Friend Request Failed
+                                        </h2>
+                                        <div className="form-content-1-subtext">
+                                            Hm, didn't work. Double check that the capitalization, spelling,
+                                            any spaces, and numbers are correct.
                                         </div>
                                     </div>
-                                </form>
-                            </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
+                </div>
             </div>
         )
     }
