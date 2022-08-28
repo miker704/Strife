8a3cb32c3f20f18fb827c42def27746d959ec919
miker704 <miker704@gmail.com>
2022-08-27 23:29:31 -0400

    updates

--- Strife/frontend/components/friends/friend_request_error_modal/friend_request_error_modal.jsx
+++ Strife/frontend/components/friends/friend_request_error_modal/friend_request_error_modal.jsx
@@ -11,7 +11,11 @@ class FriendRequestErrorModal extends React.Component{
                         <div className="frfm">
                             <div className="frfm-inner">
                                 <form className="form-class-200">
-                                    
+                                    <div className="form-content-1">
+                                        <div className="form-content-1-inner">
+                                            
+                                        </div>
+                                    </div>
                                 </form>
                             </div>
                         </div>
