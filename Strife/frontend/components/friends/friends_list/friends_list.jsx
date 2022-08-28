506cf112bf89962128d9ebd50a6b889011a39576
miker704 <miker704@gmail.com>
2022-08-28 13:33:29 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list.jsx
+++ Strife/frontend/components/friends/friends_list/friends_list.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { compose } from "redux";
 
 
 
@@ -6,24 +7,54 @@ class FriendShipIndex extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            searchText: ""
+            searchText: "",
+            noResultsFound: false
         }
         this.liveSearch = this.liveSearch.bind(this);
+        this.renderNoResults = this.renderNoResults.bind(this);
+    }
+
+
+    renderNoResults () {
+        if (this.noResultsFound) {
+            return (
+                <div className="friend-index-container">
+                    <div className="empty-state-container">
+                        <div className="blocked-users-empty">
+                            <div className="blocked-users-flex">
+                                <img className="add-friends-icon" alt="img" />
+                                <div className="block-wumpus-text">You have no friends. Here's Wumpus for now.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
     }
 
 
     liveSearch () {
         let allFriendShips = document.querySelectorAll('.friend-index-item');
+        console.log("this is all this lists li elements : ", allFriendShips);
         let search_query = document.getElementById('input-all-friends').value;
+        let count = 0;
         for (let i = 0; i < allFriendShips.length; i++) {
             if (allFriendShips[i].innerText.toLowerCase().includes(search_query.toLowerCase())) {
                 allFriendShips[i].classList.remove("is-hidden");
+                console.log("is-NOT-hidden");
             }
             else {
                 allFriendShips[i].classList.add("is-hidden");
+                console.log("is-hidden");
+                count++;
+
             }
         }
 
+        if (count === allFriendShips.length) {
+            console.log(" no results found !");
+            this.setState({ noResultsFound: true });
+        }
 
     }
 
@@ -43,6 +74,49 @@ class FriendShipIndex extends React.Component {
     render () {
         let allFriends = this.props.friends;
         let default_Photo = "https://strife-seeds.s3.amazonaws.com/defaultProfilePic.png";
+        console.log("all friends : ", allFriends);
+        console.log("this.props.friends-index : ", this.props);
+
+
+        if (this.state.noResultsFound) {
+            return (
+                <div className="friend-index-container">
+                    <div className="all-search-bar">
+                        <div className="all-search-bar-inner">
+                            <input id="input-all-friends" className="input-all-friends" type="search" placeholder="Search" onInput={() => this.liveSearch()} onChange={e => this.setState({ noResultsFound:false })} value={this.state.searchText} />
+                            <div className="magnify-icon-wrapper">
+                                <div className="magnify-icon">
+                                    <svg className="mag-icon1" aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                        <path fill="currentColor" d="M21.707 20.293L16.314 14.9C17.403 13.504 18 11.799 18 10C18 7.863 
+                                17.167 5.854 15.656 4.344C14.146 2.832 12.137 2 10 2C7.863 2 5.854 2.832 4.344 4.344C2.833 
+                                5.854 2 7.863 2 10C2 12.137 2.833 14.146 4.344 15.656C5.854 17.168 7.863 18 10 18C11.799 18 
+                                13.504 17.404 14.9 16.314L20.293 21.706L21.707 20.293ZM10 16C8.397 16 6.891 15.376 5.758 
+                                14.243C4.624 13.11 4 11.603 4 10C4 8.398 4.624 6.891 5.758 5.758C6.891 4.624 8.397 4 10 
+                                4C11.603 4 13.109 4.624 14.242 5.758C15.376 6.891 16 8.398 16 10C16 11.603 15.376 13.11 
+                                14.242 14.243C13.109 15.376 11.603 16 10 16Z">
+                                        </path>
+                                    </svg>
+                                    <svg className="clear-mag-icon1" aria-label="Clear" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                        <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
+                                        </path>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="empty-state-container">
+                        <div className="blocked-users-empty">
+                            <div className="blocked-users-flex">
+                                <img className="add-friends-icon" alt="img" />
+                                <div className="block-wumpus-text">Wumpus looked, but couldn't find anyone with that name.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+
+
 
         if (allFriends.length > 0) {
             return (
@@ -52,7 +126,7 @@ class FriendShipIndex extends React.Component {
 
                     <div className="all-search-bar">
                         <div className="all-search-bar-inner">
-                            <input id="input-all-friends" className="input-all-friends" type="search" placeholder="Search" onInput={() => this.liveSearch()} onChange={e => this.setState({ searchText: e.currentTarget.value })} value={this.state.searchText} />
+                            <input id="input-all-friends" className="input-all-friends" type="search" placeholder="Search" onInput={() => this.liveSearch()} onChange={e => this.setState({ searchText: e.currentTarget.value})} value={this.state.searchText} />
                             <div className="magnify-icon-wrapper">
                                 <div className="magnify-icon">
                                     <svg className="mag-icon1" aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
@@ -156,7 +230,7 @@ class FriendShipIndex extends React.Component {
                         <div className="blocked-users-empty">
                             <div className="blocked-users-flex">
                                 <img className="add-friends-icon" alt="img" />
-                                <div className="block-wumpus-text">You have on friends. Here's Wumpus for now.</div>
+                                <div className="block-wumpus-text">You have no friends. Here's Wumpus for now.</div>
                             </div>
                         </div>
                     </div>
