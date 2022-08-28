a0242d8e489389380dd6391559661c7a080a84ee
miker704 <miker704@gmail.com>
2022-08-28 14:29:29 -0400

    updates

--- Strife/frontend/components/friends/blocked_list/blocked_list.jsx
+++ Strife/frontend/components/friends/blocked_list/blocked_list.jsx
@@ -13,7 +13,8 @@ class BlockedList extends React.Component {
     }
 
     liveSearch () {
-        let allFriendShips = document.querySelectorAll('.friend-index-item');
+        // let allFriendShips = document.querySelectorAll('.friend-index-item');
+        let allFriendShips = document.querySelectorAll('.friend-tag');
         let search_query = document.getElementById('input-all-friends').value;
         let numberOfFriends = document.getElementById('num-of-friends');
         let count = 0;
@@ -22,7 +23,7 @@ class BlockedList extends React.Component {
             if (allFriendShips[i].innerText.toLowerCase().includes(search_query.toLowerCase())) {
                 allFriendShips[i].classList.remove("is-hidden");
                 foundCount++;
-                numberOfFriends.innerHTML = `ALL FRIENDS - ${foundCount}`;
+                numberOfFriends.innerHTML = `BLOCKED USERS - ${foundCount}`;
 
             }
             else {
@@ -66,7 +67,55 @@ class BlockedList extends React.Component {
 
         let allBlockedUsers = this.props.blockedUsers;
         let default_Photo = "https://strife-seeds.s3.amazonaws.com/defaultProfilePic.png";
+        if (this.state.noResultsFound) {
+            return (
+                <div className="friend-index-container">
+                    <div className="all-search-bar">
+                        <div className="all-search-bar-inner">
+                            <input id="input-all-friends" className="input-all-friends" type="search" placeholder="Search"
+                                onInput={() => this.liveSearch()}
+                                onChange={e => {
+                                    this.setState({ noResultsFound: false })
+                                    this.setState({ searchText: e.currentTarget.value })
+                                }
+                                }
+                                value={this.state.searchText} />
 
+
+                            <div className="magnify-icon-wrapper">
+                                <div className="magnify-icon">
+                                    <svg className="mag-icon1" aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                        <path fill="currentColor" d="M21.707 20.293L16.314 14.9C17.403 13.504 18 11.799 18 10C18 7.863 
+                                17.167 5.854 15.656 4.344C14.146 2.832 12.137 2 10 2C7.863 2 5.854 2.832 4.344 4.344C2.833 
+                                5.854 2 7.863 2 10C2 12.137 2.833 14.146 4.344 15.656C5.854 17.168 7.863 18 10 18C11.799 18 
+                                13.504 17.404 14.9 16.314L20.293 21.706L21.707 20.293ZM10 16C8.397 16 6.891 15.376 5.758 
+                                14.243C4.624 13.11 4 11.603 4 10C4 8.398 4.624 6.891 5.758 5.758C6.891 4.624 8.397 4 10 
+                                4C11.603 4 13.109 4.624 14.242 5.758C15.376 6.891 16 8.398 16 10C16 11.603 15.376 13.11 
+                                14.242 14.243C13.109 15.376 11.603 16 10 16Z">
+                                        </path>
+                                    </svg>
+                                    <svg className="clear-mag-icon1" aria-label="Clear" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                        <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
+                                        </path>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="all-friends">
+                        {`BLOCKED USERS - ${0}`}
+                    </div>
+                    <div className="empty-state-container">
+                        <div className="blocked-users-empty">
+                            <div className="blocked-users-flex">
+                                <img className="no-friends-online-icon" alt="img" />
+                                <div className="block-wumpus-text">Wumpus looked, but couldn't find anyone with that name.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
 
         if (allBlockedUsers.length > 0) {
 
@@ -77,7 +126,7 @@ class BlockedList extends React.Component {
 
                     <div className="all-search-bar">
                         <div className="all-search-bar-inner">
-                            <input className="input-all-friends" type="text" placeholder="Search" />
+                            <input id="input-all-friends" className="input-all-friends" placeholder="Search" type="search" onInput={() => this.liveSearch()} onChange={e => this.setState({ searchText: e.currentTarget.value })} value={this.state.searchText} />
                             <div className="magnify-icon-wrapper">
                                 <div className="magnify-icon">
                                     <svg className="mag-icon1" aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
@@ -101,7 +150,7 @@ class BlockedList extends React.Component {
 
 
 
-                    <div className="all-friends">
+                    <div id="num-of-friends" className="all-friends">
                         {`BLOCKED USERS - ${allBlockedUsers.length}`}
                     </div>
                     <div className="friend-index">
