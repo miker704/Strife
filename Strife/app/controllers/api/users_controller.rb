8cef4ee49922a9671eca3afcdf2e07087c21e201
miker704 <miker704@gmail.com>
2022-08-28 06:17:57 -0400

    updates

--- Strife/app/controllers/api/users_controller.rb
+++ Strife/app/controllers/api/users_controller.rb
@@ -124,8 +124,7 @@ class Api::UsersController < ApplicationController
 
 
     def fetch_via_strife_id
-       
-        if(params[:user_strife_id_tag].empty?)
+        if(params[:user_strife_id_tag] == ????)
             render json: ['Please enter proper format username + # + STRIFE ID Tag.'], status: 404
 
         end
