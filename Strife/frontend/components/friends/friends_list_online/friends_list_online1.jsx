c5f6aa9773f4816ca1183edf33482915471144d9
miker704 <miker704@gmail.com>
2022-08-29 02:23:35 -0400

    updates

--- Strife/frontend/components/friends/friends_list_online/friends_list_online1.jsx
+++ Strife/frontend/components/friends/friends_list_online/friends_list_online1.jsx
@@ -1,30 +1,41 @@
 import React from "react";
 import { useState, useRef, useEffect } from "react";
 
-const FriendShipIndexOnline1 = ({ currentUser, friends, errors, history }) => {
+const FriendShipIndexOnline1 = ({
+    currentUser,
+    friends,
+    errors,
+    dmServerErrors,
+    dmServers,
+    requestFriendships,
+    removeFriendshipErrors,
+    history,
+
+}) => {
+
     const inputRef = useRef();
     const [searchText, setSearchText] = useState("");
-    const [noResultsFound] = useState(false);
+    // const [noResultsFound] = useState(false);
     const [selectedFriends, setSelectedFriends] = useState([]);
-    const isSelected = (friend) => selectedFriends.map(friend => friend.id).includes(friend.id);
-    const findIfSelected = (toAdd) => selectedFriends.findIndex(friend => friend.id === toAdd.id);
+    // const isSelected = (friend) => selectedFriends.map(friend => friend.id).includes(friend.id);
+    // const findIfSelected = (toAdd) => selectedFriends.findIndex(friend => friend.id === toAdd.id);
     let allFriends = friends;
     let default_Photo = "https://strife-seeds.s3.amazonaws.com/defaultProfilePic.png";
 
-    const toggleSelection = (friend) => {
-        const idx = findIfSelected(friend);
-        if (idx > -1) {
-            setSelectedFriends(prevState => {
-                const newState = [...prevState];
-                newState.splice(idx, 1);
-                return newState;
-            });
+
+
+    useEffect(() => {
+        requestFriendships();
+
+        return function cleanup () {
+            if (errors.length > 0) {
+                removeFriendshipErrors();
+            }
         }
-        else {
-            setSelectedFriends(prevState => [...prevState, friend]);
-            setSearchText("");
-        }
-    }
+    })
+
+
+    
 
     const gatherMatches = () => {
 
@@ -52,7 +63,7 @@ const FriendShipIndexOnline1 = ({ currentUser, friends, errors, history }) => {
 
             numberOfFriends.innerHTML = `ONLINE - ${selectedFriends.length}`;
         }
-        else{
+        else {
             console.log("selected friends and search text blank")
             numberOfFriends.innerHTML = `ONLINE - ${friends.length}`;
         }
@@ -60,56 +71,60 @@ const FriendShipIndexOnline1 = ({ currentUser, friends, errors, history }) => {
     }
 
 
-    if (noResultsFound === true) {
-        return (
-            <div className="friend-index-container">
-                <div className="all-search-bar">
-                    <div className="all-search-bar-inner">
-                        <input id="input-all-friends" className="input-all-friends" type="text" placeholder="Search"
-                            autoFocus ref={inputRef}
-                            spellCheck={false}
-                            onChange={(e) => {
-
-                                setSearchText(e.currentTarget.value)
-                            }
-                            }
-                            value={searchText} />
 
 
-                        <div className="magnify-icon-wrapper">
-                            <div className="magnify-icon">
-                                <svg className="mag-icon1" aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
-                                    <path fill="currentColor" d="M21.707 20.293L16.314 14.9C17.403 13.504 18 11.799 18 10C18 7.863 
-                            17.167 5.854 15.656 4.344C14.146 2.832 12.137 2 10 2C7.863 2 5.854 2.832 4.344 4.344C2.833 
-                            5.854 2 7.863 2 10C2 12.137 2.833 14.146 4.344 15.656C5.854 17.168 7.863 18 10 18C11.799 18 
-                            13.504 17.404 14.9 16.314L20.293 21.706L21.707 20.293ZM10 16C8.397 16 6.891 15.376 5.758 
-                            14.243C4.624 13.11 4 11.603 4 10C4 8.398 4.624 6.891 5.758 5.758C6.891 4.624 8.397 4 10 
-                            4C11.603 4 13.109 4.624 14.242 5.758C15.376 6.891 16 8.398 16 10C16 11.603 15.376 13.11 
-                            14.242 14.243C13.109 15.376 11.603 16 10 16Z">
-                                    </path>
-                                </svg>
-                                <svg className="clear-mag-icon1" aria-label="Clear" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
-                                    <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
-                                    </path>
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="all-friends">
-                    {`ONLINE - ${0}`}
-                </div>
-                <div className="empty-state-container">
-                    <div className="blocked-users-empty">
-                        <div className="blocked-users-flex">
-                            <img className="no-friends-online-icon" alt="img" />
-                            <div className="block-wumpus-text">Wumpus looked, but couldn't find anyone with that name.</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+
+
+    // if (noResultsFound === true) {
+    //     return (
+    //         <div className="friend-index-container">
+    //             <div className="all-search-bar">
+    //                 <div className="all-search-bar-inner">
+    //                     <input id="input-all-friends" className="input-all-friends" type="text" placeholder="Search"
+    //                         autoFocus ref={inputRef}
+    //                         spellCheck={false}
+    //                         onChange={(e) => {
+
+    //                             setSearchText(e.currentTarget.value)
+    //                         }
+    //                         }
+    //                         value={searchText} />
+
+
+    //                     <div className="magnify-icon-wrapper">
+    //                         <div className="magnify-icon">
+    //                             <svg className="mag-icon1" aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+    //                                 <path fill="currentColor" d="M21.707 20.293L16.314 14.9C17.403 13.504 18 11.799 18 10C18 7.863 
+    //                         17.167 5.854 15.656 4.344C14.146 2.832 12.137 2 10 2C7.863 2 5.854 2.832 4.344 4.344C2.833 
+    //                         5.854 2 7.863 2 10C2 12.137 2.833 14.146 4.344 15.656C5.854 17.168 7.863 18 10 18C11.799 18 
+    //                         13.504 17.404 14.9 16.314L20.293 21.706L21.707 20.293ZM10 16C8.397 16 6.891 15.376 5.758 
+    //                         14.243C4.624 13.11 4 11.603 4 10C4 8.398 4.624 6.891 5.758 5.758C6.891 4.624 8.397 4 10 
+    //                         4C11.603 4 13.109 4.624 14.242 5.758C15.376 6.891 16 8.398 16 10C16 11.603 15.376 13.11 
+    //                         14.242 14.243C13.109 15.376 11.603 16 10 16Z">
+    //                                 </path>
+    //                             </svg>
+    //                             <svg className="clear-mag-icon1" aria-label="Clear" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+    //                                 <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
+    //                                 </path>
+    //                             </svg>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //             <div className="all-friends">
+    //                 {`ONLINE - ${0}`}
+    //             </div>
+    //             <div className="empty-state-container">
+    //                 <div className="blocked-users-empty">
+    //                     <div className="blocked-users-flex">
+    //                         <img className="no-friends-online-icon" alt="img" />
+    //                         <div className="block-wumpus-text">Wumpus looked, but couldn't find anyone with that name.</div>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     )
+    // }
 
     if (allFriends.length > 0) {
         return (
