40f45b495ccce7c59ec8c9d1d992be12ea023536
miker704 <miker704@gmail.com>
2022-08-29 11:25:45 -0400

    updates

--- Strife/frontend/components/friends/friends_page_main/friends_home_page.jsx
+++ Strife/frontend/components/friends/friends_page_main/friends_home_page.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import FriendShipIndexContainer from "../friends_list/friends_list_container.js";
-import FriendShipIndexOnlineContainer from "../friends_list_online/friends_list_online_container";
+import FriendShipIndexOnlineContainer from "../friends_list_online/friends_list_online_container.js";
 import BlockedListContainer from "../blocked_list/blocked_list_container";
 import PendingFriendListContainer from "../pending_list/pending_friends_list_container";
 import AddFriendsContainer from "../add_friends/add_friends_container";
 import CreateDmModalContainer from "../../dm_servers/create_new_dm/create_dm_container";
 import FriendShipIndexOnlineContainer1 from "../friends_list_online/friends_list_online_container1";
-import FriendShipIndexOnlineContainer from "../friends_list_online/friends_list_online_container.js";
 
 
 class FriendsHomePageContainer extends React.Component {
