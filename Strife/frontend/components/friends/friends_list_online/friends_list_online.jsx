d5432d8de2b00b7015778b01f5cd1d3bf5ab23e3
miker704 <miker704@gmail.com>
2022-08-29 12:29:06 -0400

    updates

--- Strife/frontend/components/friends/friends_list_online/friends_list_online.jsx
+++ Strife/frontend/components/friends/friends_list_online/friends_list_online.jsx
@@ -228,7 +228,7 @@ const FriendShipIndexOnline = ({
                                                     <div className="pending-request-actions-tool-tip">Message</div>
                                                     <div className="pending-request-actions-tool-tip-triangle"></div>
                                                 </div>
-                                                <div className="friend-options-button" onClick={() => friendOptions(friend)}>
+                                                <div className="friend-options-button" onClick={() =>  openModal("friendOptions")}>
                                                     <svg className="icon-1WVg" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
                                                         <g fill="none" fillRule="evenodd">
                                                             <path d="M24 0v24H0V0z">
