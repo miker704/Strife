8a09a1c16575d92e33932c146d5ae034895fde45
miker704 <miker704@gmail.com>
2022-08-29 11:02:35 -0400

    updates

--- Strife/frontend/components/friends/edit_friendship_modal/edit_friendship.jsx
+++ Strife/frontend/components/friends/edit_friendship_modal/edit_friendship.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
-class EditFriendshipModal extends React.Component{
-    constructor(props){
+class EditFriendshipModal extends React.Component {
+    constructor (props) {
         super(props);
     }
-    render(){
+    render () {
         return (
-            <div>
-
+            <div className="fo-layer">
+                <div className="fo-theme">
+                    <div className="fo-flex-wrapper">
+                        <div className="fo-scroller">
+                            <div className="fo-item-container">
+                                <div className="fo-item-name">Start Video Call</div>
+                            </div>
+                            <div className="fo-item-container">
+                                <div className="fo-item-name">Start Voice Call</div>
+                            </div>
+                            <div className="fo-item-container">
+                                <div className="fo-item-name-red">Remove Friend</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
