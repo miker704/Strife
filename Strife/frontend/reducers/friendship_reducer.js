d7c0b4e75de75cca8a2a334f93e4cf6538226342
miker704 <miker704@gmail.com>
2022-08-29 09:49:36 -0400

    attempt at live rerender

---
+++ Strife/frontend/reducers/friendship_reducer.js
@@ -0,0 +1,47 @@
+import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER, REMOVE_CURRENT_USER, RECEIVE_USER, RECEIVE_USERS } from "../actions/session_actions.js";
+
+import { RECEIVE_FRIENDSHIP, REMOVE_FRIENDSHIP } from '../actions/friendship_actions.js';
+
+
+//this reducer is an attempt to use async actions to fetch changes to online friends live 
+// custom Thunk Function
+
+const friendshipReducer = (state = {}, action) => {
+
+    Object.freeze(state);
+    let newState;
+
+    switch (action.type) {
+
+
+        case 'RECEIVE_ONLINE_FRIENDS':
+            return Object.assign({}, state, action.payload);
+
+        default:
+            return state;
+    }
+
+}
+
+
+export default friendshipReducer;
+
+
+export async function grabLiveUsers (dispatch, getState) {
+
+    const response = await fetch('api/friendships').then((res) => res.json());
+
+
+    let Friends_Online = response;
+    for (let i in Friends_Online) {
+
+        if (Friends_Online[i].online === true && Friends_Online[i].friend_request_status === 3) {
+            continue;
+        }
+        else {
+            delete Friends_Online[i];
+        }
+    }
+
+    dispatch({ type: 'RECEIVE_ONLINE_FRIENDS', payload: Friends_Online })
+}
