d88860c63d098c7d5c205404087d7ead8a66ae45
miker704 <miker704@gmail.com>
2022-08-29 11:41:05 -0400

    updates

--- Strife/frontend/components/friends/edit_friendship_modal/edit_friendship_container.js
+++ Strife/frontend/components/friends/edit_friendship_modal/edit_friendship_container.js
@@ -1,9 +1,8 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import FriendShipIndexOnline1 from './friends_list_online1';
+import EditFriendshipModal from './edit_friendship';
 import { selectFriendStatusOnline } from '../../../utils/selectors_api_util';
-import { requestFriendships, removeFriendshipErrors } from '../../../actions/friendship_actions';
-import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
+import { requestFriendships, removeFriendshipErrors, deleteFriendship} from '../../../actions/friendship_actions';
 
 
 const mSTP = (state) => {
@@ -11,8 +10,6 @@ const mSTP = (state) => {
         currentUser: state.entities.users[state.session.id],
         friends: selectFriendStatusOnline(state, 3),
         errors: state.errors.friendship,
-        dmServerErrors: state.errors.dmServer,
-        dmServers: Object.values(state.entities.dmServers),
     }
 };
 
@@ -21,8 +18,7 @@ const mDTP = (dispatch) => {
     return {
         requestFriendships: () => dispatch(requestFriendships()),
         removeFriendshipErrors: () => dispatch(removeFriendshipErrors()),
-        removeDmServerErrors: () => dispatch(removeDmServerErrors()),
-        createDmServer: (dmserver) => dispatch(createDmServer(dmserver)),
+        deleteFriendship: (ids) => dispatch(deleteFriendship(ids)) 
     }
 };
 
