29c0b04768a2b97591e2f5fd9db69e2e09dca78d
miker704 <miker704@gmail.com>
2022-08-29 10:27:13 -0400

    updates

--- Strife/frontend/components/friends/friends_list_online/friends_list_online_container1.js
+++ Strife/frontend/components/friends/friends_list_online/friends_list_online_container1.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import FriendShipIndexOnline1 from './friends_list_online1';
-import { selectFriendStatusOnline, selectFriendStatus } from '../../../utils/selectors_api_util';
+import { selectFriendStatusOnline } from '../../../utils/selectors_api_util';
 import { requestFriendships, removeFriendshipErrors } from '../../../actions/friendship_actions';
 import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
 
