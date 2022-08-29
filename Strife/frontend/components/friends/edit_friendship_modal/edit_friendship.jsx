b4eef55bf3bea8385a2bca1f9e5dc4379d92209d
miker704 <miker704@gmail.com>
2022-08-29 11:37:27 -0400

    updates

--- Strife/frontend/components/friends/edit_friendship_modal/edit_friendship.jsx
+++ Strife/frontend/components/friends/edit_friendship_modal/edit_friendship.jsx
@@ -1,34 +1,62 @@
 import React from "react";
 
-class EditFriendshipModal extends React.Component {
-    constructor (props) {
-        super(props);
-    }
-    render () {
-        return (
-            <div className="fo-layer">
-                <div className="fo-theme">
-                    <div className="fo-flex-wrapper">
-                        <div className="fo-scroller">
-                            <div className="fo-item-container">
-                                <div className="fo-item-name">Start Video Call</div>
-                            </div>
-                            <div className="fo-item-container">
-                                <div className="fo-item-name">Start Voice Call</div>
-                            </div>
-                            <div className="fo-item-container red">
-                                <div className="fo-item-name">Remove Friend</div>
-                            </div>
-                            <div className="fo-options-bottom-div"></div>
+
+const EditFriendshipModal = (props) => {
+
+    return (
+        <div className="fo-layer">
+            <div className="fo-theme">
+                <div className="fo-flex-wrapper">
+                    <div className="fo-scroller">
+                        <div className="fo-item-container">
+                            <div className="fo-item-name">Start Video Call</div>
                         </div>
+                        <div className="fo-item-container">
+                            <div className="fo-item-name">Start Voice Call</div>
+                        </div>
+                        <div className="fo-item-container red">
+                            <div className="fo-item-name">Remove Friend</div>
+                        </div>
+                        <div className="fo-options-bottom-div"></div>
                     </div>
                 </div>
             </div>
-        )
-    }
-
+        </div>
+    )
 }
 
+
 export default EditFriendshipModal;
+
+
+// class EditFriendshipModal extends React.Component {
+//     constructor (props) {
+//         super(props);
+//     }
+//     render () {
+//         return (
+//             <div className="fo-layer">
+//                 <div className="fo-theme">
+//                     <div className="fo-flex-wrapper">
+//                         <div className="fo-scroller">
+//                             <div className="fo-item-container">
+//                                 <div className="fo-item-name">Start Video Call</div>
+//                             </div>
+//                             <div className="fo-item-container">
+//                                 <div className="fo-item-name">Start Voice Call</div>
+//                             </div>
+//                             <div className="fo-item-container red">
+//                                 <div className="fo-item-name">Remove Friend</div>
+//                             </div>
+//                             <div className="fo-options-bottom-div"></div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+
+// }
+
 
 
