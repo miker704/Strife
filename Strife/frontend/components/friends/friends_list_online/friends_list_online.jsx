fb5cb90570a82349d59679371ab468071d9aefb9
miker704 <miker704@gmail.com>
2022-08-29 11:51:07 -0400

    updates

--- Strife/frontend/components/friends/friends_list_online/friends_list_online.jsx
+++ Strife/frontend/components/friends/friends_list_online/friends_list_online.jsx
@@ -93,7 +93,9 @@ const FriendShipIndexOnline = ({
     const friendOptions = (friend) => {
         console.log("in opening friend options modal");
         return (
-            <EditFriendshipModalContainer/>
+            <div>
+                <EditFriendshipModalContainer/>
+            </div>
         )
     }
 
