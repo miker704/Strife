b661bdf9eed26bd18a815752a1eb3b48fbd69752
miker704 <miker704@gmail.com>
2022-08-30 15:02:14 -0400

    updates

--- Strife/frontend/utils/friendship_api_util.js
+++ Strife/frontend/utils/friendship_api_util.js
@@ -2,20 +2,20 @@ export const requestFriendships = () => $.ajax({ url: `/api/friendships/`, metho
 export const createFriendship = (account_ids) => $.ajax({ url: `/api/friendships/`, method: 'POST', data: { friendship: account_ids } })
 
 
-export const updateFriendship = (account_ids) => 
-$.ajax({
-    url: `/api/friendships/`,
-    method: "PATCH",
-    data: {friendship: account_ids}
-});
+export const updateFriendship = (account_ids) =>
+    $.ajax({
+        url: `/api/friendships/`,
+        method: "PATCH",
+        data: { friendship: account_ids }
+    });
 
 
-export const deleteFriendship = (account_ids) => 
-$.ajax({
-    url: `/api/friendships/`,
-    method: "DELETE",
-    data: {friendship: account_ids}
-});
+export const deleteFriendship = (account_ids) =>
+    $.ajax({
+        url: `/api/friendships/`,
+        method: "DELETE",
+        data: { friendship: account_ids }
+    });
 
 export const receiveOnlineFriends = () => $.ajax({ url: `/api/friendships/`, method: 'GET' });
 
