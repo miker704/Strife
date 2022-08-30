5863507e97fa65776588ca8d9dd52f9453299e54
miker704 <miker704@gmail.com>
2022-08-29 22:48:05 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list_container.js
+++ Strife/frontend/components/friends/friends_list/friends_list_container.js
@@ -11,7 +11,9 @@ const mSTP = (state) => {
         currentUser: state.entities.users[state.session.id],
         friends: selectFriendStatus(state, 3),
         dmServers: Object.values(state.entities.dmServers),
-        errors: state.errors.friendship
+        errors: state.errors.friendship,
+        dmServerErrors: state.errors.dmServer
+
     }
 };
 
