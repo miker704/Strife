2238e356bd75edd6d8804aa5e13ec3166d913b35
miker704 <miker704@gmail.com>
2022-08-30 15:58:51 -0400

    updates

--- Strife/frontend/components/friends/friends_list_online/friends_list_online_container.js
+++ Strife/frontend/components/friends/friends_list_online/friends_list_online_container.js
@@ -24,7 +24,7 @@ const mDTP = (dispatch) => {
         removeFriendshipErrors: () => dispatch(removeFriendshipErrors()),
         removeDmServerErrors: () => dispatch(removeDmServerErrors()),
         createDmServer: (dmserver) => dispatch(createDmServer(dmserver)),
-        openModal: (modal) => dispatch(openModal(modal))
+        openModal: (modal) => dispatch(openModal(modal)),
     }
 };
 
