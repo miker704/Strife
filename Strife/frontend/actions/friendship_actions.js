e86a5897e021440163c40d335d7f38d98637f32c
miker704 <miker704@gmail.com>
2022-08-30 15:55:09 -0400

    updates

--- Strife/frontend/actions/friendship_actions.js
+++ Strife/frontend/actions/friendship_actions.js
@@ -84,6 +84,7 @@ export const deleteFriendship = (account_ids) => (dispatch) =>
     FRIENDSHIP_API_UTIL.deleteFriendship(account_ids).then((friendship) => dispatch(removeFriendship(friendship)), err => dispatch(receiveFriendshipErrors(err.responseJSON)));
 
 export const receiveOnlineFriends = () => (dispatch) => 
+FRIENDSHIP_API_UTIL.receiveOnlineFriends().then((onlineFriends) => console.log(onlineFriends))
 
 
 
