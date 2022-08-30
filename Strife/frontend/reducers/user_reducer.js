c813e3d7627cd73538e9274a131e24a493af0e9b
miker704 <miker704@gmail.com>
2022-08-30 15:43:38 -0400

    updates

--- Strife/frontend/reducers/user_reducer.js
+++ Strife/frontend/reducers/user_reducer.js
@@ -34,6 +34,7 @@ const userReducer = (state = {}, action) => {
             return Object.assign({}, state, { [action.user.id]: action.user });
 
         case RECEIVE_USERS:
+            
             return receiveUsers(state, action.users);
 
         case RECEIVE_FRIENDSHIP:
