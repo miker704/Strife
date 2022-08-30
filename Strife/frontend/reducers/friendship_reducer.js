c2c58905410b7439321e4e753e3bbd7f2c3ea5ec
miker704 <miker704@gmail.com>
2022-08-30 15:24:55 -0400

    updates

--- Strife/frontend/reducers/friendship_reducer.js
+++ Strife/frontend/reducers/friendship_reducer.js
@@ -25,7 +25,7 @@ const friendshipReducer = (state = {}, action) => {
         case RECEIVE_ALL_BLOCKED_USERS:
         case RECEIVE_ALL_FRIENDS:
         case RECEIVE_ALL_FRIEND_REQUESTS:
-        case RECEIVE_ALL_BLOCKED_USERS:
+        case RECEIVE_ALL_ONLINE_FRIENDS:
         case RECEIVE_ALL_BLOCKED_USERS:
 
 
