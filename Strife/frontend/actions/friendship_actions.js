25382cb4119234cf34d0c4181605b546b5765057
miker704 <miker704@gmail.com>
2022-08-30 16:52:11 -0400

    updates

--- Strife/frontend/actions/friendship_actions.js
+++ Strife/frontend/actions/friendship_actions.js
@@ -93,6 +93,6 @@ export const requestFriendRequests = () => (dispatch) =>
 
 
 export const requestBlockedUsers = () => (dispatch) =>
-FRIENDSHIP_API_UTIL.requestBlockedUsers().then((friendship))
+FRIENDSHIP_API_UTIL.requestBlockedUsers().then((friendship)) =>
 export const requestAllFriendships = () => (dispatch) =>
 FRIENDSHIP_API_UTIL.requestAllFriendships().then((friendship))
