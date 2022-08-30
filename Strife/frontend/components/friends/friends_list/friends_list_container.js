7265802ab3dfa43d8ac54d58680c4bcf881720c2
miker704 <miker704@gmail.com>
2022-08-29 22:47:57 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list_container.js
+++ Strife/frontend/components/friends/friends_list/friends_list_container.js
@@ -3,6 +3,8 @@ import { withRouter } from 'react-router';
 import FriendShipIndex from './friends_list';
 import { requestFriendships, removeFriendshipErrors } from '../../../actions/friendship_actions';
 import { selectFriendStatus } from '../../../utils/selectors_api_util';
+import { openModal } from '../../../actions/modal_actions';
+import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
 
 const mSTP = (state) => {
     return {
