4c0b525fb6c7ac922d9e55a307906270741569e9
miker704 <miker704@gmail.com>
2022-08-30 19:04:26 -0400

    updates

--- Strife/frontend/components/friends/friends_list_online/friends_list_online_container.js
+++ Strife/frontend/components/friends/friends_list_online/friends_list_online_container.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import FriendShipIndexOnline from './friends_list_online';
-import { selectFriendStatusOnline } from '../../../utils/selectors_api_util';
+import { selectFriendStatusOnline, selectOnlineFriends } from '../../../utils/selectors_api_util';
 import { requestFriendships, removeFriendshipErrors, requestAllOnlineFriends } from '../../../actions/friendship_actions';
 import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
 import { openModal } from '../../../actions/modal_actions';
@@ -11,7 +11,7 @@ const mSTP = (state) => {
     return {
         currentUser: state.entities.users[state.session.id],
         friends: selectFriendStatusOnline(state, 3),
-        onlineFriends: state.entities.friendships,
+        onlineFriends: selectOnlineFriends(state, 3),
         errors: state.errors.friendship,
         dmServerErrors: state.errors.dmServer,
         dmServers: Object.values(state.entities.dmServers),
