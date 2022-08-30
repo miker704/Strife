24a0bcafa1589e1d87cb447d72c6c61c8b69028a
miker704 <miker704@gmail.com>
2022-08-30 17:38:54 -0400

    updates

--- Strife/frontend/utils/selectors_api_util.js
+++ Strife/frontend/utils/selectors_api_util.js
@@ -20,3 +20,25 @@ export const selectFriendStatusOnline = (state, status) => {
   
     return friends.sort((a, b) => a.username > b.username ? 1 : -1);
 }
+
+
+export const selectFriendStatus = (state, status) => {
+    const friends = [];
+    for (let user of Object.values(state.entities.users)) {
+        if (user.friend_request_status === status) {
+            friends.push(user);
+        }
+    }
+    return friends.sort((a, b) => a.username > b.username ? 1 : -1);
+}
+
+export const selectFriendStatusOnline = (state, status) => {
+    const friends = [];
+    for (let user of Object.values(state.entities.users)) {
+        if (user.friend_request_status === status && user.online === true) {
+            friends.push(user);
+        }
+    }
+  
+    return friends.sort((a, b) => a.username > b.username ? 1 : -1);
+}
