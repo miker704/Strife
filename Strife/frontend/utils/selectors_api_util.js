9044f0215971c21709502dbf7f69c5ef8d292ced
miker704 <miker704@gmail.com>
2022-08-30 17:42:38 -0400

    updates

--- Strife/frontend/utils/selectors_api_util.js
+++ Strife/frontend/utils/selectors_api_util.js
@@ -55,7 +55,7 @@ export const selectFriendRequests = (state, status) => {
 
 export const selectBlockedUsers = (state, status) => {
     const blockedUsers = [];
-    for (let user of Object.values(state.entities.users)) {
+    for (let user of Object.values(state.entities.friendships)) {
         if (user.friend_request_status === status && user.online === true) {
             friends.push(user);
         }
