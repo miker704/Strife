4c1d1e841d788642dc3a22130276cfb97c404ae5
miker704 <miker704@gmail.com>
2022-08-30 21:00:30 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list_container.js
+++ Strife/frontend/components/friends/friends_list/friends_list_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import FriendShipIndex from './friends_list';
 import { requestFriendships, removeFriendshipErrors } from '../../../actions/friendship_actions';
-import { selectFriendStatus } from '../../../utils/selectors_api_util';
+import { selectFriendStatus, selectAllFriends } from '../../../utils/selectors_api_util';
 import { openModal } from '../../../actions/modal_actions';
 import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
 
@@ -22,7 +22,7 @@ const mSTP = (state) => {
 const mDTP = (dispatch) => {
     return {
         requestFriendships: () => dispatch(requestFriendships()),
-        removeFriendshipErrors:()=> dispatch(removeFriendshipErrors()),
+        removeFriendshipErrors: () => dispatch(removeFriendshipErrors()),
         removeDmServerErrors: () => dispatch(removeDmServerErrors()),
         createDmServer: (dmserver) => dispatch(createDmServer(dmserver)),
         openModal: (modal) => dispatch(openModal(modal))
