ba98924b22dad711a7e00124ab3328a9875bc214
miker704 <miker704@gmail.com>
2022-09-01 16:28:08 -0400

    updates to hover tip

--- Strife/frontend/components/dm_servers/dm_nav_bar/dm_nav_bar.jsx
+++ Strife/frontend/components/dm_servers/dm_nav_bar/dm_nav_bar.jsx
@@ -259,7 +259,7 @@ class DmNavBar extends React.Component {
 
                     <div className="dm-list-header">
                         <h4>DIRECT MESSAGES</h4>
-                        <div data-tip data-for="create-dm" className="create-channel-div" onClick={() => this.toggleCreateDmModal()}>
+                        <div className="create-channel-div" onClick={() => this.toggleCreateDmModal()}>
                             <i className="fa-solid fa-plus" />
                             {/* <div className="dm-tool-tip">
                                 Create DM
