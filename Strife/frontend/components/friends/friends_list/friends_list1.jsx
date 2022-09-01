51b1801cc6c59d944509d2d336d0fe391d2ae99a
miker704 <miker704@gmail.com>
2022-09-01 17:57:56 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list1.jsx
+++ Strife/frontend/components/friends/friends_list/friends_list1.jsx
@@ -176,8 +176,8 @@ const FriendShipIndex1 = (props) => {
                                                             </path>
                                                         </g>
                                                     </svg>
-                                                    <ReactTooltip className = "more-message-tool-tip"
-                                                     textColor="#B9BBBE" backgroundColor="#191919" id ="More" place="top" effect="solid">More</ReactTooltip>
+                                                    <ReactTooltip className="more-message-tool-tip"
+                                                        textColor="#B9BBBE" backgroundColor="#191919" id="More" place="top" effect="solid">More</ReactTooltip>
                                                 </div>
                                             </div>
 
