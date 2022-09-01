dfd07d87f6cfa419a423ff3b8a5e698b819d73d9
miker704 <miker704@gmail.com>
2022-08-31 20:28:12 -0400

    updates

--- Strife/frontend/components/friends/pending_list/pending_friends_list.jsx
+++ Strife/frontend/components/friends/pending_list/pending_friends_list.jsx
@@ -170,7 +170,7 @@ class PendingFriendList extends React.Component {
                                                         </svg>
                                                         {/* <div className="pending-request-actions-tool-tip">Accept</div> */}
                                                         {/* <div className="pending-request-actions-tool-tip-triangle"></div> */}
-                                                        <ReactTooltip className="remove-message-tool-tip" textColor="#B9BBBE"
+                                                        <ReactTooltip className="accept-message-tool-tip" textColor="#B9BBBE"
                                                             backgroundColor="#191919" id="Remove-Blocked-User" place="top" effect="solid">
                                                             Remove
                                                         </ReactTooltip>
