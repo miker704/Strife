caa563028d0d87d0ec6b2a4aa92af88223e71a95
miker704 <miker704@gmail.com>
2022-09-01 19:45:32 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list1.jsx
+++ Strife/frontend/components/friends/friends_list/friends_list1.jsx
@@ -46,8 +46,8 @@ const FriendShipIndex1 = (props) => {
     let liveSearch = () => {
         console.log("in live search");
         let allFriendShips = document.querySelectorAll('.friend-index-item');
-        let search_query = document.getElementById('input-all-friends').value;
-        let searchQuery = searchText
+        // let search_query = document.getElementById('input-all-friends').value;
+        let search_query = searchText;
         let numberOfFriends = document.getElementById('num-of-friends');
         let count = 0;
         let foundCount = 0;
