f434bf2c302c53f4a6511e833eedc42a711d48ab
miker704 <miker704@gmail.com>
2022-09-01 22:07:31 -0400

    updates

--- Strife/frontend/components/friends/friends_list_online/friends_list_online.jsx
+++ Strife/frontend/components/friends/friends_list_online/friends_list_online.jsx
@@ -218,7 +218,8 @@ const FriendShipIndexOnline = ({
                                                     {/* <div data-tip data-for="Message" className="pending-request-actions-tool-tip">Message</div> */}
                                                     {/* <div className="pending-request-actions-tool-tip-triangle"></div> */}
                                                 </div>
-                                                <div data-tip data-for = "More" className="friend-options-button" onClick={(e) => {handleSelected(friend), friendRef.current.focus(); handlePopupShow(e)}}>
+                                                <div data-tip data-for = "More" className="friend-options-button" 
+                                                onClick={(e) => {handleSelected(friend), friendRef.current.focus(); handlePopupShow(e)}}>
                                                     <svg className="icon-1WVg" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
                                                         <g fill="none" fillRule="evenodd">
                                                             <path d="M24 0v24H0V0z">
