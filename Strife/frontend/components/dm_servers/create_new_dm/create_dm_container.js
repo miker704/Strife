6bfdd31376c36322b3cf7207980c8d8ab710442c
miker704 <miker704@gmail.com>
2022-09-02 08:25:08 -0400

    attempt to fix state issues

--- Strife/frontend/components/dm_servers/create_new_dm/create_dm_container.js
+++ Strife/frontend/components/dm_servers/create_new_dm/create_dm_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import CreateDmModal from './create_dm_modal';
 import { selectFriendStatus, selectAllFriends } from '../../../utils/selectors_api_util';
-import { requestFriendships, removeFriendshipErrors } from '../../../actions/friendship_actions'; 
+import { requestFriendships, removeFriendshipErrors , requestAllFriendships} from '../../../actions/friendship_actions'; 
 import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
 import { createDmMember } from '../../../actions/dm_member_actions';
 
@@ -10,8 +10,8 @@ const mSTP = (state) => {
     return {
         currentUser: state.entities.users[state.session.id],
         dmServers: Object.values(state.entities.dmServers),
-        friends: selectFriendStatus(state, 3),
-        // friends: selectAllFriends(state, 3),
+        // friends: selectFriendStatus(state, 3),
+        friends: selectAllFriends(state, 3),
 
         errors: state.errors.friendship,
         dmServerErrors: state.errors.dmServer
@@ -20,6 +20,7 @@ const mSTP = (state) => {
 
 const mDTP = (dispatch) => {
     return {
+        requestAllFriendships: () => dispatch(requestAllFriendships()),
         requestFriendships: () => dispatch(requestFriendships()),
         createDmServer: (dmserver) => dispatch(createDmServer(dmserver)),
         createDmMember: (dm_member) => dispatch(createDmMember(dm_member)),
