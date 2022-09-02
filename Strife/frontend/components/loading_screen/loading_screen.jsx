268b0ab53ad796807d6b681972452c391cf432a0
miker704 <miker704@gmail.com>
2022-09-02 01:21:55 -0400

    updates

--- Strife/frontend/components/loading_screen/loading_screen.jsx
+++ Strife/frontend/components/loading_screen/loading_screen.jsx
@@ -10,7 +10,7 @@ class LoadingScreen extends React.Component{
     render(){
         return (
             <div className="loading-screen-wrapper">
-
+                <img src="" alt="" />
             </div>
         )
     }
