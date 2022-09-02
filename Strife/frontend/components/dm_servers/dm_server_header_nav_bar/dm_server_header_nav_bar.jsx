bf41c7e1c18bbb0420764830da90edd21380a7b6
miker704 <miker704@gmail.com>
2022-09-02 03:07:54 -0400

    updates

--- Strife/frontend/components/dm_servers/dm_server_header_nav_bar/dm_server_header_nav_bar.jsx
+++ Strife/frontend/components/dm_servers/dm_server_header_nav_bar/dm_server_header_nav_bar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 
+class DmServerHeaderNavBar extends React.Component{
+    constructor(props){
+        super(props);
+    }
 
+    
+}
 
 
 export default DmServerHeaderNavBar;
