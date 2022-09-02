c3984c693c61448491140c2a9ffff4f35c30a606
miker704 <miker704@gmail.com>
2022-09-02 00:45:56 -0400

    updates

--- Strife/frontend/components/loading_screen/loading_screen_container.js
+++ Strife/frontend/components/loading_screen/loading_screen_container.js
@@ -11,8 +11,18 @@ const mSTP = (state) => {
     const randomLoadingMessage = [
         "Strife was almost called Bonfire before we picked our name. It was meant to be nice and cozy.",
         "Loading",
-        ""
+        "Strife was almost called Wyvern before we picked our name. Not too proud of that one.",
+        "Strife is a Discord Clone made by Michael R. for App Academy Fullstack Project.",
+        "Strife is a full blown clone of Discord.",
+        "Our logo's name is Mr.Wumpus.",
+        "There are a bunch of hidden Easter Eggs in the app that happen when you click certain things...",
+        "Discord started as a game company making a mobile game called Fates Forever.",
+        "Discord’s official birthday is May 13, 2015.",
+        "We came up with the idea of Discord Nitro over morning breakfast potatoes.",
 
+        "Strife is a synom for discord",
+
+        ""
     ]
 
     
