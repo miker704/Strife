de59d552ba8bdc23fe68f87f4c341f6470b76625
miker704 <miker704@gmail.com>
2022-09-01 22:06:59 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list1.jsx
+++ Strife/frontend/components/friends/friends_list/friends_list1.jsx
@@ -220,7 +220,7 @@ const FriendShipIndex1 = (props) => {
                             {
                                 allFriends.map((friend, friendIdx) => {
                                     return (
-                                        <li className="friend-index-item" key={friend.id}>
+                                        <li className="friend-index-item" key={friend.id}  autoFocus ref={friendRef}>
                                             {showPopup && <EditFriendshipModalContainer user={props.currentUser} friend={friend} top={popupTop} setShowPopup={setShowPopup} />}
                                             <div className="friend-index-item-wrapper-inner">
                                                 <div className="friend-account-info-wrapper-super">
