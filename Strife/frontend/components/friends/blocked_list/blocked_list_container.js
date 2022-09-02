852568f842fd7f6967c1aa8111a88a5c7c0ef4e3
miker704 <miker704@gmail.com>
2022-09-02 08:27:30 -0400

    updates

--- Strife/frontend/components/friends/blocked_list/blocked_list_container.js
+++ Strife/frontend/components/friends/blocked_list/blocked_list_container.js
@@ -1,14 +1,14 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { selectFriendStatus, selectAllFriends } from '../../../utils/selectors_api_util';
-import { requestFriendships, deleteFriendship, removeFriendshipErrors } from '../../../actions/friendship_actions';
+import { requestFriendships, deleteFriendship, removeFriendshipErrors, requestAllFriendships } from '../../../actions/friendship_actions';
 import BlockedList from './blocked_list';
 
 const mSTP = (state) => {
     return {
         currentUser: state.entities.users[state.session.id],
-        blockedUsers: selectFriendStatus(state, -1),
-        // blockedUsers: selectAllFriends(state, -1),
+        // blockedUsers: selectFriendStatus(state, -1),
+        blockedUsers: selectAllFriends(state, -1),
 
         errors: state.errors.friendship
     }
