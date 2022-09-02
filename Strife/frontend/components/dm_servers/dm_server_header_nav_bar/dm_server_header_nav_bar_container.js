81328ea7dec9ae84e4168876c416fea367b1f1a3
miker704 <miker704@gmail.com>
2022-09-02 03:09:33 -0400

    updates

--- Strife/frontend/components/dm_servers/dm_server_header_nav_bar/dm_server_header_nav_bar_container.js
+++ Strife/frontend/components/dm_servers/dm_server_header_nav_bar/dm_server_header_nav_bar_container.js
@@ -1,3 +1,8 @@
 import DmServerHeaderNavBar from "./dm_server_header_nav_bar";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
+
+
+const mSTP =(state) => {
+    
+}
