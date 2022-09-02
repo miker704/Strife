169e92bf4b01c934b44b3461a688b1c0ba85a1ca
miker704 <miker704@gmail.com>
2022-09-02 19:31:39 -0400

    updates workihg on dm servers

--- Strife/frontend/components/dm_servers/dm_server_members/dm_server_list_container.js
+++ Strife/frontend/components/dm_servers/dm_server_members/dm_server_list_container.js
@@ -1,17 +1,23 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import DmServerMemberList from "./dm_server_list";
-
+import { selectDmMembers } from "../../../utils/selectors_api_util.js";
+import { fetchDmServer } from "../../../actions/dm_server_actions.js";
 
 const mSTP = (state, ownProps) => {
     return {
+        dmServerId: ownProps.match.params.dmServerId,
+        dmServerMembers: selectDmMembers(state,ownProps.match.params.dmServerId),
+        errors : state.errors.dmServer,
+        dmServer: state.entities.dmServers[ownProps.match.params.dmServerId],
+        currentUser: state.entities.users[state.session.id],
 
-        dmMembers: state.entities.dmServers[ownProps.match.params.dmServerId].members
     }
 }
 
 const mDTP = (dispatch, ownProps) => {
     return {
+        fetchDmServer: () => dispatch(fetchDmServer(ownProps.match.params.dmServerId)),
 
     }
 }
