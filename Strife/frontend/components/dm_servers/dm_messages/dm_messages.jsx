300dc57e38a5f580a6ef75b1f96c70de458c53e7
miker704 <miker704@gmail.com>
2022-09-02 05:41:24 -0400

    updates

--- Strife/frontend/components/dm_servers/dm_messages/dm_messages.jsx
+++ Strife/frontend/components/dm_servers/dm_messages/dm_messages.jsx
@@ -109,7 +109,7 @@ class DmMessages extends React.Component {
                                     id="dmshb-invite-members"
                                     place="top"
                                     effect="solid">
-                                    Start Video Call
+                                    Add Friends to DM
                                 </ReactTooltip>
                             </div>
                             <div className="dmshbar-search-bar-wrapper" >
@@ -153,25 +153,23 @@ class DmMessages extends React.Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="dmshb-tool-icon-wrapper" data-tip data-for="pin-messages">
+                            <div className="dmshb-tool-icon-wrapper" data-tip data-for="inbox-messages-tip">
                                 <svg x="0" y="0" className="icon-dms-inbox" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                     <path d="M19 3H4.99C3.88 3 3.01 3.89 3.01 5L3 19C3 20.1 3.88 21 4.99 21H19C20.1 21 21 20.1 21
                                  19V5C21 3.89 20.1 3 19 3ZM19 15H15C15 16.66 13.65 18 12 18C10.35 18 9 16.66 9 15H4.99V5H19V15Z" fill="currentColor">
                                     </path>
                                 </svg>
                                 <ReactTooltip
-                                    className="pinned-messages-tool-tip"
+                                    className="dmshb-inbox-message-tool-tip"
                                     textColor="#B9BBBE"
                                     backgroundColor="#191919"
-                                    id="pin-messages"
+                                    id="inbox-messages-tip"
                                     place="top"
                                     effect="solid">
-                                    Start Video Call
+                                        Inbox
                                 </ReactTooltip>
                             </div>
-                            {/* <div className="dmshb-tool-icon-wrapper">
-
-                            </div> */}
+                         
                             <a className="help-tool-bar" href="https://support.discord.com" target="_blank">
                                 <div className="help-tool-bar-icon-wrapper" data-tip data-for="pin-messages">
                                     <svg x="0" y="0" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
