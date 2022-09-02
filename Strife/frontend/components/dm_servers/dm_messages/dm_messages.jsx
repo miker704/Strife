591f2b457f3e9a91082bf7dde030f81348ab0ef7
miker704 <miker704@gmail.com>
2022-09-02 15:09:47 -0400

    making an attemptt to fix major state issues by moving friends from users to friemdships when referring to it while users will only load friends when updating creating or removing friends

--- Strife/frontend/components/dm_servers/dm_messages/dm_messages.jsx
+++ Strife/frontend/components/dm_servers/dm_messages/dm_messages.jsx
@@ -204,10 +204,10 @@ class DmMessages extends React.Component {
                 <div className="dm-messages-container">
                     <ul>
                         {
-                            // this.props.dmMembers.map((dmMember, dmMemberIdx) => {
-                            //     return (
-                            //     <li key={dmMember.id}>{dmMember.username}</li>)
-                            // })
+                            Object.values(this.props.dmServerMembers).map((dmMember, dmMemberIdx) => {
+                                return (
+                                <li key={dmMember.id}>{dmMember.username}</li>)
+                            })
                         }
 
 
