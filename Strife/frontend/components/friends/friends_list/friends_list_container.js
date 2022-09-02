32ddb6d541723f1ccfe006e479f9937cf72131cc
miker704 <miker704@gmail.com>
2022-09-02 15:01:06 -0400

    updates

--- Strife/frontend/components/friends/friends_list/friends_list_container.js
+++ Strife/frontend/components/friends/friends_list/friends_list_container.js
@@ -1,8 +1,8 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import FriendShipIndex from './friends_list';
-import { requestFriendships, removeFriendshipErrors } from '../../../actions/friendship_actions';
-import { selectFriendStatus} from '../../../utils/selectors_api_util';
+import { requestFriendships, removeFriendshipErrors, requestAllFriendships } from '../../../actions/friendship_actions';
+import { selectFriendStatus, selectAllFriends } from '../../../utils/selectors_api_util';
 import { openModal } from '../../../actions/modal_actions';
 import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
 
@@ -10,6 +10,8 @@ const mSTP = (state) => {
     return {
         currentUser: state.entities.users[state.session.id],
         friends: selectFriendStatus(state, 3),
+        // friends: selectAllFriends(state, 3),
+
         dmServers: Object.values(state.entities.dmServers),
         errors: state.errors.friendship,
         dmServerErrors: state.errors.dmServer
@@ -24,7 +26,8 @@ const mDTP = (dispatch) => {
         removeFriendshipErrors: () => dispatch(removeFriendshipErrors()),
         removeDmServerErrors: () => dispatch(removeDmServerErrors()),
         createDmServer: (dmserver) => dispatch(createDmServer(dmserver)),
-        openModal: (modal) => dispatch(openModal(modal))
+        openModal: (modal) => dispatch(openModal(modal)),
+        requestAllFriendships: () => dispatch(requestAllFriendships())
     }
 };
 
