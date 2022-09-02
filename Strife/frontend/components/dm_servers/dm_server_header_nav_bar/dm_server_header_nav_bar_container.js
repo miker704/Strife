fe07ab53ae602d795a3680dff375afd57628c679
miker704 <miker704@gmail.com>
2022-09-02 15:22:59 -0400

    updates

--- Strife/frontend/components/dm_servers/dm_server_header_nav_bar/dm_server_header_nav_bar_container.js
+++ Strife/frontend/components/dm_servers/dm_server_header_nav_bar/dm_server_header_nav_bar_container.js
@@ -1,21 +1,25 @@
 import DmServerHeaderNavBar from "./dm_server_header_nav_bar";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-
+import { removeDmMessageErrors } from "../../../actions/dm_messages_actions";
+import { removeDmServerErrors } from "../../../actions/dm_server_actions";
 
 const mSTP = (state, ownProps) => {
     return {
         errors: state.errors.dmServer,
-        dmMessageErrors: state.errors.dmMessage
+        dmMessageErrors: state.errors.dmMessage,
+        currentUser: state.entities.users[state.session.id],
+
     }
 
 }
 
-const mDTP = (state,ownProps) => {
+const mDTP = (state, ownProps) => {
     return {
-        
+        removeDmServerErrors: () => dispatch(removeDmServerErrors()),
+        removeDmMessageErrors: () => dispatch(removeDmMessageErrors())
     }
 }
 
-const DmServerHeaderNavBarContainer = withRouter(connect(mSTP,mDTP)(DmServerHeaderNavBar));
+const DmServerHeaderNavBarContainer = withRouter(connect(mSTP, mDTP)(DmServerHeaderNavBar));
 export default DmServerHeaderNavBarContainer;
