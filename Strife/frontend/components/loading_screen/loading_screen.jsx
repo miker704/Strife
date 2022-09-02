0953912f4c3af08a37bcea5788abdd215806673b
miker704 <miker704@gmail.com>
2022-09-02 01:43:42 -0400

    updates

--- Strife/frontend/components/loading_screen/loading_screen.jsx
+++ Strife/frontend/components/loading_screen/loading_screen.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 
 
-class LoadingScreen extends React.Component{
-    constructor(props){
+class LoadingScreen extends React.Component {
+    constructor (props) {
         super(props);
         this.loadingMessage = this.props.selectedLoadingMsg;
     }
-    
-    render(){
+
+
+    componentDidMount () {
+        if (this.props.history.location.pathname !== "/channels/@me") {
+            setTimeout(() => {
+                this.props.history.push("/channels/@me");
+            }, 5000)
+        }
+    }
+
+    render () {
         return (
             <div className="loading-screen-wrapper">
-                <img className = "loading-screen-img" alt="loadingimg" />
+                <img className="loading-screen-img" alt="loadingimg" />
                 <h1>{this.loadingMessage}</h1>
             </div>
         )
