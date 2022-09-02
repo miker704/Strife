db81ed8759904f1d4456d4f3133be433208da1dd
miker704 <miker704@gmail.com>
2022-09-02 15:35:03 -0400

    making an attemptt to fix major state issues by moving friends from users to friemdships when referring to it while users will only load friends when updating creating or removing friends

--- Strife/frontend/components/dm_servers/dm_server_header_nav_bar/dm_server_header_nav_bar_container.js
+++ Strife/frontend/components/dm_servers/dm_server_header_nav_bar/dm_server_header_nav_bar_container.js
@@ -1,8 +1,8 @@
 import DmServerHeaderNavBar from "./dm_server_header_nav_bar";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import { removeDmMessageErrors } from "../../../actions/dm_messages_actions";
-import { removeDmServerErrors } from "../../../actions/dm_server_actions";
+import { removeDmMessageErrors,  } from "../../../actions/dm_messages_actions";
+import { removeDmServerErrors, updateDmServer } from "../../../actions/dm_server_actions";
 
 const mSTP = (state, ownProps) => {
     return {
@@ -17,7 +17,8 @@ const mSTP = (state, ownProps) => {
 const mDTP = (state, ownProps) => {
     return {
         removeDmServerErrors: () => dispatch(removeDmServerErrors()),
-        removeDmMessageErrors: () => dispatch(removeDmMessageErrors())
+        removeDmMessageErrors: () => dispatch(removeDmMessageErrors()),
+        updateDmServer: (dmServerId,dmServer) => dispatch(updateDmServer(dmServerId,dmServer))
     }
 }
 
