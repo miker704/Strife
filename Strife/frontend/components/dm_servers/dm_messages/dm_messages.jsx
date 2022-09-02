994da6a4da5741a2ce35619281863b30223419b6
miker704 <miker704@gmail.com>
2022-09-02 05:42:19 -0400

    updates

--- Strife/frontend/components/dm_servers/dm_messages/dm_messages.jsx
+++ Strife/frontend/components/dm_servers/dm_messages/dm_messages.jsx
@@ -171,7 +171,7 @@ class DmMessages extends React.Component {
                             </div>
                          
                             <a className="help-tool-bar" href="https://support.discord.com" target="_blank">
-                                <div className="help-tool-bar-icon-wrapper" data-tip data-for="pin-messages">
+                                <div className="help-tool-bar-icon-wrapper" data-tip data-for="help-messages-tip">
                                     <svg x="0" y="0" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
                                         <path fill="currentColor" d="M12 2C6.486 2 2 6.487 2 12C2 17.515 6.486 22 12 22C17.514 22 22 17.515
                                      22 12C22 6.487 17.514 2 12 2ZM12 18.25C11.31 18.25 10.75 17.691 10.75 17C10.75 16.31 11.31
@@ -180,16 +180,14 @@ class DmMessages extends React.Component {
                                         10 10H8C8 7.795 9.795 6 12 6C14.205 6 16 7.795 16 10C16 11.861 14.723 13.429 13 13.875Z">
                                         </path>
                                     </svg>
-                                    <div className="home-bar-tool-tip">Help</div>
-                                    <div className="home-bar-tool-tip-triangle"></div>
                                     <ReactTooltip
-                                        className="pinned-messages-tool-tip"
+                                        className="dmshb-help-tool-tip"
                                         textColor="#B9BBBE"
                                         backgroundColor="#191919"
-                                        id="pin-messages"
+                                        id="help-messages-tip"
                                         place="top"
                                         effect="solid">
-                                        Start Video Call
+                                        Help
                                     </ReactTooltip>
                                 </div>
                             </a>
