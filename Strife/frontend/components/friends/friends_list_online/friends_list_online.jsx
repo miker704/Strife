2ecc5a03df6fed1539537ac0ae0f9b5d126a5ac8
miker704 <miker704@gmail.com>
2022-09-01 21:07:06 -0400

    updates

--- Strife/frontend/components/friends/friends_list_online/friends_list_online.jsx
+++ Strife/frontend/components/friends/friends_list_online/friends_list_online.jsx
@@ -181,7 +181,7 @@ const FriendShipIndexOnline = ({
                                     return (
                                         
                                         <li id="fii" className="friend-index-item" key={friend.id}>
-                                            { showPopup && <EditFriendshipModalContainer user={currentUser} friend={friend} top={popupTop} setShowPopup={setShowPopup}/>}
+                                            { showPopup && <EditFriendshipModalContainer user={currentUser} friend={selectFriend} top={popupTop} setShowPopup={setShowPopup}/>}
                                             <div className="friend-index-item-wrapper-inner">
                                                 <div className="friend-account-info-wrapper-super">
                                                     <div className="friend-info">
