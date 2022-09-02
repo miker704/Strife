354d36e8442316e4c73ac449f1b934089ae954b1
miker704 <miker704@gmail.com>
2022-09-01 21:25:16 -0400

    updates

--- Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
+++ Strife/frontend/components/dm_servers/create_new_dm/create_dm_modal.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useRef, useEffect } from "react";
 
 const CreateDmModal = ({
-    top, dmServers, setShowPopup, currentUser, friends, createDmServer, history, createDmMember,
+    top, dmServers, setShowPopup, currentUser, friends, createDmServer, history, createDmMember,requestFriendships
 }) => {
     const inputRef = useRef();
     const popupRef = useRef();
