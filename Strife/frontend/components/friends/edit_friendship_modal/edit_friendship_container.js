4b1f0b1d9768ca3b2554393fcfc746227de5307b
miker704 <miker704@gmail.com>
2022-09-03 16:23:16 -0400

    update rendering dmserver names chnaged to if name is not null render that name else render usernames

--- Strife/frontend/components/friends/edit_friendship_modal/edit_friendship_container.js
+++ Strife/frontend/components/friends/edit_friendship_modal/edit_friendship_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import EditFriendshipModal from './edit_friendship';
 import { selectFriendStatusOnline} from '../../../utils/selectors_api_util';
-import { requestFriendships, removeFriendshipErrors, deleteFriendship} from '../../../actions/friendship_actions';
+import { requestFriendships, removeFriendshipErrors, deleteFriendship, updateFriendship , blockUser} from '../../../actions/friendship_actions';
 
 
 const mSTP = (state) => {
@@ -18,7 +18,9 @@ const mDTP = (dispatch) => {
     return {
         requestFriendships: () => dispatch(requestFriendships()),
         removeFriendshipErrors: () => dispatch(removeFriendshipErrors()),
-        deleteFriendship: (ids) => dispatch(deleteFriendship(ids)) 
+        deleteFriendship: (ids) => dispatch(deleteFriendship(ids)),
+        blockUser: (ids) => dispatch(blockUser(ids)),
+         
     }
 };
 
