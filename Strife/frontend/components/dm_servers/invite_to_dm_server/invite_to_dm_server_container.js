9f98efd237931d8abbe41069156a0993a51b229c
miker704 <miker704@gmail.com>
2022-09-04 14:50:50 -0400

    updates

---
+++ Strife/frontend/components/dm_servers/invite_to_dm_server/invite_to_dm_server_container.js
@@ -0,0 +1,34 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import CreateDmModal from './create_dm_modal';
+import { selectFriendStatus, selectAllFriends } from '../../../utils/selectors_api_util';
+import { requestFriendships, removeFriendshipErrors , requestAllFriendships} from '../../../actions/friendship_actions'; 
+import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
+import { createDmMember } from '../../../actions/dm_member_actions';
+
+const mSTP = (state) => {
+    return {
+        currentUser: state.entities.users[state.session.id],
+        dmServers: Object.values(state.entities.dmServers),
+        // friends: selectFriendStatus(state, 3),
+        friends: selectAllFriends(state, 3),
+
+        errors: state.errors.friendship,
+        dmServerErrors: state.errors.dmServer
+    }
+}
+
+const mDTP = (dispatch) => {
+    return {
+        requestAllFriendships: () => dispatch(requestAllFriendships()),
+        requestFriendships: () => dispatch(requestFriendships()),
+        createDmServer: (dmserver) => dispatch(createDmServer(dmserver)),
+        createDmMember: (dm_member) => dispatch(createDmMember(dm_member)),
+        removeDmServerErrors: () => dispatch(removeDmServerErrors()),
+        removeFriendshipErrors: () => dispatch(removeFriendshipErrors())
+    }
+}
+
+
+const CreateDmModalContainer = withRouter(connect(mSTP, mDTP)(CreateDmModal));
+export default CreateDmModalContainer;
