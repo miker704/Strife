177f66dd3316c9f56bee62562866851ed2cc5d8b
miker704 <miker704@gmail.com>
2022-09-04 00:34:49 -0400

    removed unnesscearry comps

--- Strife/frontend/components/dm_servers/dm_messages/dm_messages.jsx
+++ Strife/frontend/components/dm_servers/dm_messages/dm_messages.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactTooltip from "react-tooltip";
-import DmServerHeaderNavBarContainer from "../dm_server_header_nav_bar/dm_server_header_nav_bar_container";
-import DmServerMemberListContainer from "../dm_server_members/dm_server_list_container";
 
 class DmMessages extends React.Component {
     constructor (props) {
@@ -12,13 +10,7 @@ class DmMessages extends React.Component {
         this.props.fetchDmServer(this.props.dmServerId);
     }
 
-    renderDmMemberContainer () {
-        if (Object.values(this.props.dmServerMembers).length > 2) {
-            return (
-                <DmServerMemberListContainer dmServerMembers={this.props.dmServerMembers} />
-            )
-        }
-    }
+
 
     render () {
         console.log("dmserver messages props", this.props);
@@ -26,34 +18,19 @@ class DmMessages extends React.Component {
         console.log(this.props.dmServer);
 
         return (
-            <div className="dm-server-container">
 
-                <DmServerHeaderNavBarContainer dmServerMembers={this.props.dmServerMembers} />
-
-                {/* <DmServerMemberListContainer/> */}
-                {/* {this.renderDmMemberContainer()} */}
-
-                {/* <div className="dm-messages-container"> */}
-                <div className="dm-server-content-container">
-                    <div className="dm-messages-container-wrapper">
-                        <p>vdfrdgrsdgwre</p>
-                        <p>vdfrdgrsdgwre</p>
-                        <p>vdfrdgrsdgwre</p>
-                        <p>vdfrdgrsdgwre</p>
-                        <p>vdfrdgrsdgwre</p>
-                        <p>vdfrdgrsdgwre</p>
-                        <p>vdfrdgrsdgwre</p>
-                        <p>vdfrdgrsdgwre</p>
-                        <p>vdfrdgrsdgwre</p>
-                    </div>
-                    {this.renderDmMemberContainer()}
-                </div> 
-                {/* <div className="empty-messages-container is-hidden"></div> */}
+            <div className="dm-messages-container-wrapper">
+                <p>vdfrdgrsdgwre</p>
+                <p>vdfrdgrsdgwre</p>
+                <p>vdfrdgrsdgwre</p>
+                <p>vdfrdgrsdgwre</p>
+                <p>vdfrdgrsdgwre</p>
+                <p>vdfrdgrsdgwre</p>
+                <p>vdfrdgrsdgwre</p>
+                <p>vdfrdgrsdgwre</p>
+                <p>vdfrdgrsdgwre</p>
             </div>
         )
-
-
-
 
     }
 }
