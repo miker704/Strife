42478ddf10e57aead4930e8407af641e6472b9f6
miker704 <miker704@gmail.com>
2022-09-04 00:41:11 -0400

    updates

--- Strife/frontend/components/friends/user_options/user_options_container.jsx
+++ Strife/frontend/components/friends/user_options/user_options_container.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState, useRef, useEffect } from "react";
 import { closeHookModalOnOutsideClick, closeOnEsc } from "../../../utils/close_hook_modals_api_utils";
-const EditFriendshipModal = ({
+
+const UserOptionsModal = ({
     currentUser,
     friends,
     errors,
@@ -111,34 +112,7 @@ const EditFriendshipModal = ({
 export default EditFriendshipModal;
 
 
-// class EditFriendshipModal extends React.Component {
-//     constructor (props) {
-//         super(props);
-//     }
-//     render () {
-//         return (
-//             <div className="fo-layer">
-//                 <div className="fo-theme">
-//                     <div className="fo-flex-wrapper">
-//                         <div className="fo-scroller">
-//                             <div className="fo-item-container">
-//                                 <div className="fo-item-name">Start Video Call</div>
-//                             </div>
-//                             <div className="fo-item-container">
-//                                 <div className="fo-item-name">Start Voice Call</div>
-//                             </div>
-//                             <div className="fo-item-container red">
-//                                 <div className="fo-item-name">Remove Friend</div>
-//                             </div>
-//                             <div className="fo-options-bottom-div"></div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
 
-// }
 
 
 
