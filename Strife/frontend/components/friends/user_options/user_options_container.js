18fe9021a0c1dfc5022b40c8e02d9338e4049eef
miker704 <miker704@gmail.com>
2022-09-04 14:05:56 -0400

    updates

--- Strife/frontend/components/friends/user_options/user_options_container.js
+++ Strife/frontend/components/friends/user_options/user_options_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { selectFriendStatusOnline} from '../../../utils/selectors_api_util';
 import { requestFriendships, removeFriendshipErrors, deleteFriendship, updateFriendship , blockUser, createFriendship} from '../../../actions/friendship_actions';
-import { createDmServer, receiveDmServerErrors } from '../../../actions/dm_server_actions';
+import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
 import { createDmMember, deleteDmMember} from '../../../actions/dm_member_actions';
 import UserOptionsModal from './user_options_modal';
 
@@ -21,6 +21,7 @@ const mDTP = (dispatch) => {
     return {
         requestFriendships: () => dispatch(requestFriendships()),
         removeFriendshipErrors: () => dispatch(removeFriendshipErrors()),
+
         deleteFriendship: (ids) => dispatch(deleteFriendship(ids)),
         blockUser: (ids) => dispatch(blockUser(ids)),
         updateFriendship: (ids) => dispatch(updateFriendship(ids)),
