f7df5ad0cc6896e3e12b288935989339b7b224c5
miker704 <miker704@gmail.com>
2022-09-04 14:22:43 -0400

    updates

--- Strife/frontend/components/friends/user_options/user_options_modal.jsx
+++ Strife/frontend/components/friends/user_options/user_options_modal.jsx
@@ -115,9 +115,10 @@ const UserOptionsModal = ({
                         <div className="fo-item-container red" onClick={() => handleDeleteFriendShip()}>
                             <div className="fo-item-name">Unblock User</div>
                         </div>
-                        <div className="fo-item-container red" onClick={() => handleKickUser()}>
+                        {/* <div className="fo-item-container red" onClick={() => handleKickUser()}>
                             <div className="fo-item-name">Kick User</div>
-                        </div>
+                        </div> */}
+                        {kickUserOption}
                         <div className="fo-options-bottom-div"></div>
                     </div>
                 </div>
