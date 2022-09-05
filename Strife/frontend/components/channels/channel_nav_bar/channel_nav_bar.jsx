9ed975e81e5d62292c50aa7952e0c971eef9b677
miker704 <miker704@gmail.com>
2022-09-05 09:16:42 -0400

    started dmserver messages and action cable

--- Strife/frontend/components/channels/channel_nav_bar/channel_nav_bar.jsx
+++ Strife/frontend/components/channels/channel_nav_bar/channel_nav_bar.jsx
@@ -184,7 +184,17 @@ class ChannelNavBar extends React.Component {
                                                 </div>
                                             </Link>
                                             <div className="child-buttons">
+
                                                 <div className="create-channel-invite-icon-wrapper">
+                                                    <svg className="open-chat-icon" aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 24 24" fill="none">
+                                                        <path fill="currentColor" d="M4.79805 3C3.80445 3 2.99805 3.8055 2.99805 4.8V15.6C2.99805 16.5936 3.80445 
+                                                    17.4 4.79805 17.4H7.49805V21L11.098 17.4H19.198C20.1925 17.4 20.998 16.5936 20.998 15.6V4.8C20.998 3.8055 
+                                                    20.1925 3 19.198 3H4.79805Z">
+                                                        </path>
+                                                    </svg>
+                                                </div>
+                                                <div className="create-channel-invite-icon-wrapper">
+
                                                     <svg className="create-channel-invite-icon" aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 16 16">
                                                         <path fill="currentColor" d="M14 2H16V3H14V5H13V3H11V2H13V0H14V2Z"></path>
                                                         <path fill="currentColor" d="M6.5 8.00667C7.88 8.00667 9 6.88667 9 5.50667C9 4.12667 7.88 3.00667 6.5 
