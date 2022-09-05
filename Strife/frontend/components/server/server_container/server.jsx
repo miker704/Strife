b99766cad1dae4859ffcee3eb7e0285dfc2fb612
miker704 <miker704@gmail.com>
2022-09-05 04:39:33 -0400

    updates

--- Strife/frontend/components/server/server_container/server.jsx
+++ Strife/frontend/components/server/server_container/server.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ChannelNavBarContainer from "../../channels/channel_nav_bar/channel_nav_bar_container";
+
 
 class Server extends React.Component {
     constructor(props) {
