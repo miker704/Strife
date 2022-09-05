3a0660bbbe69bebcd40c1ff366228bc0eec38bca
miker704 <miker704@gmail.com>
2022-09-05 05:05:29 -0400

    started dmserver messages and action cable

--- Strife/frontend/components/server/server_container/server.jsx
+++ Strife/frontend/components/server/server_container/server.jsx
@@ -3,23 +3,44 @@ import ChannelNavBarContainer from "../../channels/channel_nav_bar/channel_nav_b
 
 
 class Server extends React.Component {
-    constructor(props) {
+    constructor (props) {
         super(props);
         this.state = {
         }
-       
+
     }
-    componentWillUnmount() {
+    componentWillUnmount () {
     }
 
 
-  
-
-    render() {
 
 
-     
+    render () {
+
+        //if server is rendered and fetch render it else return null FAIL SAFE for refresh or odd application state 
+        if (this.props.server) {
+
+            return (
+                <div className="server-base">
+
+                    <div className="server-content">
+                            
+
+
+                    </div>
+
+                </div>
+            )
+
+        }
+
+
+        else {
+            return null;
+        }
+
     }
+
 }
 
 export default Server;
