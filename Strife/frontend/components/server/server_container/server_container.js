26bd837f56dba41f867301e77d829ab194de8aa5
miker704 <miker704@gmail.com>
2022-09-05 00:36:09 -0400

    updates

---
+++ Strife/frontend/components/server/server_container/server_container.js
@@ -0,0 +1,42 @@
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
+import { fetchDmServer, fetchDmServers } from "../../../actions/dm_server_actions.js";
+import { createDmMessage } from "../../../actions/dm_messages_actions.js";
+import { selectDmMembers } from "../../../utils/selectors_api_util.js";
+import Server from "./server.jsx";
+
+const mSTP = (state, ownProps) => {
+
+
+    return {
+        dmMessage: {
+            body: "",
+            sender_id: state.session.id,
+            dm_server_id: ownProps.match.params.dmServerId
+        },
+        dmMessages: Object.values(state.entities.dmMessages),
+        currentUserId: state.session.id,
+        currentUser: state.entities.users[state.session.id],
+        dmMessagesIds: Object.keys(state.entities.dmMessages),
+        // dmMembers: state.entities.dmServers[ownProps.match.params.dmServerId],
+        dmServerId: ownProps.match.params.dmServerId,
+        dmServer: state.entities.dmServers[ownProps.match.params.dmServerId],
+        errors: state.errors.dmMessage,
+        dmServerMembers: selectDmMembers(state,ownProps.match.params.dmServerId),
+        dmServerErrors : state.errors.dmServer,
+
+    }
+}
+
+
+const mDTP = (dispatch, ownProps) => {
+    return {
+        fetchDmServers: (userId) => dispatch(fetchDmServers(userId)),
+        fetchDmServer: () => dispatch(fetchDmServer(ownProps.match.params.dmServerId)),
+        createDmMessage: (dmmessage) => dispatch(createDmMessage(dmmessage))
+    }
+}
+
+
+const ServerContainer = withRouter(connect(mSTP, mDTP)(Server));
+export default ServerContainer;
