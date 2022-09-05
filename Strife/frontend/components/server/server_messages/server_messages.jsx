fddeddf91c1020f876d344ddc6a99784a5d35497
miker704 <miker704@gmail.com>
2022-09-05 17:17:26 -0400

    up[dates

--- Strife/frontend/components/server/server_messages/server_messages.jsx
+++ Strife/frontend/components/server/server_messages/server_messages.jsx
@@ -17,6 +17,32 @@ class ServerMessages extends React.Component {
                                 <div className="chat-divider">
                                     <span className="chat-divider-content"></span>
                                 </div>
+                                <li className="chat-message-item">
+                                    <div className="message-wrapper">
+                                        <div className="message-wrapper-contents">
+                                            <div className="chat-member-avatar-img">
+                                                insert img here
+                                            </div>
+                                            <h2 className="chat-member-username-header">
+                                                <span className="chat-member-username-wrap">
+                                                    <span className="chat-member-username">insert username here</span>
+                                                </span>
+                                                <span className="chat-message-timestamp-wrap">
+                                                            <time className="chat-message-timestamp">
+                                                                    <i className="chat-message-timestamp-i">
+                                                                        insert time stamp here
+                                                                    </i>
+                                                            </time>
+                                                </span>
+                                            </h2>
+                                            <div className="chat-message">insert message content here
+                                                <span className="mention-wrapper">@mentionuser</span>
+                                            </div>
+                                        </div>
+                                        <div className="message-accessories"></div>
+                                    </div>
+                                </li>
+                                <div className="ol-scroller-spacer"></div>
                             </ol>
                         </div>
                     </div>
