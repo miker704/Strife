6176a1cf5918755858b42cf68e2790c97e8980ee
miker704 <miker704@gmail.com>
2022-09-05 04:48:04 -0400

    started dmserver messages and action cable

--- Strife/frontend/components/server/server_container/server_container.js
+++ Strife/frontend/components/server/server_container/server_container.js
@@ -1,29 +1,26 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import { fetchDmServer, fetchDmServers } from "../../../actions/dm_server_actions.js";
-import { createDmMessage } from "../../../actions/dm_messages_actions.js";
-import { selectDmMembers } from "../../../utils/selectors_api_util.js";
 import Server from "./server.jsx";
+import { fetchChannel, createChannel, updateChannel, deleteChannel } from "../../../actions/channel_actions.js";
+import { fetchServer, fetchServers, removeServerErrors, deleteServer, updateServer } from "../../../actions/server_actions.js";
+import { createChannelMembership, deleteChannelMembership } from "../../../actions/channel_membership_actions.js";
+import { createServerMembership, deleteServerMembership } from "../../../actions/server_membership_actions.js";
+
 
 const mSTP = (state, ownProps) => {
 
 
     return {
-        dmMessage: {
-            body: "",
-            sender_id: state.session.id,
-            dm_server_id: ownProps.match.params.dmServerId
-        },
-        dmMessages: Object.values(state.entities.dmMessages),
-        currentUserId: state.session.id,
         currentUser: state.entities.users[state.session.id],
-        dmMessagesIds: Object.keys(state.entities.dmMessages),
-        // dmMembers: state.entities.dmServers[ownProps.match.params.dmServerId],
-        dmServerId: ownProps.match.params.dmServerId,
-        dmServer: state.entities.dmServers[ownProps.match.params.dmServerId],
-        errors: state.errors.dmMessage,
-        dmServerMembers: selectDmMembers(state,ownProps.match.params.dmServerId),
-        dmServerErrors : state.errors.dmServer,
+        server: state.entities.servers[ownProps.match.params.serverId],
+        channels: Object.values(state.entities.channels),
+        currentChannelId: ownProps.match.params.channelId,
+        serverId: ownProps.match.params.serverId,
+        dmServers: Object.values(state.entities.dmServers),
+        errors: state.errors.channel,
+        serverErrors: state.errors.server
+        
+
 
     }
 }
@@ -31,9 +28,13 @@ const mSTP = (state, ownProps) => {
 
 const mDTP = (dispatch, ownProps) => {
     return {
-        fetchDmServers: (userId) => dispatch(fetchDmServers(userId)),
-        fetchDmServer: () => dispatch(fetchDmServer(ownProps.match.params.dmServerId)),
-        createDmMessage: (dmmessage) => dispatch(createDmMessage(dmmessage))
+        fetchChannel: (channelId) => { dispatch(fetchChannel(channelId)) },
+        openModal: modal => dispatch(openModal(modal)),
+        fetchServers: () => dispatch(fetchServers()),
+        fetchServer: serverId => dispatch(fetchServer(serverId)),
+        deleteServer: serverId => dispatch(deleteServer(serverId)),
+        removeChannelMembership: (channelmembershiphash) => dispatch(removeChannelMembership(channelmembershiphash)),
+        removeServerMembership: (membershiphash) => dispatch(removeServerMembership(membershiphash))
     }
 }
 
