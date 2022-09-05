31da1ba5b73cb5ba3a7959888b5c80afd7f3c448
miker704 <miker704@gmail.com>
2022-09-05 15:28:22 -0400

    started dmserver messages and action cable

---
+++ Strife/frontend/components/server/server_messages/server_messages.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+class ServerMessages extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+        <div className="server-chat-container">
+
+        </div>
+        )
+    }
+}
+
+export default ServerMessages;
