afef06e65329b04489e88313c3eb71b6b410dd50
miker704 <miker704@gmail.com>
2022-09-07 15:40:46 -0400

    updates

--- Strife/frontend/components/server/server_nav_bar/server_nav_bar.jsx
+++ Strife/frontend/components/server/server_nav_bar/server_nav_bar.jsx
@@ -97,7 +97,6 @@ class ServerNavBar extends React.Component {
                             {/* <button id="create-server" onClick={() => this.props.openModal('createServerForm')}> */}
                             <button id="create-server" onClick={() => this.renderModal('createServerForm')}>
 
-                                {/* <i className="fa-solid fa-plus" /> */}
                                 <svg aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
                                     <path fill="currentColor" d="M20 11.1111H12.8889V4H11.1111V11.1111H4V12.8889H11.1111V20H12.8889V12.8889H20V11.1111Z">
                                     </path>
