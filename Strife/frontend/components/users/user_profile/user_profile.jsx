7e8de0398d49aab05a97c6083cae39b4812b9e1a
miker704 <miker704@gmail.com>
2022-09-07 15:21:31 -0400

    updates

--- Strife/frontend/components/users/user_profile/user_profile.jsx
+++ Strife/frontend/components/users/user_profile/user_profile.jsx
@@ -640,13 +640,14 @@ class UserProfile extends React.Component {
                     <li className="user-profile-item">HypeSquad</li>
                     <div className="user-settings-separator"></div>
                     <li className="user-profile-item" onClick={() => this.handleLogout()}>
-                      <span>Log Out</span>
-                      {/* <i className="fa-solid fa-arrow-right"></i> */}
-                      <svg className = "upm-logout-icon" aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M18 2H7C5.897 2 5 2.898 5 4V11H12.59L10.293 8.708L11.706 7.292L16.414 11.991L11.708 16.706L10.292 15.294L12.582 
+                      <div className="user-profile-item-logout-sec">
+                         Log Out
+                        <svg className="upm-logout-icon" aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 24 24">
+                          <path fill="currentColor" d="M18 2H7C5.897 2 5 2.898 5 4V11H12.59L10.293 8.708L11.706 7.292L16.414 11.991L11.708 16.706L10.292 15.294L12.582 
                                 13H5V20C5 21.103 5.897 22 7 22H18C19.103 22 20 21.103 20 20V4C20 2.898 19.103 2 18 2Z">
-                        </path>
-                      </svg>
+                          </path>
+                        </svg>
+                      </div>
                     </li>
                     <div className="user-settings-separator"></div>
                     <div className="user-profile-socials">
