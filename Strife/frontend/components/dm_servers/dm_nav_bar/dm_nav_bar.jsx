bf18fb4ecb90dbb43e716dc0d46b297410ec62d6
miker704 <miker704@gmail.com>
2022-09-08 17:09:13 -0400

    bug fix failed dn allow rendering outside of hp

--- Strife/frontend/components/dm_servers/dm_nav_bar/dm_nav_bar.jsx
+++ Strife/frontend/components/dm_servers/dm_nav_bar/dm_nav_bar.jsx
@@ -65,13 +65,14 @@ class DmNavBar extends React.Component {
     }
 
     toggleCreateDmModal () {
-        document.getElementById('itbgdm').style = "z-index:-1;";
+        // document.getElementById('itbgdm').style = "z-index:-1;";
         this.setState({ createDmModal: true })
     }
 
     closeCreateDmModal () {
         if (this.mounted) {
-            document.getElementById('itbgdm').style = "z-index:auto;";
+            // document.getElementById('itbgdm').style = "z-index:auto;";
+            console.log("closing modal");
             this.setState({ createDmModal: false })
             window.removeEventListener('keyup', this.handleESC, false);
         }
