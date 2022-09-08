cb02b5cf7f1f59d4a005567c64e6d4e83c20b13c
miker704 <miker704@gmail.com>
2022-09-08 01:38:38 -0400

    updates

--- Strife/frontend/components/dm_servers/dm_nav_bar/dm_nav_bar.jsx
+++ Strife/frontend/components/dm_servers/dm_nav_bar/dm_nav_bar.jsx
@@ -261,7 +261,11 @@ class DmNavBar extends React.Component {
                     <div className="dm-list-header">
                         <h4>DIRECT MESSAGES</h4>
                         <div className="create-channel-div" onClick={() => this.toggleCreateDmModal()}>
-                            <i className="fa-solid fa-plus" />
+                           
+                            <svg x="0" y="0" aria-hidden="true" className= "dm-add-button" role="img" width="24" height="24" viewBox="0 0 18 18">
+                                <polygon fillRule="nonzero" fill="currentColor" points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8"></polygon>
+                            </svg>
+
                             <div className="dm-tool-tip">
                                 Create DM
                             </div>
