49013427a239b3523da445a846dc04fb1ad651e9
miker704 <miker704@gmail.com>
2022-09-08 13:33:31 -0400

    fixed modal bug

--- Strife/frontend/components/friends/friends_page_main/friends_home_page.jsx
+++ Strife/frontend/components/friends/friends_page_main/friends_home_page.jsx
@@ -167,7 +167,7 @@ class FriendsHomePageContainer extends React.Component {
 
                 <div className="clear-modal-wrapper homeBar" onClick={() => this.closeCreateDmModal()}>
                     <div onSubmit={() => this.handleSubmit()}>
-                        <CreateDmModalContainer />
+                        <CreateDmModalContainer/>
                     </div>
                 </div>
             )
@@ -213,7 +213,7 @@ class FriendsHomePageContainer extends React.Component {
                     {this.renderCreateDMModal()}
                     <div className="home-nav-tool-bar">
                         <div className="invite-tool-bar">
-                            <div className="invite-tool-bar-group-dm">
+                            <div className="invite-tool-bar-group-dm" id="itbgdm">
                                 <svg onClick={() => this.toggleCreateDmModal()} x="0" y="0" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
                                     <path fill="currentColor" fillRule="evenodd" clipRule="evenodd" d="M20.998 0V3H23.998V5H20.998V8H18.998V5H15.998V3H18.998V0H20.998ZM2.99805
                                          20V24L8.33205 20H14.998C16.102 20 16.998 19.103 16.998 18V9C16.998 7.896 16.102 7 14.998 7H1.99805C0.894047 7 -0.00195312 7.896 
