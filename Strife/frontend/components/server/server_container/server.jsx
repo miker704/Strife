db4bc507527635ff3ef12ed6a8785dd8a512f72d
miker704 <miker704@gmail.com>
2022-09-09 18:28:27 -0400

    updates

--- Strife/frontend/components/server/server_container/server.jsx
+++ Strife/frontend/components/server/server_container/server.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ChannelNavBarContainer from "../../channels/channel_nav_bar/channel_nav_bar_container";
 import ServerHeaderNavBarContainer from "../server_header_nav_bar/server_header_nav_bar_container";
 import ServerMessages from "../server_messages/server_messages";
+import ServerMembersListContainer from "../server_members/server_members_list_container";
+
 class Server extends React.Component {
     constructor (props) {
         super(props);
