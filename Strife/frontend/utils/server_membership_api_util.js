dc7251b9df667390653add0b5eda4f86ab0f677d
miker704 <miker704@gmail.com>
2022-09-10 23:56:20 -0400

    fixed sytnax error on route

--- Strife/frontend/utils/server_membership_api_util.js
+++ Strife/frontend/utils/server_membership_api_util.js
@@ -1,4 +1,4 @@
 
 
 export const createServerMembership = (membership) => $.ajax({ url: "/api/server_memberships/", method: "POST", data: { server_membership: membership } });
-export const deleteServerMembership = (servermembershipId,servermembership) => $.ajax({ url: `/api/server_membership/${servermembershipId}`, method: "DELETE", data: { server_membership: servermembership } });
+export const deleteServerMembership = (servermembershipId,servermembership) => $.ajax({ url: `/api/server_memberships/${servermembershipId}`, method: "DELETE", data: { server_membership: servermembership } });
