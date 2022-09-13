1ed6496867983f9b2d082a287b8d430f5430bab7
miker704 <miker704@gmail.com>
2022-09-13 14:21:19 -0400

    updated server update function

--- Strife/frontend/utils/server_api_util.js
+++ Strife/frontend/utils/server_api_util.js
@@ -1,14 +1,21 @@
 
 
-export const fetchServers = (userId) =>  $.ajax({ url: "/api/servers/", method: "GET", data: { user: userId } });
+export const fetchServers = (userId) => $.ajax({ url: "/api/servers/", method: "GET", data: { user: userId } });
 
-export const fetchServer = (serverId) =>  $.ajax({ url: `/api/servers/${serverId}`, method: "GET" });
+export const fetchServer = (serverId) => $.ajax({ url: `/api/servers/${serverId}`, method: "GET" });
 
-export const createServer = (server) =>  $.ajax({ url: `/api/servers/`, method: "POST", data:{server:server} });
+export const createServer = (server) => $.ajax({ url: `/api/servers/`, method: "POST", data: { server: server } });
 
 
-export const updateServer = (server) =>  $.ajax({ url: `/api/servers/${server.id}`, method: "PATCH", data:{server:server} });
+export const updateServer = (serverId, formData) =>
+    $.ajax({
+        url: `/api/servers/${serverId}`,
+        method: "PATCH",
+        data: formData,
+        contentType: false,
+        processData: false
+    });
 
-export const deleteServer = (serverId) =>  $.ajax({ url: `/api/servers/${serverId}`, method: "DELETE" });
+export const deleteServer = (serverId) => $.ajax({ url: `/api/servers/${serverId}`, method: "DELETE" });
 
-export const joinServer = (inviteCode) => $.ajax({url: `/api/servers/join`, method: "POST", data:{inviteCode}});
+export const joinServer = (inviteCode) => $.ajax({ url: `/api/servers/join`, method: "POST", data: { inviteCode } });
