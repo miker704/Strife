08a5ef93797501f3b1796e9875e83d7e326de438
miker704 <miker704@gmail.com>
2022-09-14 10:06:30 -0400

    adjusted ajax to handle files upon server create

--- Strife/frontend/utils/server_api_util.js
+++ Strife/frontend/utils/server_api_util.js
@@ -4,7 +4,20 @@ export const fetchServers = (userId) => $.ajax({ url: "/api/servers/", method: "
 
 export const fetchServer = (serverId) => $.ajax({ url: `/api/servers/${serverId}`, method: "GET" });
 
-export const createServer = (server) => $.ajax({ url: `/api/servers/`, method: "POST", data: { server: server } });
+// export const createServer = (server) => $.ajax({ url: `/api/servers/`, method: "POST", data: { server: server } });
+
+
+export const createServer = (server) => 
+$.ajax({ 
+    url: `/api/servers/`, 
+    method: "POST", 
+    data: server,
+    contentType: false,
+    processData: false
+});
+
+
+
 
 
 export const updateServer = (serverId, formData) =>
