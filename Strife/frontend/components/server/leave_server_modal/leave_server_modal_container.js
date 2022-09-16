2f7102e19fb5204f6974c6f61c4f5847ba0d19a4
miker704 <miker704@gmail.com>
2022-09-16 13:19:43 -0400

    finished modal

--- Strife/frontend/components/server/leave_server_modal/leave_server_modal_container.js
+++ Strife/frontend/components/server/leave_server_modal/leave_server_modal_container.js
@@ -1,26 +1,77 @@
-import {connect } from "react-redux";
+import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import DeleteServerModal from "./delete_server_modal";
+import { fetchChannel, deleteChannel, removeChannelErrors } from "../../../actions/channel_actions.js";
+import { fetchServer, fetchServers, removeServerErrors } from "../../../actions/server_actions.js";
+import { deleteChannelMembership } from "../../../actions/channel_membership_actions.js";
+import { deleteServerMembership } from "../../../actions/server_membership_actions.js";
+import { closeModal } from "../../../actions/modal_actions.js";
+import { handleKeyUp } from "../../../utils/modal_api_util";
+import LeaveServerModal from "./leave_server_modal.jsx";
 
-const mSTP = (state,ownProps) => {
+const extractServerProps = (state, ownProps) => {
+    let locationString = ownProps.location.pathname;
+    let newLoc = locationString.split('/channels/').join('').split('/');
+    return newLoc;
+}
+
+
+
+const mSTP = (state, ownProps) => {
+
+    const getIds = extractServerProps(state, ownProps);
+
     return {
+
+
         currentUser: state.entities.users[state.session.id],
-        servers: Object.values(state.entities.servers),
-        serverId: ownProps.match.params.serverId,
-        errors: state.errors.session,
-        // server: state.entities.servers[2]
+        server: state.entities.servers[parseInt(getIds[0])],
+        channel: state.entities.channels[parseInt(getIds[1])],
+        channels: Object.values(state.entities.channels),
+        currentChannelId: getIds[1],
+        serverId: getIds[0],
+        errors: state.errors.server,
+        channelErrors: state.errors.channel,
+        sessionErrors: state.errors.session,
+        servers: state.entities.servers
 
     }
 }
 
-const mDTP = (dispatch) => {
+
+const mDTP = (dispatch, ownProps) => {
+
+
+
     return {
-        // openModal: (modal) => dispatch(openModal(modal)),
-        // fetchServers: () => dispatch(fetchServers()),
-        // fetchServer: (serverId) => dispatch(fetchServer(serverId))
+
+        //server api functions
+        fetchServer: (serverId) => dispatch(fetchServer(serverId)),
+        fetchUserServers: (user) => dispatch(fetchServers(user)),
+        removeServerErrors: () => dispatch(removeServerErrors()),
+
+
+        //channel api functions
+
+        fetchChannel: (channelId) => { dispatch(fetchChannel(channelId)) },
+        deleteChannel: (channelId) => dispatch(deleteChannel(channelId)),
+        removeChannelErrors: () => dispatch(removeChannelErrors()),
+
+        //server membership api functions
+
+        deleteServerMembership: (servermembershipId, servermembership) =>
+            dispatch(deleteServerMembership(servermembershipId, servermembership)),
+
+        //channel membership api functions 
+        deleteChannelMembership: (channelmembershipId, channelmembership) =>
+            dispatch(deleteChannelMembership(channelmembershipId, channelmembership)),
+
+        // modal api functions
+        closeModal: () => dispatch(closeModal()),
+        handleESC: (e) => handleKeyUp(e),
+
     }
 }
 
 
-const DeleteServerModalContainer = withRouter(connect(mSTP,mDTP)(DeleteServerModal));
-export default DeleteServerModalContainer;
+const LeaveServerModalContainer = withRouter(connect(mSTP, mDTP)(LeaveServerModal));
+export default LeaveServerModalContainer;
