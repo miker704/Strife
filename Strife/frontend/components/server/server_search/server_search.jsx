0694050edbeb9ff52818039e95e683b9e619fc99
miker704 <miker704@gmail.com>
2022-09-18 05:56:06 -0400

    updates

---
+++ Strife/frontend/components/server/server_search/server_search.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useEffect, useRef, useState } from "react";
+
+
+const ExploreServers = (props) => {
+
+
+
+    return ( 
+        <div>
+            
+        </div>
+
+    )
+
+}
+
+export default ExploreServers;
