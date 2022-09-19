4a83fbb7f056c32314d30012b9a6ff3f174f4b0a
miker704 <miker704@gmail.com>
2022-09-19 07:29:05 -0400

    updates

--- Strife/frontend/components/channels/create_channel_modal/create_channel_modal_container.js
+++ Strife/frontend/components/channels/create_channel_modal/create_channel_modal_container.js
@@ -1,13 +1,10 @@
-import ServerSettingsModal from "./server_settings_modal";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import { fetchChannel, createChannel, updateChannel, deleteChannel, removeChannelErrors } from "../../../actions/channel_actions.js";
-import { fetchServer, fetchServers, removeServerErrors, deleteServer, updateServer } from "../../../actions/server_actions.js";
-import { createChannelMembership, deleteChannelMembership } from "../../../actions/channel_membership_actions.js";
-import { createServerMembership, deleteServerMembership } from "../../../actions/server_membership_actions.js";
+import { fetchChannel, createChannel, removeChannelErrors } from "../../../actions/channel_actions.js";
+import { fetchServer, fetchServers, removeServerErrors } from "../../../actions/server_actions.js";
 import { openModal, closeModal } from "../../../actions/modal_actions.js";
 import { handleKeyUp } from "../../../utils/modal_api_util";
-import { logoutUser, removeSessionErrors } from "../../../actions/session_actions";
+import CreateChannelModal from "./create_channel_modal.jsx";
 
 const extractServerProps = (state, ownProps) => {
     let locationString = ownProps.location.pathname;
@@ -16,13 +13,11 @@ const extractServerProps = (state, ownProps) => {
 }
 
 
-
 const mSTP = (state, ownProps) => {
 
     const getIds = extractServerProps(state, ownProps);
-
     return {
-    
+
 
         currentUser: state.entities.users[state.session.id],
         server: state.entities.servers[parseInt(getIds[0])],
@@ -30,8 +25,8 @@ const mSTP = (state, ownProps) => {
         channels: Object.values(state.entities.channels),
         currentChannelId: getIds[1],
         serverId: getIds[0],
-        errors: state.errors.server,
-        channelErrors: state.errors.channel,
+        errors: state.errors.channel,
+        serverErrors: state.errors.server,
         sessionErrors: state.errors.session,
         servers: state.entities.servers
 
@@ -41,41 +36,17 @@ const mSTP = (state, ownProps) => {
 
 const mDTP = (dispatch, ownProps) => {
 
-
-
     return {
 
-        //logout user
-        logoutUser: () => { dispatch(logoutUser()) },
-        removeSessionErrors: () => dispatch(removeSessionErrors()),
         //server api functions
         fetchServer: (serverId) => dispatch(fetchServer(serverId)),
         fetchUserServers: (user) => dispatch(fetchServers(user)),
         fetchServers: () => dispatch(fetchServers()),
-        updateServer: (serverId,formData) => dispatch(updateServer(serverId,formData)),
-        deleteServer: (serverId) => dispatch(deleteServer(serverId)),
         removeServerErrors: () => dispatch(removeServerErrors()),
-
-
         //channel api functions
-
         fetchChannel: (channelId) => { dispatch(fetchChannel(channelId)) },
         createChannel: (channel) => dispatch(createChannel(channel)),
-        updateChannel: (channel) => dispatch(updateChannel(channel)),
-        deleteChannel: (channelId) => dispatch(deleteChannel(channelId)),
         removeChannelErrors: () => dispatch(removeChannelErrors()),
-
-        //server membership api functions
-
-        createServerMembership: (servermembership) => dispatch(createServerMembership(servermembership)),
-        deleteServerMembership: (servermembershipId, servermembership) =>
-            dispatch(deleteServerMembership(servermembershipId, servermembership)),
-
-        //channel membership api functions 
-        createChannelMembership: (channelmembership) => dispatch(createChannelMembership(channelmembership)),
-        deleteChannelMembership: (channelmembershipId, channelmembership) =>
-            dispatch(deleteChannelMembership(channelmembershipId, channelmembership)),
-
         // modal api functions
         openModal: modal => dispatch(openModal(modal)),
         closeModal: () => dispatch(closeModal()),
@@ -85,5 +56,5 @@ const mDTP = (dispatch, ownProps) => {
 }
 
 
-const ServerSettingsModalContainer = withRouter(connect(mSTP, mDTP)(ServerSettingsModal));
-export default ServerSettingsModalContainer;
+const CreateChannelModalContainer = withRouter(connect(mSTP, mDTP)(CreateChannelModal));
+export default CreateChannelModalContainer;
