64cf250cd779877cbb2dd0a81fb573906daff6d4
miker704 <miker704@gmail.com>
2022-09-19 04:48:18 -0400

    updates

--- Strife/frontend/actions/modal_actions.js
+++ Strife/frontend/actions/modal_actions.js
@@ -1,5 +1,7 @@
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
+export const OPEN_MODAL_WITH_PROPS = 'OPEN_MOD_W/PROPS';
+
 
 export const openModal = modal => {
   return {
@@ -13,3 +15,18 @@ export const closeModal = () => {
     type: CLOSE_MODAL
   };
 };
+
+export const openModalWithProps = (modal_With_Props) => {
+  return {
+    type: OPEN_MODAL_WITH_PROPS,
+    modal_With_Props
+  };
+};
+
+// export const openModalWithProps = (modal,modal_With_Props) => {
+//   return {
+//     type: OPEN_MODAL_WITH_PROPS,
+//     modal,
+//     modal_With_Props
+//   };
+// };
