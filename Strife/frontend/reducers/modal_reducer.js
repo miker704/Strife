e11ab417bd30edf3f74c155973082fec1426a3b9
miker704 <miker704@gmail.com>
2022-09-19 04:49:11 -0400

    updates

--- Strife/frontend/reducers/modal_reducer.js
+++ Strife/frontend/reducers/modal_reducer.js
@@ -1,7 +1,12 @@
 
-import { OPEN_MODAL, CLOSE_MODAL } from '../actions/modal_actions';
+import { OPEN_MODAL, CLOSE_MODAL} from '../actions/modal_actions';
 
-export default function modalReducer(state = null, action) {
+
+
+
+
+export default function modalReducer (state = {}, action) {
+
   switch (action.type) {
     case OPEN_MODAL:
       return action.modal;
