27cbe2db58bd41d843a1fd1740ac118818d25d52
miker704 <miker704@gmail.com>
2022-09-19 04:49:19 -0400

    updates

--- Strife/frontend/reducers/ui_reducer.js
+++ Strife/frontend/reducers/ui_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import modalReducer from './modal_reducer';
+import modalPropsReducer from './modal_with_props_reducer';
 
 const uiReducer = combineReducers({
-  modal: modalReducer
+  modal: modalReducer,
+  modalProps: modalPropsReducer
 });
 
 export default uiReducer
