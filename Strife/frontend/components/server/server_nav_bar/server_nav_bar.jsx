5093527dbbf7b1e791dd85688b4cf02f5ba780ac
miker704 <miker704@gmail.com>
2022-09-21 15:10:21 -0400

    updates

--- Strife/frontend/components/server/server_nav_bar/server_nav_bar.jsx
+++ Strife/frontend/components/server/server_nav_bar/server_nav_bar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+
 class ServerNavBar extends React.Component {
     constructor (props) {
         super(props);
@@ -123,16 +124,18 @@ class ServerNavBar extends React.Component {
                     </li>
                     <li className="server-bubbles" key="serverSearch">
                         <div className="server-nav-bar-list-item">
-                            <button id="search-servers" onClick={() => this.props.openModal('serverSearch')}>
-                                <div id="fill-pill" className="pill-box"><span className="pill-box-item"></span></div>
-                                <svg aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
-                                    <path fill="currentColor" d="M12 10.9C11.39 10.9 10.9 11.39 10.9 12C10.9 12.61 11.39 13.1 12 
+                            <Link to={`/channels/guild-discovery/`}>
+                                <button id="search-servers">
+                                    <div id="fill-pill" className="pill-box"><span className="pill-box-item"></span></div>
+                                    <svg aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                        <path fill="currentColor" d="M12 10.9C11.39 10.9 10.9 11.39 10.9 12C10.9 12.61 11.39 13.1 12 
                                                                 13.1C12.61 13.1 13.1 12.61 13.1 12C13.1 11.39 12.61 10.9 12 10.9ZM12 
                                                                 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 
                                                                 2 12 2ZM14.19 14.19L6 18L9.81 9.81L18 6L14.19 14.19Z">
-                                    </path>
-                                </svg>
-                            </button>
+                                        </path>
+                                    </svg>
+                                </button>
+                            </Link>
                         </div>
                         <div className="server-nav-bar-tool-kit">Explore Public Servers</div>
                     </li>
