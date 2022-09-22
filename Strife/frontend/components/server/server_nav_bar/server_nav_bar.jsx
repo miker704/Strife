a85db8b5c727ebecf9e9843c63508939c88e151d
miker704 <miker704@gmail.com>
2022-09-22 06:24:21 -0400

    updates

--- Strife/frontend/components/server/server_nav_bar/server_nav_bar.jsx
+++ Strife/frontend/components/server/server_nav_bar/server_nav_bar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class ServerNavBar extends React.Component {
     constructor (props) {
