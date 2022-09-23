2119cce7e376c7e17e81a9818ec242d35915675b
miker704 <miker704@gmail.com>
2022-09-23 14:04:31 -0400

    rm useess code

--- Strife/frontend/reducers/session_reducer.js
+++ Strife/frontend/reducers/session_reducer.js
@@ -3,7 +3,8 @@ import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_ac
 // create a default state 
 const _nullSession = Object.freeze( {
     // currentUser: null
-    id: null
+    id: null,
+
 });
 
 const sessionReducer = (state = _nullSession ,action) => {
@@ -11,10 +12,9 @@ const sessionReducer = (state = _nullSession ,action) => {
     
     switch(action.type){
         case RECEIVE_CURRENT_USER:
-        // return Object.assign({},{currentUser: action.user});
-        // return Object.assign({},{id: action.user.id});
+
         return Object.assign({},{id: action.currentUser.id});
-        // return {id: action.user.id};
+
         case  LOGOUT_CURRENT_USER:
             return _nullSession;
             default:
