fbf7313d85eb4f9c2a90889c858dc0db05212f1b
miker704 <miker704@gmail.com>
2022-09-23 12:52:17 -0400

    updates

--- Strife/frontend/actions/server_membership_actions.js
+++ Strife/frontend/actions/server_membership_actions.js
@@ -2,6 +2,7 @@ import * as ServerMembershipAPI from "../utils/server_membership_api_util.js"
 
 export const RECEIVE_SERVER_MEMBERSHIP = "RECEIVE_SERVER_MEMBERSHIP";
 export const REMOVE_SERVER_MEMBERSHIP = "REMOVE_SERVER_MEMBERSHIP";
+// export const INVITE_TO_SERVER = "INVITE_TO_SERVER";
 
 
 export const receiveServerMembership = servermembership => {
@@ -17,8 +18,24 @@ export const removeServerMembership = (membershiphash) => {
     }
 }
 
+
+// export const inviteToServer = () => {
+//     return {
+//         type: INVITE_TO_SERVER
+//     }
+// }
+
+
+// export const createServerMembership = (servermembership) => (dispatch) => 
+// ServerMembershipAPI.createServerMembership(servermembership).then((servermembership) => {
+//     dispatch(inviteToServer());
+//     return dispatch(receiveServerMembership(servermembership));
+// })
+
+
 export const createServerMembership = (servermembership) => (dispatch) => 
-ServerMembershipAPI.createServerMembership(servermembership).then((servermembership) => dispatch(receiveServerMembership(servermembership)))
+ServerMembershipAPI.createServerMembership(servermembership).then((servermembership) => 
+dispatch(receiveServerMembership(servermembership)))
 
 
  export const deleteServerMembership = (servermembershipId,servermembership) => (dispatch) => 
