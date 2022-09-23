55e5405400c033ecfec68f16c112a917e10278d1
miker704 <miker704@gmail.com>
2022-09-23 05:13:18 -0400

    updates

--- Strife/frontend/components/server/server_search/server_search_container.js
+++ Strife/frontend/components/server/server_search/server_search_container.js
@@ -1,35 +1,19 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import { fetchChannel, removeChannelErrors } from "../../../actions/channel_actions.js";
-import { fetchServer, fetchServers, removeServerErrors, exploreServers } from "../../../actions/server_actions.js";
-import { createServerMembership} from "../../../actions/server_membership_actions.js";
-import { openModal, closeModal } from "../../../actions/modal_actions.js";
+import { fetchServer, fetchServers, removeServerErrors, exploreServers, 
+        clearUnexploredServers, joiningServer } from "../../../actions/server_actions.js";
+import { createServerMembership } from "../../../actions/server_membership_actions.js";
 import ExploreServers from "./server_search";
-
-const extractServerProps = (state, ownProps) => {
-    let locationString = ownProps.location.pathname;
-    let newLoc = locationString.split('/channels/').join('').split('/');
-    return newLoc;
-}
-
 
 
 const mSTP = (state, ownProps) => {
 
-    const getIds = extractServerProps(state, ownProps);
 
     return {
-        
 
         currentUser: state.entities.users[state.session.id],
-        server: state.entities.servers[parseInt(getIds[0])],
-        channel: state.entities.channels[parseInt(getIds[1])],
         channels: Object.values(state.entities.channels),
-        currentChannelId: getIds[1],
-        serverId: getIds[0],
         errors: state.errors.server,
-        channelErrors: state.errors.channel,
-        sessionErrors: state.errors.session,
         servers: state.entities.servers,
         unExploredServers: state.entities.unExploredServers
 
@@ -49,15 +33,11 @@ const mDTP = (dispatch, ownProps) => {
         fetchServers: () => dispatch(fetchServers()),
         exploreServers: () => dispatch(exploreServers()),
         removeServerErrors: () => dispatch(removeServerErrors()),
-        //channel api functions
-        fetchChannel: (channelId) => { dispatch(fetchChannel(channelId)) },
-        removeChannelErrors: () => dispatch(removeChannelErrors()),
         //server membership api functions
         createServerMembership: (servermembership) => dispatch(createServerMembership(servermembership)),
         // modal api functions
-        openModal: modal => dispatch(openModal(modal)),
-        closeModal: () => dispatch(closeModal()),
-
+        clearUnexploredServers: () => dispatch(clearUnexploredServers()),
+        joiningServer: (inviteCode) => dispatch(joiningServer(inviteCode))
     }
 }
 
