09145daffabbf99fe13e41ce250444ef58d3ee4c
miker704 <miker704@gmail.com>
2022-09-24 10:03:47 -0400

    uypdates

--- Strife/frontend/strife.jsx
+++ Strife/frontend/strife.jsx
@@ -5,29 +5,33 @@ import configureStore from "./store/store.js"
 // import {grabLiveUsers} from "../frontend/reducers/friendship_reducer"
 
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
 	let store;
 	let preloadedState = undefined;
 	if (window.currentUser) {
-	  preloadedState = {
-		entities:{
-			users: {[window.currentUser.id]: window.currentUser}
-		},
-		session: {
-		//   currentUser: window.currentUser
-		id: window.currentUser.id
-		}
-	  };
-	  store = configureStore(preloadedState);
-	  delete window.currentUser;
+		preloadedState = {
+			entities: {
+				users: { [window.currentUser.id]: window.currentUser },
+
+			},
+			session: {
+				// currentUser: window.currentUser,
+				id: window.currentUser.id,
+
+			},
+
+			currentUser: window.currentUser
+		};
+		store = configureStore(preloadedState);
+		delete window.currentUser;
 	}
-	else{
+	else {
 		store = configureStore();
 	}
 	// store.dispatch(grabLiveUsers);
 	window.getState = store.getState;
 	window.dispatch = store.dispatch;
-	
+
 	// store = configureStore(preloadedState);
-	ReactDOM.render(<Root store={store}/>,document.getElementById('root'))
+	ReactDOM.render(<Root store={store} />, document.getElementById('root'))
 })
