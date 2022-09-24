38d8c1e46ad7ecac985c1b524f7c1350be88e0f2
miker704 <miker704@gmail.com>
2022-09-24 10:04:54 -0400

    updates

--- Strife/app/models/server_membership.rb
+++ Strife/app/models/server_membership.rb
@@ -20,7 +20,6 @@ class ServerMembership < ApplicationRecord
     validates :user_id, uniqueness: {scope: :server_id}
     belongs_to :server, class_name: "Server", foreign_key: "server_id"
     belongs_to :user, class_name: "User", foreign_key: "user_id"
-
     before_destroy :destroy_Channel_MemberShips
 
     def destroy_Channel_MemberShips
@@ -28,7 +27,7 @@ class ServerMembership < ApplicationRecord
             if @user
                 @server = Server.find(self.server_id)
                  @server.channels.each do |channel|
-                     @found = channel.channel_members.find_by(channel_id: channel.id, receiver_id:self.user_id)
+                     @found = channel.channel_members.find_by(channel_id: channel.id, receiver_id: self.user_id)
                      if @found
                         ChannelMembership.find(@found.id).destroy
                      end
