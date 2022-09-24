1fd6a1c9d2f8dbff6cd54cc099499e8384ca069f
miker704 <miker704@gmail.com>
2022-09-24 10:06:13 -0400

    updates

--- Strife/frontend/reducers/root_reducer.js
+++ Strife/frontend/reducers/root_reducer.js
@@ -3,10 +3,12 @@ import sessionReducer from "./session_reducer";
 import EntitiesReducer from "./entities_reducer";
 import errorsReducer from "./errors_reducer";
 import uiReducer from "./ui_reducer.js";
+import currentUserReducer from "./current_user_reducer";
 
 const rootReducer = combineReducers({
     entities: EntitiesReducer,
     session: sessionReducer,
+    currentUser: currentUserReducer,
     errors: errorsReducer,
     ui: uiReducer
 })
