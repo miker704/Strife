0e6f50143d9a681d0c7fb9fa4e70f4f061144b2c
miker704 <miker704@gmail.com>
2022-09-24 10:04:20 -0400

    updates

--- Strife/app/channels/dm_channel.rb
+++ Strife/app/channels/dm_channel.rb
@@ -3,9 +3,17 @@ class DmChannel < ApplicationCable::Channel
     # stream_from "some_channel"
     @dm_server = DmServer.find_by(id: params[:id])
     stream_for @dm_server
+
+    # self.class.broadcast_to @dm_server,
+    # type: 'RECEIVE_USER',
+    # user: current_user.slice(:id, :username, :photo, :strife_id_tag, :color_tag)
+
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    # self.class.broadcast_to @dm_server, 
+    #   type: 'REMOVE_USER',
+    #   id: current_user.id
   end
 end
