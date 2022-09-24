fe7ba7ab6eecc041ebf31e728f57f75f9703139b
miker704 <miker704@gmail.com>
2022-09-24 10:05:35 -0400

    updates

---
+++ Strife/frontend/components/server/server_security/protect_server_container.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ServerContainer from "../server_container/server_container";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
+import { Route, Redirect } from "react-router";
+
+const mSTP = (state,ownProps) => {
+    return {
+        isMember: state.entities.users[state.session.id].serversJoined.includes(parseInt(ownProps.match.params.serverId))
+    }
+}
+
+const PROTECTED_SERVER = ({ isMember, path, exact }) => {
+    if (isMember === false || isMember === undefined) { console.warn('UNAUTHORIZED ACCESS ERROR : 401 -> REDIRECTING ...'); /*return null;*/ }
+    return (
+        <Route
+            path={path}
+            exact={exact}
+            render={props => (
+                isMember ? <ServerContainer {...props} /> : <Redirect to={'/$TR!F3-INTRUSION-PREVENTION/'} />
+            )} />
+    )
+}
+
+
+const PROTECTED_SERVER_CONTAINER = withRouter(connect(mSTP)(PROTECTED_SERVER));
+export default PROTECTED_SERVER_CONTAINER; 
