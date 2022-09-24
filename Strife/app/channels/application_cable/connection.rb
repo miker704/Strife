90fe7b86462cc153837ac9a3db5284741168a63a
miker704 <miker704@gmail.com>
2022-09-24 10:04:15 -0400

    updates

--- Strife/app/channels/application_cable/connection.rb
+++ Strife/app/channels/application_cable/connection.rb
@@ -1,8 +1,13 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
+    identified_by :current_user, :DmServer
     def current_user
       @current_user ||= User.find_by(session_token: request.session[:session_token])
     end
+
+    def DmServer
+      @dm_server ||= []
+    end
+
   end
 end
