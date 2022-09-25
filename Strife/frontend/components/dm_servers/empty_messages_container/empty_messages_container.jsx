55aa12cde590aa2bcadf5e8061119b4d52bf4534
miker704 <miker704@gmail.com>
2022-09-25 18:55:13 -0400

    moved

---
+++ Strife/frontend/components/dm_servers/empty_messages_container/empty_messages_container.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class EmptyMessagesContainer extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+            return (
+                <div className="empty-messages-container"></div>
+            )
+    }
+}
+
+export default EmptyMessagesContainer;
