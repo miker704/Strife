7b0f087102f47b614f16bbb6403b0419736a8922
miker704 <miker704@gmail.com>
2022-09-25 18:57:30 -0400

    animation update

--- Strife/frontend/components/loading_screen/loading_screen.jsx
+++ Strife/frontend/components/loading_screen/loading_screen.jsx
@@ -19,8 +19,14 @@ class LoadingScreen extends React.Component {
     render () {
         return (
             <div className="loading-screen-wrapper">
-                <img className="loading-screen-img" alt="loadingimg" />
-                <h1>{this.loadingMessage}</h1>
+                <div className="circle-wrap">
+                    <img className="loading-screen-img" alt="loadingimg" />
+                    <div className="shiny-button-container">
+                         <div className="shiny-button-flex">
+                         </div>
+                     </div>
+                </div>
+                <h1 className="loading-screen-img-h2">{this.loadingMessage}</h1>
             </div>
         )
     }
