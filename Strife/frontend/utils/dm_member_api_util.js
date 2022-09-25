b9977757d1d92ba76ff1f995618ce8ed846769af
miker704 <miker704@gmail.com>
2022-09-25 07:59:39 -0400

    updates

--- Strife/frontend/utils/dm_member_api_util.js
+++ Strife/frontend/utils/dm_member_api_util.js
@@ -6,3 +6,6 @@ $.ajax({ url: "/api/dm_members/", method: "POST", data: { dm_member: dm_member }
 
 export const deleteDmMember = (dm_memberId,dm_member) => 
 $.ajax({ url: `/api/dm_members/${dm_memberId}`, method: "DELETE", data: { dm_member: dm_member } });
+
+export const fetchDmMemberShipStatus = (dm_member) => 
+$.ajax({ url: "/api/dm_members/dms_status", method: "GET", data: { dm_member: dm_member } });
