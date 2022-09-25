6d6a55665040aa49e42e9063ba5238a7be00d2a2
miker704 <miker704@gmail.com>
2022-09-25 07:58:20 -0400

    updatews

--- Strife/frontend/actions/dm_member_actions.js
+++ Strife/frontend/actions/dm_member_actions.js
@@ -3,25 +3,35 @@ import * as DmMemberAPI from "../utils/dm_member_api_util.js";
 
 export const RECEIVE_DM_MEMBER = "RECEIVE_DM_MEMBER";
 export const REMOVE_DM_MEMBER = "REMOVE_DM_MEMBER";
+export const RECEIVE_DM_MEMBERSHIP_STATUS = "RECEIVE_DM_MEMBERSHIP_STATUS";
 
 
 export const receiveDmMember = (dm_member) => {
-  return{
-    type: RECEIVE_DM_MEMBER, 
+  return {
+    type: RECEIVE_DM_MEMBER,
     dm_member
   }
 }
 
 export const removeDmMember = (dm_member_hash) => {
-  return{
-    type: REMOVE_DM_MEMBER, 
+  return {
+    type: REMOVE_DM_MEMBER,
     dm_member_hash
   }
 }
 
-export const createDmMember = (dm_member) => (dispatch) => 
-DmMemberAPI.createDmMember(dm_member).then((dm_member) => dispatch(receiveDmMember(dm_member)))
+export const fetchDmMSStatus = (dm_member) => {
+  return {
+    type: RECEIVE_DM_MEMBERSHIP_STATUS,
+    dm_member
+  }
+}
 
-export const deleteDmMember = (dm_memberId, dm_member) => (dispatch) => 
-DmMemberAPI.deleteDmMember(dm_memberId,dm_member).then(( dm_member_hash) => dispatch(removeDmMember( dm_member_hash)))
- 
+export const createDmMember = (dm_member) => (dispatch) =>
+  DmMemberAPI.createDmMember(dm_member).then((dm_member) => dispatch(receiveDmMember(dm_member)))
+
+export const deleteDmMember = (dm_memberId, dm_member) => (dispatch) =>
+  DmMemberAPI.deleteDmMember(dm_memberId, dm_member).then((dm_member_hash) => dispatch(removeDmMember(dm_member_hash)))
+
+export const fetchDmMemberShipStatus = (dm_member) => (dispatch) =>
+  DmMemberAPI.fetchDmMemberShipStatus(dm_member).then(dm_member => dispatch(fetchDmMSStatus(dm_member)));
