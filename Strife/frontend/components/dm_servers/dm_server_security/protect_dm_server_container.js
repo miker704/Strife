8759d3e7636c0663ba7fd443e2cfd5dcbb02fd5f
miker704 <miker704@gmail.com>
2022-09-25 18:57:40 -0400

    dm security updates

--- Strife/frontend/components/dm_servers/dm_server_security/protect_dm_server_container.js
+++ Strife/frontend/components/dm_servers/dm_server_security/protect_dm_server_container.js
@@ -6,14 +6,11 @@ import DmServerContainer from "../dm_server_container/dm_server_container";
 const mSTP = (state, ownProps) => {
 
     return {
-        // isMember: state.entities.users[state.session.id].dmServersJoined.includes(parseInt(ownProps.match.params.dmServerId))
         isMember: state.currentUser.dmServersJoined.includes(parseInt(ownProps.match.params.dmServerId))
-
     }
 }
 
 const PROTECTED_DM_SERVER = ({isMember, path, exact }) => {
-    console.log("inside dm Server protected route");
     if (isMember === false || isMember === undefined ||isMember === null ) { 
         console.warn('UNAUTHORIZED ACCESS ERROR : 401 -> REDIRECTING ...');
         /*return null;*/ }
