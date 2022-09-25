fa19424f6c38fdb16841f5564f81782d5f3c2bad
miker704 <miker704@gmail.com>
2022-09-24 21:34:53 -0400

    updates

--- Strife/frontend/components/loading_screen/instrusion_loading_screen_container.js
+++ Strife/frontend/components/loading_screen/instrusion_loading_screen_container.js
@@ -1,23 +1,28 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
+import { fetchDmServers } from "../../actions/dm_server_actions";
+import { fetchServers } from "../../actions/server_actions";
+import { fetchUser } from "../../actions/session_actions";
 import IntrusionPreventionLoadingScreen from "./instrusion_loading_screen";
 
 
 const mSTP = (state) => {
     return {
-        currentUser: state.entities.users[state.session.id],
-        selectedLoadingMsg:"$TR!F3 Intrusion Prevention - Warning You are Not Authorized to access this Server!",
+        currentUserId: state.session.id,
+        selectedLoadingMsg: "$TR!F3 Intrusion Prevention - Warning You are Not Authorized to access this Server!",
     }
 }
 
 
 const mDTP = (dispatch) => {
     return {
-
+        fetchUsersServers: (currentUserId) => dispatch(fetchServers(currentUserId)),
+        fetchUsersDmServers: (currentUserId) => dispatch(fetchDmServers(currentUserId)),
+        fetchUser: (currentUserId) => dispatch(fetchUser(currentUserId))
     }
 }
 
 
 
-const IntrusionPreventionLoadingScreenContainer = withRouter(connect(mSTP,mDTP)(IntrusionPreventionLoadingScreen));
+const IntrusionPreventionLoadingScreenContainer = withRouter(connect(mSTP, mDTP)(IntrusionPreventionLoadingScreen));
 export default IntrusionPreventionLoadingScreenContainer;
