88ae30d66d34d90790254af5e151e130ef85b84f
miker704 <miker704@gmail.com>
2022-09-27 11:11:00 -0400

    updates

--- Strife/app/controllers/api/server_memberships_controller.rb
+++ Strife/app/controllers/api/server_memberships_controller.rb
@@ -12,6 +12,7 @@ class Api::ServerMembershipsController < ApplicationController
                     channel_id: channel.id, 
                     receiver_id: @server_Membership.user_id
                 )
+                async_welcome_new_member(@server,@server_Membership,channel)
             end
           render "api/servers/show"
         else
@@ -19,11 +20,29 @@ class Api::ServerMembershipsController < ApplicationController
         end
     end
     
+
+    def async_welcome_new_member(server, new_server_member, channel)
+        @new_Server_Member = User.find_by(id: new_server_member.user_id)
+        @response_Message = "Welcome @#{@new_Server_Member.username} to #{server.server_name}!"
+        @message=Message.create!(body: @response_Message, author_id: 1, channel_id: channel.id)
+        ServerChannel.broadcast_to(channel, message: @message, head: 100)
+    end
+
+    def async_ban_member_from_Server(server, banned_user)
+        @response_Message = "@#{banned_user.username} has been banned from the server"
+        server.channels.each do |channel|
+            @message=Message.create!(body: @response_Message, author_id: 1, channel_id: channel.id)
+            ServerChannel.broadcast_to(channel, message: @message, head: 101, path: '/$TR!F3-INTRUSION-PREVENTION/')
+        end
+    end
+
     def destroy
         @server_Membership = ServerMembership.find_by(server_membership_params)
         @server = Server.find_by(id: @server_Membership.server_id)
         @user = User.find_by(id: @server_Membership.user_id)
         if @server_Membership
+
+            async_ban_member_from_Server(@server, @user)
             @server_Membership.destroy
             render 'api/servers/show'
             # render 'api/users/show'
