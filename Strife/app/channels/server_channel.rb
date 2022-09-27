fc4edad5da0a1f1a5b754862499dccd77e6884c3
miker704 <miker704@gmail.com>
2022-09-27 11:10:53 -0400

    updates

--- Strife/app/channels/server_channel.rb
+++ Strife/app/channels/server_channel.rb
@@ -2,11 +2,18 @@ class ServerChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     @server_channel = Channel.find_by(id: params[:id])
+    serverChannelsStreaming << @server_channel
     stream_for @server_channel
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    serverChannelsStreaming.delete(@server_channel)
+
+  end
+
+  def find
+    return serverChannelsStreaming[0]
   end
 
  
