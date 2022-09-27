02a161122c675da12c429aa11b5771bd64a1e259
miker704 <miker704@gmail.com>
2022-09-27 11:10:56 -0400

    updates

--- Strife/app/controllers/api/dm_members_controller.rb
+++ Strife/app/controllers/api/dm_members_controller.rb
@@ -5,6 +5,10 @@ class Api::DmMembersController < ApplicationController
         @dm_server = DmServer.find_by(id: @dm_membership.dm_server_id)
         
         if @dm_membership.save
+            if @dm_server.members.length >= 2
+                async_welcome_new_member(@dm_server,@dm_membership)
+            end
+            
           render 'api/dm_servers/show'
         else
           render json: @dm_membership.errors.full_messages, status: 400
@@ -21,17 +25,30 @@ class Api::DmMembersController < ApplicationController
         end
     end
 
+    def async_welcome_new_member(dmServer, new_dm_member)
+        @new_Dm_Member = User.find_by(id: new_dm_member.dm_member_id)
+        @response_Message = "Welcome @#{@new_Dm_Member.username} to the chat!"
+        @dm_message_via_bot = DmMessage.create!(body: @response_Message, sender_id: 1, dm_server_id: @dm_server.id)
+        DmChannel.broadcast_to(dmServer, head:101 )
+    end
 
+    def async_redirect_Kick_User(dmServer,dmMember_kicked)
+        @user_kicked = User.find_by(id: dmMember_kicked.dm_member_id)
+        @dms_owner = User.find_by(id: dmServer.owner_id)
+        @response_Message = "@#{@dms_owner.username} has kicked @#{@user_kicked.username}"
+        @dm_message_via_bot = DmMessage.create!(body: @response_Message, sender_id: 1, dm_server_id: @dm_server.id)
+        DmChannel.broadcast_to(dmServer, head:401, path: '/$TR!F3-INTRUSION-PREVENTION/' )
+    end
 
     def destroy
         @dm_membership = DmMember.find_by(dm_member_params)
     
-        # @dm_membership = DmMember.find_by(id: params[:id])
         @dm_server = DmServer.find_by(id: @dm_membership.dm_server_id)
-        # if @dm_membership.dm_member_id == current_user.id
-        #     @dm_membership.destroy
-        #     render 'api/dm_servers/show'
         if @dm_membership
+
+            async_redirect_Kick_User(@dm_server,@dm_membership)
+
+            #before destroy membership invoke redirect to home
             @dm_membership.destroy
             render 'api/dm_servers/show'
         else
