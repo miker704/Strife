38d8a258e6b99cc4daab26c595ced8f9374d87cf
miker704 <miker704@gmail.com>
2022-09-29 08:11:18 -0400

    async functions

--- Strife/app/controllers/api/channels_controller.rb
+++ Strife/app/controllers/api/channels_controller.rb
@@ -1,18 +1,17 @@
 class Api::ChannelsController < ApplicationController
 
-
-
-
     def show
         @channel = Channel.find_by(id: params[:id])
         render :show
     end
-  
-
 
     def create
         @channel = Channel.new(channel_params)
         if @channel.save
+            @server = Server.find_by(id: @channel.server_id)
+            @server.channels.each do |channel|
+                ServerChannel.broadcast_to(channel, head: 201, type: 'NewChannel')
+            end
           render :show
         else
             render json: @channel.errors.full_messages, status: 400
@@ -24,6 +23,10 @@ class Api::ChannelsController < ApplicationController
         if @channel.update(channel_params)
           @default_channel_message = @channel.messages.first
           @default_channel_message.update(body: "Welcome to ##{@channel.channel_name} channel !")
+          @server = Server.find_by(id: @channel.server_id)
+            @server.channels.each do |channel|
+                ServerChannel.broadcast_to(channel, head: 202, type: 'UpdateChannel')
+            end
           render :show
         else
             render json: @channel.errors.full_messages, status: 400
@@ -32,7 +35,13 @@ class Api::ChannelsController < ApplicationController
 
     def destroy
         @channel = Channel.find_by(id: params[:id])
-        if @channel.destroy
+        @server = Server.find_by(id: @channel.server_id)
+        if @channel
+            @server.channels.each do |channel|
+                ServerChannel.broadcast_to(channel, head: 302, type: 'DeleteChannel',  channel: @channel)
+            end
+        #   sleep(2)
+          @channel.destroy
           render :delete
         else
             render json: @channel.errors.full_messages, status: 400
