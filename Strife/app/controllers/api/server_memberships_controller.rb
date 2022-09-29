41f73da50edb5bede6aa0090c88007fb09bec77a
miker704 <miker704@gmail.com>
2022-09-29 08:11:27 -0400

    async functions

--- Strife/app/controllers/api/server_memberships_controller.rb
+++ Strife/app/controllers/api/server_memberships_controller.rb
@@ -25,27 +25,36 @@ class Api::ServerMembershipsController < ApplicationController
         @new_Server_Member = User.find_by(id: new_server_member.user_id)
         @response_Message = "Welcome @#{@new_Server_Member.username} to #{server.server_name}!"
         @message=Message.create!(body: @response_Message, author_id: 1, channel_id: channel.id)
-        ServerChannel.broadcast_to(channel, message: @message, head: 100)
+        ServerChannel.broadcast_to(channel,head: 100)
     end
 
-    def async_ban_member_from_Server(server, banned_user)
-        @response_Message = "@#{banned_user.username} has been banned from the server"
+    def async_ban_member_from_Server(server, banned_user, banned)
+        @response_Message = ''
+        if banned == '1'
+            @response_Message = "@#{banned_user.username} has been banned from the server"
+        elsif banned == '0'
+            @response_Message = "@#{banned_user.username} has left the server"
+        end
         server.channels.each do |channel|
             @message=Message.create!(body: @response_Message, author_id: 1, channel_id: channel.id)
-            ServerChannel.broadcast_to(channel, message: @message, head: 101, path: '/$TR!F3-INTRUSION-PREVENTION/')
+            # ServerChannel.broadcast_to(channel, message: @message, head: 101, path: '/$TR!F3-INTRUSION-PREVENTION/', banned: banned, bannedUser: banned_user)
+            ServerChannel.broadcast_to(channel, head: 101, path: '/$TR!F3-INTRUSION-PREVENTION/', banned: banned, bannedUser: banned_user.id)
+
         end
     end
 
     def destroy
+        @extract_params = params[:server_membership]
+        @is_banned = @extract_params[:banned]
+
         @server_Membership = ServerMembership.find_by(server_membership_params)
         @server = Server.find_by(id: @server_Membership.server_id)
         @user = User.find_by(id: @server_Membership.user_id)
         if @server_Membership
 
-            async_ban_member_from_Server(@server, @user)
+            async_ban_member_from_Server(@server, @user, @is_banned)
             @server_Membership.destroy
             render 'api/servers/show'
-            # render 'api/users/show'
         else
             render json:  @server_Membership.errors.full_messages, status: 401
         end
