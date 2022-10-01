415c8ecfe1f9c296e9a02570bf47f48e3efee13d
miker704 <miker704@gmail.com>
2022-10-01 02:22:56 -0400

    updates

--- frontend/components/friends/friends_list_online/friends_list_online_container.js
+++ frontend/components/friends/friends_list_online/friends_list_online_container.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import FriendShipIndexOnline from './friends_list_online';
-import { selectFriendStatusOnline, selectOnlineFriends } from '../../../utils/selectors_api_util';
+import { selectFriendStatusOnline} from '../../../utils/selectors_api_util';
 import { requestFriendships, removeFriendshipErrors, requestAllFriendships} from '../../../actions/friendship_actions';
 import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
 import { openModal } from '../../../actions/modal_actions';
@@ -12,7 +12,6 @@ const mSTP = (state) => {
         currentUser: state.entities.users[state.session.id],
         currentUserId: state.session.id,
         friends: selectFriendStatusOnline(state, 3),
-        // friends: selectOnlineFriends(state, 3),
         errors: state.errors.friendship,
         dmServerErrors: state.errors.dmServer,
         dmServers: Object.values(state.entities.dmServers),
