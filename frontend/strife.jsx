adb1099747bc925fc8dd9e6b6373ab6394d1a1d8
miker704 <miker704@gmail.com>
2022-10-03 07:45:40 -0400

    async state

--- frontend/strife.jsx
+++ frontend/strife.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/root.jsx"
 import configureStore from "./store/store.js"
-
+import throttle from "lodash.throttle";
+import {saveMyState} from './utils/state_storage_async_util.js';
 
 document.addEventListener("DOMContentLoaded", () => {
 	let store;
@@ -24,6 +25,21 @@ document.addEventListener("DOMContentLoaded", () => {
 	else {
 		store = configureStore();
 	}
+
+	//atttempt to perform an async state save cmmt out as it is not used/ no current need for it yet
+	// store.subscribe(throttle(()=>{
+	// 	console.log("Save my state is syncing up");
+	// 	saveMyState({
+	// 		entities: store.getState().entities,
+	// 		session: store.getState().session,
+	// 		currentUser: store.getState().currentUser
+	// 	});
+	// }, 1000));
+
+
+
+
+
 	window.getState = store.getState;
 	window.dispatch = store.dispatch;
 	ReactDOM.render(<Root store={store} />, document.getElementById('root'))
