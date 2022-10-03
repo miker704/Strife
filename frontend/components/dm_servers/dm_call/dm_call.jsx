038fb96491868d48e1f20991817a187d9d3e0d15
miker704 <miker704@gmail.com>
2022-10-03 07:49:11 -0400

    rm clg

--- frontend/components/dm_servers/dm_call/dm_call.jsx
+++ frontend/components/dm_servers/dm_call/dm_call.jsx
@@ -27,9 +27,6 @@ const WEBRTCDMCallModal = ({
 
 }) => {
 
-    console.log("dmServer: ", dmServer);
-    console.log("dmServermembers4call: ", dmMembersForCall);
-    // console.log()
     // mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button'));
 
     const configuration = {
@@ -64,7 +61,6 @@ const WEBRTCDMCallModal = ({
         document.querySelector('#createBtn').disabled = true;
         document.querySelector('#joinBtn').disabled = true;
 
-        console.log('Create PeerConnection with configuration: ', configuration);
         peerConnection = new RTCPeerConnection(configuration);
 
         registerPeerConnectionListeners();
@@ -86,9 +82,7 @@ const WEBRTCDMCallModal = ({
         // Code for collecting ICE candidates above
 
         peerConnection.addEventListener('track', event => {
-            console.log('Got remote track:', event.streams[0]);
             event.streams[0].getTracks().forEach(track => {
-                console.log('Add a track to the remoteStream:', track);
                 remoteStream.addTrack(track);
             });
         });
@@ -108,7 +102,6 @@ const WEBRTCDMCallModal = ({
         document.querySelector('#confirmJoinBtn').
             addEventListener('click', async () => {
                 roomId = document.querySelector('#room-id').value;
-                console.log('Join room: ', roomId);
                 document.querySelector(
                     '#currentRoom').innerText = `Current room is ${roomId} - You are the callee!`;
                 await joinRoomById(roomId);
@@ -120,10 +113,8 @@ const WEBRTCDMCallModal = ({
         const db = firebase.firestore();
         const roomRef = db.collection('rooms').doc(`${roomId}`);
         const roomSnapshot = await roomRef.get();
-        console.log('Got room:', roomSnapshot.exists);
 
         if (roomSnapshot.exists) {
-            console.log('Create PeerConnection with configuration: ', configuration);
             peerConnection = new RTCPeerConnection(configuration);
             registerPeerConnectionListeners();
             localStream.getTracks().forEach(track => {
@@ -134,9 +125,7 @@ const WEBRTCDMCallModal = ({
 
             // Code for collecting ICE candidates above
             peerConnection.addEventListener('track', event => {
-                console.log('Got remote track:', event.streams[0]);
                 event.streams[0].getTracks().forEach(track => {
-                    console.log('Add a track to the remoteStream:', track);
                     remoteStream.addTrack(track);
                 });
             });
@@ -159,7 +148,6 @@ const WEBRTCDMCallModal = ({
         remoteStream = new MediaStream();
         document.querySelector('#remoteVideo').srcObject = remoteStream;
 
-        console.log('Stream:', document.querySelector('#localVideo').srcObject);
         document.querySelector('#cameraBtn').disabled = true;
         document.querySelector('#joinBtn').disabled = false;
         document.querySelector('#createBtn').disabled = false;
@@ -208,21 +196,16 @@ const WEBRTCDMCallModal = ({
 
     function registerPeerConnectionListeners () {
         peerConnection.addEventListener('icegatheringstatechange', () => {
-            console.log(
-                `ICE gathering state changed: ${peerConnection.iceGatheringState}`);
+       
         });
 
         peerConnection.addEventListener('connectionstatechange', () => {
-            console.log(`Connection state change: ${peerConnection.connectionState}`);
         });
 
         peerConnection.addEventListener('signalingstatechange', () => {
-            console.log(`Signaling state change: ${peerConnection.signalingState}`);
         });
 
         peerConnection.addEventListener('iceconnectionstatechange ', () => {
-            console.log(
-                `ICE connection state change: ${peerConnection.iceConnectionState}`);
         });
     }
 
