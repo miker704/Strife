60dfa42df1cf6f7b78075f4f2d861962292e33a5
miker704 <miker704@gmail.com>
2022-10-03 09:06:11 -0400

    timezone adjust might break evrything well see

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  config.time_zone = 'Eastern Time (US & Canada)'
+  config.active_record.default_timezone = :local
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::FileUpdateChecker
