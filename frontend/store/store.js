81e152472353014f6146d345e0007d606f48811f
miker704 <miker704@gmail.com>
2022-10-03 07:45:49 -0400

    async store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware} from "redux";
 import thunk from 'redux-thunk';
 import rootReducer from "../reducers/root_reducer";
 // import logger from "redux-logger";
+import { loadMyState } from "../utils/state_storage_async_util";
 import {composeWithDevTools} from "@redux-devtools/extension";
 
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -19,6 +20,12 @@ if (process.env.NODE_ENV !== "production") {
 const configureStore = (preLoadedState = {}) => {
     // return createStore(rootReducer, preLoadedState, composeEnhancers(applyMiddleware(thunk, logger)));
     // return createStore(rootReducer, preLoadedState, composeWithDevTools(applyMiddleware(thunk, logger)));
+   // custom mimic async state
+    // const existingState = loadMyState();
+    // const finalState = Object.assign({}, preLoadedState, existingState);
+    // return createStore(rootReducer, finalState, composeWithDevTools(applyMiddleware(...middleWares)));
+    
+    
     return createStore(rootReducer, preLoadedState, composeWithDevTools(applyMiddleware(...middleWares)));
 
 
