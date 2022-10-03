481d678bc95a352d772f335f30de85bd3d0eca1a
miker704 <miker704@gmail.com>
2022-10-03 09:06:14 -0400

    timezone adjust might break evrything well see

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,8 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-
+  config.time_zone = 'Eastern Time (US & Canada)'
+  config.active_record.default_timezone = :local
   # Store uploaded files on the local file system (see config/storage.yml for options)
   # config.active_storage.service = :local
   config.active_storage.service = :amazon_prod
