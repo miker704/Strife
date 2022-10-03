7fb3b1a28b1b7b50bf3ec47b969b684355ba73af
miker704 <miker704@gmail.com>
2022-10-03 07:42:01 -0400

    webrtc begining intergration

---
+++ frontend/actions/video_and_voice_calls_actions.js
@@ -0,0 +1,8 @@
+export const JOIN_CALL = "JOIN_CALL";
+export const EXCHANGE = "EXCHANGE";
+export const LEAVE_CALL = "LEAVE_CALL";
+export const ice = { iceServers: [
+   { 
+      urls: "stun:stun2.l.google.com:19302" 
+   }
+]};
