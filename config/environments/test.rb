2d85933ae2a6c7456c55f2f7505a0d4415964c95
miker704 <miker704@gmail.com>
2022-10-03 09:06:19 -0400

    timezone adjust might break evrything well see

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,7 +32,8 @@ Rails.application.configure do
   config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
-
+  config.time_zone = 'Eastern Time (US & Canada)'
+  config.active_record.default_timezone = :local
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
