11de93762b1ecd3ea538cbbc327f1b1845edc14c
miker704 <miker704@gmail.com>
2022-10-06 17:51:50 -0400

    filled out activity list

--- frontend/components/friends/friends_page_main/friends_home_page_container.js
+++ frontend/components/friends/friends_page_main/friends_home_page_container.js
@@ -6,12 +6,14 @@ import FriendsHomePageContainer from './friends_home_page';
 import { reSyncCurrentUser } from '../../../actions/session_actions';
 import { fetchDmServers } from '../../../actions/dm_server_actions';
 import { fetchServers } from '../../../actions/server_actions';
+import { selectFriendStatusOnline } from '../../../utils/selectors_api_util';
 
 const mSTP = (state) => {
     return {
         currentUser: state.entities.users[state.session.id],
         currentUserId: state.session.id,
         errors: state.errors.friendship,
+        onlineFriends: selectFriendStatusOnline(state, 3),
         dmServerErrors: state.errors.dmServer
     }
 }
