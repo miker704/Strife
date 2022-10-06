45585e27f7c9ca9141c7a92022699021b0f47e90
miker704 <miker704@gmail.com>
2022-10-06 17:51:58 -0400

    filled out activity list

--- frontend/components/friends/friends_page_main/friends_home_page.jsx
+++ frontend/components/friends/friends_page_main/friends_home_page.jsx
@@ -5,6 +5,7 @@ import BlockedListContainer from "../blocked_list/blocked_list_container.js";
 import PendingFriendListContainer from "../pending_list/pending_friends_list_container.js";
 import AddFriendsContainer from "../add_friends/add_friends_container.js";
 import CreateDmModalContainer from "../../dm_servers/create_new_dm/create_dm_container.js";
+import default_User_PFP from "../../../../app/assets/images/discord_PFP.svg";
 
 class FriendsHomePageContainer extends React.Component {
     constructor (props) {
@@ -19,8 +20,9 @@ class FriendsHomePageContainer extends React.Component {
             createDmModal: false,
             showPopUp: false
 
-
         }
+
+
         this.handleClick = this.handleClick.bind(this);
         this.renderAllFriendShips = this.renderAllFriendShips.bind(this);
         this.renderAllFriendShipsOnline = this.renderAllFriendShipsOnline.bind(this);
@@ -35,6 +37,7 @@ class FriendsHomePageContainer extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleESC = this.handleESC.bind(this);
         this.setShowPopUp = this.setShowPopUp.bind(this);
+        this.randomActivityMessage = this.randomActivityMessage.bind(this);
 
 
 
@@ -136,7 +139,7 @@ class FriendsHomePageContainer extends React.Component {
     renderAllFriendShips () {
         if (this.state.All === true) {
             return (
-                <FriendShipIndexContainer/>
+                <FriendShipIndexContainer />
             )
         }
     }
@@ -180,7 +183,7 @@ class FriendsHomePageContainer extends React.Component {
 
                 <div className="clear-modal-wrapper homeBar" onClick={() => this.closeCreateDmModal()}>
                     <div onSubmit={() => this.handleSubmit()}>
-                        <CreateDmModalContainer/>
+                        <CreateDmModalContainer />
                     </div>
                 </div>
             )
@@ -188,10 +191,91 @@ class FriendsHomePageContainer extends React.Component {
     }
 
 
+    randomActivityMessage (user) {
+
+        const _AUTH_IDS = [1, 2, 3, 4]
+
+        const randomActivityMessages = [
+            'Playing Games.', 'Studying.', 'Listening to Spotify.', 'Playing Centipede.js.', 'Playing BlueStacks 5.0.', 'Playing Fortnite.',
+            'In a Group Call.', 'Eating Food.', 'Playing Paint-By-Numbers.', 'Playing Doom.', 'Playing Quake.', 'Playing BattleField.',
+            'Studying For an Upcoming App Academy Assessment.', 'Working on MERN Project With Partners.', 'Playing Call of Duty.',
+            'Playing Wolfenstein.', 'Playing Grand Theft Auto V.', 'Playing Roblox.', 'Study for an Interview.',
+            'Studying Data Structures and Algorithms.', 'Playing Minecraft.', 'Playing League of Legends.', 'Playing Dota 2.',
+            'Playing Overwatch.', 'Playing Rocket League.', 'Playing Cyberpunk 2077.', 'Playing Monster Hunter - Iceborne.',
+            "Playing PlayerUnknown's Battlegrounds.", 'Playing ARK: Survival Evolved.', 'Playing Red Dead Redemption 2.',
+            'Playing Rainbow 6.', 'Playing Apex Legends.', 'Playing Destiny 2.', 'Playing Turok.', 'Playing Saints Row.',
+            'Playing Counter-Strike.', 'Playing World of Warcraft.', 'Playing Fall Guys.', 'Playing Among Us.',
+            'Playing Forza Horizon 5.', 'Playing Valorant.', 'Playing Bloodborne.', 'Playing Demon Souls.', 'Playing Elden Ring.',
+            'Playing Dark Souls.', 'Playing Borderlands.', 'Playing Yakuza.', 'Playing Tetris.', 'Playing Pac-Man.', 'Playing Face-Man.',
+            'Playing Tax-Man.', 'Playing Candy Crush.', 'Playing Halo.', 'Watching Anime.', 'Playing Angry Birds.',
+            'Playing DRAGON BALL Z: KAKAROT.', 'Playing Dead by Daylight.', 'Playing Portal 2.', 'Playing Half-Life.', 'Playing Cuphead.',
+            'Playing MultiVersus.', 'Playing For Honor.', 'Playing Far Cry 6.', 'Playing Crysis.', 'Playing Resident Evil 7.',
+            'Playing Dino Crisis.', "Playing Papers, Please.", 'Playing Carnivores.', 'Playing Jurassic World Evolution.',
+            'Playing Chasm: The Rift.', 'Playing Microsoft Flight Simulator.', 'Playing Genshin Impact.', 'Playing Super Mario 2.',
+            'Playing Team Fortress 2.', 'Playing Unreal Tournament.', 'Playing Blood.', 'Playing Duke Nukem.', 'Playing Serious Sam.',
+            'Playing Max Payne.', 'Playing Left 4 Dead 2.', 'Playing Dead Island.', 'Playing Planet Zoo.', 'Playing Alien Isolation.',
+            'Playing AMID EVIL.', 'Playing Super Castlevania IV.', "Playing Garry's Mod.", 'Playing Dusk.', 'Playing Super Mario Bros. 2.',
+            'Playing Plague Inc: Evolved.', 'Playing Monster Hunter Rise.', 'Playing Diablo III.', 'Playing Goat Simulator.',
+            'Playing The Elder Scrolls V: Skyrim.', 'Playing The Elder Scrolls IV: Oblivion.', 'Playing DOOM.', 'Playing DOOM ETERNAL.',
+            'Playing QUAKE CHAMPIONS.', 'Playing Call of Duty Warzone.', 'Playing Turok 2 - Seeds of Evil.', 'Playing Killing Floor 2.'
+        ]
+
+        if (_AUTH_IDS.includes(user.id) === true) {
+            return 'Beep..boo..doo..bop..boo-doo..beep...Monitoring $TR!F3'
+        }
+        else {
+            const msgIdx = Math.floor(Math.random() * randomActivityMessages.length);
+            return randomActivityMessages[msgIdx];
+        }
+
+    }
+
+
+
 
 
 
     render () {
+        console.log("friends homepage props : ", this.props);
+
+        console.log("online friends : ", this.props.onlineFriends)
+        const rendered_User_PFP = default_User_PFP;
+        const onlineFriends = this.props.onlineFriends;
+        const onlineActivityMap = onlineFriends.map((friend, idx) => {
+            return (
+                <li id="fii" className="friend-index-item activl" key={friend.id} >
+
+                    <div className="friend-index-item-wrapper-inner">
+                        <div className="friend-account-info-wrapper-super">
+
+                            <div className={`${friend.photo === undefined ?
+                                `user-pfp-svg-render color-${friend.color_tag}` : `friend-info`}`}>
+                                <img src={`${friend.photo === undefined ? rendered_User_PFP : friend.photo}`} alt="pfp" />
+                            </div>
+                            <div className={`${friend.online ? "circle-online-1" : "circle-offline-1"}`}></div>
+
+                            <div className="friend-account-info-wrapper">
+                                <div className="friend-account-info">
+                                    <div className="friend-tag">
+                                        {friend.username}
+                                        <span>#{friend.strife_id_tag}</span>
+                                    </div>
+                                </div>
+                                <div className="subtext">
+                                    <div className="subtext-inner">
+                                        {this.randomActivityMessage(friend)}
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </li>
+            )
+        })
+
+
+
         return (
             <div className="home-nav-bar-container">
                 <div className="home-nav-bar">
@@ -204,7 +288,6 @@ class FriendsHomePageContainer extends React.Component {
                                                 15.67,10 13,10 C13,10 12.83,10 12.75,10 C8,10 4,6 4,1.5 L4,0 L0.5,0 Z M13,0 
                                                 C10.790861,0 9,1.790861 9,4 C9,6.209139 10.790861,8 13,8 C15.209139,8 17,6.209139 
                                                 17,4 C17,1.790861 15.209139,0 13,0 Z" transform="translate(2 4)">
-
                                     </path>
                                     <path d="M0,0 L24,0 L24,24 L0,24 L0,0 Z M0,0 L24,0 L24,24 L0,24 
                                                 L0,0 Z M0,0 L24,0 L24,24 L0,24 L0,0 Z">
@@ -223,12 +306,10 @@ class FriendsHomePageContainer extends React.Component {
 
                         </div>
                     </div>
-                    {/* {this.renderCreateDMModal()} */}
-                    {this.state.showPopUp && <CreateDmModalContainer setShowPopUp={this.setShowPopUp} topBar={true}/>}
+                    {this.state.showPopUp && <CreateDmModalContainer setShowPopUp={this.setShowPopUp} topBar={true} />}
                     <div className="home-nav-tool-bar">
                         <div className="invite-tool-bar">
                             <div className="invite-tool-bar-group-dm" id="itbgdm">
-                                {/* <svg onClick={() => this.toggleCreateDmModal()} x="0" y="0" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24"> */}
                                 <svg onClick={() => this.setShowPopUp()} x="0" y="0" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
                                     <path fill="currentColor" fillRule="evenodd" clipRule="evenodd" d="M20.998 0V3H23.998V5H20.998V8H18.998V5H15.998V3H18.998V0H20.998ZM2.99805
                                          20V24L8.33205 20H14.998C16.102 20 16.998 19.103 16.998 18V9C16.998 7.896 16.102 7 14.998 7H1.99805C0.894047 7 -0.00195312 7.896 
@@ -275,22 +356,27 @@ class FriendsHomePageContainer extends React.Component {
                         {this.renderPendingList()}
                         {this.renderAddFriend()}
 
-                    <div className="active-now-section-wrapper">
-                        <div className="active-now-section">
-                            <div className="active-now-section-scroller">
-                                <div className="active-now-header"><h3>Active Now</h3></div>
-                                <div className="empty-card">
-                                    <h5 className="empty-card-header">It's quiet for now...</h5>
-                                    <div className="empty-card-text">
-                                        When a friend starts an activity—like playing a game or hanging out on voice—we'll show it here!
+                        <div className="active-now-section-wrapper">
+                            <div className="active-now-section">
+                                <div className="active-now-section-scroller">
+                                    <div className="active-now-header"><h3>Active Now</h3></div>
+                                    <div className="empty-card">
+                                        <h5 className={`empty-card-header ${onlineFriends.length === 0 ? `` : `is-hidden`}`}>It's quiet for now...</h5>
+                                        <div className={`empty-card-text ${onlineFriends.length === 0 ? `` : `is-hidden`}`}>
+                                            When a friend starts an activity—like playing a game or hanging out on voice—we'll show it here!
+                                        </div>
+                                        <div className="friend-index activl" id='ul-fiiw'>
+                                            <div className="friend-index-item-wrapper" >
+                                                <ul>{onlineActivityMap}</ul>
+                                            </div>
+                                        </div>
                                     </div>
+                                    <div className="empty-card-div"></div>
                                 </div>
-                                <div className="empty-card-div"></div>
                             </div>
                         </div>
                     </div>
                 </div>
-                    </div>
 
 
             </div>
