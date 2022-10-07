12730105d93d27ce3e584a220a651863fbc4fc76
miker704 <miker704@gmail.com>
2022-10-07 08:28:11 -0400

    formatting update

--- app/channels/call_channel.rb
+++ app/channels/call_channel.rb
@@ -2,7 +2,7 @@ class CallChannel < ApplicationCable::Channel
     def subscribed
       # stream_from "some_channel"
       # channel doesnot exist make a fake one
-      stream_from 'call_channel_'+params[:id]
+      stream_from "call_channel_" + params[:id]
     end
   
     def unsubscribed
