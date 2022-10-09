81f1257e47c835393f28d2e80eb7de47e3ec4c35
miker704 <miker704@gmail.com>
2022-10-09 18:02:04 -0400

    fixed a bug with left over state

--- frontend/reducers/explore_servers_reducer.js
+++ frontend/reducers/explore_servers_reducer.js
@@ -1,5 +1,5 @@
 import { EXPLORE_SERVERS, REMOVE_UNEXPLORED_SERVERS } from "../actions/server_actions.js"
-
+import { LOGOUT_CURRENT_USER } from "../actions/session_actions.js";
 
 //filter out private servers from being displayed in the explored servers list 
 // privates servers can only be joined via an invite link from the create server modal join via
@@ -29,10 +29,12 @@ const exploreServersReducer = (state = {}, action) => {
     switch (action.type) {
         case EXPLORE_SERVERS:
             return action.servers;
-            // return receiveUnexploredServers(state, action.servers);
+        // return receiveUnexploredServers(state, action.servers);
         // remove the state when leaving the page as it is not needed anymore only when in the page it is needed
         case REMOVE_UNEXPLORED_SERVERS:
-            return action.servers
+            return action.servers;
+        case LOGOUT_CURRENT_USER:
+            return {};
         default:
             return state;
     }
