8a5d70dac6e764e36b96a32c468a3ba142c02fd0
miker704 <miker704@gmail.com>
2022-10-09 18:01:39 -0400

    fixed a bug with left over state

--- frontend/reducers/modal_with_props_reducer.js
+++ frontend/reducers/modal_with_props_reducer.js
@@ -1,6 +1,6 @@
 
-import {  OPEN_MODAL_WITH_PROPS } from '../actions/modal_actions';
-
+import { OPEN_MODAL_WITH_PROPS } from '../actions/modal_actions';
+import { LOGOUT_CURRENT_USER } from '../actions/session_actions';
 
 export default function modalPropsReducer (state = {}, action) {
 
@@ -9,6 +9,8 @@ export default function modalPropsReducer (state = {}, action) {
   switch (action.type) {
     case OPEN_MODAL_WITH_PROPS:
       return nextState[action.modal_With_Props] = action.modal_With_Props;
+    case LOGOUT_CURRENT_USER:
+      return {};
     default:
       return state;
   }
