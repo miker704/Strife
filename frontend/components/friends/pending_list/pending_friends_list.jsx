6480901c0e62393583c443de632599b40860b888
miker704 <miker704@gmail.com>
2022-10-11 11:46:29 -0400

    search icon to x icon works

--- frontend/components/friends/pending_list/pending_friends_list.jsx
+++ frontend/components/friends/pending_list/pending_friends_list.jsx
@@ -96,17 +96,17 @@ class PendingFriendList extends React.Component {
                             <input id="input-all-friends" className="input-all-friends" placeholder="Search" type="search" onInput={() => this.liveSearch()} onChange={e => this.setState({ searchText: e.currentTarget.value })} value={this.state.searchText} />
                             <div className="magnify-icon-wrapper">
                                 <div className="magnify-icon">
-                                    <svg className="mag-icon1" aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                    <svg className={`mag-icon1 ${this.state.searchText.length === 0 ? `visible-x` : ``}`} aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
                                         <path fill="currentColor" d="M21.707 20.293L16.314 14.9C17.403 13.504 18 11.799 18 10C18 7.863 
-                                17.167 5.854 15.656 4.344C14.146 2.832 12.137 2 10 2C7.863 2 5.854 2.832 4.344 4.344C2.833 
-                                5.854 2 7.863 2 10C2 12.137 2.833 14.146 4.344 15.656C5.854 17.168 7.863 18 10 18C11.799 18 
-                                13.504 17.404 14.9 16.314L20.293 21.706L21.707 20.293ZM10 16C8.397 16 6.891 15.376 5.758 
-                                14.243C4.624 13.11 4 11.603 4 10C4 8.398 4.624 6.891 5.758 5.758C6.891 4.624 8.397 4 10 
-                                4C11.603 4 13.109 4.624 14.242 5.758C15.376 6.891 16 8.398 16 10C16 11.603 15.376 13.11 
-                                14.242 14.243C13.109 15.376 11.603 16 10 16Z">
+                                             17.167 5.854 15.656 4.344C14.146 2.832 12.137 2 10 2C7.863 2 5.854 2.832 4.344 4.344C2.833 
+                                             5.854 2 7.863 2 10C2 12.137 2.833 14.146 4.344 15.656C5.854 17.168 7.863 18 10 18C11.799 18 
+                                             13.504 17.404 14.9 16.314L20.293 21.706L21.707 20.293ZM10 16C8.397 16 6.891 15.376 5.758 
+                                             14.243C4.624 13.11 4 11.603 4 10C4 8.398 4.624 6.891 5.758 5.758C6.891 4.624 8.397 4 10 
+                                             4C11.603 4 13.109 4.624 14.242 5.758C15.376 6.891 16 8.398 16 10C16 11.603 15.376 13.11 
+                                             14.242 14.243C13.109 15.376 11.603 16 10 16Z">
                                         </path>
                                     </svg>
-                                    <svg className="clear-mag-icon1" aria-label="Clear" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                    <svg className={`clear-mag-icon1 mag-icon1 ${this.state.searchText.length === 0 ? `` : `visible-x`}`} aria-label="Clear" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
                                         <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
                                         </path>
                                     </svg>
@@ -141,7 +141,7 @@ class PendingFriendList extends React.Component {
 
                                                 <div className="friend-index-item-wrapper-inner">
                                                     <div className="friend-account-info-wrapper-super">
-                                                        
+
                                                         <div className={`${friend.photo === undefined ?
                                                             `user-pfp-svg-render color-${friend.color_tag}` : `friend-info`}`}>
                                                             <img src={`${friend.photo === undefined ? rendered_User_PFP : friend.photo}`} alt="pfp" />
@@ -176,7 +176,7 @@ class PendingFriendList extends React.Component {
                                                 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
                                                             </path>
                                                         </svg>
-                                                       
+
                                                         <ReactTooltip className="accept-message-tool-tip" textColor="#B9BBBE"
                                                             backgroundColor="#191919" id="accept-friend-request" place="top" effect="solid">
                                                             Accept
@@ -190,7 +190,7 @@ class PendingFriendList extends React.Component {
                                                         18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
                                                             </path>
                                                         </svg>
-                                                        
+
                                                         <ReactTooltip className="ignore-message-tool-tip" textColor="#B9BBBE"
                                                             backgroundColor="#191919" id="ignore-friend-request" place="top" effect="solid">
                                                             Ignore
@@ -214,7 +214,7 @@ class PendingFriendList extends React.Component {
                                                             `user-pfp-svg-render color-${friend.color_tag}` : `friend-info`}`}>
                                                             <img src={`${friend.photo === undefined ? rendered_User_PFP : friend.photo}`} alt="pfp" />
                                                         </div>
-                                                       <div className={`${friend.online ? "circle-online-1" : "circle-offline-1"}`}></div>
+                                                        <div className={`${friend.online ? "circle-online-1" : "circle-offline-1"}`}></div>
                                                         <div className="friend-account-info-wrapper">
                                                             <div className="friend-account-info">
                                                                 <div className="friend-tag">
