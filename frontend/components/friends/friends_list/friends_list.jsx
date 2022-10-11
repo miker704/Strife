f615acf52adbddd7d0e9441600dea6511aa20b77
miker704 <miker704@gmail.com>
2022-10-11 12:17:42 -0400

    spell check off

--- frontend/components/friends/friends_list/friends_list.jsx
+++ frontend/components/friends/friends_list/friends_list.jsx
@@ -11,7 +11,7 @@ const FriendShipIndex = (props) => {
     const [searchText, setSearchText] = useState("");
     const [selectedFriends, setSelectedFriends] = useState([]);
     const [showPopup, setShowPopup] = useState(false);
-    const [selectFriend,toggleSelected] = useState([]);
+    const [selectFriend, toggleSelected] = useState([]);
 
     const dmMembersArray = (a, b) => a.length === b.length && a.every((val, idx) => val === b[idx]);
 
@@ -129,7 +129,7 @@ const FriendShipIndex = (props) => {
 
     }
 
-   
+
 
 
     if (allFriends.length > 0) {
@@ -137,7 +137,7 @@ const FriendShipIndex = (props) => {
 
 
             <div className="friend-index-container">
-                     {showPopup && <EditFriendshipModalContainer user={props.currentUser} friend={selectFriend} top={popupTop} setShowPopup={setShowPopup} />}
+                {showPopup && <EditFriendshipModalContainer user={props.currentUser} friend={selectFriend} top={popupTop} setShowPopup={setShowPopup} />}
                 <div className="all-search-bar">
                     <div className="all-search-bar-inner">
                         <input
@@ -145,6 +145,7 @@ const FriendShipIndex = (props) => {
                             className="input-all-friends"
                             type="search"
                             placeholder="Search"
+                            spellCheck={false}
                             autoFocus ref={inputRef}
                             onInput={() => liveSearch()}
                             onChange={e => setSearchText(e.currentTarget.value)}
@@ -153,7 +154,8 @@ const FriendShipIndex = (props) => {
 
                         <div className="magnify-icon-wrapper">
                             <div className="magnify-icon">
-                                <svg className={`mag-icon1 ${searchText.length === 0 ? `visible-x`:``}`} aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                <svg className={`mag-icon1 ${searchText.length === 0 ? `visible-x` : ``}`}
+                                    aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
                                     <path fill="currentColor" d="M21.707 20.293L16.314 14.9C17.403 13.504 18 11.799 18 10C18 7.863 
                                          17.167 5.854 15.656 4.344C14.146 2.832 12.137 2 10 2C7.863 2 5.854 2.832 4.344 4.344C2.833 
                                          5.854 2 7.863 2 10C2 12.137 2.833 14.146 4.344 15.656C5.854 17.168 7.863 18 10 18C11.799 18 
@@ -163,7 +165,7 @@ const FriendShipIndex = (props) => {
                                          14.242 14.243C13.109 15.376 11.603 16 10 16Z">
                                     </path>
                                 </svg>
-                                <svg className={`clear-mag-icon1 mag-icon1 ${searchText.length === 0 ? ``:`visible-x`}`} aria-label="Clear" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                <svg className={`clear-mag-icon1 mag-icon1 ${searchText.length === 0 ? `` : `visible-x`}`} aria-label="Clear" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
                                     <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
                                     </path>
                                 </svg>
@@ -194,15 +196,15 @@ const FriendShipIndex = (props) => {
                                 allFriends.map((friend, friendIdx) => {
                                     return (
                                         <li className="friend-index-item" key={friend.id} >
-                                           
+
                                             <div className="friend-index-item-wrapper-inner">
                                                 <div className="friend-account-info-wrapper-super">
-                                                    
-                                                    <div className={`${friend.photo === undefined ? 
-                                                    `user-pfp-svg-render color-${friend.color_tag}`:`friend-info`}`}>
+
+                                                    <div className={`${friend.photo === undefined ?
+                                                        `user-pfp-svg-render color-${friend.color_tag}` : `friend-info`}`}>
                                                         <img src={`${friend.photo === undefined ? rendered_User_PFP : friend.photo}`} alt="pfp" />
                                                     </div>
-                                                        <div className={`${friend.online ? "circle-online-1" : "circle-offline-1"}`}></div>
+                                                    <div className={`${friend.online ? "circle-online-1" : "circle-offline-1"}`}></div>
                                                     <div className="friend-account-info-wrapper">
                                                         <div className="friend-account-info">
                                                             <div className="friend-tag">
@@ -213,7 +215,7 @@ const FriendShipIndex = (props) => {
                                                         <div className="subtext">
                                                             <div className="subtext-inner">
                                                                 {`${friend.online ? "online" : "offline"}`}
-                                                                 {/* <div className={`${friend.online ? "circle-online" : "circle-offline"}`}></div> */}
+                                                                {/* <div className={`${friend.online ? "circle-online" : "circle-offline"}`}></div> */}
                                                             </div>
                                                         </div>
                                                     </div>
@@ -234,13 +236,13 @@ const FriendShipIndex = (props) => {
                                                     </ReactTooltip>
 
                                                 </div>
-                                                <div data-tip data-for="More" 
-                                                     className="friend-options-button" 
-                                                     onClick={(e) => {
-                                                        handleSelected(friend); 
+                                                <div data-tip data-for="More"
+                                                    className="friend-options-button"
+                                                    onClick={(e) => {
+                                                        handleSelected(friend);
                                                         handlePopupShow(e)
                                                     }}
-                                                     >
+                                                >
 
                                                     <svg className="icon-1WVg" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
                                                         <g fill="none" fillRule="evenodd">
