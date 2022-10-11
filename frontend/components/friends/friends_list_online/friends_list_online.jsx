c976587fe741464a42176166154655c7aae9c0b8
miker704 <miker704@gmail.com>
2022-10-11 12:17:15 -0400

    spell check off

--- frontend/components/friends/friends_list_online/friends_list_online.jsx
+++ frontend/components/friends/friends_list_online/friends_list_online.jsx
@@ -154,6 +154,7 @@ const FriendShipIndexOnline = ({
                             type="search"
                             placeholder="Search"
                             autoFocus ref={inputRef}
+                            spellCheck={false}
                             onInput={() => liveSearch()}
                             onChange={e => setSearchText(e.currentTarget.value)}
                             value={searchText}
@@ -163,7 +164,8 @@ const FriendShipIndexOnline = ({
 
                         <div className="magnify-icon-wrapper">
                             <div className="magnify-icon">
-                                <svg className={`mag-icon1 ${searchText.length === 0 ? `visible-x`:``}`} aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                <svg className={`mag-icon1 ${searchText.length === 0 ? `visible-x` : ``}`}
+                                    aria-label="Search" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
                                     <path fill="currentColor" d="M21.707 20.293L16.314 14.9C17.403 13.504 18 11.799 18 10C18 7.863 
                                          17.167 5.854 15.656 4.344C14.146 2.832 12.137 2 10 2C7.863 2 5.854 2.832 4.344 4.344C2.833 
                                          5.854 2 7.863 2 10C2 12.137 2.833 14.146 4.344 15.656C5.854 17.168 7.863 18 10 18C11.799 18 
@@ -173,7 +175,8 @@ const FriendShipIndexOnline = ({
                                          14.242 14.243C13.109 15.376 11.603 16 10 16Z">
                                     </path>
                                 </svg>
-                                <svg className={`clear-mag-icon1 mag-icon1 ${searchText.length === 0 ? ``:`visible-x`}`} aria-label="Clear" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
+                                <svg className={`clear-mag-icon1 mag-icon1 ${searchText.length === 0 ? `` : `visible-x`}`}
+                                    aria-label="Clear" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
                                     <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
                                     </path>
                                 </svg>
