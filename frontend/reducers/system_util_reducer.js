48466d629b4ce67947223afd110d33dbc373a479
miker704 <miker704@gmail.com>
2022-10-12 16:33:21 -0400

    rm useless data fro hardcoded bot

--- frontend/reducers/system_util_reducer.js
+++ frontend/reducers/system_util_reducer.js
@@ -1,29 +1,29 @@
 import { FETCH_STRIFE_BOT } from "../actions/session_actions";
 // import bot_PFP from "../../app/assets/images/discord_Strife_logo.png";
 import bot_PFP from "/app/assets/images/Strife_logo_compressed.png";
-import { LOGOUT_CURRENT_USER } from "../actions/session_actions";
+import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
 //stop fetching the bot just hard code it as it will always be avaliable and no need to slow down every thing
 const _STRIFE_BOT = {
     1: {
         "id": 1,
         "username": "Mr.Wumpus",
-        "email": "wumbo@strife.com",
+        // "email": "wumbo@strife.com",
         "online": true,
-        "phone_number": null,
+        // "phone_number": null,
         "strife_id_tag": "1",
         "color_tag": 10,
         "photo": bot_PFP,
-        "friend_request_status": 3,
-        "ownedServers": [],
-        "serversJoined": [],
-        "dmServersJoined": [],
+        // "friend_request_status": 3,
+        // "ownedServers": [],
+        // "serversJoined": [],
+        // "dmServersJoined": [],
     }
 }
 
 const systemUtilReducer = (state = { _STRIFE_BOT }, action) => {
     Object.freeze(state);
-    return state;
+    // return state;
     // switch (action.type) {
     //     case FETCH_STRIFE_BOT:
     //         let bot = Object.assign({}, state, { ['StrifeBot']: { [action.bot.id]: action.bot } });
@@ -32,12 +32,14 @@ const systemUtilReducer = (state = { _STRIFE_BOT }, action) => {
     //         return state;
     // }
 
-    // switch (action.type) {
-    //     case LOGOUT_CURRENT_USER:
-    //         return {};
-    //     default:
-    //         return state;
-    // }
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return state;
+        case LOGOUT_CURRENT_USER:
+            return {};
+        default:
+            return state;
+    }
 
 }
 
