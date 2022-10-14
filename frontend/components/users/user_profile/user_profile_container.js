b0b9095b89715047120dd043b559788dc841dd9e
miker704 <miker704@gmail.com>
2022-10-14 02:17:25 -0400

    replace curruser  to use self not from user state

--- frontend/components/users/user_profile/user_profile_container.js
+++ frontend/components/users/user_profile/user_profile_container.js
@@ -1,30 +1,30 @@
 import { connect } from "react-redux";
 import UserProfile from "./user_profile";
-import {removeUserAccount, logoutUser, updateUserInfo, removeSessionErrors, disableUserAccount} from '../../../actions/session_actions'
+import { removeUserAccount, logoutUser, updateUserInfo, removeSessionErrors, disableUserAccount } from '../../../actions/session_actions'
 import { openModal, closeModal } from "../../../actions/modal_actions";
 import { withRouter } from "react-router";
 import { handleKeyUp } from "../../../utils/modal_api_util";
 
 const mSTP = (state) => {
-  return{
-    currentUser: state.entities.users[state.session.id],
+  return {
+    currentUser: state.currentUser,
     errors: state.errors.session
   }
 }
 
 const mDTP = (dispatch) => {
-  return{
-    updateUserInfo : (user) => {dispatch(updateUserInfo(user))},
-    deleteUserAccount : (userId) => {dispatch(removeUserAccount(userId))},
-    logoutUser : () => {dispatch(logoutUser())},
+  return {
+    updateUserInfo: (user) => { dispatch(updateUserInfo(user)) },
+    deleteUserAccount: (userId) => { dispatch(removeUserAccount(userId)) },
+    logoutUser: () => { dispatch(logoutUser()) },
     openModal: (modal) => dispatch(openModal(modal)),
     closeModal: () => dispatch(closeModal()),
     removeSessionErrors: () => dispatch(removeSessionErrors()),
-    handleESC : (e) => handleKeyUp(e),
-    disableUserAccount: (user)=> dispatch(disableUserAccount(user))
+    handleESC: (e) => handleKeyUp(e),
+    disableUserAccount: (user) => dispatch(disableUserAccount(user))
 
   }
 }
 
-const UserProfileContainer = withRouter(connect(mSTP,mDTP)(UserProfile))
+const UserProfileContainer = withRouter(connect(mSTP, mDTP)(UserProfile))
 export default UserProfileContainer;
