ba23b6b69aaac06a601e3dc806f45ffd373a4bd4
miker704 <miker704@gmail.com>
2022-10-13 22:38:42 -0400

    rm usless stuff

--- frontend/components/users/user_edit_banner_form/user_edit_banner_container.js
+++ frontend/components/users/user_edit_banner_form/user_edit_banner_container.js
@@ -2,7 +2,7 @@ import { withRouter } from "react-router";
 import { connect } from "react-redux";
 import { handleKeyUp } from "../../../utils/modal_api_util";
 import { closeModal, openModal } from "../../../actions/modal_actions";
-import { removeSessionErrors, updateUserInfo, changeUserPFP, changeUserBanner } from "../../../actions/session_actions";
+import { removeSessionErrors, changeUserBanner } from "../../../actions/session_actions";
 import EditUserBanner from "./user_edit_banner_form";
 
 
@@ -15,9 +15,7 @@ const mSTP = (state) => {
 
 const mDTP = (dispatch) => {
     return {
-        changeUserPFP: (userId, formData) => dispatch(changeUserPFP(userId, formData)),
         changeUserBanner: (userId, formData) => dispatch(changeUserBanner(userId, formData)),
-        updateUserInfo: (user) => { dispatch(updateUserInfo(user)) },
         openModal: (modal) => dispatch(openModal(modal)),
         closeModal: () => dispatch(closeModal()),
         removeSessionErrors: () => dispatch(removeSessionErrors()),
