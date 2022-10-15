423dbb3d0895adf086586ba22b43c88ee697532c
miker704 <miker704@gmail.com>
2022-10-15 12:52:24 -0400

    show updates

--- app/channels/dm_channel.rb
+++ app/channels/dm_channel.rb
@@ -4,20 +4,24 @@ class DmChannel < ApplicationCable::Channel
     @dm_server = DmServer.find_by(id: params[:id])
     dmServers << @dm_server
     stream_for @dm_server
-
-   # self.class.broadcast_to @dm_server,
+    # self.class.broadcast_to @dm_server,
     # type: 'RECEIVE_USER',
     # user: current_user.slice(:id, :username, :photo, :strife_id_tag, :color_tag)
-
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
-    # self.class.broadcast_to @dm_server, 
+    # self.class.broadcast_to @dm_server,
     #   type: 'REMOVE_USER',
     #   id: current_user.id
     dmServers.delete(@dm_server)
-   
   end
 
+  def self.active_users(dm_server)
+    ActionCable.server.connections.select { |conn| conn.dmServers.include?(dm_server) }
+               .map { |conn| conn.current_user }
+  end
+
+
+  
 end
