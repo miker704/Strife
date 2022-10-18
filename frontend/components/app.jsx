e71758d5d2b166bff93cd65400fe572f449c278d
miker704 <miker704@gmail.com>
2022-10-18 08:57:32 -0400

    new routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../utils/route_util.jsx';
 import SplashContainer from "./splash/splash_container";
 import SessionSignInFormContainer from './session/session_signin_form_container';
@@ -11,47 +11,55 @@ import DMNavBarContainer from './dm_servers/dm_nav_bar/dm_nav_bar_container.js';
 import HomePageContainer from './friends/friends_page_main/friends_home_page_container.js';
 import LoadingScreenContainer from './loading_screen/loading_screen_container.js';
 import IntrusionPreventionLoadingScreenContainer from './loading_screen/instrusion_loading_screen_container.js';
+import DeletedServerLoadingScreenContainer from './loading_screen/delete_server_loading_screen_container.js';
 import ExploreServersContainer from './server/server_search/server_search_container.js';
 import PROTECTED_DM_SERVER_CONTAINER from './dm_servers/dm_server_security/protect_dm_server_container.js';
 import PROTECTED_SERVER_CONTAINER from './server/server_security/protect_server_container.js';
 
-const App = () => (
-    <div>
-
-        <Route path='/' component={ModalManagerContainer}></Route>
-        <ProtectedRoute path="/channels/:serverId/" component={ServerNavBarContainer} />
-        <ProtectedRoute path="/channels/" component={UserNavContainer} />
-
-        <Switch>
-            <ProtectedRoute path="/channels/@me" component={DMNavBarContainer} />
-        </Switch>
 
 
-        {/* render proper component for messages type or friends list */}
-        <Switch>
-
-            <ProtectedRoute path="/channels/@me/:dmServerId" component={PROTECTED_DM_SERVER_CONTAINER}/>
-            <ProtectedRoute path="/channels/:serverId/:channelId" component={PROTECTED_SERVER_CONTAINER} />
-            <ProtectedRoute path="/channels/@me" component={HomePageContainer} />
-
-        </Switch>
-
-        {/* alt routes to other areas not involving main app */}
-        <Switch>
-            <ProtectedRoute path="/loading/" component={LoadingScreenContainer} />
-            <ProtectedRoute path='/$TR!F3-INTRUSION-PREVENTION/' component={IntrusionPreventionLoadingScreenContainer}/>
-            <ProtectedRoute path="/channels/guild-discovery/" component={ExploreServersContainer} />
-        </Switch>
+const App = () => {
 
 
-        {/* this is the user auth routes */}
-        <Switch>
-            <AuthRoute exact path="/" component={SplashContainer} />
-            <AuthRoute path="/register" component={SessionSignUpFormContainer} />
-            <AuthRoute path="/login" component={SessionSignInFormContainer} />
-        </Switch>
 
-    </div>
-)
+    return (
+        <div>
+            <Route path='/' component={ModalManagerContainer}></Route>
+            <ProtectedRoute path="/channels/:serverId/" component={ServerNavBarContainer} />
+            <ProtectedRoute path="/channels/" component={UserNavContainer} />
+
+            <Switch>
+                <ProtectedRoute path="/channels/@me" component={DMNavBarContainer} />
+            </Switch>
+
+
+            {/* render proper component for messages type or friends list */}
+            <Switch>
+
+                <ProtectedRoute path="/channels/@me/:dmServerId" component={PROTECTED_DM_SERVER_CONTAINER} />
+                <ProtectedRoute path="/channels/:serverId/:channelId" component={PROTECTED_SERVER_CONTAINER} />
+                <ProtectedRoute path="/channels/@me" component={HomePageContainer} />
+
+            </Switch>
+
+            {/* alt routes to other areas not involving main app */}
+            <Switch>
+                <ProtectedRoute path="/loading/" component={LoadingScreenContainer} />
+                <ProtectedRoute path="/telefrag/" component={DeletedServerLoadingScreenContainer} />
+                <ProtectedRoute path='/$TR!F3-INTRUSION-PREVENTION/' component={IntrusionPreventionLoadingScreenContainer} />
+                <ProtectedRoute path="/channels/guild-discovery/" component={ExploreServersContainer} />
+            </Switch>
+
+
+            {/* this is the user auth routes */}
+            <Switch>
+                <AuthRoute exact path="/" component={SplashContainer} />
+                <AuthRoute path="/register" component={SessionSignUpFormContainer} />
+                <AuthRoute path="/login" component={SessionSignInFormContainer} />
+            </Switch>
+
+        </div>
+    )
+}
 
 export default App;
