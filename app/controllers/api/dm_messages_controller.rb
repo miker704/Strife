cf8d819e9eab7dddf81c701230399b14a75d2c84
miker704 <miker704@gmail.com>
2022-10-18 08:48:10 -0400

    optimization changes

--- app/controllers/api/dm_messages_controller.rb
+++ app/controllers/api/dm_messages_controller.rb
@@ -5,69 +5,47 @@ class Api::DmMessagesController < ApplicationController
     @dm_message = DmMessage.new(dm_message_params)
     @dm_server = DmServer.find_by(id: @dm_message[:dm_server_id])
     if @dm_message.save
-        #import actioncable
-      # DmChannel.broadcast_to(@dm_server,@dm_message)
-
-      # DmChannel.broadcast_to @dm_server,
-      # type: 'RECEIVE_DM_MESSAGE', 
-      # from_template("api/dm_messages/show", dm_message: @dm_message)
-
-      # DmChannel.broadcast_to @dm_server,
-      # from_template('/api/dm_messages/show', dm_message: @dm_message)
-      # DmChannel.broadcast_to @dm_server,
-      # type: 'RECEIVE_DM_MESSAGE' from_template('api/dm_messages/show', dm_message: @dm_message)
-
-      # DmChannel.broadcast_to @dm_server, from_template('api/dm_messages/show', dm_message: @dm_message)
 
       DmChannel.broadcast_to @dm_server,
-      from_template('api/dm_messages/show', dm_message: @dm_message)
+                             from_template('api/dm_messages/show', dm_message: @dm_message)
       render json: nil, status: :ok
 
-        # render :show
     else
       render json: @dm_message.errors.full_messages, status: 400
     end
   end
-  
+
   def update
     @dm_message = DmMessage.find_by(id: params[:id])
     @dm_server = DmServer.find_by(id: @dm_message[:dm_server_id])
 
-      if @dm_message.update(dm_message_params)
-        DmChannel.broadcast_to @dm_server,
-        from_template('api/dm_messages/show', dm_message: @dm_message)
-       # render :show
-        render json: nil, status: :ok
-      else
-        render json: @dm_message.errors.full_messages, status: 400
-        # 422
-      end
+    if @dm_message.update(dm_message_params)
+      DmChannel.broadcast_to @dm_server,
+                             from_template('api/dm_messages/show', dm_message: @dm_message)
+      render json: nil, status: :ok
+    else
+      render json: @dm_message.errors.full_messages, status: 400
+      # 422
+    end
   end
-  
+
   def destroy
     @dm_message = DmMessage.find_by(id: params[:id])
     @dm_server = DmServer.find_by(id: @dm_message[:dm_server_id])
 
     if @dm_message.destroy
       DmChannel.broadcast_to @dm_server,
-      from_template('api/dm_messages/show', dm_message: @dm_message)
-      
-        # render :show
-        render json: nil, status: :ok
+                             from_template('api/dm_messages/show', dm_message: @dm_message)
+      render json: nil, status: :ok
 
     else
-        render json: @dm_message.errors.full_messages, status: 400
+      render json: @dm_message.errors.full_messages, status: 400
     end
   end
-  
 
-  
   private
+
   def dm_message_params
-      return params.require(:dm_message).permit(:dm_server_id,:sender_id,:body)
+    params.require(:dm_message).permit(:dm_server_id, :sender_id, :body)
   end
-
-
-
-
 end
