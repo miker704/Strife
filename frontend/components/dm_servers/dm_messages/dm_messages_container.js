89f2f0a3c85ee4b615abe64fbdd0843572f7af8c
miker704 <miker704@gmail.com>
2022-10-18 08:42:40 -0400

    optimization changes

--- frontend/components/dm_servers/dm_messages/dm_messages_container.js
+++ frontend/components/dm_servers/dm_messages/dm_messages_container.js
@@ -17,18 +17,18 @@ const mSTP = (state, ownProps) => {
         },
         DmMessages: Object.values(state.entities.dmMessages),
         currentUserId: state.session.id,
-        currentUser: state.entities.users[state.session.id],
+        currentUser: state.currentUser,
         dmMessagesIds: Object.keys(state.entities.dmMessages),
-        // dmMembers: state.entities.dmServers[ownProps.match.params.dmServerId],
         dmServerId: ownProps.match.params.dmServerId,
         dmServer: state.entities.dmServers[ownProps.match.params.dmServerId],
         errors: state.errors.dmMessage,
         dmServerMembers: selectDmMembers(state,ownProps.match.params.dmServerId),
         dmServerErrors : state.errors.dmServer,
-        dmMembers: state.entities.users,
         dmServers : Object.values(state.entities.dmServers),
         users: Object.values(state.entities.users),
-        isMember: state.currentUser.dmServersJoined.includes(parseInt(ownProps.match.params.dmServerId))
+        isMember: state.currentUser.dmServersJoined.includes(parseInt(ownProps.match.params.dmServerId)),
+        strifeBot: state.systemUtils._STRIFE_BOT,
+        dmMembers: Object.values(state.entities.dmServers[ownProps.match.params.dmServerId].members),
 
     }
 }
