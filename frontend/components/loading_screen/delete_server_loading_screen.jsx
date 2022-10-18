977576af5fe068f4535b528e76de8398ff7b3c23
miker704 <miker704@gmail.com>
2022-10-18 08:51:05 -0400

    telefrag route

---
+++ frontend/components/loading_screen/delete_server_loading_screen.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+class DeletedServerLoadingScreen extends React.Component {
+    constructor (props) {
+        super(props);
+    }
+
+    componentDidMount () {
+        if (this.props.history.location.pathname !== "/channels/@me") {
+            setTimeout(() => {
+                this.props.history.push("/channels/@me");
+            }, 5000)
+        }
+    }
+    render () {
+        return (
+            <div className="loading-screen-wrapper">
+                <div className="circle-wrap">
+                    <img className="loading-screen-img" alt="loadingimg" />
+                    <div className="shiny-button-container">
+                         <div className="shiny-button-flex">
+                         </div>
+                     </div>
+                </div>
+                <h1 className="loading-screen-img-h2">SERVER HAS BEEN DELETED TAKE YOU HOME</h1>
+            </div>
+        )
+    }
+
+}
+
+export default DeletedServerLoadingScreen;
