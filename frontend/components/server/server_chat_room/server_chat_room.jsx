1a65ec3b03b5f88b935434ce75adee8cee8aa71d
miker704 <miker704@gmail.com>
2022-11-16 21:15:47 -0500

    adjust redis

--- frontend/components/server/server_chat_room/server_chat_room.jsx
+++ frontend/components/server/server_chat_room/server_chat_room.jsx
@@ -68,7 +68,8 @@ class ServerChatRoom extends React.Component {
 
         //plug the cable
         // const cable = createConsumer('ws://localhost:3000/cable'); // /cable mounts to local host that rails server is running on 
-        const cable = createConsumer('wss://strife-v1.herokuapp.com/cable'); // /cable mounts to local host that rails server is running on 
+        // const cable = createConsumer('wss://strife-v1.herokuapp.com/cable'); // /cable mounts to local host that rails server is running on
+        const cable = createConsumer('wss://strife.onrender.com/cable');
         this.subscription = cable.subscriptions.create(
             { channel: 'StrifeServer', id: this.props.newMessage.channel_id },
             {
