cb24dcb8424ccd7d3ef112059fdc644b0e1ba9d9
miker704 <miker704@gmail.com>
2023-03-02 15:39:55 -0500

    switched current user state

--- frontend/components/users/user_delete_account_form/user_delete_account_container.js
+++ frontend/components/users/user_delete_account_form/user_delete_account_container.js
@@ -1,13 +1,15 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import DeleteUserAccountForm from "./user_delete_account_form.jsx";
-import { removeSessionErrors, removeUserAccount, disableUserAccount, logoutUser } from "../../../actions/session_actions";
+import { removeSessionErrors, removeUserAccount, disableUserAccount, logoutUser} from "../../../actions/session_actions";
 import { closeModal } from "../../../actions/modal_actions.js";
 
 
 const mSTP = (state) => {
     return {
-        currentUser: state.entities.users[state.session.id],
+        // currentUser: state.entities.users[state.session.id],
+        currentUser: state.currentUser,
+
         errors: state.errors.session,
         serverErrors: state.errors.session
     }
