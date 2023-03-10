73b23cd9ff557cd1924f6b15ee478ccb3e44db43
miker704 <miker704@gmail.com>
2023-03-10 16:19:40 -0500

    state swap

--- frontend/components/server/delete_server_modal/delete_server_modal_container.js
+++ frontend/components/server/delete_server_modal/delete_server_modal_container.js
@@ -5,11 +5,12 @@ import { fetchServer, fetchServers, removeServerErrors, deleteServer, verifyName
 import { deleteServerMembership } from "../../../actions/server_membership_actions.js";
 import { closeModal } from "../../../actions/modal_actions.js";
 import { handleKeyUp } from "../../../utils/modal_api_util";
+import { reSyncCurrentUser } from "../../../actions/session_actions.js";
 import DeleteServerModal from "./delete_server_modal.jsx";
 
 const extractServerProps = (state, ownProps) => {
     let locationString = ownProps.location.pathname;
-    let newLoc = locationString.split('/channels/').join('').split('/');
+    let newLoc = locationString.split('/$/channels/').join('').split('/');
     return newLoc;
 }
 
@@ -21,7 +22,8 @@ const mSTP = (state, ownProps) => {
 
     return {
 
-        currentUser: state.entities.users[state.session.id],
+        // currentUser: state.entities.users[state.session.id],
+        currentUser: state.currentUser,
         server: state.entities.servers[parseInt(getIds[0])],
         channel: state.entities.channels[parseInt(getIds[1])],
         channels: Object.values(state.entities.channels),
@@ -41,6 +43,8 @@ const mDTP = (dispatch, ownProps) => {
 
     return {
 
+        //resync current user after delete of server
+        reSyncCurrentUser: (userId) => dispatch(reSyncCurrentUser(userId)),
         //server api functions
         fetchServer: (serverId) => dispatch(fetchServer(serverId)),
         fetchUserServers: (user) => dispatch(fetchServers(user)),
