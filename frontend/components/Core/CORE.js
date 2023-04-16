55bd88faec24d857a44f1b6d11d33478264eaab0
miker704 <miker704@gmail.com>
2023-04-16 18:26:21 -0400

    potiental new features & optimizations discovered

--- frontend/components/Core/CORE.js
+++ frontend/components/Core/CORE.js
@@ -2,12 +2,18 @@ import React from "react";
 import _STRIFE_CORE_CABLE_ from "./CORE_CABLE";
 import { useEffect } from "react";
 import { useStore } from "react-redux";
-
+import { useHistory } from 'react-router-dom';
 const _STRIFE_CORE_ = (props) => {
     const _CABLE_STORE_ = useStore();
+    const _HISTORY_ = useHistory();
     useEffect(() => {
         if (props.loggedIn === true) {
+            // _STRIFE_CORE_CABLE_(_CABLE_STORE_,props,props.history); //nope some how 404 upon reboot of pc
+            // _STRIFE_CORE_CABLE_(_CABLE_STORE_, _HISTORY_, props);
+            // _STRIFE_CORE_CABLE_(_CABLE_STORE_, _HISTORY_);
             _STRIFE_CORE_CABLE_(_CABLE_STORE_);
+
+
         }
         return function _DECONSTRUCTOR_ () {
             if (_CABLE_STORE_.getState().session.id === null) {
