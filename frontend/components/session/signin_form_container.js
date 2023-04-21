02c5e6d668546986daea82d75a0d532b43f9a003
miker704 <miker704@gmail.com>
2023-04-21 17:35:26 -0400

    Deprecated File

--- frontend/components/session/signin_form_container.js
+++ frontend/components/session/signin_form_container.js
@@ -1,21 +1,23 @@
-import { connect } from "react-redux";
-import { signInUser, removeSessionErrors } from "../../actions/session_actions";
-import SignIn from "./signin";
+//REDACTED FILE NOT IN USE SINCE APPS PRE ALPHA BUILD
+
+// import { connect } from "react-redux";
+// import { signInUser, removeSessionErrors } from "../../actions/session_actions";
+// import SignIn from "./signin";
 
 
-const mSTP = state => {
-    return {   
-       errors: state.errors.session
-   }
+// const mSTP = state => {
+//     return {   
+//        errors: state.errors.session
+//    }
    
-   }
+//    }
 
-const mDTP = dispatch => (
+// const mDTP = dispatch => (
 
-    {
-        signInUser: formUser => dispatch(signInUser(formUser)),
-        removeSessionErrors: ()=> dispatch(removeSessionErrors())
-    })
+//     {
+//         signInUser: formUser => dispatch(signInUser(formUser)),
+//         removeSessionErrors: ()=> dispatch(removeSessionErrors())
+//     })
 
-const SignInFormContainer = connect(mSTP, mDTP)(SignIn);
-export default SignInFormContainer;
+// const SignInFormContainer = connect(mSTP, mDTP)(SignIn);
+// export default SignInFormContainer;
