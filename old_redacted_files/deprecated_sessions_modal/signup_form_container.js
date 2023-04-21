1c927efae5b0601c81525b355f4cab7d14573946
miker704 <miker704@gmail.com>
2023-04-21 17:38:49 -0400

    moved file to dedicated folders

---
+++ old_redacted_files/deprecated_sessions_modal/signup_form_container.js
@@ -0,0 +1,24 @@
+//REDACTED FILE NOT IN USE SINCE APPS PRE ALPHA BUILD
+
+// import { connect } from "react-redux";
+// import { signUpUser, removeSessionErrors} from "../../actions/session_actions";
+// import SignUp from "./signup";
+
+// const mSTP = state => {
+//  return {   
+//     errors: state.errors.session
+// }
+
+// }
+
+
+// const mDTP = dispatch => (
+
+//     {
+//         signUpUser: formUser => dispatch(signUpUser(formUser)),
+//         removeSessionErrors: ()=> dispatch(removeSessionErrors())
+//     })
+
+// // const SignUpFormContainer = connect(null, mDTP)(SignUp);
+// const SignUpFormContainer = connect(mSTP, mDTP)(SignUp);
+// export default SignUpFormContainer;
