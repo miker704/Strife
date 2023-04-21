1a3bbbb999d7203f6a160d3147fb30cdafba0057
miker704 <miker704@gmail.com>
2023-04-21 17:35:36 -0400

    Deprecated File

--- frontend/components/session/signup_form_container.js
+++ frontend/components/session/signup_form_container.js
@@ -1,22 +1,24 @@
-import { connect } from "react-redux";
-import { signUpUser, removeSessionErrors} from "../../actions/session_actions";
-import SignUp from "./signup";
+//REDACTED FILE NOT IN USE SINCE APPS PRE ALPHA BUILD
 
-const mSTP = state => {
- return {   
-    errors: state.errors.session
-}
+// import { connect } from "react-redux";
+// import { signUpUser, removeSessionErrors} from "../../actions/session_actions";
+// import SignUp from "./signup";
 
-}
+// const mSTP = state => {
+//  return {   
+//     errors: state.errors.session
+// }
+
+// }
 
 
-const mDTP = dispatch => (
+// const mDTP = dispatch => (
 
-    {
-        signUpUser: formUser => dispatch(signUpUser(formUser)),
-        removeSessionErrors: ()=> dispatch(removeSessionErrors())
-    })
+//     {
+//         signUpUser: formUser => dispatch(signUpUser(formUser)),
+//         removeSessionErrors: ()=> dispatch(removeSessionErrors())
+//     })
 
-// const SignUpFormContainer = connect(null, mDTP)(SignUp);
-const SignUpFormContainer = connect(mSTP, mDTP)(SignUp);
-export default SignUpFormContainer;
+// // const SignUpFormContainer = connect(null, mDTP)(SignUp);
+// const SignUpFormContainer = connect(mSTP, mDTP)(SignUp);
+// export default SignUpFormContainer;
