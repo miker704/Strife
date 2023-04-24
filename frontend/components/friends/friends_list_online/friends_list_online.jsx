4672a880bbc8ca06747150a65e878c92f12f2679
miker704 <miker704@gmail.com>
2023-04-23 20:51:30 -0400

    added clear search function

--- frontend/components/friends/friends_list_online/friends_list_online.jsx
+++ frontend/components/friends/friends_list_online/friends_list_online.jsx
@@ -154,7 +154,12 @@ const FriendShipIndexOnline = ({
         }
 
     }
-
+    
+    const resetSearch = () => {
+        setSearchText("");
+        document.getElementById('input-all-friends').value="";
+        liveSearch();
+    }
 
     if (allFriends.length > 0) {
         return (
@@ -194,6 +199,7 @@ const FriendShipIndexOnline = ({
                                     </path>
                                 </svg>
                                 <svg className={`clear-mag-icon1 mag-icon1 ${searchText.length === 0 ? `` : `visible-x`}`}
+                                    onClick={() => { resetSearch(); }}
                                     aria-label="Clear" aria-hidden="false" role="img" width="24" height="24" viewBox="0 0 24 24">
                                     <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
                                     </path>
