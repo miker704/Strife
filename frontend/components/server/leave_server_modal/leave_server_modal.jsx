a710a373df7c097867ed919103b56e5b9408e1a2
miker704 <miker704@gmail.com>
2023-05-14 15:06:59 -0400

    added better listeners

--- frontend/components/server/leave_server_modal/leave_server_modal.jsx
+++ frontend/components/server/leave_server_modal/leave_server_modal.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const LeaveServerModal = (props) => {
+    const popupRef = useRef();
 
 
     useEffect(() => {
-        window.addEventListener('keyup', props.handleESC, false);
+        // window.addEventListener('keyup', props.handleESC, false);
+        window.addEventListener('keyup', handleESC, false);
+
         return function cleanUp () {
             //remove any errors resulting in server leaving and modal closing server name invokes an error once state is lost and modal is 
             //still open this helps remove anything else besides that 
@@ -13,11 +16,14 @@ const LeaveServerModal = (props) => {
             //deleting server membership invokes channel membership deletion
             props.removeChannelErrors();
             // remove esc listener
-            window.removeEventListener('keyup', props.handleESC, false);
+            // window.removeEventListener('keyup', props.handleESC, false);
+            window.removeEventListener('keyup', handleESC, false);
+
         }
     }, [])
 
-    const handleLeaveServer = () => {
+    const handleLeaveServer = (e) => {
+        e.preventDefault()
         let subState = { user_id: props.currentUser.id, server_id: props.server.id, banned: 0 };
         //this is done carefully doing these out of order invoke state and prop errors
         // close the modal first preventing errors waiving from missing server values of server name etc
@@ -26,24 +32,52 @@ const LeaveServerModal = (props) => {
         // to rerender the server bar to remove the left server and prevent entering it again waving massive errors
         // if this step is not complete to ensure we invoke a then() func to ensure that if the membership is deleted
         // rerun the render if delete membership fails user can still enter and try again with no errors
-       
+
         props.closeModal();
         props.history.push('/$/channels/@me')
         props.deleteServerMembership(props.currentUser.id, subState).then(() => {
-        props.fetchUserServers(props.currentUser.id)
+            props.fetchUserServers(props.currentUser.id)
         })
         return;
     }
 
 
+    const handleESC = (e) => {
+        const keys = {
+            27: () => {
+                e.preventDefault();
+                document.getElementById("leave-server-modal").classList.add("transition-out");
+                setTimeout(() => {
+                    props.closeModal();
+                    window.removeEventListener('keyup', handleESC, false);
+                }, 300);
+
+            },
+        };
+        if (keys[e.keyCode]) {
+            keys[e.keyCode]();
+        }
+    }
+
+    const handleCloseOnOuterClick = (e) => {
+        e.preventDefault();
+        document.getElementById("leave-server-modal").classList.add("transition-out");
+        setTimeout(() => {
+            props.closeModal();
+            window.removeEventListener('keyup', handleESC, false);
+        }, 300);
+    }
+
+
+
     return (
-        <div className="leave-server-wrapper"  >
+        <div className="leave-server-wrapper" onClick={(e) => handleCloseOnOuterClick(e)}>
             <div className="leave-server-backdrop"></div>
             <div className="leave-server-layer">
-                <div id="leave-server-focus-lock" className="leave-server-focus-lock" onClick={(e) => e.stopPropagation()}>
-                    <div className="leave-server-root" >
+                <div id="leave-server-focus-lock" className="leave-server-focus-lock" onClick={(e) => e.stopPropagation()} ref={popupRef}>
+                    <div className="leave-server-root" id="leave-server-modal">
                         <div className="leave-server-flex" >
-                            <h2 className="remove-phone-header">
+                            <h2 className="leave-server-header">
                                 Leave '{`${props.server.server_name}`}'
                             </h2>
                         </div>
@@ -53,11 +87,11 @@ const LeaveServerModal = (props) => {
                                 <strong>{`${props.server.server_name}`}</strong>
                                 ? You won't be able to rejoin this server unless you are re-invited.
                             </div>
-                            <div className="username-edit-sep"></div>
+                            <div className="leave-server-sep"></div>
                         </div>
                         <form onSubmit={handleLeaveServer} className="leave-server-button-flex-wrapper">
-                            <button type="submit" className="delete-server-submit-button">Leave Server</button>
-                            <button type="button" onClick={() => props.closeModal()} className="username-edit-cancel-button">Cancel</button>
+                            <button type="submit" className="leave-server-submit-button">Leave Server</button>
+                            <button type="button" onClick={(e) => handleCloseOnOuterClick(e)} className="leave-server-cancel-button">Cancel</button>
                         </form>
                     </div>
                 </div>
