fec8746ed2f00c0ff6c2e8ed123ee376e2fb08a4
miker704 <miker704@gmail.com>
2023-06-10 00:33:03 -0400

    added new selector function added jsdocs

--- frontend/utils/selectors_api_util.js
+++ frontend/utils/selectors_api_util.js
@@ -46,6 +46,33 @@ export const selectAllFriends = (state, status) => {
     return allFriends.sort((a, b) => a.username > b.username ? 1 : -1);
 }
 
+
+/**
+ * Function to return any relationship(s) that are connected between the current user and others
+ * @param {Redux_State} state - Friendship State from Redux Store
+ * @param {Number} status - status of friend from range [-2,-1,0,1,2,3](-2 is not a usable attribute)
+ * @returns {(Array.<{id:Number,username:String,email:String,online:boolean,strife_id_tag:String,
+ * photo: URL.<String> | undefined,color_tag: Number,friend_request_status:Number,}>|[])}
+ * An Array of Objects containing State data of users that have some relation with the current user.
+ * Data is returned sorted by usernames in alphabetical order
+ * returns an empty array ([]) if no data can be obtained.
+ * NOTE: REQUIRES the FRIENDSHIP STATE TO CONTAIN DATA! Dispatch to fetch a users friends of the state is empty 
+ * It is not necessary to call a dispatch everytime before using this function only when the
+ * state is empty. 
+ */
+export const selectFriendsViaStatusType = (state, status) => {
+    const allFriends = [];
+    for (let friend of Object.values(state.entities.friendships)) {
+        if (friend.friend_request_status === status) {
+            allFriends.push(friend);
+        }
+    }
+    return allFriends.sort((a, b) => a.username > b.username ? 1 : -1);
+}
+
+
+
+
 export const selectOnlineFriends = (state, status) => {
     const onlineFriends = [];
     for (let friend of Object.values(state.entities.friendships)) {
