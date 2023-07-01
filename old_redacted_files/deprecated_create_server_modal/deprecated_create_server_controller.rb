5168fd4dcf3a3cd4943aa73714d83d950216052d
miker704 <miker704@gmail.com>
2023-07-01 05:35:49 -0400

    deprecated create server backend controller

---
+++ old_redacted_files/deprecated_create_server_modal/deprecated_create_server_controller.rb
@@ -0,0 +1,8 @@
+# def create
+#     @server = Server.new(server_params)
+#     if @server.save
+#         render :show
+#     else
+#       render json: @server.errors.full_messages, status: 422
+#     end
+# end
