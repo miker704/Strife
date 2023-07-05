cf1266ddaa1dd46190936eb76d7177c236d6c74b
miker704 <miker704@gmail.com>
2023-07-05 09:03:54 -0400

    util function to return mask color

---
+++ frontend/utils/user_status_badge_color_api_util.js
@@ -0,0 +1,26 @@
+
+
+const returnUserBadgeFillColor = (userStatus) => {
+
+    let fillColors = {
+        "online": `rgb(35, 165, 90)`,
+        "offline": `rgb(128, 132, 142)`,
+        "invisible": `rgb(128, 132, 142)`,
+        "idle": `rgb(240, 178, 50)`,
+        "dnd": `rgb(242, 63, 67)`,
+    }
+
+    // using condtion temp as other status are not implemented fully currently using physical online offline (physically logged in ? )
+    if (userStatus === false) {
+        return fillColors['offline']
+    }
+    else if (userStatus === true) {
+        return fillColors['online'];
+    }
+    // else {
+    //     return fillColors[userStatus];
+    // }
+
+}
+
+export default returnUserBadgeFillColor;
