9f2a1c66230071d003d8beb632cb5b5c3133af6b
miker704 <miker704@gmail.com>
2023-07-23 09:11:25 -0400

    moved to deprecated

---
+++ old_redacted_files/deprecated_splash_page/deprecated_splash_main_container.jsx
@@ -0,0 +1,21 @@
+// import React from "react";
+// import SplashNav from "./splash_nav";
+// import Splash from "./splash";
+// import SplashFooter from "./splash_footer";
+
+// class SplashMain extends React.Component{
+//     constructor(props){
+//         super(props);
+//     }
+//     render(){
+//          return(
+//             <div>
+//                 <SplashNav/>
+//                 <Splash/>
+//                 <SplashFooter/>
+//             </div>
+//          )
+//     }
+// }
+
+// export default SplashMain;
