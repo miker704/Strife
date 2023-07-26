4ede8538731e558636e8f527435ec1c16f30495d
miker704 <miker704@gmail.com>
2023-07-26 02:07:30 -0400

    backed up mui comp

---
+++ old_redacted_files/deprecated_sessions_modal/back_mui_select_components.jsx
@@ -0,0 +1,238 @@
+// const SelectMonth = styled(Select)(({ theme }) => ({
+
+//     fontSize: '16px',
+//     lineHeight: '20px',
+//     fontFamily: "gg sans",
+//     fontWeight: '400',
+//     color: 'var(--interactive-normal)',
+
+//     '.MuiSelect-select.Mui-disabled': {
+//         opacity: '0.6',
+//         color: '#dbdee1',
+//         cursor: 'not-allowed',
+//         WebkitTextFillColor: '#dbdee1',
+//     },
+//     '.MuiSelect-select em': {
+//         WebkitTextFillColor: '#949BA4',
+//         color: '#949BA4',
+//         boxSizing: 'border-box',
+//     },
+
+//     '.MuiSelect-select': {
+//         backgroundColor: 'var(--input-background)',
+//         borderColor: 'var(--input-background)',
+//         cursor: 'pointer',
+//         outline: '0',
+//         border: '1px solid transparent',
+//         boxSizing: 'border-box',
+//         display: 'flex',
+//         alignItems: 'center',
+//         borderRadius: '4px',
+//         width: '145.59px',
+//         minWidth: '145.59px',
+//         maxWidth: '145.59px',
+//         minHeight: '40px',
+//         height: '40px',
+//         maxHeight: '40px',
+//         flexWrap: 'wrap',
+//         fontWeight: '500',
+//         letterSpacing: 'normal',
+//         color: 'var(--interactive-normal)',
+
+//         '.selectRegionSvgCheckMark': {
+//             display: 'none !important',
+//         },
+
+//     },
+//     '.MuiSvgIcon-root': {
+//         color: 'var(--interactive-normal)',
+//         display: 'flex',
+//         boxSizing: 'border-box',
+//         opacity: '1',
+//         transition: 'color 150ms ease 0s',
+//         cursor: 'pointer',
+//         fontSize: '1.25em',
+//         right: '8.7px',
+//         top: 'calc(47% - 0.5em)'
+//     },
+
+//     "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+//         border: "none",
+//         borderRadius: "5px 5px 0 0",
+//         borderColor: 'transparent',
+//     },
+//     "& .MuiOutlinedInput-notchedOutline": {
+//         border: "none"
+//     },
+//     "&:hover .MuiOutlinedInput-notchedOutline": {
+//         border: "none",
+//     },
+
+//     '.MuiOutlinedInput-input': {
+//         padding: '2px 10px',
+//     },
+
+
+// }));
+
+
+// const SelectDate = styled(Select)(({ theme }) => ({
+
+//     fontSize: '16px',
+//     lineHeight: '20px',
+//     fontFamily: "gg sans",
+//     fontWeight: '400',
+//     color: 'var(--interactive-normal)',
+
+//     '.MuiSelect-select.Mui-disabled': {
+//         opacity: '0.6',
+//         color: '#dbdee1',
+//         cursor: 'not-allowed',
+//         WebkitTextFillColor: '#dbdee1',
+//     },
+
+//     '.MuiSelect-select em': {
+//         WebkitTextFillColor: '#949BA4',
+//         color: '#949BA4',
+//         boxSizing: 'border-box',
+//     },
+
+//     '.MuiSelect-select': {
+//         backgroundColor: 'var(--input-background)',
+//         borderColor: 'var(--input-background)',
+//         cursor: 'pointer',
+//         outline: '0',
+//         border: '1px solid transparent',
+//         boxSizing: 'border-box',
+//         display: 'flex',
+//         alignItems: 'center',
+//         borderRadius: '4px',
+//         width: '124.8px',
+//         minWidth: '124.8px',
+//         maxWidth: '124.8px',
+//         minHeight: '40px',
+//         height: '40px',
+//         maxHeight: '40px',
+//         padding: '2px 8px',
+//         flexWrap: 'wrap',
+//         fontWeight: '500',
+//         letterSpacing: 'normal',
+//         color: 'var(--interactive-normal)',
+
+//         '.selectRegionSvgCheckMark': {
+//             display: 'none !important',
+//         },
+
+//     },
+//     '.MuiSvgIcon-root': {
+//         color: 'var(--interactive-normal)',
+//         display: 'flex',
+//         boxSizing: 'border-box',
+//         opacity: '1',
+//         transition: 'color 150ms ease 0s',
+//         cursor: 'pointer',
+//         fontSize: '1.25em',
+//         right: '8.7px',
+//         top: 'calc(47% - 0.5em)'
+//     },
+
+//     "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+//         border: "none",
+//         borderRadius: "5px 5px 0 0",
+//         borderColor: 'transparent',
+//     },
+//     "& .MuiOutlinedInput-notchedOutline": {
+//         border: "none"
+//     },
+//     "&:hover .MuiOutlinedInput-notchedOutline": {
+//         border: "none",
+//     },
+
+//     '.MuiOutlinedInput-input': {
+//         padding: '2px 10px',
+//     },
+
+
+// }));
+
+
+// const SelectYear = styled(Select)(({ theme }) => ({
+
+//     fontSize: '16px',
+//     lineHeight: '20px',
+//     fontFamily: "gg sans",
+//     fontWeight: '400',
+//     color: 'var(--interactive-normal)',
+
+//     '.MuiSelect-select.Mui-disabled': {
+//         opacity: '0.6',
+//         color: '#dbdee1',
+//         cursor: 'not-allowed',
+//         WebkitTextFillColor: '#dbdee1',
+//     },
+
+
+//     '.MuiSelect-select em': {
+//         WebkitTextFillColor: '#949BA4',
+//         color: '#949BA4',
+//         boxSizing: 'border-box',
+//     },
+
+//     '.MuiSelect-select': {
+//         backgroundColor: 'var(--input-background)',
+//         borderColor: 'var(--input-background)',
+//         cursor: 'pointer',
+//         outline: '0',
+//         border: '1px solid transparent',
+//         boxSizing: 'border-box',
+//         display: 'flex',
+//         alignItems: 'center',
+//         borderRadius: '4px',
+//         width: '124.8px',
+//         minWidth: '124.8px',
+//         maxWidth: '124.8px',
+//         minHeight: '40px',
+//         height: '40px',
+//         maxHeight: '40px',
+//         padding: '2px 8px',
+//         flexWrap: 'wrap',
+//         fontWeight: '500',
+//         letterSpacing: 'normal',
+//         color: 'var(--interactive-normal)',
+
+//         '.selectRegionSvgCheckMark': {
+//             display: 'none !important',
+//         },
+
+//     },
+//     '.MuiSvgIcon-root': {
+//         color: 'var(--interactive-normal)',
+//         display: 'flex',
+//         boxSizing: 'border-box',
+//         opacity: '1',
+//         transition: 'color 150ms ease 0s',
+//         cursor: 'pointer',
+//         fontSize: '1.25em',
+//         right: '8.7px',
+//         top: 'calc(47% - 0.5em)'
+
+//     },
+
+//     "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+//         border: "none",
+//         borderRadius: "5px 5px 0 0",
+//         borderColor: 'transparent',
+//     },
+//     "& .MuiOutlinedInput-notchedOutline": {
+//         border: "none"
+//     },
+//     "&:hover .MuiOutlinedInput-notchedOutline": {
+//         border: "none",
+//     },
+
+//     '.MuiOutlinedInput-input': {
+//         padding: '2px 10px',
+//     },
+
+
+// }));
