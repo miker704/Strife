581f3ee1e2b4f8eb3b3e7f6f306247eeaf478373
miker704 <miker704@gmail.com>
2023-08-02 08:46:51 -0400

    deprecated app puller animations

---
+++ old_redacted_files/deprecated_app_puller/deprecated_app_puller.js
@@ -0,0 +1,29 @@
+// const appPullerOpen = () => {
+// document.getElementById("app-puller").animate(
+//     {
+//         transform: ['scale(0.93) translateZ(0px)', 'scale(0.94) translateZ(0px)', 'scale(0.95) translateZ(0px)', 'scale(0.97) translateZ(0px)', 'scale(0.99) translateZ(0px)'],
+//         opacity: [0, 0.2, 0.3, 0.4, 0.5],
+//         offset: [0.0, 0.25, 0.5, 0.75, 1.0],
+//     },
+//     {
+//         easing: "linear(0, 0.25 75%, 1)",
+//         direction: 'reverse',
+//         duration: 300,
+//         iterations: 1,
+//     }
+// );
+// }
+
+// const appPullerClose = () => {
+//     document.getElementById("grab-wrapper").style.opacity = 0.6;
+//     document.getElementById('app-puller').animate(
+//         {
+//             transform: ['scale(0.93) translateZ(0px)', 'scale(0.94) translateZ(0px)', 'scale(0.95) translateZ(0px)', 'scale(0.97) translateZ(0px)', 'scale(0.99) translateZ(0px)'],
+//             offset: [0.0, 0.25, 0.5, 0.75, 1.0],
+//         },
+//         {
+//             duration: 200,
+//             iterations: 1,
+//         }
+//     );
+// }
