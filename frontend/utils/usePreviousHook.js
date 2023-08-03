b1895b90230ed8f8bbe0046f83aff1ab040199f8
miker704 <miker704@gmail.com>
2023-08-03 19:14:10 -0400

    - Added  A new version of use previos hook in utils.

---
+++ frontend/utils/usePreviousHook.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+
+const usePrevious = (value) => {
+    const ref = useRef();
+
+    useEffect(() => {
+
+        ref.current = value;
+
+    }, [value]);
+
+    return ref.current;
+}
+
+export default usePrevious;
