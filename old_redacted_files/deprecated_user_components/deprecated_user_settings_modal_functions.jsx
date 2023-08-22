03f33a2839bf2d635c94a511a550a76dd27b50ba
miker704 <miker704@gmail.com>
2023-08-22 00:06:16 -0400

    - Backed up an old user settings function to deprecated folder.

---
+++ old_redacted_files/deprecated_user_components/deprecated_user_settings_modal_functions.jsx
@@ -0,0 +1,11 @@
+  // const openModal = (field) => {
+  //   setCurrentSubModal({ [field]: true });
+  //   window.removeEventListener('keyup', overrideCloseModal, false);
+  //   setIsSubModMounted(true)
+  // }
+
+  // const closeForm = (field) => {
+  //   setCurrentSubModal({ [field]: false });
+  //   window.addEventListener('keyup', overrideCloseModal, false);
+  //   setIsSubModMounted(false)
+  // }
