33a0e1ad7df04752a91546d78c288a7c7b088da3
miker704 <miker704@gmail.com>
2023-09-11 04:32:55 -0400

    updated selectors

--- frontend/utils/selectors_api_util.js
+++ frontend/utils/selectors_api_util.js
@@ -172,3 +172,130 @@ export const receiveUnexploredServers = (state) => {
     }
     return showServers;
 }
+
+
+
+
+
+
+
+
+/**
+ * Function to return the first channel of a server if it exists.
+ * @param {Result_From_Redux_$AJAX_Dispatch} result.channels - result from redux ajax dipatch take the channels from server
+ * @returns {Number}
+ * returns a number indicating the the id of the first channel in the server. if there are no channels in this server
+ * returns a -1 indicating that the user must teleport to just the server itself
+*/
+export const firstChannelId = channels => {
+    ("channels", channels)
+    const channelIds = Object.values(channels).map(channel => channel.id);
+    return Math.min(...channelIds);
+};
+
+
+
+//Deprecated - DO NOT USE MODIFIES ACTUAL REDUX STATE
+
+export const selectAllFriendsStartConversationVersion = (state, status) => {
+    const allFriends = [];
+    for (let friend of Object.values(state.entities.friendships)) {
+        if (friend.friend_request_status === status) {
+            friend.TYPE = "FRIEND";
+            friend.searchQuery = "@";
+            allFriends.push(friend);
+        }
+    }
+    return allFriends.sort((a, b) => a.username.toLowerCase() > b.username.toLowerCase() ? 1 : -1);
+}
+
+//Deprecated - DO NOT USE MODIFIES ACTUAL REDUX STATE
+
+export const extractChannelsFromAllServers = (state, get_channel_type) => {
+
+    let allChannels = [];
+    const getChannels = Object.values(state.entities.servers).map((server, idx) => {
+        for (let channel of Object.values(server.channels)) {
+            channel.server_name = server.server_name;
+            channel.username = channel.channel_name;
+            if (channel.channel_type === 1) {
+                channel.TYPE = "TEXT_CHANNEL";
+                channel.searchQuery = "#";
+            }
+            else {
+                channel.TYPE = "VOICE_CHANNEL";
+                channel.searchQuery = "!";
+            }
+        }
+        return Object.values(server.channels);
+    }).flat(Infinity);
+
+    allChannels = getChannels.filter((channel) => channel.channel_type === get_channel_type);
+    return allChannels;
+}
+
+//Deprecated - DO NOT USE MODIFIES ACTUAL REDUX STATE
+
+export const extractServersForStartConversationModal = (state) => {
+    let allServers = [];
+    const extractServers = Object.values(state.entities.servers).map((server, idx) => {
+        server.TYPE = "SERVER";
+        server.username = server.server_name;
+        server.searchQuery = "*";
+    })
+    allServers = extractServers;
+    return allServers;
+}
+
+
+
+
+
+
+//Deprecated - DO NOT USE MODIFIES ACTUAL REDUX STATE
+
+const generateDmServerIconColor = (dmServer, currentUser) => {
+
+    let membersOfThisDmS = Object.values(dmServer.members);
+    let color_tag = "";
+    if (membersOfThisDmS.length === 2) {
+        let user = membersOfThisDmS.filter(member => member.id !== currentUser.id);
+        color_tag = user[0].color_tag;
+    }
+    else if (membersOfThisDmS.length > 2) {
+        let user = membersOfThisDmS.at(-1);
+        color_tag = user.color_tag;
+    }
+    return color_tag;
+}
+
+//Deprecated - DO NOT USE MODIFIES ACTUAL REDUX STATE
+
+const generateDmServerName = (dmServer, currentUser) => {
+    let displayName = "";
+    if (dmServer.dm_server_name === null) {
+        displayName = Object.values(dmServer.members).filter(member => member.id !== currentUser.id).map(member => member.username).join(", ");
+    }
+    else if (dmServer.dm_server_name !== null || dmServer.dm_server_name !== undefined || dmServer.dm_server_name !== "") {
+        displayName = dmServer.dm_server_name;
+    }
+    return displayName;
+}
+
+
+
+//Deprecated - DO NOT USE MODIFIES ACTUAL REDUX STATE
+export const extractDmServersForStartConversationModal = (dmServers, currentUser) => {
+    let allDmServers = [];
+    // let currentUser = state.currentUser;
+    let allDMS = dmServers;
+
+    const extractDmServers = allDMS.map((dmServer, idx) => {
+        dmServer.username = generateDmServerName(dmServer, currentUser);
+        dmServer.color_tag = generateDmServerIconColor(dmServer, currentUser);
+        dmServer.TYPE = "DM_SERVER";
+        dmServer.searchQuery = "$";
+    })
+    allDmServers = extractDmServers;
+    return allDmServers;
+}  
