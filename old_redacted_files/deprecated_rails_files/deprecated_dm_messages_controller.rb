88b7ee5007d3fa582b5cbb35cce803d5678d7323
miker704 <miker704@gmail.com>
2023-09-19 18:54:16 -0400

    - Backed up deprecated dm Messages controller to deprecated folder.

---
+++ old_redacted_files/deprecated_rails_files/deprecated_dm_messages_controller.rb
@@ -0,0 +1,60 @@
+# class Api::DmMessagesController < ApplicationController
+#     skip_before_action :verify_authenticity_token
+  
+#     def create
+#       @dm_message = DmMessage.new(dm_message_params)
+#       @dm_server = DmServer.find_by(id: @dm_message[:dm_server_id])
+#       if @dm_message.save
+  
+#         # DmChannel.broadcast_to @dm_server,
+#         #                        from_template('api/dm_messages/show', dm_message: @dm_message)
+  
+#         DmChannel.broadcast_to @dm_server, type: 'RECEIVE_DM_MESSAGE',
+#                                **from_template('api/dm_messages/show', dm_message: @dm_message)                       
+#         render json: nil, status: :ok
+  
+#       else
+#         render json: @dm_message.errors.full_messages, status: 400
+#       end
+#     end
+  
+#     def update
+#       @dm_message = DmMessage.find_by(id: params[:id])
+#       @dm_server = DmServer.find_by(id: @dm_message[:dm_server_id])
+  
+#       if @dm_message.update(dm_message_params)
+#         # DmChannel.broadcast_to @dm_server,
+#         #                        from_template('api/dm_messages/show', dm_message: @dm_message)
+#         DmChannel.broadcast_to @dm_server, type: 'UPDATE_DM_MESSAGE',
+#         **from_template('api/dm_messages/show', dm_message: @dm_message)                    
+#         render json: nil, status: :ok
+#       else
+#         render json: @dm_message.errors.full_messages, status: 400
+#         # 422
+#       end
+#     end
+  
+#     def destroy
+#       @dm_message = DmMessage.find_by(id: params[:id])
+#       @dm_server = DmServer.find_by(id: @dm_message[:dm_server_id])
+#         # DmChannel.broadcast_to(@dm_server, dm_message: @dm_message.id)
+  
+#       if @dm_message.destroy
+#         # DmChannel.broadcast_to @dm_server,
+#         #                        from_template('api/dm_messages/show', dm_message: @dm_message)
+#         DmChannel.broadcast_to @dm_server, type: "REMOVE_DM_MESSAGE",
+#         **from_template('api/dm_messages/show', dm_message: @dm_message)
+#         render json: nil, status: :ok
+  
+#       else
+#         render json: @dm_message.errors.full_messages, status: 400
+#       end
+#     end
+  
+#     private
+  
+#     def dm_message_params
+#       params.require(:dm_message).permit(:dm_server_id, :sender_id, :body)
+#     end
+#   end
+  
