6f53cc0037303410507f8fccb700ffb02c68c5ba
miker704 <miker704@gmail.com>
2023-09-19 20:25:49 -0400

    deprecated suop messenger setup

---
+++ old_redacted_files/Deprecated_suop_modal/deprecated_server_user_options_modal_compressed/depreccated_suop_messenger_setup.jsx
@@ -0,0 +1,33 @@
+// for either or import suop messenger modal.
+//servers
+// const renderServerUserOptionsModal = () => {
+    //     if (showPopup === true) {
+    //         return (
+    //             <ServerUserOptionsModalContainer
+    //                 serverType={'SERVER'} member={selectedMember} memberId={selectedMember.id}
+    //                 ServerOwner={props.server.server_owner_id} server={props.server} serverMemberShipDate={selectedMember.serverMembershipDateJoined}
+    //                 top={popupTop} setShowPopup={setShowPopup} ServerID={props.server.id}
+    //                 setShowDeletePopup={setShowDeletePopup} setShowBlockUserPopup={setShowBlockUserPopup}
+    //                 popupRawTop={popupRawTop} setShowMegaPopUp={setShowMegaPopUp} messageVersion={true}
+    //             />
+    //         )
+    //     }
+    // }
+
+
+    //dmServers
+
+        // const renderServerUserOptionsModal = () => {
+    //     if (showPopup === true) {
+    //         return (
+    //             <ServerUserOptionsMessengerModalContainer
+    //                 user={props.currentUser} member={selectedMember} memberId={selectedMember?.id}
+    //                 top={popupTop} setShowPopup={setShowPopup} userNameClicked={userNameClicked}
+    //                 DmServerOwner={props.dmServer.owner_id} DmServerId={props.dmServer.id}
+    //                 serverType={'DM_SERVER'} dmServerMembers={props.dmServerMembers} getClientRect={getClientRect}
+    //                 setShowDeletePopup={setShowDeletePopup} setShowBlockUserPopup={setShowBlockUserPopup}
+    //                 popupRawTop={popupRawTop} setShowMegaPopUp={setShowMegaPopUp} messageVersion={true}
+    //             />
+    //         )
+    //     }
+    // }
