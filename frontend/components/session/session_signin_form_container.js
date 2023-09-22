703680369bec73ff910bdcb3ac99409787044ff9
miker704 <miker704@gmail.com>
2023-09-22 07:56:15 -0400

    - Finalized Session Sign In Form Container.

--- frontend/components/session/session_signin_form_container.js
+++ frontend/components/session/session_signin_form_container.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
-import { signInUser,removeSessionErrors } from "../../actions/session_actions";
+import { signInUser, removeSessionErrors } from "../../actions/session_actions";
 import SessionForm from "./session_form";
-import SessionForm2 from "./ss2_form";
-import SessionForm4 from "./session_form4";
-import { Link } from "react-router-dom";
 
-
-const mSTP  = state => {
+const mSTP = state => {
     return {
         errors: state.errors.session,
         formType: 'Sign In',
-        navLink: <span className="navlinks">Need an account?{" "}<Link to="/register">Register</Link></span>
-      
+
     }
 }
 
-const mDTP = dispatch =>{
+const mDTP = dispatch => {
     return {
         processForm: (user) => dispatch(signInUser(user)),
-        removeSessionErrors: ()=> dispatch(removeSessionErrors())
+        removeSessionErrors: () => dispatch(removeSessionErrors())
     }
 }
 
-const SessionSignInFormContainer = connect(mSTP,mDTP)(SessionForm);
+const SessionSignInFormContainer = connect(mSTP, mDTP)(SessionForm);
 export default SessionSignInFormContainer;
