24d144d9323b2a633a00ff46aee5d3a6a7620a9c
miker704 <miker704@gmail.com>
2023-09-22 10:00:31 -0400

    - Finalized Edit Friendship Modal.

--- frontend/components/friends/edit_friendship_modal/edit_friendship.jsx
+++ frontend/components/friends/edit_friendship_modal/edit_friendship.jsx
@@ -1,104 +1,110 @@
-import React from "react";
-import {  useRef, useEffect } from "react";
+import React from 'react';
+import { useRef, useEffect, useState } from "react";
+import REACT_PORTAL from '../../../utils/ReactPortal_api_util';
 import { closeHookModalOnOutsideClick, closeOnEsc } from "../../../utils/close_hook_modals_api_utils";
-const EditFriendshipModal = ({
-    currentUser,
-    friends,
-    errors,
-    requestFriendships,
-    removeFriendshipErrors,
-    deleteFriendship,
-    friend,
-    setShowPopup,
-    blockUser,
-    updateFriendship,
-    history,
-    user,
-    top,
-    left
-}) => {
 
 
-    if (friend.id === currentUser.id) {
+const EditFriendshipModal = (props) => {
+
+    if (props.friend.id === props.currentUser.id) {
         return null;
     }
 
-    const popupRef = useRef();
-    closeHookModalOnOutsideClick(popupRef, setShowPopup);
-    closeOnEsc(setShowPopup);
+    const popUpRef = useRef(null);
+    closeOnEsc(props.setShowPopup);
+
+    useEffect(() => {
+        return function cleanUp () {
+            if (props.errors.length > 0) {
+                props.removeFriendshipErrors();
+            }
+        }
+    }, []);
+
+
+
+    const handleSubModalCloseOnOutsideClick = (e) => {
+        e.preventDefault();
+        props.setShowPopup(false);
+        window.removeEventListener('keyup', handleESC, false);
+    }
+
+
+    const handleESC = (e) => {
+        const keys = {
+            27: () => {
+                e.preventDefault();
+                props.setShowPopup(false);
+                window.removeEventListener('keyup', handleESC, false);
+            },
+        };
+        if (keys[e.keyCode]) {
+            keys[e.keyCode]();
+        }
+    }
+
+
 
     const handleDeleteFriendShip = () => {
 
-        deleteFriendship({ user_id: currentUser.id, friend_id: friend.id }).then(() => {
-            App.StrifeCore.perform('parse_delete_friend_request',{ user_id: currentUser.id, friend_id: friend.id });
-            setShowPopup(false);
-        });
+        // props.deleteFriendship({ user_id: props.currentUser.id, friend_id: props.friend.id }).then(() => {
+        //     App.StrifeCore.perform('parse_delete_friend_request', { user_id: props.currentUser.id, friend_id: props.friend.id });
+        //     props.setShowPopup(false);
+        // });
+
+        props.setShowDeletePopup(true);
+        props.setShowPopup(false);
         return;
     }
 
     const handleupdateFriendShip = () => {
 
-        updateFriendship({ user_id: currentUser.id, friend_id: friend.id }).then(() => {
-            setShowPopup(false);
-
+        props.updateFriendship({ user_id: props.currentUser.id, friend_id: props.friend.id }).then(() => {
+            props.setShowPopup(false);
         });
         return;
     }
 
     const handleBlockUser = () => {
 
-        blockUser({ user_id: currentUser.id, friend_id: friend.id }).then(() => {
-            setShowPopup(false);
-
+        props.blockUser({ user_id: props.currentUser.id, friend_id: props.friend.id }).then(() => {
+            props.setShowPopup(false);
         });
         return;
     }
 
-  
-    useEffect(() => {
+    let lastOption = props.friend.friend_request_status === 3 ? (
 
-        return function cleanup () {
-            if (errors.length > 0) {
-                removeFriendshipErrors();
-            }
-        }
-
-    }, []);
-
-    let lastOption = friend.friend_request_status === 3 ? (
-
-        <div className="fo-item-container red" onClick={() => handleDeleteFriendShip()}>
-            <div className="fo-item-name">Block User</div>
+        <div className="mfom-item red" onClick={() => handleDeleteFriendShip()}>
+            <div className="mfom-item-label">Block User</div>
         </div>
-    ) :
-        ("");
+    ) : ("");
 
 
     return (
-        <div className="fo-layer" >
-            <div className="fo-theme" style={{ top: `${top}px`, left: `${left}px` }} ref={popupRef}>
-
-                <div className="fo-flex-wrapper" >
-                    <div className="fo-scroller" onClick={(e) => e.stopPropagation()} >
-                        <div className="fo-item-container">
-                            <div className="fo-item-name">Start Video Call</div>
+        <REACT_PORTAL wrapperId={'sub-modal'} classNameId={'subModal'} onClick={(e) => e.stopPropagation()}>
+            <div className="mfom-layer" onClick={(e) => handleSubModalCloseOnOutsideClick(e)}>
+                <div className='mfom-inner-layer' style={{ top: `${props.top}px`, left: `${props.left}px` }} >
+                    <div className='mfom-menu' onClick={(e) => e.stopPropagation()} ref={popUpRef}>
+                        <div className='mini-menu-scroller global-scroll-thin-raw-attributes global-scroller-base' style={{ overflow: `hidden scroll`, paddingRight: `0px` }}>
+                            <div className="mfom-item">
+                                <div className="mfom-item-label">Start Video Call</div>
+                            </div>
+                            <div className="mfom-item">
+                                <div className="mfom-item-label">Start Voice Call</div>
+                            </div>
+                            <div className="mfom-item red" onClick={(e) => handleDeleteFriendShip(e)}>
+                                <div className="mfom-item-label">Remove Friend</div>
+                            </div>
+                            <div className='mfom-bottom-div'></div>
                         </div>
-                        <div className="fo-item-container">
-                            <div className="fo-item-name">Start Voice Call</div>
-                        </div>
-                        <div className="fo-item-container red" onClick={() => handleDeleteFriendShip()}>
-                            <div className="fo-item-name">Remove Friend</div>
-                        </div>
-
-                        <div className="fo-options-bottom-div"></div>
                     </div>
                 </div>
             </div>
-        </div>
+        </REACT_PORTAL >
     )
+
+
 }
 
 export default EditFriendshipModal;
-
-
-
