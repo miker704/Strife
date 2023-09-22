816351e13615624f2e6bacb54c9f00e2bba9e5e2
miker704 <miker704@gmail.com>
2023-09-22 02:57:18 -0400

    - Finalized Download Apps Modal.

--- frontend/components/server/download_apps/download_apps_modal.jsx
+++ frontend/components/server/download_apps/download_apps_modal.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
+import REACT_PORTAL from "../../../utils/ReactPortal_api_util";
+import { CloseXIcon } from "../../front_end_svgs/Strife_svgs";
 
 const DownloadApps = (props) => {
 
@@ -14,12 +16,7 @@ const DownloadApps = (props) => {
         const keys = {
             27: () => {
                 e.preventDefault();
-                document.getElementById("da-modal").classList.add("transition-out");
-                setTimeout(() => {
-                    props.closeModal();
-                    window.removeEventListener('keyup', handleESC, false);
-                }, 200);
-
+                handleCloseOnOuterClick(e);
             },
         };
         if (keys[e.keyCode]) {
@@ -29,131 +26,142 @@ const DownloadApps = (props) => {
 
     const handleCloseOnOuterClick = (e) => {
         e.preventDefault();
-        document.getElementById("da-modal").classList.add("transition-out");
-        setTimeout(() => {
+        let modalToClose = document.getElementById("da-modal");
+        if (modalToClose) {
+            modalToClose.classList.add("transition-out");
+            Promise.all(modalToClose.getAnimations().map((animation) => animation.finished),)
+                .then(() => {
+                    props.closeModal();
+                    window.removeEventListener('keyup', handleESC, false);
+
+                }, () => {
+                    props.closeModal()
+                    window.removeEventListener('keyup', handleESC, false);
+                });
+        }
+        else {
             props.closeModal();
             window.removeEventListener('keyup', handleESC, false);
-        }, 200);
+        }
     }
 
 
     return (
-        <div className="da-layer" onClick={(e) => handleCloseOnOuterClick(e)}>
-            <div className="da-backdrop"></div>
-            <div className="da-flex-layer">
-                <div className="da-focus-lock" onClick={e => e.stopPropagation()}>
-                    <div id="da-modal" className="da-dl-apps">
-                        <div className="da-dl-apps-inner">
-                            <button type="button" className="close-da" onClick={(e) => handleCloseOnOuterClick(e)}>
-                                <div className="close-da-contents">
-                                    <svg className="close-da-icon" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
-                                        <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12
-                                             13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
-                                        </path>
-                                    </svg>
-                                </div>
-                            </button>
-                            <div className="platform-wraps">
-                                <h3 className="platform-wraps-h3">
-                                    Get $TR!F3 at Home
-                                </h3>
-                                <ul className="platforms-ul">
-                                    <li className="platforms-li-item">
-                                        <div className="platforms-li-item-inner-wrapper">
-                                            <div className="platforms-icon-wrap">
-                                                <div className="platform-apple-icon"></div>
-                                                <div className="platform-apple-icon-active"></div>
-                                            </div>
-                                            <div>
-                                                <h3 className="da-icon-header">
-                                                    MacOS
-                                                </h3>
-                                                <div className="download-app-buttons">
-                                                    <a target="_blank" className="download-app-buttons-anchor" href="https://discord.com/api/download?platform=osx">Download</a>
+        <REACT_PORTAL wrapperId={'sub-modal'} classNameId={'subModal'} onClick={(e) => e.stopPropagation()}>
+            <div className="da-layer" onClick={(e) => handleCloseOnOuterClick(e)}>
+                <div className="da-backdrop"></div>
+                <div className="da-flex-layer">
+                    <div className="da-focus-lock" onClick={e => e.stopPropagation()}>
+                        <div id="da-modal" className="da-dl-apps">
+                            <div className="da-dl-apps-inner">
+                                <button type="button" className="close-da" onClick={(e) => handleCloseOnOuterClick(e)}>
+                                    <div className="close-da-contents">
+                                        <CloseXIcon className="close-da-icon" />
+                                    </div>
+                                </button>
+                                <div className="platform-wraps">
+                                    <h3 className="platform-wraps-h3">
+                                        Get $TR!F3 at Home
+                                    </h3>
+                                    <ul className="platforms-ul">
+                                        <li className="platforms-li-item">
+                                            <div className="platforms-li-item-inner-wrapper">
+                                                <div className="platforms-icon-wrap">
+                                                    <div className="platform-apple-icon"></div>
+                                                    <div className="platform-apple-icon-active"></div>
+                                                </div>
+                                                <div>
+                                                    <h3 className="da-icon-header">
+                                                        MacOS
+                                                    </h3>
+                                                    <div className="download-app-buttons">
+                                                        <a target="_blank" className="download-app-buttons-anchor" href="https://discord.com/api/download?platform=osx">Download</a>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </li>
-                                    <li className="platforms-li-item">
-                                        <div className="platforms-li-item-inner-wrapper">
-                                            <div className="platforms-icon-wrap">
-                                                <div className="platform-windows-icon"></div>
-                                                <div className="platform-windows-icon-active"></div>
-                                            </div>
-                                            <div>
-                                                <h3 className="da-icon-header">
-                                                    Windows
-                                                </h3>
-                                                <div className="download-app-buttons">
-                                                    <a target="_blank" className="download-app-buttons-anchor" href="https://discord.com/api/download?platform=win">Download</a>
+                                        </li>
+                                        <li className="platforms-li-item">
+                                            <div className="platforms-li-item-inner-wrapper">
+                                                <div className="platforms-icon-wrap">
+                                                    <div className="platform-windows-icon"></div>
+                                                    <div className="platform-windows-icon-active"></div>
+                                                </div>
+                                                <div>
+                                                    <h3 className="da-icon-header">
+                                                        Windows
+                                                    </h3>
+                                                    <div className="download-app-buttons">
+                                                        <a target="_blank" className="download-app-buttons-anchor" href="https://discord.com/api/download?platform=win">Download</a>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </li>
-                                    <li className="platforms-li-item">
-                                        <div className="platforms-li-item-inner-wrapper">
-                                            <div className="platforms-icon-wrap">
-                                                <div className="platform-linux-icon"></div>
-                                                <div className="platform-linux-icon-active"></div>
-                                            </div>
-                                            <div>
-                                                <h3 className="da-icon-header">
-                                                    Linux
-                                                </h3>
-                                                <div className="download-app-buttons">
-                                                    <a target="_blank" className="download-app-buttons-anchor-2"
-                                                        href="https://discord.com/api/download?platform=linux&amp;format=deb">Deb</a>
+                                        </li>
+                                        <li className="platforms-li-item">
+                                            <div className="platforms-li-item-inner-wrapper">
+                                                <div className="platforms-icon-wrap">
+                                                    <div className="platform-linux-icon"></div>
+                                                    <div className="platform-linux-icon-active"></div>
+                                                </div>
+                                                <div>
+                                                    <h3 className="da-icon-header">
+                                                        Linux
+                                                    </h3>
+                                                    <div className="download-app-buttons">
+                                                        <a target="_blank" className="download-app-buttons-anchor-2"
+                                                            href="https://discord.com/api/download?platform=linux&amp;format=deb">Deb</a>
 
-                                                    <a target="_blank" className="download-app-buttons-anchor-3"
-                                                        href="https://discord.com/api/download?platform=linux&format=tar.gz">Tar</a>
+                                                        <a target="_blank" className="download-app-buttons-anchor-3"
+                                                            href="https://discord.com/api/download?platform=linux&format=tar.gz">Tar</a>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="mobile-platforms">
-                                <h3 className="mobile-header">Or on the go</h3>
-                                <ul className="platforms-ul">
-                                    <li className="platforms-li-item">
-                                        <div className="platforms-li-item-inner-wrapper">
-                                            <div className="platforms-icon-wrap">
-                                                <div className="platform-apple-ios-icon"></div>
-                                                <div className="platform-apple-ios-icon-active"></div>
-                                            </div>
-                                            <div>
-                                                <h3 className="da-icon-header">
-                                                    Apple iOS
-                                                </h3>
-                                                <div className="download-app-buttons">
-                                                    <a target="_blank" className="download-app-buttons-anchor" href="https://itunes.apple.com/app/discord/id985746746">Download</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="mobile-platforms">
+                                    <h3 className="mobile-header">Or on the go</h3>
+                                    <ul className="platforms-ul">
+                                        <li className="platforms-li-item">
+                                            <div className="platforms-li-item-inner-wrapper">
+                                                <div className="platforms-icon-wrap">
+                                                    <div className="platform-apple-ios-icon"></div>
+                                                    <div className="platform-apple-ios-icon-active"></div>
+                                                </div>
+                                                <div>
+                                                    <h3 className="da-icon-header">
+                                                        Apple iOS
+                                                    </h3>
+                                                    <div className="download-app-buttons">
+                                                        <a target="_blank" className="download-app-buttons-anchor" href="https://itunes.apple.com/app/discord/id985746746">Download</a>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </li>
-                                    <li className="platforms-li-item">
-                                        <div className="platforms-li-item-inner-wrapper">
-                                            <div className="platforms-icon-wrap">
-                                                <div className="platform-android-icon"></div>
-                                                <div className="platform-android-icon-active"></div>
-                                            </div>
-                                            <div>
-                                                <h3 className="da-icon-header">
-                                                    Android
-                                                </h3>
-                                                <div className="download-app-buttons">
-                                                    <a target="_blank" className="download-app-buttons-anchor" href="https://play.google.com/store/apps/details?id=com.discord">Download</a>
+                                        </li>
+                                        <li className="platforms-li-item">
+                                            <div className="platforms-li-item-inner-wrapper">
+                                                <div className="platforms-icon-wrap">
+                                                    <div className="platform-android-icon"></div>
+                                                    <div className="platform-android-icon-active"></div>
+                                                </div>
+                                                <div>
+                                                    <h3 className="da-icon-header">
+                                                        Android
+                                                    </h3>
+                                                    <div className="download-app-buttons">
+                                                        <a target="_blank" className="download-app-buttons-anchor" href="https://play.google.com/store/apps/details?id=com.discord">Download</a>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </li>
-                                </ul>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </REACT_PORTAL >
+
     )
 
 }
