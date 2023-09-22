d140ed6b1ae93f9359da463e4f6c725fbd65e8b4
miker704 <miker704@gmail.com>
2023-09-22 08:02:57 -0400

    - Finalized User Asset Search Modal container.

--- frontend/components/friends/user_asset_search_modal/user_asset_search_modal_container.js
+++ frontend/components/friends/user_asset_search_modal/user_asset_search_modal_container.js
@@ -1,40 +1,35 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { openModal, openModalWithProps, closeModal } from "../../../actions/modal_actions.js";
-import { createDmServer, fetchDmServer } from "../../../actions/dm_server_actions.js";
-import { fetchServer } from "../../../actions/server_actions.js";
+import { createDmServer, fetchDmServer, fetchDmServers } from "../../../actions/dm_server_actions.js";
+import { fetchServers, fetchServer } from "../../../actions/server_actions.js";
 import { handleKeyUp } from "../../../utils/modal_api_util.js";
 import { selectAllFriends } from "../../../utils/selectors_api_util.js";
 import { requestAllFriendships } from "../../../actions/friendship_actions.js";
 import { reSyncCurrentUser } from "../../../actions/session_actions.js";
 import StartConversationSearchBarModal from "./user_asset_search_modal.jsx";
 
-
-
 const mSTP = (state, ownProps) => {
     return {
-
         currentUser: state.currentUser,
         currentUserId: state.session.id,
         servers: Object.values(state.entities.servers),
         channels: state.entities.channels,
         dmServers: Object.values(state.entities.dmServers),
         friends: selectAllFriends(state,3)
-
-
-     
     }
-
 }
 
 const mDTP = (dispatch) => {
     return {
-      
+
         // modal api functions
         fetchServer: (serverId) => dispatch(fetchServer(serverId)),
-        fetchDmServer:(dmServerId) => dispatch(fetchDmServer(dmServerId)),
-        createDmServer:(dmServer) => dispatch(createDmServer(dmServer)),
-        requestAllFriendships: ()=> dispatch(requestAllFriendships()),
+        fetchServers: (userId) => dispatch(fetchServers(userId)),
+        fetchDmServer: (dmServerId) => dispatch(fetchDmServer(dmServerId)),
+        fetchDmServers: (userId) => dispatch(fetchDmServers(userId)),
+        createDmServer: (dmServer) => dispatch(createDmServer(dmServer)),
+        requestAllFriendships: () => dispatch(requestAllFriendships()),
         reSyncCurrentUser: (currentUserId) => dispatch(reSyncCurrentUser(currentUserId)),
         openModal: modal => dispatch(openModal(modal)),
         openModalWithProps: (modal_with_props) => dispatch(openModalWithProps(modal_with_props)),
