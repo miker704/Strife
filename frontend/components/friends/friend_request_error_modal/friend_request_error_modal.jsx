5affde180b8ec49927a651eaef083ee5e5590c85
miker704 <miker704@gmail.com>
2023-09-22 03:32:54 -0400

    - Finalized Friend Request Error Modal.

--- frontend/components/friends/friend_request_error_modal/friend_request_error_modal.jsx
+++ frontend/components/friends/friend_request_error_modal/friend_request_error_modal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useRef } from "react";
+import REACT_PORTAL from "../../../utils/ReactPortal_api_util";
 
 const FriendRequestErrorModal = (props) => {
 
@@ -20,12 +21,7 @@ const FriendRequestErrorModal = (props) => {
         const keys = {
             27: () => {
                 e.preventDefault();
-                document.getElementById("frfm").classList.add("transition-out");
-                setTimeout(() => {
-                    window.removeEventListener('keyup', handleESCCloseModal, false);
-                    props.closeModal();
-                }, 300);
-
+                handleExitOnOutSideClick(e);
             },
         };
         if (keys[e.keyCode]) {
@@ -37,38 +33,53 @@ const FriendRequestErrorModal = (props) => {
     const handleExitOnOutSideClick = (e) => {
         e.preventDefault();
         let modalToClose = document.getElementById("frfm");
-        modalToClose.classList.add("transition-out");
-        setTimeout(() => {
-            props.closeModal();
-        }, 250);
+        if (modalToClose) {
+            modalToClose.classList.add("transition-out");
+            Promise.all(modalToClose.getAnimations().map((animation) => animation.finished),)
+                .then(() => {
+                    props.setRenderFFEModal(false);
+                    window.removeEventListener('keyup', handleESCCloseModal, false);
+
+                }, () => {
+                    props.setRenderFFEModal(false);
+                    window.removeEventListener('keyup', handleESCCloseModal, false);
+                });
+        }
+        else {
+            props.setRenderFFEModal(false);
+            window.removeEventListener('keyup', handleESCCloseModal, false);
+        }
     }
 
     return (
-        <div className="ffrm-layer-container">
-            <div className="frfm-backdrop" onClick={(e) => handleExitOnOutSideClick(e)}></div>
-            <div className="frfm-wrapper">
-                <div className="frfm" id="frfm" onClick={e => e.stopPropagation()} ref={popupRef}>
-                    <div className="frfm-inner">
-                        <div className="form-class-200">
-                            <div className="form-content-1">
-                                <div className="form-content-1-inner">
-                                    <h2 className="form-content-1-header">
-                                        Friend Request Failed
-                                    </h2>
-                                    <div className="form-content-1-subtext">
-                                        Hm, didn’t work. Double check that the username is correct.
+        <REACT_PORTAL wrapperId={'sub-modal-2'} classNameId={'subModal'} onClick={(e) => e.stopPropagation()}>
+            <div className="ffrm-layer-container">
+                <div className="frfm-backdrop" onClick={(e) => handleExitOnOutSideClick(e)}></div>
+                <div className="frfm-wrapper">
+                    <div className="frfm" id="frfm" onClick={e => e.stopPropagation()} ref={popupRef}>
+                        <div className="frfm-inner">
+                            <div className="form-class-200">
+                                <div className="form-content-1 global-scroll-thin-raw-attributes global-scroller-base" style={{ overflow: `hidden scroll`, paddingRight: `8px` }}>
+                                    <div className="form-content-1-inner">
+                                        <h2 className="form-content-1-header">
+                                            Friend Request Failed
+                                        </h2>
+                                        <div className="form-content-1-subtext">
+                                            Hm, didn’t work. Double check that the username is correct.
+                                        </div>
                                     </div>
+                                    <div className="frfm-div-sep"></div>
                                 </div>
-                                <div className="frfm-div-sep"></div>
-                            </div>
-                            <div className="frfm-button-sec">
-                                <button type="button" onClick={(e) => { handleExitOnOutSideClick(e) }} className="frfm-button">Okay</button>
+                                <div className="frfm-button-sec">
+                                    <button type="button" onClick={(e) => { handleExitOnOutSideClick(e) }} className="frfm-button">Okay</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </REACT_PORTAL >
+
     );
 }
 
