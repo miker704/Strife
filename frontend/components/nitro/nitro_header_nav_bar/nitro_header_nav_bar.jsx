933044c6a7eb74e39439704327f8a3985b87a706
miker704 <miker704@gmail.com>
2023-09-22 04:40:53 -0400

    - Finalized Nitro Header Nav Bar.

---
+++ frontend/components/nitro/nitro_header_nav_bar/nitro_header_nav_bar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import { StrifeNitroBadgeIcon } from "../../front_end_svgs/Strife_svgs";
+
+
+const NitroHeaderNavBar = (props) => {
+    return (
+        <section className="nitro-header-nav-bar">
+            <div className="nitro-header-nav-bar-children">
+                <div className="nitro-header-nav-bar-icon-wrapper">
+                    <StrifeNitroBadgeIcon className="nhnbi-ball" height={24} width={24} />
+                </div>
+                <div className="nitro-header-nav-bar-title-wrapper">
+                    <div className="nitro-header-nav-bar-title-contents">
+                        Nitro
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default NitroHeaderNavBar;
