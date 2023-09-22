8ead8d2958bbfc30b24e58098b55667700da40ce
miker704 <miker704@gmail.com>
2023-09-22 10:38:03 -0400

    - Finalized Online Friends List Container.

--- frontend/components/friends/friends_list_online/friends_list_online_container.js
+++ frontend/components/friends/friends_list_online/friends_list_online_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import FriendShipIndexOnline from './friends_list_online';
 import { selectFriendStatusOnline} from '../../../utils/selectors_api_util';
-import { requestFriendships, removeFriendshipErrors, requestAllFriendships} from '../../../actions/friendship_actions';
+import { requestFriendships, removeFriendshipErrors, requestAllFriendships } from '../../../actions/friendship_actions';
 import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
 import { openModal } from '../../../actions/modal_actions';
 import { reSyncCurrentUser } from '../../../actions/session_actions';
@@ -27,8 +27,8 @@ const mDTP = (dispatch) => {
         removeDmServerErrors: () => dispatch(removeDmServerErrors()),
         createDmServer: (dmserver) => dispatch(createDmServer(dmserver)),
         openModal: (modal) => dispatch(openModal(modal)),
-        requestAllFriendships: ()=> dispatch(requestAllFriendships()),
-        reSyncCurrentUser : (currentUserId) => dispatch(reSyncCurrentUser(currentUserId))
+        requestAllFriendships: () => dispatch(requestAllFriendships()),
+        reSyncCurrentUser: (currentUserId) => dispatch(reSyncCurrentUser(currentUserId))
     }
 };
 
