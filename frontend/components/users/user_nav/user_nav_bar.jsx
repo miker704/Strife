644c0ad3595bc7bbb99cae656e1670f28c806053
miker704 <miker704@gmail.com>
2023-09-22 03:35:20 -0400

    - Finalized User Nav Bar.

--- frontend/components/users/user_nav/user_nav_bar.jsx
+++ frontend/components/users/user_nav/user_nav_bar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import default_user_PFP from "../../../../app/assets/images/discord_PFP.svg";
-import {useState } from "react";
-import ReactTooltip from "react-tooltip";
+import { useState } from "react";
+import DefaultPFPSVG from "../../../../app/assets/images/defaultPFPSVG.svg";
+import { Tooltip } from "react-tooltip";
+import { HeadPhoneIcon, HeadPhoneMutedIcon, MicroPhoneIcon, MicroPhoneMutedIcon, SettingsIcon_Medium } from "../../front_end_svgs/Strife_svgs";
+import MiniUPCModalContainer from "../mini_user_profile_card/mini_user_profile_card_modal_container";
+import ConfirmLogoutModalContainer from "../logout_modal/logout_modal_container";
+import MegaUPCModalContainer from "../mega_user_card_modal/mega_user_card_modal_container";
+import { appPullerPullAndHoldAnimation, appPullerPullAnimation } from "../../../utils/appPullerAnimation_api_util";
 
 
 const UserNavBar = (props) => {
@@ -9,138 +14,139 @@ const UserNavBar = (props) => {
 
     const [micMute, setMicMute] = useState(false);
     const [deafen, setDeafen] = useState(false);
-    let rendered_User_PFP = props.currentUser.photo === undefined ? default_user_PFP : props.currentUser.photo;
+    const [hover, setHover] = useState(false);
+    const [showPopup, setShowPopup] = useState(false);
+    const [showLogOut, setShowLogOut] = useState(false);
+    const [showMegaPopUp, setShowMegaPopUp] = useState(false);
+
+    const [currentSubModal, setCurrentSubModal] = useState({
+        logoutConfirm: false,
+    });
+
+    const openMod = (field) => {
+        setCurrentSubModal({ [field]: true });
+    }
+    const closeForm = (field) => {
+        setCurrentSubModal({ [field]: false });
+    }
 
     let mic_component = micMute === false ?
         (
-
-            <svg className="svg-microphone-1" aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24">
-                <path fillRule="evenodd" clipRule="evenodd" d="M14.99 11C14.99 12.66 13.66 14 12 14C10.34 14 9 12.66 9 11V5C9 
-                     3.34 10.34 2 12 2C13.66 2 15 3.34 15 5L14.99 11ZM12 16.1C14.76 16.1 17.3 14 17.3 11H19C19 14.42 16.28 17.24 
-                     13 17.72V21H11V17.72C7.72 17.23 5 14.41 5 11H6.7C6.7 14 9.24 16.1 12 16.1ZM12 4C11.2 4 11 4.66667 11 5V11C11 
-                     11.3333 11.2 12 12 12C12.8 12 13 11.3333 13 11V5C13 4.66667 12.8 4 12 4Z" fill="currentColor">
-                </path>
-                <path fillRule="evenodd" clipRule="evenodd" d="M14.99 11C14.99 12.66 13.66 14 12 14C10.34 14 9 12.66 9 11V5C9 
-                    3.34 10.34 2 12 2C13.66 2 15 3.34 15 5L14.99 11ZM12 16.1C14.76 16.1 17.3 14 17.3 
-                    11H19C19 14.42 16.28 17.24 13 17.72V22H11V17.72C7.72 17.23 5 14.41 5 11H6.7C6.7 14 9.24 16.1 12 16.1Z" fill="currentColor">
-                </path>
-            </svg>
-
+            <MicroPhoneIcon className="svg-microphone" />
         ) :
-        (<svg className="svg-muted-microphone-1" aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24">
-            <path d="M6.7 11H5C5 12.19 5.34 13.3 5.9 14.28L7.13 13.05C6.86 12.43 6.7 11.74 6.7 11Z" fill="currentColor">
-            </path>
-            <path d="M9.01 11.085C9.015 11.1125 9.02 11.14 9.02 11.17L15 5.18V5C15 3.34 13.66 2 12 2C10.34 2 9 3.34 9 5V11C9 
-                         11.03 9.005 11.0575 9.01 11.085Z" fill="currentColor">
-            </path>
-            <path d="M11.7237 16.0927L10.9632 16.8531L10.2533 17.5688C10.4978 17.633 10.747 17.6839 11 17.72V22H13V17.72C16.28 
-                           17.23 19 14.41 19 11H17.3C17.3 14 14.76 16.1 12 16.1C11.9076 16.1 11.8155 16.0975 11.7237 16.0927Z" fill="currentColor">
-            </path>
-            <path d="M21 4.27L19.73 3L3 19.73L4.27 21L8.46 16.82L9.69 15.58L11.35 13.92L14.99 10.28L21 4.27Z"
-                className="strikethrough-mic"
-                fill="currentColor">
-            </path>
-        </svg>);
+        (
+            <MicroPhoneMutedIcon className="svg-muted-microphone" />
+        );
 
     let headphone_component = deafen === false ?
-        (<svg className="svg-headphone-1"  aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24">
-            <svg width="24" height="24" viewBox="0 0 24 24">
-                <path d="M12 2.00305C6.486 2.00305 2 6.48805 2 12.0031V20.0031C2 21.1071 2.895 22.0031 4 22.0031H6C7.104 
-                22.0031 8 21.1071 8 20.0031V17.0031C8 15.8991 7.104 15.0031 6 15.0031H4V12.0031C4 7.59105 7.589 4.00305 12 
-                4.00305C16.411 4.00305 20 7.59105 20 12.0031V15.0031H18C16.896 15.0031 16 15.8991 16 17.0031V20.0031C16 21.1071 
-                16.896 22.0031 18 22.0031H20C21.104 22.0031 22 21.1071 22 20.0031V12.0031C22 6.48805 17.514 2.00305 12 2.00305Z" fill="currentColor">
-                </path>
-            </svg>
-        </svg>) :
-        (<svg className="svg-muted-headphone-1" aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24">
-            <path d="M6.16204 15.0065C6.10859 15.0022 6.05455 15 6 15H4V12C4 7.588 7.589 4 12 4C13.4809 4 14.8691 4.40439 
-             16.0599 5.10859L17.5102 3.65835C15.9292 2.61064 14.0346 2 12 2C6.486 2 2 6.485 2 12V19.1685L6.16204 15.0065Z" fill="currentColor">
-            </path>
-            <path d="M19.725 9.91686C19.9043 10.5813 20 11.2796 20 12V15H18C16.896 15 16 15.896 16 17V20C16 21.104 16.896 22 18 
-                22H20C21.105 22 22 21.104 22 20V12C22 10.7075 21.7536 9.47149 21.3053 8.33658L19.725 9.91686Z" fill="currentColor">
-            </path>
-            <path d="M3.20101 23.6243L1.7868 22.2101L21.5858 2.41113L23 3.82535L3.20101 23.6243Z" className="strikethrough-mic" fill="currentColor">
-            </path>
-        </svg>);
+        (
+            <HeadPhoneIcon className="svg-headphone" />
+        ) :
+        (
+            <HeadPhoneMutedIcon className="svg-muted-headphone" />
+        );
 
 
+    const renderShowMiniUPCModal = () => {
+        if (showPopup === true) {
+            return (
+                <MiniUPCModalContainer setShowPopup={setShowPopup} setShowLogOut={setShowLogOut} setShowMegaPopUp={setShowMegaPopUp} />
+            )
+        }
+    }
 
+    const renderLogOutModal = () => {
+        if (showLogOut === true) {
+            return (
+                <ConfirmLogoutModalContainer closeSubMod={setShowLogOut} formName={false} />
+            )
+        }
+    }
 
-
-
-
+    const renderMegaUpcModal = () => {
+        if (showMegaPopUp === true) {
+            return (
+                <MegaUPCModalContainer setShowMegaPopUp={setShowMegaPopUp} member={props.currentUser} memberId={props.currentUser.id} />
+            )
+        }
+    }
 
     return (
-        <div className="user-nav-bar-1">
-            <div className="user-nav-bar-pfp-container">
-                <div className="user-nav-bar-pfp-wrapper">
-                    <div
-                        className={`${props.currentUser.photo === undefined ?
-                            `user-pfp-svg-render color-${props.currentUser.color_tag}` : `user-PFP`}`}>
-                        <img src={rendered_User_PFP} alt="pfp" />
-                    </div>
-                </div>
 
-                <div className="user-nav-bar-info-container">
-                    <div className="user-nav-bar-username-container">
-                        <div className="user-nav-bar-username">
-                            {props.currentUser.username}
+        <section className="user-nav-bar-panel">
+            {renderShowMiniUPCModal()}
+            {renderLogOutModal()}
+            {renderMegaUpcModal()}
+            <div className="user-nav-blank-wrapper"></div>
+            <div className="user-nav-bar" onMouseEnter={(e) => setHover(true)}
+                onMouseLeave={(e) => setHover(false)}>
+                <div className="user-nav-bar-pfp-container" onClick={(e) => setShowPopup(true)}>
+                    <div className="user-nav-bar-pfp-wrapper">
+                        <svg width="40" height="40" viewBox="0 0 40 40" className="user-nav-pfp-avatar-mask user-nav-pfp-avatar-svg" aria-hidden="true">
+                            <foreignObject x="0" y="0" width="32" height="32" mask="url(#svg-mask-avatar-status-round-32)">
+                                <div className="user-nav-pfp-avatar-stack">
+                                    {
+                                        props.currentUser.photo === undefined ? (
+                                            <img className={`user-nav-pfp-avatar color-${props.currentUser.color_tag}`} src={DefaultPFPSVG} alt=" " aria-hidden="true" />
+                                        ) : (
+                                            <img className="user-nav-pfp-avatar" src={props.currentUser.photo} alt=" " aria-hidden="true" />
+                                        )
+                                    }
+                                </div>
+                            </foreignObject>
+                            <rect width="10" height="10" x="22" y="22" fill={`${props.currentUser.online ? `rgb(35, 165, 90)` : `rgb(128, 132, 142)`}`} mask={`url(#svg-mask-status-${props.currentUser.online ? `online` : `offline`})`} className="user-nav-pfp-pointer-events">
+                            </rect>
+                        </svg>
+                    </div>
+
+                    <div className="user-nav-bar-info-container">
+                        <div className="user-nav-bar-username-container">
+                            {/* switch to display name */}
+                            <div className="user-nav-bar-username">
+                                {props.currentUser.username}
+                            </div>
+                        </div>
+                        <div className="user-nav-bar-subtext-container">
+                            <div className="user-nav-bar-subtexts">
+                                {/* switch to username formated when display name is enabled */}
+                                <div className={`user-nav-bar-subtext-hover-roll ${hover ? `forced-hover` : ``}`}>
+                                    <div className="unb-hovered">
+                                        {props.currentUser.username.toLowerCase()}
+                                        #{props.currentUser.strife_id_tag}
+                                    </div>
+                                    <div className="unb-default">{`${props.currentUser.online ? `Online` : `Offline`}`}</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="user-nav-bar-strife-tag">
-                        #{props.currentUser.strife_id_tag}
-                    </div>
+
                 </div>
+                <div className="user-nav-bar-button-container">
+                    <button type="button" className="user-nav-bar-buttons" onClick={() => setMicMute(!micMute)}
+                        data-tooltip-place="top" data-tooltip-id="user-nav-mic-thread-tip" data-tooltip-content={`${micMute === false ? `Mute` : `Unmute`}`}>
+                        <div className="unb-button-contents">
+                            {mic_component}
+                            <Tooltip className="user-nav-bar-mic-settings-tool-tip" place="top" id="user-nav-mic-thread-tip" />
+                        </div>
+                    </button>
 
+                    <button type="button" className="user-nav-bar-buttons" onClick={() => setDeafen(!deafen)}
+                        data-tooltip-place="top" data-tooltip-id="user-nav-headphone-thread-tip" data-tooltip-content={`${deafen === false ? `Deafen` : `Undeafen`}`}>
+                        <div className="unb-button-contents">
+                            {headphone_component}
+                            <Tooltip className="user-nav-bar-headphone-settings-tool-tip" place="top" id="user-nav-headphone-thread-tip" />
+                        </div>
+                    </button>
+                    <button type="button" className="user-nav-bar-buttons" onClick={() => {appPullerPullAndHoldAnimation(); props.openModal('userSettings')}}
+                        data-tooltip-place="top" data-tooltip-id="user-nav-settings-thread-tip" data-tooltip-content={`User Settings`}>
+                        <div className="unb-button-contents">
+                            <SettingsIcon_Medium className="svg-gear" height={20} width={20} />
+                            <Tooltip className="user-nav-bar-user-settings-tool-tip" place="top-start" id="user-nav-settings-thread-tip" positionStrategy="fixed" />
+                        </div>
+                    </button>
+                </div>
             </div>
-            <div className="user-nav-bar-button-container">
-                <button className="user-nav-bar-buttons" onClick={() => setMicMute(!micMute)} data-tip data-for="mic-tool-tip">
-                    {mic_component}
-                    <ReactTooltip
-                        className="user-nav-bar-mic-settings-tool-tip"
-                        textColor="#B9BBBE"
-                        backgroundColor="#191919"
-                        id="mic-tool-tip"
-                        place="top"
-                        effect="solid">
-                        {`${micMute === false ? `Mute` : `Unmute`}`}
-                    </ReactTooltip>
-                </button>
-                <button className="user-nav-bar-buttons" onClick={() => setDeafen(!deafen)} data-tip data-for="headphone-tool-tip">
-                    {headphone_component}
-                    <ReactTooltip
-                        className="user-nav-bar-headphone-settings-tool-tip"
-                        textColor="#B9BBBE"
-                        backgroundColor="#191919"
-                        id="headphone-tool-tip"
-                        place="top"
-                        effect="solid">
-                        {`${deafen === false ? `Deafen` : `Undeafen`}`}
-                    </ReactTooltip>
-                </button>
-                <button className="user-nav-bar-buttons" onClick={() => props.openModal('userProfile')} data-tip data-for="user-settings-tool-tip" >
-
-                    <svg aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24">
-                        <path fill="currentColor" fillRule="evenodd" clipRule="evenodd" d="M19.738 10H22V14H19.739C19.498 14.931 
-                         19.1 15.798 18.565 16.564L20 18L18 20L16.565 18.564C15.797 19.099 14.932 19.498 14 19.738V22H10V19.738C9.069 
-                         19.498 8.203 19.099 7.436 18.564L6 20L4 18L5.436 16.564C4.901 15.799 4.502 14.932 4.262 14H2V10H4.262C4.502
-                         9.068 4.9 8.202 5.436 7.436L4 6L6 4L7.436 5.436C8.202 4.9 9.068 4.502 10 4.262V2H14V4.261C14.932 4.502 15.797 4.9
-                         16.565 5.435L18 3.999L20 5.999L18.564 7.436C19.099 8.202 19.498 9.069 19.738 10ZM12 16C14.2091 16 16 14.2091 16 
-                         12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z">
-                        </path>
-                    </svg>
-                    <ReactTooltip
-                        className="user-nav-bar-user-settings-tool-tip"
-                        textColor="#B9BBBE"
-                        backgroundColor="#191919"
-                        id="user-settings-tool-tip"
-                        place="top"
-                        effect="solid">
-                        User Settings
-                    </ReactTooltip>
-                </button>
-            </div>
-        </div>
+        </section>
     )
 
 }
