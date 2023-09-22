1c5f353d0f6cb9aba067036b919a6d9ae4cbe9d7
miker704 <miker704@gmail.com>
2023-09-22 02:37:12 -0400

    - Finalized Edit User Password Modal.

--- frontend/components/users/user_edit_password_form/user_edit_password_form.jsx
+++ frontend/components/users/user_edit_password_form/user_edit_password_form.jsx
@@ -1,134 +1,227 @@
-import React from "react"
-import { Link, Redirect } from 'react-router-dom'
+import React from 'react';
+import { useState, useRef, useEffect } from "react";
+import REACT_PORTAL from '../../../utils/ReactPortal_api_util';
+import { CloseXIcon } from '../../front_end_svgs/Strife_svgs';
 
+const EditUserPasswordForm = (props) => {
+    const popUpRef = useRef(null);
+    const [password, setPassword] = useState('');
+    const [confirmNewPassword, setConfirmNewPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
 
-class EditUserPasswordForm extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            user: this.props.currentUser,
-            password: "",
-            confirmNewPassword: "",
-            newPassword: "",
+    useEffect(() => {
+
+        window.addEventListener('keyup', handleESC, false);
+
+        return function cleanUp () {
+            props.removeSessionErrors();
+            window.removeEventListener('keyup', handleESC, false);
+
         }
-        this.cancel = false;
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleInput = this.handleInput.bind(this);
-        this.passwordErrors = this.passwordErrors.bind(this);
+    }, []);
 
 
+    const handleSubModalCloseOnOutsideClick = (e) => {
+        e.preventDefault();
+        let modalToClose = document.getElementById("user-sub-modal");
+        if (modalToClose) {
+            modalToClose.classList.add("transition-out");
+            Promise.all(modalToClose.getAnimations().map((animation) => animation.finished),)
+                .then(() => {
+                    props.closeSubMod(props.formName);
+                    window.removeEventListener('keyup', handleESC, false);
+                }, () => {
+                    props.closeSubMod(props.formName);
+                    window.removeEventListener('keyup', handleESC, false);
+                });
+        }
+        else {
+            props.closeSubMod(props.formName);
+            window.removeEventListener('keyup', handleESC, false);
+        }
+    }
+
+    const handleESC = (e) => {
+        const keys = {
+            27: () => {
+                e.preventDefault();
+                handleSubModalCloseOnOutsideClick(e);
+            },
+        };
+        if (keys[e.keyCode]) {
+            keys[e.keyCode]();
+        }
+    }
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        let submissionState = {
+            id: props.currentUser.id,
+            password: password,
+            newPassword: newPassword,
+            confirmNewPassword: confirmNewPassword
+        }
+        props.changePassword(submissionState).then(() => {
+            handleSubModalCloseOnOutsideClick(e);
+        });
     }
 
 
-
-    passwordErrors () {
+    const passwordErrors = () => {
 
         let passwordErrorList = [
             'Error Incorrect Password !',
-            'Error new password cannot match your previous password !',
-            'Error new password does not match confirm password !', 
-            'Error new password must be 8 to 72 in length'
         ];
         let passwordErrorMessages = {
-            0: " - incorrect Password",
-            1: " - cannot be an old password",
-            2: ' - must match confirm field',
-            3: ' - must be 8-72 characters in length'
+            0: "incorrect Password",
         }
 
         for (let i = 0; i < passwordErrorList.length; i++) {
-            if (this.props.errors.includes(passwordErrorList[i])) {
+            if (props.errors.includes(passwordErrorList[i])) {
+                return passwordErrorMessages[i];
+            }
+        }
+        return "";
+    }
+
+    const newPasswordErrors = () => {
+
+        let passwordErrorList = [
+            'Error new password cannot match your previous password !',
+            'Error new password must be 8 to 72 in length',
+            'Error new password cannot be blank'
+        ];
+        let passwordErrorMessages = {
+            0: "cannot be an old password",
+            1: 'must be 8-72 characters in length',
+            2: "Your new password cannot be empty"
+        }
+
+        for (let i = 0; i < passwordErrorList.length; i++) {
+            if (props.errors.includes(passwordErrorList[i])) {
                 return passwordErrorMessages[i];
             }
         }
 
         return "";
 
-       
     }
 
+    const confirmPasswordErrors = () => {
 
-
-
-    componentWillUnmount () {
-        this.props.removeSessionErrors()
-    }
-
-    handleInput (field) {
-        return (e) => { this.setState({ [field]: e.currentTarget.value }) }
-    }
-    handleSubmit (e) {
-        e.preventDefault();
-        if (this.cancel === true) {
-            this.props.removeSessionErrors();
-            return;
+        let passwordErrorList = [
+            'Error new password does not match confirm password !',
+        ];
+        let passwordErrorMessages = {
+            0: 'Passwords do not match!',
         }
-        let submissionState = {
-            id: this.props.currentUser.id,
-            password: this.state.password,
-            newPassword: this.state.newPassword,
-            confirmNewPassword: this.state.confirmNewPassword
+
+        for (let i = 0; i < passwordErrorList.length; i++) {
+            if (props.errors.includes(passwordErrorList[i])) {
+                return passwordErrorMessages[i];
+            }
         }
-        // this.props.updateUserInfo(submissionState);
-        this.props.changePassword(submissionState);
+        return "";
     }
 
-    render () {
-        let passwordErrorTag = this.props.errors.length > 0 ? "field-error" : "";
 
-        return (
-            <div className="password-modal" >
-                <div className="password-modal-inner-wrapper">
-                    <div className="password-header-flex-container">
-                        <div className="password-main-header">
-                            Update your password
-                        </div>
-                        <div className="password-header-info">
-                            Enter a your current password and a new password.
-                        </div>
-                    </div>
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="form-container1">
+    let ifNewPasswordError = props.errors.length > 0 && newPasswordErrors() !== "" ? (
+        <span className='user-sub-modal-field-input-error'>
+            <span className='user-sub-modal-field-input-error-sep'>-</span>
+            {newPasswordErrors()}
+        </span>
+    ) : ("");
+    let ifConfirmPasswordError = props.errors.length > 0 && confirmPasswordErrors() !== "" ? (
+        <span className='user-sub-modal-field-input-error'>
+            <span className='user-sub-modal-field-input-error-sep'>-</span>
+            {confirmPasswordErrors()}
+        </span>
+    ) : ("");
+    let ifPassWordError = props.errors.length > 0 && passwordErrors() !== "" ? (
+        <span className='user-sub-modal-field-input-error'>
+            <span className='user-sub-modal-field-input-error-sep'>-</span>
+            {passwordErrors()}
+        </span>
+    ) : ("")
 
-                            <div className="form-username-sec">
-                                <h5 className="form-username-header"><label className={passwordErrorTag}>Current Password{this.passwordErrors()}</label></h5>
-                                <div>
-                                    <div className="input-3-password-wrapper">
-                                        <input value={this.state.password} onChange={this.handleInput("password")} className="input-3-password" type="password" />
+
+
+    return (
+        <REACT_PORTAL wrapperId={'sub-modal'} classNameId={'subModal'} onClick={(e) => e.stopPropagation()}>
+            <div className="sub-modal-layer" onClick={(e) => handleSubModalCloseOnOutsideClick(e)}>
+                <div className='sub-modal-backdrop'></div>
+                <div className='user-sub-modal-wrapper'>
+                    <div className='user-sub-modal-focus-lock'>
+                        <div className='user-sub-modal' id="user-sub-modal" ref={popUpRef} onClick={(e) => e.stopPropagation()}>
+                            <div className='user-sub-modal-header-container-username-edit' style={{ flex: `0 0 auto` }}>
+                                <div className='user-sub-modal-title'>Update your password</div>
+                                <div className='user-sub-modal-title-subtext'>Enter your current password and a new password.</div>
+                                <button type='button' className='user-sub-modal-x-to-close-button' onClick={(e) => handleSubModalCloseOnOutsideClick(e)}>
+                                    <div className='global-button-contents'>
+                                        <CloseXIcon className="closeIconX" />
+                                    </div>
+                                </button>
+                            </div>
+                            <form onSubmit={handleSubmit}>
+                                <div className='user-sub-modal-form-content global-scroll-thin-raw-attributes global-scroller-base' style={{ overflow: `hidden scroll`, paddingRight: `8px` }}>
+
+                                    <div>
+                                        <h2 className={`usubm-current-password-subtext ${passwordErrors() !== "" ? `usubm-field-input-error-color` : ``}`}>
+                                            Current Password
+                                            {ifPassWordError}
+                                        </h2>
+                                        <div className='user-sub-modal-password-wrapper'>
+                                            <input className="user-sub-modal-password-input-field" type="password"
+                                                maxLength={999} value={password} autoComplete='off'
+                                                spellCheck={false} onChange={(e) => setPassword(e.currentTarget.value)}
+                                            />
+                                        </div>
                                     </div>
 
+                                    <div className='user-sub-modal-password-container'>
+                                        <h2 className={`usubm-current-password-subtext ${newPasswordErrors() !== "" ? `usubm-field-input-error-color` : ``}`}>
+                                            New Password
+                                            {ifNewPasswordError}
+                                        </h2>
+                                        <div className='user-sub-modal-password-wrapper'>
+                                            <input className="user-sub-modal-password-input-field" type="password"
+                                                maxLength={999} value={newPassword} autoComplete='off'
+                                                spellCheck={false} onChange={(e) => setNewPassword(e.currentTarget.value)}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='user-sub-modal-password-container'>
+                                        <h2 className={`usubm-current-password-subtext ${confirmPasswordErrors() !== "" ? `usubm-field-input-error-color` : ``}`}>
+                                            Confirm New Password
+                                            {ifConfirmPasswordError}
+                                        </h2>
+                                        <div className='user-sub-modal-password-wrapper'>
+                                            <input className="user-sub-modal-password-input-field" type="password"
+                                                maxLength={999} value={confirmNewPassword} autoComplete='off'
+                                                spellCheck={false} onChange={(e) => setConfirmNewPassword(e.currentTarget.value)}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className='user-sub-modal-form-sep'></div>
                                 </div>
-                            </div>
-                            <div className="password-section">
-                                <h5 className="password-header1">
-                                    <label className={passwordErrorTag}>New Password{this.passwordErrors()}</label>
-                                </h5>
-                                <div className="input-3-password-wrapper">
-                                    <input value={this.state.newPassword} onChange={this.handleInput("newPassword")} type="password" className="input-3-password" />
+                                <div className='user-sub-modal-button-footer-container'>
+                                    <button id="okay-button" type='submit' className='user-sub-modal-submit-button'>
+                                        <div id="submit-button-text" className='look-filled-button-contents global-button-contents'>Done</div>
+                                    </button>
+                                    <button type='button' className='user-sub-modal-cancel-button' onClick={(e) => handleSubModalCloseOnOutsideClick(e)}>
+                                        <div className='look-filled-button-contents global-button-contents'>Cancel</div>
+                                    </button>
                                 </div>
-                            </div>
-                            <div className="password-section">
-                                <h5 className="password-header1">
-                                    <label className={passwordErrorTag}>Confirm New Password{this.passwordErrors()}</label>
-                                </h5>
-                                <div className="input-3-password-wrapper">
-                                    <input value={this.state.confirmNewPassword} onChange={this.handleInput("confirmNewPassword")} type="password" className="input-3-password" />
-                                </div>
-                            </div>
-                            <div className="username-edit-sep"></div>
+                            </form>
                         </div>
-                        <div className="username-edit-button-sec">
-                            <button type="submit" className="username-edit-submit-button">Done</button>
-                            <button type="submit" onClick={() => this.cancel = true} className="username-edit-cancel-button">Cancel</button>
-                        </div>
-
-
-
-                    </form>
+                    </div>
                 </div>
             </div>
-        )
-    }
+        </REACT_PORTAL >
+    )
+
+
 
 }
 
