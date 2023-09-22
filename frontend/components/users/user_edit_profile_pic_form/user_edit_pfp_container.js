56357f6f450c1b9ef4ae4003167846efe9bcc7cc
miker704 <miker704@gmail.com>
2023-09-22 02:39:31 -0400

    - Finalized Edit User PFP Modal Container.

--- frontend/components/users/user_edit_profile_pic_form/user_edit_pfp_container.js
+++ frontend/components/users/user_edit_profile_pic_form/user_edit_pfp_container.js
@@ -11,7 +11,6 @@ const mSTP = (state) => {
     return {
         // currentUser: state.entities.users[state.session.id],
         currentUser: state.currentUser,
-
         errors: state.errors.session
     }
 };
