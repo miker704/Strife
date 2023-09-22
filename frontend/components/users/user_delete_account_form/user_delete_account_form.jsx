32dde70344890a94d2ad2ff72402d12954fc744b
miker704 <miker704@gmail.com>
2023-09-22 02:43:49 -0400

    - Finalized Delete User Account Modal.

--- frontend/components/users/user_delete_account_form/user_delete_account_form.jsx
+++ frontend/components/users/user_delete_account_form/user_delete_account_form.jsx
@@ -1,150 +1,214 @@
-import React from "react"
-import { Link, Redirect } from 'react-router-dom'
+import React from 'react';
+import { useState, useRef, useEffect } from "react";
+import REACT_PORTAL from '../../../utils/ReactPortal_api_util';
+import { reSyncCurrentUser } from '../../../utils/session_api_util';
+
+const DeleteUserAccountForm = (props) => {
 
 
-class DeleteUserAccountForm extends React.Component {
-    constructor (props) {
-        super(props)
+    const popUpRef = useRef(null);
+    const [password, setPassword] = useState('');
+    const [hasServers, setHasServers] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
-        this.state = {
-            password: "",
-            hasServers: false
+
+    useEffect(() => {
+        window.addEventListener('keyup', handleESC, false);
+
+        reSyncCurrentUser(props.currentUser.id).then((result) => {
+            if (result.ownedServers.length > 0) {
+                setHasServers(true);
+                setIsLoading(false);
+            }
+            else {
+                setHasServers(false);
+                setIsLoading(false);
+            }
+
+        }, (error) => {
+            props.closeSubMod(props.formName);
+        })
+
+
+        return function cleanUp () {
+            props.removeSessionErrors();
+            window.removeEventListener('keyup', handleESC, false);
+
         }
+    }, []);
 
-        this.cancel = false;
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleInput = this.handleInput.bind(this);
-        this.passwordErrors = this.passwordErrors.bind(this);
 
+
+    const handleSubModalCloseOnOutsideClick = (e) => {
+        e.preventDefault();
+        let modalToClose = document.getElementById("user-sub-modal");
+        if (modalToClose) {
+            modalToClose.classList.add("transition-out");
+            Promise.all(modalToClose.getAnimations().map((animation) => animation.finished),)
+                .then(() => {
+                    props.closeSubMod(props.formName);
+                    window.removeEventListener('keyup', handleESC, false);
+                }, () => {
+                    props.closeSubMod(props.formName);
+                    window.removeEventListener('keyup', handleESC, false);
+                });
+        }
+        else {
+            props.closeSubMod(props.formName);
+            window.removeEventListener('keyup', handleESC, false);
+        }
     }
 
-    passwordErrors () {
-        if (this.props.errors.includes('Login or password is invalid')) {
-            return " - Password does not match.";
+    const handleESC = (e) => {
+        const keys = {
+            27: () => {
+                e.preventDefault();
+                handleSubModalCloseOnOutsideClick(e);
+            },
+        };
+        if (keys[e.keyCode]) {
+            keys[e.keyCode]();
         }
-        else if (this.props.errors.includes("Error Incorrect Password !")) {
-            return " - Password does not match.";
+    }
+
+    const passwordErrors = () => {
+
+        let passwordErrorList = [
+            'Login or password is invalid',
+            'Error Incorrect Password !',
+        ]
+        //error messages can be a bit big lets make a reduced version 
+        let passwordErrorMessages = {
+            0: "Password does not match",
+            1: "Password does not match",
         }
+
+        for (let i = 0; i < passwordErrorList.length; i++) {
+            if (props.errors.includes(passwordErrorList[i])) {
+                return passwordErrorMessages[i];
+            }
+        }
+
         return "";
     }
 
-    componentDidMount () {
-        if (this.props.currentUser.ownedServers.length > 0) {
-            this.setState({ hasServers: true });
-        }
-
-    }
-
-    componentWillUnmount () {
-        this.props.removeSessionErrors()
-
-    }
-
-    handleInput (field) {
-        return (e) => { this.setState({ [field]: e.currentTarget.value }) }
-    }
-
-    handleSubmit (e) {
+    const submissionBlocker = (e) => {
         e.preventDefault();
-
-        if (this.cancel) {
-            this.props.removeSessionErrors();
-            return;
+        if (document.getElementById("rpn-pwi").value === "" || document.getElementById("rpn-pwi").value === null) {
+            document.getElementById("delete-submit-button").disabled = true;
         }
+        else {
+            document.getElementById("delete-submit-button").disabled = false;
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
         let subState = {
-            id: this.props.currentUser.id,
-            password: this.state.password
+            id: props.currentUser.id,
+            password: password
         }
 
-        this.props.disableUserAccount(subState).then(() =>{
-            this.props.closeModal();
-            this.props.logoutUser();
-            this.props.removeUserAccount(this.props.currentUser.id);
+        props.disableUserAccount(subState).then(() => {
+            props.closeModal();
+            props.logoutUser();
+            props.removeUserAccount(props.currentUser.id);
         })
     }
 
 
+    let ifPassWordError = props.errors.length > 0 && passwordErrors() !== "" ? (
+        <div className='user-sub-modal-acc-rmv-pw-error'>
+            {passwordErrors()}
+        </div>
+    ) : ("")
 
-    render () {
-        let passwordErrorTag = this.props.errors.length > 0 ? "field-error" : "";
-
-
-        let hasServers = this.state.hasServers === true ? (<div id="edit-userInfo-model" className="edit-userInfo-model" >
-            <div className="remove-phone-form-header-wrapper">
-                <div className="remove-phone-header">
-                    You Own Servers!
-                </div>
-            </div>
-            <div className="has-servers-subtitle-wrapper">
-                <div className="has-servers-subtitle">In order to delete or disable your account you must
-                    first transfer ownership of all servers that you own.</div>
-            </div>
-
-            <div className="username-edit-sep"></div>
-            <div className="username-edit-sep"></div>
-
-
-            <form onSubmit={this.handleSubmit}>
-
-                <div className="username-edit-button-sec">
-                    <button type="submit" onClick={() => this.cancel = true} className="username-edit-submit-button">Okay</button>
-                </div>
-
-            </form>
-        </div>) : ("");
-
-
-
-
-        let delAcc = this.state.hasServers === false ? (<div id="edit-userInfo-model" className="edit-userInfo-model" >
-            <div className="remove-phone-form-header-wrapper">
-                <div className="remove-phone-header">
-                    Delete Account
-                </div>
-            </div>
-            <div className="disable-or-delete-container-warning2">
-                Are You sure that you want to delete your account? This will immediately
-                log you out of your account and you will not be able to log in again.
-            </div>
-            <div className="username-edit-sep"></div>
-
-            <form onSubmit={this.handleSubmit}>
-                <div className="form-container1">
-
-
-                    <div className="password-section">
-                        <h5 className="password-header1">
-                            <label className={passwordErrorTag}>Current Password{this.passwordErrors()}</label>
-                        </h5>
-                        <div className="input-3-password-wrapper">
-                            <input value={this.state.password} onChange={this.handleInput("password")} type="password" className="input-3-password" />
+    if (hasServers === true && isLoading === false) {
+        return (
+            <REACT_PORTAL wrapperId={'sub-modal'} classNameId={'subModal'} onClick={(e) => e.stopPropagation()}>
+                <div className="sub-modal-layer" onClick={(e) => handleSubModalCloseOnOutsideClick(e)}>
+                    <div className='sub-modal-backdrop'></div>
+                    <div className='user-sub-modal-wrapper'>
+                        <div className='user-sub-modal-focus-lock'>
+                            <div className='user-sub-modal' id="user-sub-modal" ref={popUpRef} onClick={(e) => e.stopPropagation()}>
+                                <div className='user-sub-modal-uownssw-flex-wrapper' style={{ flex: `0 0 auto` }}>
+                                    <h1 className='user-sub-modal-uownssw-title'>You Own Servers!</h1>
+                                </div>
+                                <div className='user-sub-modal-form-content-uownssw global-scroll-thin-raw-attributes global-scroller-base' style={{ overflow: `hidden scroll`, paddingRight: `8px` }}>
+                                    <div className='usubm-uownssw-subtext'>
+                                        In order to delete or disable your account you must first transfer ownership of all servers that you own.
+                                    </div>
+                                    <div className='user-sub-modal-form-uownssw-sep'></div>
+                                </div>
+                                <form onSubmit={handleSubModalCloseOnOutsideClick}>
+                                    <div className='user-sub-modal-button-footer-container'>
+                                        <button id="okay-button" type='submit' className='user-sub-modal-submit-button'>
+                                            <div id="submit-button-text" className='look-filled-button-contents global-button-contents'>Okay</div>
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                    <div className="username-edit-sep"></div>
                 </div>
-                <div className="username-edit-button-sec">
-                    <button type="submit" className="username-edit-submit-button">Delete Account</button>
-                    <button type="submit" onClick={() => this.cancel = true} className="username-edit-cancel-button">Cancel</button>
-                </div>
-
-            </form>
-        </div>
-
-
-        ) : ("");
-
-
-        return (
-            <div id="edit-userInfo-model" className="edit-userInfo-model" >
-              {hasServers}
-              {delAcc}
-            </div>
-
+            </REACT_PORTAL >
         )
-
     }
 
+    else if (hasServers === false && isLoading === false) {
+
+        return (
+            <REACT_PORTAL wrapperId={'sub-modal'} classNameId={'subModal'} onClick={(e) => e.stopPropagation()}>
+                <div className="sub-modal-layer" onClick={(e) => handleSubModalCloseOnOutsideClick(e)}>
+                    <div className='sub-modal-backdrop'></div>
+                    <div className='user-sub-modal-wrapper'>
+                        <div className='user-sub-modal-focus-lock'>
+                            <div className='user-sub-modal' id="user-sub-modal" ref={popUpRef} onClick={(e) => e.stopPropagation()}>
+
+                                <form onSubmit={handleSubmit}>
+
+                                    <div className='user-sub-modal-header-container-remove-phone-number' style={{ flex: `0 0 auto` }}>
+                                        <h1 className='user-sub-modal-acc-rmv-header'>Delete Account</h1>
+                                    </div>
+
+                                    <div className='user-sub-modal-acc-rmv-form-content global-scroll-thin-raw-attributes global-scroller-base' style={{ overflow: `hidden scroll`, paddingRight: `8px` }}>
+                                        <div className='user-sub-modal-acc-rmv-card-warning-wrapper'>
+                                            <div className='user-sub-modal-acc-rmv-card-warning'>
+                                                Are you sure that you want to delete your account? This will immediately log you out of your account and you will not be able to log in again.
+                                            </div>
+                                        </div>
+                                        <div className='user-sub-modal-remove-phone-pw-wrap'>
+                                            <h2 className='usubm-field-header'>
+                                                Password
+                                            </h2>
+                                            <div className='user-sub-modal-password-wrapper'>
+                                                <input className="user-sub-modal-password-input-field" type="password" id="rpn-pwi" autoFocus={true}
+                                                    maxLength={999} value={password} autoComplete='off' onKeyUp={(e) => submissionBlocker(e)}
+                                                    spellCheck={false} onChange={(e) => setPassword(e.currentTarget.value)}
+                                                />
+                                            </div>
+                                            {ifPassWordError}
+                                        </div>
+                                        <div className='user-sub-modal-form-remove-phone-sep'></div>
+                                    </div>
+                                    <div className='user-sub-modal-button-footer-container'>
+                                        <button id="delete-submit-button" type='submit' className='user-sub-modal-submit-button' disabled>
+                                            <div id="submit-button-text" className='look-filled-button-contents global-button-contents'>Delete Account</div>
+                                        </button>
+                                        <button type='button' className='user-sub-modal-acc-rmv-cancel-button' onClick={(e) => handleSubModalCloseOnOutsideClick(e)}>
+                                            <div className='look-filled-button-contents global-button-contents'>Cancel</div>
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </REACT_PORTAL>
+        )
+    }
 
 }
+
 
 export default DeleteUserAccountForm;
