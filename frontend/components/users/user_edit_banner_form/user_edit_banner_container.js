bd1e58740c1c69a304a40d9a2af95a3db1fbf8a7
miker704 <miker704@gmail.com>
2023-09-22 02:40:58 -0400

    - Finalized Edit User Banner Modal Container.

--- frontend/components/users/user_edit_banner_form/user_edit_banner_container.js
+++ frontend/components/users/user_edit_banner_form/user_edit_banner_container.js
@@ -5,7 +5,6 @@ import { closeModal, openModal } from "../../../actions/modal_actions";
 import { removeSessionErrors, changeUserBanner } from "../../../actions/session_actions";
 import EditUserBanner from "./user_edit_banner_form";
 
-
 const mSTP = (state) => {
     return {
         currentUser: state.currentUser,
