57916ce3c2523f28f094af9441dd89f8839ef4c7
miker704 <miker704@gmail.com>
2023-09-22 02:39:10 -0400

    - Finalized Edit User PFP Modal.

--- frontend/components/users/user_edit_profile_pic_form/user_edit_pfp_form.jsx
+++ frontend/components/users/user_edit_profile_pic_form/user_edit_pfp_form.jsx
@@ -1,212 +1,261 @@
 import React from "react"
-import user_Default_PFP from '../../../../app/assets/images/discord_PFP.svg';
-class EditUserPFP extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            photo: '',
-            photo_url: this.props.currentUser.photo,
-            submit: false,
-            remove_PFP: false
+import REACT_PORTAL from "../../../utils/ReactPortal_api_util";
+import { useState, useRef, useEffect } from "react";
+import DefaultPFPSVG3 from '../../../../app/assets/images/defaultPFPSVG3.svg';
+import { CloseXIcon } from "../../front_end_svgs/Strife_svgs";
+
+const EditUserPFP = (props) => {
+
+    const popUpRef = useRef(null);
+    const [photo, setPhoto] = useState('');
+    const [photo_url, setPhotoUrl] = useState('');
+    const [remove_PFP, setRemovePFP] = useState(false);
+    const [renderButton, setRenderButton] = useState('');
+    const file_Input_Ref = useRef(null);
+    const [inSubmission, setInSubmission] = useState(false);
+
+    useEffect(() => {
+        setPhotoUrl(props.currentUser.photo);
+        if (props.currentUser.photo === undefined) {
+            setRenderButton(saveButton);
+        }
+        else {
+            setRenderButton(removeButton);
+        }
+
+        window.addEventListener('keyup', handleESC, false);
+
+        return function cleanUp () {
+            props.removeSessionErrors();
+            window.removeEventListener('keyup', handleESC, false);
 
         }
 
-        this.file_input = null;
-        this.cancel = false;
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleInput = this.handleInput.bind(this);
-        this.handleFileInput = this.handleFileInput.bind(this);
-        this.fileProcessingErrors = this.fileProcessingErrors.bind(this);
-        this.handleRemovePFPSubmission = this.handleRemovePFPSubmission.bind(this);
-    }
+    }, []);
 
 
-    componentDidUpdate (prevProps) {
-        if (this.state.submit && prevProps.currentUser !== this.props.currentUser) { return };
-    }
+
+    useEffect(() => {
+
+        if (props.currentUser.photo !== undefined) {
+            if (photo_url !== props.currentUser.photo) {
+                setRenderButton(saveButton);
+            }
+            else if (photo_url === props.currentUser.photo) {
+                setRenderButton(removeButton);
+            }
+        }
+        else {
+            setRenderButton(saveButton);
+        }
+    }, [photo_url]);
 
 
-    handleRemovePFPSubmission (e) {
+
+    const handleSubModalCloseOnOutsideClick = (e) => {
         e.preventDefault();
+        if (inSubmission === false) {
 
-        if (this.props.currentUser.photo) {
-            this.setState({
-                remove_PFP: true,
-                photo: null,
-                photo_url: ''
-            })
+            let modalToClose = document.getElementById("user-sub-modal-edit-pfp");
+            if (modalToClose) {
+                modalToClose.classList.add("transition-out");
+                Promise.all(modalToClose.getAnimations().map((animation) => animation.finished),)
+                    .then(() => {
+                        props.closeSubMod(props.formName);
+                        window.removeEventListener('keyup', handleESC, false);
+                    }, () => {
+                        props.closeSubMod(props.formName);
+                        window.removeEventListener('keyup', handleESC, false);
+                    });
+            }
+            else {
+                props.closeSubMod(props.formName);
+                window.removeEventListener('keyup', handleESC, false);
+            }
         }
-        let formData = new FormData();
-        formData.append('user[remove_PFP]', this.state.remove_PFP);
-        //check user location after updating if they are in a server broadcast a signal to rerender the server
-        this.props.changeUserPFP(this.props.currentUser.id, formData).then(() => {
-            App.StrifeCore.perform('transmit_to_other_channel', { currrentUserLocation: this.props.location.pathname })
-        });
-
     }
 
-    fileProcessingErrors () {
-        if (this.props.errors.includes('cannot process file')) {
-            return " - file is bad format/failed to process";
+    const handleESC = (e) => {
+        const keys = {
+            27: () => {
+                e.preventDefault();
+                handleSubModalCloseOnOutsideClick(e);
+            },
+        };
+        if (keys[e.keyCode]) {
+            keys[e.keyCode]();
+        }
+    }
+
+
+
+    const fileProcessingErrors = () => {
+        if (props.errors.includes('cannot process file')) {
+            return "file is bad format/failed to process";
         }
         return "";
     }
 
+    const handleRemovePFPSubmission = (e) => {
+        e.preventDefault();
+        props.removeSessionErrors();
+        let submitButton = document.getElementById('submit-pfp');
+        submitButton.disabled = true;
 
-    handleFileInput (e) {
+        if (props.currentUser.photo) {
+            setRemovePFP(true);
+            setPhoto(null);
+            setPhotoUrl('');
+
+        }
+        let formData = new FormData();
+        formData.append('user[remove_PFP]', remove_PFP);
+        //check user location after updating if they are in a server broadcast a signal to rerender the server
+        props.changeUserPFP(props.currentUser.id, formData).then(() => {
+            App.StrifeCore.perform('transmit_to_other_channel', { currrentUserLocation: props.location.pathname })
+            submitButton.disabled = false;
+        }, (error) => {
+            submitButton.disabled = false;
+        });
+    }
+
+    const handleFileInput = (e) => {
+
         const fileReader = new FileReader();
         const file = e.currentTarget.files[0];
 
         fileReader.onloadend = () => {
-            this.setState({
-                photo_url: fileReader.result,
-                photo: file
-            });
-
-
-            if (this.props.currentUser.photo !== undefined) {
-
-                if (this.state.photo_url !== this.props.currentUser.photo) {
-                    document.getElementById('rm-pfp').classList.add('is-hidden');
-                    document.getElementById('save-pfp').classList.remove('is-hidden');
-                }
-
-            }
-
+            setPhotoUrl(fileReader.result)
+            setPhoto(file);
         }
 
         if (file) {
             fileReader.readAsDataURL(file);
         }
         else {
-            this.setState({ photo_url: "", photo: null })
+            setPhotoUrl("");
+            setPhoto(null);
         }
 
-
-
     }
 
-    componentDidMount () {
-        this.file_input = document.querySelector("input[type=file]");
-    }
 
-    componentWillUnmount () {
-        this.props.removeSessionErrors()
-    }
-
-    handleInput (input) {
-        return (e) => { this.setState({ [input]: e.currentTarget.value }) }
-    }
-
-    handleSubmit (e) {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        if (this.cancel === true) {
-            this.props.removeSessionErrors();
-            return;
-        }
+        props.removeSessionErrors();
 
+        let submitButton = document.getElementById('submit-pfp');
+        submitButton.disabled = true;
+        setInSubmission(true);
 
         let formData = new FormData();
 
-
-
-        let submissionState = {};
-        formData.append('user[username]', this.props.currentUser.username);
-        if (this.state.photo) {
-            formData.append('user[photo]', this.state.photo);
-            submissionState = {
-                id: this.props.currentUser.id,
-                photo: this.state.photo
-            }
+        formData.append('user[username]', props.currentUser.username);
+        if (photo) {
+            formData.append('user[photo]', photo);
         }
 
-        this.props.changeUserPFP(this.props.currentUser.id, formData).then(() => {
-            App.StrifeCore.perform('transmit_to_other_channel', { currrentUserLocation: this.props.location.pathname })
+        props.changeUserPFP(props.currentUser.id, formData).then(() => {
+            App.StrifeCore.perform('transmit_to_other_channel', { currrentUserLocation: props.location.pathname });
+            handleSubModalCloseOnOutsideClick(e);
+        }, (error) => {
+            setInSubmission(false);
+            submitButton.disabled = false;
         });
-
-        this.setState({
-            submit: true,
-        })
     }
 
-    render () {
+
+    let ifFileError = props.errors.length > 0 && fileProcessingErrors() !== "" ? (
+        <span className='user-sub-modal-field-input-error'>
+            <span className='user-sub-modal-field-input-error-sep'>-</span>
+            {fileProcessingErrors()}
+        </span>
+    ) : ("");
 
 
-        let fileErrorTag = this.props.errors.length > 0 ? "field-error" : "";
+    let removeButton = (
+        <button id='submit-pfp' type='button' className={`user-sub-modal-edit-pfp-submit-button`}
+            onClick={(e) => { handleRemovePFPSubmission(e) }}>
+            <div id="submit-button-text" className='look-filled-button-contents global-button-contents'>Remove</div>
+        </button>
+    );
+    let saveButton = (
+        <button id='submit-pfp' type='submit' className={`user-sub-modal-edit-pfp-submit-button`}>
+            <div id="submit-button-text" className='look-filled-button-contents global-button-contents'>Apply</div>
+        </button>
+    );
+
+    return (
 
 
-        return (
+        <REACT_PORTAL wrapperId={'sub-modal'} classNameId={'subModal'} onClick={(e) => e.stopPropagation()}>
+            <div className="sub-modal-layer" onClick={(e) => handleSubModalCloseOnOutsideClick(e)}>
+                <div className='sub-modal-backdrop'></div>
+                <div className='user-sub-modal-wrapper'>
+                    <div className='user-sub-modal-focus-lock'>
+                        <div className='user-sub-modal-edit-pfp' id="user-sub-modal-edit-pfp" ref={popUpRef} onClick={(e) => e.stopPropagation()}>
+                            <div className='usub-edit-pfp-header' style={{ flex: `0 0 auto` }}>
+                                <div className='usub-edit-pfp-header-title'>
+                                    Change Your Avatar
+                                </div>
+                                <div className='usub-edit-pfp-header-title-subtext'>Upload a .jpg, .jpeg, .gif, or .png file</div>
+                                <button type='button' className='user-sub-modal-x-to-close-button' onClick={(e) => handleSubModalCloseOnOutsideClick(e)}>
+                                    <div className='global-button-contents'>
+                                        <CloseXIcon className="closeIconX" />
+                                    </div>
+                                </button>
+                            </div>
 
-            <div id="edit-userInfo-model" className="edit-userInfo-model" >
-                <div className="edit-username-header-section">
-                    <div className="edit-username-header">
-                        Change your Avatar
-                    </div>
-                    <div className="edit-username-header-info">
-                        Upload a .jpg, .jpeg, .gif, or .png file
-                    </div>
-                </div>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-container1">
+                            <form onSubmit={handleSubmit}>
 
-                        <div className="form-username-sec">
-                            <h5 className="form-username-header"> <label className={fileErrorTag}>Avatar{this.fileProcessingErrors()}</label></h5>
-                            <div className="username-form-input-sec">
-                                <div className="username-input-wrapper1">
+                                <div className='user-sub-modal-edit-pfp-scroll global-scroll-thin-raw-attributes global-scroller-base' style={{ overflow: `hidden scroll`, paddingRight: `8px` }}>
+                                    <div className="usub-edit-pfp-input-wrapper">
+                                        <h2 className={`usubm-field-header ${fileProcessingErrors() !== "" ? `usubm-field-input-error-color` : ``}`}>
+                                            Avatar
+                                            {ifFileError}
+                                        </h2>
+                                    </div>
+                                    <div className='usub-edit-pfp-display-container' onClick={(e) => file_Input_Ref.current.click()}>
+                                        {
+                                            photo_url ? (
+                                                <img className="usub-edit-pfp-display-img" src={photo_url} alt=" " />
+                                            ) : (
+                                                <img className={`usub-edit-pfp-display-img color-${props.currentUser.color_tag}`} src={DefaultPFPSVG3} alt=" " />
+                                            )
+                                        }
+                                        <div className="usub-edit-pfp-hover-hint">Upload New Avatar</div>
+                                        <img className="usub-img-upload-hint-icon" alt=" " />
+                                        <div className="usub-overlay-edit-pfp"></div>
+                                        <input type='file' accept=".jpg, .jpeg, .png, .gif" onChange={(e) => handleFileInput(e)} ref={file_Input_Ref} />
 
-                                    <div
-                                        className={`user-pfp-wrapper ${this.state.photo_url === undefined ||
-                                            this.state.photo_url === '' ? `svgwrap color-${this.props.currentUser.color_tag}` : ``}`}
-                                        onClick={() => this.file_input.click()}>
-                                        <p className="user-pfp-header">Upload new avatar</p>
-                                        <img className="img-upload-hint-icon" />
-                                        <img
-                                            className={`${this.state.photo_url === undefined ||
-                                                this.state.photo_url === '' ?
-                                                `user-pfp-svgwr` : `user-pfp`}`}
-                                            id="display-user-pfp"
-                                            src={`${this.state.photo_url === undefined ||
-                                                this.state.photo_url === '' ?
-                                                user_Default_PFP : this.state.photo_url}`}
-
-                                            alt={'defaultUserPFP'} />
-                                        {/* <img className="user-pfp" id="display-user-pfp" src={this.state.photo_url} alt={this.state.photo_url} /> */}
                                     </div>
 
-                                    <input type='file' accept=".jpg, .jpeg, .png, .gif" onChange={this.handleFileInput} />
+                                    <div className='user-sub-modal-form-sep'></div>
                                 </div>
-                            </div>
+
+                                <div className='user-sub-modal-edit-pfp-button-footer-container'>
+                                    <div className='usubm-edit-pfp-buttons-right'>
+                                        <button type='button' className='user-sub-modal-edit-pfp-cancel-button' onClick={(e) => handleSubModalCloseOnOutsideClick(e)}>
+                                            <div className='look-filled-button-contents global-button-contents'>Cancel</div>
+                                        </button>
+                                        {renderButton}
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-
-                        <div className="username-edit-sep"></div>
                     </div>
-                    <div className="username-edit-button-sec">
-                        <button type="submit" id='save-pfp'
-                            className={`username-edit-submit-button ${this.props.currentUser.photo === undefined ? `` : `is-hidden`}`}>
-                            Save
-                        </button>
-                        <button type="button" id='rm-pfp' onClick={(e) => this.handleRemovePFPSubmission(e)}
-                            className={`username-edit-submit-button ${this.props.currentUser.photo !== undefined ? `` : `is-hidden`}`}>
-                            Remove
-                        </button>
-                        <button type="submit" onClick={() => this.cancel = true} className="username-edit-cancel-button">Cancel</button>
-                    </div>
-
-
-
-                </form>
+                </div>
             </div>
-
-
-
-
-        )
-
-
-    }
+        </REACT_PORTAL>
+    )
 
 
 
 }
+
+
+
+
+
 
 export default EditUserPFP;
