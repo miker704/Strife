feef5f72ebb4722f4eb969c9e43e4e6d861e6d9d
miker704 <miker704@gmail.com>
2023-09-21 21:58:59 -0400

    finalized dm_member api util.js

--- frontend/utils/dm_member_api_util.js
+++ frontend/utils/dm_member_api_util.js
@@ -1,11 +1,16 @@
 
 
-export const createDmMember = (dm_member) => 
-$.ajax({ url: "/api/dm_members/", method: "POST", data: { dm_member: dm_member } });
+export const createDmMember = (dm_member) =>
+    $.ajax({ url: "/api/dm_members/", method: "POST", data: { dm_member: dm_member } });
 
+export const deleteDmMember = (dm_memberId, dm_member) =>
+    $.ajax({ url: `/api/dm_members/${dm_memberId}`, method: "DELETE", data: { dm_member: dm_member } });
 
-export const deleteDmMember = (dm_memberId,dm_member) => 
-$.ajax({ url: `/api/dm_members/${dm_memberId}`, method: "DELETE", data: { dm_member: dm_member } });
+export const fetchDmMemberShip = (dm_member) =>
+    $.ajax({ url: "/api/dm_members/getDmMember/", method: "GET", data: { dm_member: dm_member } });
 
-export const fetchDmMemberShipStatus = (dm_member) => 
-$.ajax({ url: "/api/dm_members/dms_status", method: "GET", data: { dm_member: dm_member } });
+export const fetchDmMemberShipData = (dm_member) =>
+    $.ajax({ url: `/api/dm_servers/${dm_member['dm_server_id']}/dm_members/getDmMemberData/`, method: "GET", data: { dm_member: dm_member } });
+
+export const fetchIsDmMember = (dm_member) =>
+    $.ajax({ url: `/api/dm_servers/${dm_member['dm_server_id']}/dm_members/isdmMember/`, method: "GET", data: { dm_member: dm_member } });
