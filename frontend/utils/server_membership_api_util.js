2e0036608512ece2e392b5e55347e0e59659c9f1
miker704 <miker704@gmail.com>
2023-09-23 01:14:44 -0400

    - Finalized Server Membership Api Util.js.

--- frontend/utils/server_membership_api_util.js
+++ frontend/utils/server_membership_api_util.js
@@ -1,4 +1,8 @@
 
 
 export const createServerMembership = (membership) => $.ajax({ url: "/api/server_memberships/", method: "POST", data: { server_membership: membership } });
-export const deleteServerMembership = (servermembershipId,servermembership) => $.ajax({ url: `/api/server_memberships/${servermembershipId}`, method: "DELETE", data: { server_membership: servermembership } });
+export const deleteServerMembership = (servermembershipId, servermembership) => $.ajax({ url: `/api/server_memberships/${servermembershipId}`, method: "DELETE", data: { server_membership: servermembership } });
+export const createServerMembershipViaInjectionOfDmMembers = (membership) => $.ajax({ url: "/api/server_memberships/injectviadms/", method: "POST", data: { server_membership: membership } });
+export const fetchServerMembership = (membership) => $.ajax({ url: "/api/server_memberships/memberdata/", method: "GET", data: { server_membership: membership } });
+export const fetchServerMembershipData = (membership) => $.ajax({ url: `/api/servers/${membership['server_id']}/server_memberships/memberdata/`, method: "GET", data: { server_membership: membership } });
+export const fetchIsServerMember = (membership) => $.ajax({ url: `/api/servers/${membership['server_id']}/server_memberships/isservermember/`, method: "GET", data: { server_membership: membership } });
