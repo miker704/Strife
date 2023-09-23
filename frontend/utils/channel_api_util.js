07ed2af482d502d429bbe0175bd7dd30294b661f
miker704 <miker704@gmail.com>
2023-09-23 01:12:23 -0400

    - Finalized Channel Api Util.js.

--- frontend/utils/channel_api_util.js
+++ frontend/utils/channel_api_util.js
@@ -1,11 +1,12 @@
 
 
-export const fetchChannel = (channelId) =>  $.ajax({ url: `/api/channels/${channelId}`, method: "GET" });
+export const fetchChannel = (channelId) => $.ajax({ url: `/api/channels/${channelId}`, method: "GET" });
 
 
-export const createChannel = (channel) =>  $.ajax({ url: `/api/channels/`, method: "POST", data:{channel:channel}  });
+export const createChannel = (channel) => $.ajax({ url: `/api/channels/`, method: "POST", data: { channel: channel } });
+export const createChannelsViaServerTemplate = (channelInjextion) => $.ajax({ url: `/api/channels/createviaservertemplate/`, method: "POST", data: { channel: channelInjextion } });
 
 
-export const updateChannel = (channel) =>  $.ajax({ url: `/api/channels/${channel.id}`, method: "PATCH", data:{channel:channel} });
+export const updateChannel = (channel) => $.ajax({ url: `/api/channels/${channel.id}`, method: "PATCH", data: { channel: channel } });
 
-export const deleteChannel = (channelId) =>  $.ajax({  url: `/api/channels/${channelId}`, method: "DELETE" });
+export const deleteChannel = (channelId) => $.ajax({ url: `/api/channels/${channelId}`, method: "DELETE" });
