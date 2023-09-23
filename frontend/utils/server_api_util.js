ea30648dc32f86961742259f77f4e90d557f5b16
miker704 <miker704@gmail.com>
2023-09-23 01:10:28 -0400

    finalized servers api util.js

--- frontend/utils/server_api_util.js
+++ frontend/utils/server_api_util.js
@@ -4,9 +4,6 @@ export const fetchServers = (userId) => $.ajax({ url: "/api/servers/", method: "
 
 export const fetchServer = (serverId) => $.ajax({ url: `/api/servers/${serverId}`, method: "GET" });
 
-// export const createServer = (server) => $.ajax({ url: `/api/servers/`, method: "POST", data: { server: server } });
-
-
 export const createServer = (server) =>
     $.ajax({
         url: `/api/servers/`,
@@ -15,10 +12,6 @@ export const createServer = (server) =>
         contentType: false,
         processData: false
     });
-
-
-
-
 
 export const updateServer = (serverId, formData) =>
     $.ajax({
@@ -29,12 +22,30 @@ export const updateServer = (serverId, formData) =>
         processData: false
     });
 
+export const changeServerBanner = (serverId, formData) =>
+    $.ajax({
+        url: `/api/servers/${serverId}/changeServerBanner/`,
+        method: "PATCH",
+        data: formData,
+        contentType: false,
+        processData: false
+    });
+
+export const changeServerInviteSplash = (serverId, formData) =>
+    $.ajax({
+        url: `/api/servers/${serverId}/changeServerInviteSplash/`,
+        method: "PATCH",
+        data: formData,
+        contentType: false,
+        processData: false
+    });
+
+
 export const deleteServer = (serverId) => $.ajax({ url: `/api/servers/${serverId}`, method: "DELETE" });
 
 export const joinServer = (inviteCode) => $.ajax({ url: `/api/servers/join`, method: "POST", data: { inviteCode } });
 
 export const verifyName = (server) => $.ajax({ url: `/api/servers/${server.id}/verifyDeletion/`, method: "PATCH", data: { server } });
-
 
 // export const exploreServers = () => $.ajax({ url: "/api/servers/", method: "GET" });
 export const exploreServers = (userId) => $.ajax({ url: "/api/exploreServers/", method: "GET", data: { userId: userId } });
