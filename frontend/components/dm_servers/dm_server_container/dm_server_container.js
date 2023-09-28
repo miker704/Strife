e8bc0d1dfb44a87b9f74abb7a4b590870a49c2ac
miker704 <miker704@gmail.com>
2023-09-28 13:16:01 -0400

    - Finalized Dm Server Container.

--- frontend/components/dm_servers/dm_server_container/dm_server_container.js
+++ frontend/components/dm_servers/dm_server_container/dm_server_container.js
@@ -1,10 +1,13 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import { fetchDmServer, fetchDmServers, removeDmServer } from "../../../actions/dm_server_actions.js";
-import { createDmMessage } from "../../../actions/dm_messages_actions.js";
+import { fetchDmServer, fetchDmServers, removeDmServer, removeDmServerErrors } from "../../../actions/dm_server_actions.js";
+import { createDmMessage, removeDmMessageErrors } from "../../../actions/dm_messages_actions.js";
 import { selectDmMembers } from "../../../utils/selectors_api_util.js";
 import DmServer from "./dm_server.jsx";
 import { reSyncCurrentUser } from "../../../actions/session_actions.js";
+import { openModal, openModalWithProps, closeModal } from "../../../actions/modal_actions.js";
+
+
 
 const mSTP = (state, ownProps) => {
 
@@ -27,7 +30,11 @@ const mSTP = (state, ownProps) => {
         dmServerMembers: selectDmMembers(state, ownProps.match.params.dmServerId),
         dmServerErrors: state.errors.dmServer,
         dmServers: Object.values(state.entities.dmServers),
-
+        ui_modal: state.ui.modal,
+        users: Object.values(state.entities.users),
+        member: state.entities.dmServers[ownProps.match.params.dmServerId]?.other_o2o_member || {},
+        memberId: state.entities.dmServers[ownProps.match.params.dmServerId]?.other_o2o_member?.id || -1,
+        other_o2o_user: state.entities.users[state.entities.dmServers[ownProps.match.params.dmServerId]?.other_o2o_member?.id] || {},
     }
 }
 
@@ -38,7 +45,12 @@ const mDTP = (dispatch, ownProps) => {
         fetchDmServer: () => dispatch(fetchDmServer(ownProps.match.params.dmServerId)),
         createDmMessage: (dmmessage) => dispatch(createDmMessage(dmmessage)),
         reSyncCurrentUser: (currentUserId) => dispatch(reSyncCurrentUser(currentUserId)),
-        removeDmServer: (dmServerId) => dispatch(removeDmServer(dmServerId))
+        removeDmServer: (dmServerId) => dispatch(removeDmServer(dmServerId)),
+        removeDmServerErrors: () => dispatch(removeDmServerErrors()),
+        removeDmMessageErrors: () => dispatch(removeDmMessageErrors()),
+        openModal: modal => dispatch(openModal(modal)),
+        closeModal: () => dispatch(closeModal()),
+        openModalWithProps: (modal_with_props) => dispatch(openModalWithProps(modal_with_props))
 
     }
 }
