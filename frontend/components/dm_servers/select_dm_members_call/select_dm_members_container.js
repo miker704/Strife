e6beb7c3575c9b22a75efc3657e702e9e4929556
miker704 <miker704@gmail.com>
2023-09-27 23:21:47 -0400

    - Finalized Select DmServer Members for Call Modal Container.

--- frontend/components/dm_servers/select_dm_members_call/select_dm_members_container.js
+++ frontend/components/dm_servers/select_dm_members_call/select_dm_members_container.js
@@ -2,22 +2,22 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
 import { createDmMember } from '../../../actions/dm_member_actions';
-import { selectAllFriends } from '../../../utils/selectors_api_util';
-import { requestFriendships, removeFriendshipErrors , requestAllFriendships} from '../../../actions/friendship_actions'; 
+import { selectFriendsViaStatusType } from '../../../utils/selectors_api_util';
+import { requestFriendships, removeFriendshipErrors, requestAllFriendships } from '../../../actions/friendship_actions';
 import InviteToDMCallModal from './select_dm_members_call.jsx';
-import { openModal, openModalWithProps } from '../../../actions/modal_actions';
+import { openModal, openModalWithProps, closeModal } from '../../../actions/modal_actions';
 
-const mSTP = (state,ownProps) => {
+const mSTP = (state, ownProps) => {
     let dmMembers = [];
-    if(state.entities.dmServers[ownProps.match.params.dmServerId]){
-        dmMembers= state.entities.dmServers[ownProps.match.params.dmServerId].members;
+    if (state.entities.dmServers[ownProps.compParams.dmServerId]) {
+        dmMembers = state.entities.dmServers[ownProps.compParams.dmServerId].members;
     }
     return {
         currentUser: state.currentUser,
         currentUserId: state.session.id,
         dmServers: Object.values(state.entities.dmServers),
-        dmServer: state.entities.dmServers[ownProps.match.params.dmServerId],
-        friends: selectAllFriends(state, 3),
+        dmServer: state.entities.dmServers[ownProps.compParams.dmServerId],
+        friends: selectFriendsViaStatusType(state, 3),
         dmServerErrors: state.errors.dmServer,
         dmMembers: Object.values(dmMembers),
         errors: state.errors.friendship,
@@ -34,7 +34,9 @@ const mDTP = (dispatch) => {
         removeDmServerErrors: () => dispatch(removeDmServerErrors()),
         removeFriendshipErrors: () => dispatch(removeFriendshipErrors()),
         openModal: (modal) => dispatch(openModal(modal)),
-        openModalWithProps: (modal_with_props) => dispatch(openModalWithProps(modal_with_props))
+        openModalWithProps: (modal_with_props) => dispatch(openModalWithProps(modal_with_props)),
+        closeModal: () => dispatch(closeModal()),
+        
     }
 }
 
