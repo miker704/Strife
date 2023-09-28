d201815062893e19010f8915479c6a11f1239d38
miker704 <miker704@gmail.com>
2023-09-27 23:23:29 -0400

    - Finalized Select DmServer Header Nav Bar Container.

--- frontend/components/dm_servers/dm_server_header_nav_bar/dm_server_header_nav_bar_container.js
+++ frontend/components/dm_servers/dm_server_header_nav_bar/dm_server_header_nav_bar_container.js
@@ -2,8 +2,10 @@ import DmServerHeaderNavBar from "./dm_server_header_nav_bar";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { removeDmMessageErrors, } from "../../../actions/dm_messages_actions";
-import { removeDmServerErrors, updateDmServer, fetchDmServer } from "../../../actions/dm_server_actions";
+import { removeDmServerErrors, updateDmServer, fetchDmServer, updateDmServerName } from "../../../actions/dm_server_actions";
 import { selectDmMembers, extractDmServerProps } from "../../../utils/selectors_api_util.js";
+import { openModal, openModalWithProps } from "../../../actions/modal_actions";
+
 
 const mSTP = (state, ownProps) => {
     return {
@@ -12,21 +14,24 @@ const mSTP = (state, ownProps) => {
         // currentUser: state.entities.users[state.session.id],
         currentUser: state.currentUser,
         // dmServer: state.entities.dmServers[ownProps.match.params.dmServerId],
-        dmServer: extractDmServerProps(state,ownProps.match.params.dmServerId),
+        dmServer: extractDmServerProps(state, ownProps.match.params.dmServerId),
 
         dmServerId: ownProps.match.params.dmServerId,
-        dmServerMembers: selectDmMembers(state,ownProps.match.params.dmServerId),
-        dmServerUsers : state.entities.dmServers[ownProps.match.params.dmServerId].users
+        dmServerMembers: selectDmMembers(state, ownProps.match.params.dmServerId),
+        users: Object.values(state.entities.users),
     }
 
 }
 
-const mDTP = (dispatch,ownProps) => {
+const mDTP = (dispatch, ownProps) => {
     return {
         fetchDmServer: () => dispatch(fetchDmServer(ownProps.match.params.dmServerId)),
         removeDmServerErrors: () => dispatch(removeDmServerErrors()),
         removeDmMessageErrors: () => dispatch(removeDmMessageErrors()),
-        updateDmServer: (dmServerId, dmServer) => dispatch(updateDmServer(dmServerId, dmServer))
+        updateDmServer: (dmServerId, dmServer) => dispatch(updateDmServer(dmServerId, dmServer)),
+        updateDmServerName: (dmServerId, dmServer) => dispatch(updateDmServerName(dmServerId, dmServer)),
+        openModal: (modal) => dispatch(openModal(modal)),
+        openModalWithProps: (modal_Props) => dispatch(openModalWithProps(modal_Props)),
     }
 }
 
