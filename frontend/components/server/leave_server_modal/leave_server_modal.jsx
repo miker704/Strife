fb0c6f0d98ba03538dd379f7344fb5e3b02309a8
miker704 <miker704@gmail.com>
2023-09-27 23:10:40 -0400

    - Finalized Leave server modal.

--- frontend/components/server/leave_server_modal/leave_server_modal.jsx
+++ frontend/components/server/leave_server_modal/leave_server_modal.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useEffect, useRef } from "react";
+import REACT_PORTAL from "../../../utils/ReactPortal_api_util";
+
+
 
 const LeaveServerModal = (props) => {
     const popupRef = useRef();
 
 
     useEffect(() => {
-        // window.addEventListener('keyup', props.handleESC, false);
         window.addEventListener('keyup', handleESC, false);
 
         return function cleanUp () {
@@ -16,7 +18,6 @@ const LeaveServerModal = (props) => {
             //deleting server membership invokes channel membership deletion
             props.removeChannelErrors();
             // remove esc listener
-            // window.removeEventListener('keyup', props.handleESC, false);
             window.removeEventListener('keyup', handleESC, false);
 
         }
@@ -32,8 +33,7 @@ const LeaveServerModal = (props) => {
         // to rerender the server bar to remove the left server and prevent entering it again waving massive errors
         // if this step is not complete to ensure we invoke a then() func to ensure that if the membership is deleted
         // rerun the render if delete membership fails user can still enter and try again with no errors
-
-        props.closeModal();
+        props.setRenderLeaveServer(false);
         props.history.push('/$/channels/@me')
         props.deleteServerMembership(props.currentUser.id, subState).then(() => {
             props.fetchUserServers(props.currentUser.id)
@@ -46,11 +46,8 @@ const LeaveServerModal = (props) => {
         const keys = {
             27: () => {
                 e.preventDefault();
-                document.getElementById("leave-server-modal").classList.add("transition-out");
-                setTimeout(() => {
-                    props.closeModal();
-                    window.removeEventListener('keyup', handleESC, false);
-                }, 300);
+                handleCloseOnOuterClick(e);
+                window.removeEventListener('keyup', handleESC, false);
 
             },
         };
@@ -61,42 +58,59 @@ const LeaveServerModal = (props) => {
 
     const handleCloseOnOuterClick = (e) => {
         e.preventDefault();
-        document.getElementById("leave-server-modal").classList.add("transition-out");
-        setTimeout(() => {
-            props.closeModal();
+        let modalToClose = document.getElementById("leave-server-modal");
+        if (modalToClose) {
+            modalToClose.classList.add("transition-out");
+            Promise.all(modalToClose.getAnimations().map((animation) => animation.finished),)
+                .then(() => {
+                    props.setRenderLeaveServer(false);
+                    window.removeEventListener('keyup', handleESC, false);
+                }, () => {
+                    props.setRenderLeaveServer(false);
+                    window.removeEventListener('keyup', handleESC, false);
+                });
+        }
+        else {
+            props.setRenderLeaveServer(false);
             window.removeEventListener('keyup', handleESC, false);
-        }, 300);
+        }
     }
 
 
-
     return (
-        <div className="leave-server-wrapper" onClick={(e) => handleCloseOnOuterClick(e)}>
-            <div className="leave-server-backdrop"></div>
-            <div className="leave-server-layer">
-                <div id="leave-server-focus-lock" className="leave-server-focus-lock" onClick={(e) => e.stopPropagation()} ref={popupRef}>
-                    <div className="leave-server-root" id="leave-server-modal">
-                        <div className="leave-server-flex" >
-                            <h2 className="leave-server-header">
-                                Leave '{`${props.server.server_name}`}'
-                            </h2>
-                        </div>
-                        <div className="leave-server-content-scroll-base" >
-                            <div className="leave-server-text">
-                                Are you sure you want to leave {` `}
-                                <strong>{`${props.server.server_name}`}</strong>
-                                ? You won't be able to rejoin this server unless you are re-invited.
+        <REACT_PORTAL wrapperId={'sub-modal-2'} classNameId={'subModal'} onClick={(e) => e.stopPropagation()}>
+
+            <div className="leave-server-wrapper" onClick={(e) => handleCloseOnOuterClick(e)}>
+                <div className="leave-server-backdrop"></div>
+                <div className="leave-server-layer">
+                    <div id="leave-server-focus-lock" className="leave-server-focus-lock" onClick={(e) => e.stopPropagation()} ref={popupRef}>
+                        <div className="leave-server-root" id="leave-server-modal">
+                            <div className="leave-server-flex" >
+                                <h2 className="leave-server-header">
+                                    Leave '{`${props.server.server_name}`}'
+                                </h2>
                             </div>
-                            <div className="leave-server-sep"></div>
+                            <div className="leave-server-content-scroll-base global-scroller-base global-scroll-thin-raw-attributes" style={{ overflow: "hidden scroll", paddingRight: `8px` }}>
+                                <div className="leave-server-text">
+                                    Are you sure you want to leave {` `}
+                                    <strong>{`${props.server.server_name}`}</strong>
+                                    ? You won't be able to rejoin this server unless you are re-invited.
+                                </div>
+                                <div className="leave-server-sep"></div>
+                            </div>
+                            <form onSubmit={handleLeaveServer} className="leave-server-button-flex-wrapper">
+                                <button type="submit" className="leave-server-submit-button">
+                                    <div className="leave-server-button-contents">Leave Server</div>
+                                </button>
+                                <button type="button" onClick={(e) => handleCloseOnOuterClick(e)} className="leave-server-cancel-button">
+                                    <div className="leave-server-button-contents">Cancel</div>
+                                </button>
+                            </form>
                         </div>
-                        <form onSubmit={handleLeaveServer} className="leave-server-button-flex-wrapper">
-                            <button type="submit" className="leave-server-submit-button">Leave Server</button>
-                            <button type="button" onClick={(e) => handleCloseOnOuterClick(e)} className="leave-server-cancel-button">Cancel</button>
-                        </form>
                     </div>
                 </div>
             </div>
-        </div>
+        </REACT_PORTAL >
 
 
     )
