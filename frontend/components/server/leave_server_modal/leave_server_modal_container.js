f4f08684540ff4a11a4828cedb6a595a1b16c7f6
miker704 <miker704@gmail.com>
2023-09-27 23:10:57 -0400

    - Finalized Leave server modal container.

--- frontend/components/server/leave_server_modal/leave_server_modal_container.js
+++ frontend/components/server/leave_server_modal/leave_server_modal_container.js
@@ -1,34 +1,27 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import { fetchChannel,  removeChannelErrors } from "../../../actions/channel_actions.js";
+import { removeChannelErrors } from "../../../actions/channel_actions.js";
 import { fetchServer, fetchServers, removeServerErrors } from "../../../actions/server_actions.js";
 import { deleteServerMembership } from "../../../actions/server_membership_actions.js";
-import { closeModal } from "../../../actions/modal_actions.js";
-import { handleKeyUp } from "../../../utils/modal_api_util";
 import LeaveServerModal from "./leave_server_modal.jsx";
 
-const extractServerProps = (state, ownProps) => {
-    let locationString = ownProps.location.pathname;
-    let newLoc = locationString.split('/$/channels/').join('').split('/');
-    return newLoc;
-}
+// const extractServerProps = (state, ownProps) => {
+//     let locationString = ownProps.location.pathname;
+//     let newLoc = locationString.split('/$/channels/').join('').split('/');
+//     return newLoc;
+// }
 
 
 
 const mSTP = (state, ownProps) => {
 
-    const getIds = extractServerProps(state, ownProps);
+    // const getIds = extractServerProps(state, ownProps);
 
     return {
 
-
-        // currentUser: state.entities.users[state.session.id],
-        currentUser:state.currentUser,
-        server: state.entities.servers[parseInt(getIds[0])],
-        channel: state.entities.channels[parseInt(getIds[1])],
-        channels: Object.values(state.entities.channels),
-        currentChannelId: getIds[1],
-        serverId: getIds[0],
+        currentUser: state.currentUser,
+        server: state.entities.servers[ownProps.serverParams.serverId],
+        serverId: ownProps.serverParams.serverId,
         errors: state.errors.server,
         channelErrors: state.errors.channel,
         sessionErrors: state.errors.session,
@@ -40,8 +33,6 @@ const mSTP = (state, ownProps) => {
 
 const mDTP = (dispatch, ownProps) => {
 
-
-
     return {
 
         //server api functions
@@ -49,21 +40,14 @@ const mDTP = (dispatch, ownProps) => {
         fetchUserServers: (user) => dispatch(fetchServers(user)),
         removeServerErrors: () => dispatch(removeServerErrors()),
 
-
         //channel api functions
 
-        fetchChannel: (channelId) => { dispatch(fetchChannel(channelId)) },
         removeChannelErrors: () => dispatch(removeChannelErrors()),
 
         //server membership api functions
 
         deleteServerMembership: (servermembershipId, servermembership) =>
             dispatch(deleteServerMembership(servermembershipId, servermembership)),
-
-       
-        // modal api functions
-        closeModal: () => dispatch(closeModal()),
-        handleESC: (e) => handleKeyUp(e),
 
     }
 }
