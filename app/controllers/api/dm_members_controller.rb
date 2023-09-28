1d94040d5b74f0e203abc390b0bc445a6953fd08
miker704 <miker704@gmail.com>
2023-09-27 22:34:47 -0400

    - Finalized Dm Members Controller.rb for patch v4.

--- app/controllers/api/dm_members_controller.rb
+++ app/controllers/api/dm_members_controller.rb
@@ -18,8 +18,18 @@ class Api::DmMembersController < ApplicationController
     
     def show
         @dm_membership = DmMember.find_by(dm_member_params)
+        @dm_server = DmServer.find_by(id: @dm_membership.dm_server_id)
         if @dm_membership
-            # render 'api/dm_members/show'
+            render :show
+        else
+            render json: ['No DM Server Membership Detected!'], status: 404
+        end
+    end
+
+
+    def is_dm_member 
+        @dm_membership = DmMember.find_by(dm_member_params)
+        if @dm_membership
             render json: true
         else
             render json: false
@@ -30,7 +40,8 @@ class Api::DmMembersController < ApplicationController
         @new_Dm_Member = User.find_by(id: new_dm_member.dm_member_id)
         @response_Message = "Welcome @#{@new_Dm_Member.username} to the chat!"
         @dm_message_via_bot = DmMessage.create!(body: @response_Message, sender_id: 1, dm_server_id: @dm_server.id)
-        DmChannel.broadcast_to(dmServer, head:101 )
+        DmChannel.broadcast_to dmServer, head:101,
+        type: 'NEW_DMS_MEMBER',**from_template('api/dm_messages/show', dm_message: @dm_message_via_bot)
     end
 
     def async_redirect_Kick_User(dmServer,dmMember_kicked)
@@ -38,12 +49,12 @@ class Api::DmMembersController < ApplicationController
         @dms_owner = User.find_by(id: dmServer.owner_id)
         @response_Message = "@#{@dms_owner.username} has kicked @#{@user_kicked.username}"
         @dm_message_via_bot = DmMessage.create!(body: @response_Message, sender_id: 1, dm_server_id: @dm_server.id)
-        DmChannel.broadcast_to(dmServer, head:401, path: '/$/$TR!F3-INTRUSION-PREVENTION/' )
+        DmChannel.broadcast_to dmServer, head:401, path: '/$/$TR!F3-INTRUSION-PREVENTION/', type: 'REMOVE_DMS_MEMBER',
+            **from_template('api/dm_messages/show', dm_message: @dm_message_via_bot)
     end
 
     def destroy
         @dm_membership = DmMember.find_by(dm_member_params)
-    
         @dm_server = DmServer.find_by(id: @dm_membership.dm_server_id)
         if @dm_membership
 
@@ -52,6 +63,7 @@ class Api::DmMembersController < ApplicationController
             #before destroy membership invoke redirect to home
             @dm_membership.destroy
             render 'api/dm_servers/show'
+            # render json: {dm_server_id: @dm_membership.dm_server_id,  user_id: @dm_membership.dm_member_id}
         else
             render json:  @dm_membership.errors.full_messages, status: 401
         end
