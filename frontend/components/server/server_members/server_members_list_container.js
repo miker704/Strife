b01489aa8bc68152aaf715987bda32c6d8b6e515
miker704 <miker704@gmail.com>
2023-09-27 23:04:40 -0400

    - Finalized Server Member List Container.

--- frontend/components/server/server_members/server_members_list_container.js
+++ frontend/components/server/server_members/server_members_list_container.js
@@ -1,10 +1,11 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import { fetchChannel,removeChannelErrors } from "../../../actions/channel_actions.js";
-import { fetchServer, fetchServers, removeServerErrors} from "../../../actions/server_actions.js";
+import { fetchChannel, removeChannelErrors } from "../../../actions/channel_actions.js";
+import { fetchServer, fetchServers, removeServerErrors } from "../../../actions/server_actions.js";
 import { createServerMembership, deleteServerMembership } from "../../../actions/server_membership_actions.js";
 import { openModal } from "../../../actions/modal_actions.js";
 import ServerMembersList from "./server_members_list.jsx";
+
 
 const mSTP = (state, ownProps) => {
     return {
@@ -15,29 +16,29 @@ const mSTP = (state, ownProps) => {
         server: state.entities.servers[ownProps.match.params.serverId],
         channels: Object.values(state.entities.channels),
         currentChannelId: ownProps.match.params.channelId,
-        channelErrors: state.errors.channel
+        channelErrors: state.errors.channel,
     }
 
 }
 
-const mDTP = (dispatch,ownProps) => {
+const mDTP = (dispatch, ownProps) => {
     return {
         //server api functions
         fetchServer: serverId => dispatch(fetchServer(serverId)),
         fetchUserServers: (user) => dispatch(fetchServers(user)),
         fetchServers: () => dispatch(fetchServers()),
         removeServerErrors: () => dispatch(removeServerErrors()),
-        
+
         //channel api functions
-        
+
         fetchChannel: (channelId) => { dispatch(fetchChannel(channelId)) },
         removeChannelErrors: () => dispatch(removeChannelErrors()),
 
         //server membership api functions
 
         createServerMembership: (servermembership) => dispatch(createServerMembership(servermembership)),
-        deleteServerMembership: (servermembershipId, servermembership) => 
-        dispatch(deleteServerMembership(servermembershipId,servermembership)),
+        deleteServerMembership: (servermembershipId, servermembership) =>
+            dispatch(deleteServerMembership(servermembershipId, servermembership)),
 
         // modal api functions
         openModal: modal => dispatch(openModal(modal)),
