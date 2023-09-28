9668a7203e64b01bcb1c72518845c36f517d0dcf
miker704 <miker704@gmail.com>
2023-09-27 23:17:38 -0400

    - Finalized User Reducer.

--- frontend/reducers/user_reducer.js
+++ frontend/reducers/user_reducer.js
@@ -60,7 +60,7 @@ const userReducer = (state = {}, action) => {
         // case RECEIVE_DM_MEMBER:
         //     return Object.assign({}, state, { [action.dm_member.user.id]: action.dm_member.user });
         case RECEIVE_DM_SERVER:
-            return action.dmserver.users;
+            return action.dmserver.members;
 
         case REMOVE_FRIENDSHIP:
             newState = Object.assign({}, state);
