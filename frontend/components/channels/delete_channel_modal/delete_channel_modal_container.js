2cae0a1354cd2286393e942f5dec8918dde20383
miker704 <miker704@gmail.com>
2023-09-27 22:15:56 -0400

    - Finalized Delete Channel Modal Container.

--- frontend/components/channels/delete_channel_modal/delete_channel_modal_container.js
+++ frontend/components/channels/delete_channel_modal/delete_channel_modal_container.js
@@ -1,35 +1,23 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { fetchChannel, deleteChannel, removeChannelErrors } from "../../../actions/channel_actions.js";
-import { fetchServer, fetchServers, removeServerErrors} from "../../../actions/server_actions.js";
+import { fetchServer, fetchServers, removeServerErrors } from "../../../actions/server_actions.js";
 import { deleteChannelMembership } from "../../../actions/channel_membership_actions.js";
 import { closeModal } from "../../../actions/modal_actions.js";
 import { handleKeyUp } from "../../../utils/modal_api_util";
 import DeleteChannelModal from "./delete_channel_modal.jsx";
 
-
-
-const extractServerProps = (state, ownProps) => {
-    let locationString = ownProps.location.pathname;
-    let newLoc = locationString.split('/$/channels/').join('').split('/');
-    return newLoc;
-}
-
-
-
 const mSTP = (state, ownProps) => {
 
-    const getIds = extractServerProps(state, ownProps);
 
     return {
 
-        // currentUser: state.entities.users[state.session.id],
         currentUser: state.currentUser,
-        server: state.entities.servers[parseInt(getIds[0])],
-        channel: state.entities.channels[parseInt(getIds[1])],
+        server: state.entities.servers[ownProps.serverParams.serverId],
+        channel: state.entities.channels[ownProps.serverParams.channelId] || null,
         channels: Object.values(state.entities.channels),
-        currentChannelId: getIds[1],
-        serverId: getIds[0],
+        currentChannelId: ownProps.mod_Channel_ID,
+        serverId: ownProps.serverParams.serverId,
         errors: state.errors.server,
         channelErrors: state.errors.channel,
         servers: state.entities.servers
@@ -39,7 +27,6 @@ const mSTP = (state, ownProps) => {
 
 
 const mDTP = (dispatch, ownProps) => {
-
 
 
     return {
