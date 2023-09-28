0d00868acb5dd2cfe3d5f1b929862d6ea6caddec
miker704 <miker704@gmail.com>
2023-09-27 23:00:39 -0400

    - Finalized invite to dm server modal container.

--- frontend/components/dm_servers/invite_to_dm_server/invite_to_dm_server_container.js
+++ frontend/components/dm_servers/invite_to_dm_server/invite_to_dm_server_container.js
@@ -1,18 +1,19 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { selectAllFriends } from '../../../utils/selectors_api_util';
+import { selectAllFriends, selectFriendsViaStatusType } from '../../../utils/selectors_api_util';
 import { requestFriendships, removeFriendshipErrors , requestAllFriendships} from '../../../actions/friendship_actions'; 
 import { createDmServer, removeDmServerErrors } from '../../../actions/dm_server_actions';
 import { createDmMember } from '../../../actions/dm_member_actions';
 import InviteToDmModal from './invite_to_dm_server_modal';
+// import InviteToDmModal from './i2dm';
+import { closeModal, openModal, openModalWithProps } from '../../../actions/modal_actions';
 
 
 const mSTP = (state) => {
     return {
-        // currentUser: state.entities.users[state.session.id],
         currentUser: state.currentUser,
         dmServers: Object.values(state.entities.dmServers),
-        friends: selectAllFriends(state, 3),
+        friends: selectFriendsViaStatusType(state, 3),
         errors: state.errors.friendship,
         dmServerErrors: state.errors.dmServer
     }
@@ -25,7 +26,10 @@ const mDTP = (dispatch) => {
         createDmServer: (dmserver) => dispatch(createDmServer(dmserver)),
         createDmMember: (dm_member) => dispatch(createDmMember(dm_member)),
         removeDmServerErrors: () => dispatch(removeDmServerErrors()),
-        removeFriendshipErrors: () => dispatch(removeFriendshipErrors())
+        removeFriendshipErrors: () => dispatch(removeFriendshipErrors()),
+        closeModal: () => dispatch(closeModal()),
+        openModal: (modal) => dispatch(openModal(modal)),
+        openModalWithProps: (modal_Props) => dispatch(openModalWithProps(modal_Props)),
     }
 }
 
