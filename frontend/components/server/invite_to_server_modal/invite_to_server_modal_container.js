cc23e9563f2fa80249bcf6cdfe1b5baf794da40b
miker704 <miker704@gmail.com>
2023-09-28 11:07:36 -0400

    - Finalized Invite to Server Modal Container.

--- frontend/components/server/invite_to_server_modal/invite_to_server_modal_container.js
+++ frontend/components/server/invite_to_server_modal/invite_to_server_modal_container.js
@@ -2,64 +2,33 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { fetchChannel, removeChannelErrors } from "../../../actions/channel_actions.js";
 import { fetchServer, fetchServers, removeServerErrors } from "../../../actions/server_actions.js";
-import { createServerMembership } from "../../../actions/server_membership_actions.js";
+import { createServerMembership, createServerMembershipViaInjectionOfDmMembers } from "../../../actions/server_membership_actions.js";
 import { closeModal } from "../../../actions/modal_actions.js";
 import { handleKeyUp } from "../../../utils/modal_api_util";
 import { requestFriendships, removeFriendshipErrors, requestAllFriendships } from "../../../actions/friendship_actions.js";
+import { fetchDmServers, removeDmServerErrors } from "../../../actions/dm_server_actions.js";
 import { selectAllFriends } from "../../../utils/selectors_api_util.js";
 import InviteToServerModal from "./invite_to_server_modal.jsx";
 
-
-
-const extractServerProps = (state, ownProps) => {
-    let locationString = ownProps.location.pathname;
-    let newLoc = locationString.split('/$/channels/').join('').split('/');
-    return newLoc;
-}
-
-const getChannelName = (state, ownProps) => {
-    let channelName = "";
-    let locationString = ownProps.location.pathname;
-    let newLoc = locationString.split('/$/channels/').join('').split('/');
-
-    if (state.entities.servers[parseInt(newLoc[0])]) {
-        channelName = state.entities.channels[state.entities.servers[parseInt(newLoc[0])].general_channel_id].channel_name;
-    }
-    return channelName;
-}
-
-const getCurrentChannel = (state, ownProps) => {
-    let serverChannels = Object.values(state.entities.channels);
-    // const findThis = ownProps.mod_Channel_ID['ChannelId'];
-    const findThis = ownProps.mod_Channel_ID;
-    let currentChannelTarget = serverChannels.find((channel) => channel.id === findThis);
-    return currentChannelTarget;
-
-}
-
 const mSTP = (state, ownProps) => {
-
-    const getIds = extractServerProps(state, ownProps);
-    // let getGeneralChannelName = getChannelName(state, ownProps);
-    // const currentChannelTarget = getCurrentChannel(state, ownProps);
     return {
 
         currentUser: state.currentUser,
-        server: state.entities.servers[parseInt(getIds[0])],
-        channel: state.entities.channels[parseInt(getIds[1])],
-
-        // currentChannel: currentChannelTarget,
-        currentChannel: state.entities.channels[parseInt(ownProps.mod_Channel_ID)],
-
-        // generalChannelName: getGeneralChannelName,
+        server: state.entities.servers[ownProps.serverParams.serverId],
+        channel: state.entities.channels[state.ui.modalProps.ChannelId] || null,
+        currentChannel: state.entities.channels[parseInt(state.ui.modalProps.ChannelId)] || null,
+        currentChannelId: state.ui.modalProps.ChannelId || -1,
+        serverId: ownProps.serverParams.serverId,
+        mod_Channel_ID: state.ui.modalProps.ChannelId || -1,
+        i2smType : state.ui.modalProps.ChannelId === -1 ? "CDDM" : "NORMAL",  
         channels: Object.values(state.entities.channels),
-        currentChannelId: getIds[1],
-        serverId: getIds[0],
         errors: state.errors.server,
         channelErrors: state.errors.channel,
         friendShipErrors: state.errors.friendship,
         servers: state.entities.servers,
         friends: selectAllFriends(state, 3),
+        dmServers: state.entities.dmServers,
+        channel_belongs_to_server_id: state.ui.modalProps.channel_belongs_to_server_id || -1,
 
     }
 }
@@ -75,9 +44,17 @@ const mDTP = (dispatch, ownProps) => {
         fetchServers: () => dispatch(fetchServers()),
         removeServerErrors: () => dispatch(removeServerErrors()),
 
+        //dmserver fetches
+        fetchDmServers: (userId) => dispatch(fetchDmServers(userId)),
+        removeDmServerErrors: () => dispatch(removeDmServerErrors()),
+
+        //friend ship stuff
         removeFriendshipErrors: () => dispatch(removeFriendshipErrors()),
         requestAllFriendships: () => dispatch(requestAllFriendships()),
         requestFriendships: () => dispatch(requestFriendships()),
+
+
+
 
         //channel api functions
 
@@ -87,6 +64,7 @@ const mDTP = (dispatch, ownProps) => {
         //server membership api functions
 
         createServerMembership: (servermembership) => dispatch(createServerMembership(servermembership)),
+        createServerMembershipViaInjectionOfDmMembers: (servermembership) => dispatch(createServerMembershipViaInjectionOfDmMembers(servermembership)),
         // modal api functions
         closeModal: () => dispatch(closeModal()),
         handleESC: (e) => handleKeyUp(e),
