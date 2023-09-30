08061299ecda19652db0cc72cc779c133e048fa5
miker704 <miker704@gmail.com>
2023-09-30 17:17:29 -0400

    - Finalized dm messages with new style formating.

--- frontend/components/dm_servers/dms_messages/dm_messages.jsx
+++ frontend/components/dm_servers/dms_messages/dm_messages.jsx
@@ -157,93 +157,16 @@ const DMMessages = (props) => {
     return (
         <>
             {
-                    props.dmMsgIdx === 0 || props.dmMessage.seconds - props.DmMessages[props.dmMsgIdx - 1].seconds >= 300 || props.dmMessage.sender_id !== props.DmMessages[props.dmMsgIdx - 1].sender_id ?
-                        (
-                            <>
-                                {
-                                    props.dmMsgIdx === 0 || props.dmMessage.date !== props.DmMessages[props.dmMsgIdx - 1].date ? (
-                                        <div className="chat-divider">
-                                            <span className="chat-divider-content">{`${props.dmMessage.date}`}</span>
-                                        </div>
-                                    ) : (null)
-                                }
-                                <li id={`dmMessage-id-${props.dmMessage.id}`} className={`chat-message-item ${showDmEdit === true ? `selected` : ``}`} key={props.dmMessage.id} ref={popUpRef} tabIndex={-1}
-                                    // onMouseEnter={(e) => { e.preventDefault(); popUpRef.current.focus({ preventScroll: true, focusVisible: false }); }}
-                                    // onMouseLeave={(e) => { popUpRef.current.blur(); setShift(false); }}
-                                    // onKeyUp={(e) => { if (popUpRef?.current.focus && !e.shiftKey) { setShift(false); } }}
-                                    // onKeyDown={(e) => { if (popUpRef?.current.focus && e.shiftKey) { setShift(true); } }}
-                                    onMouseEnter={(e) => {
-                                        document.addEventListener("keydown", expandButtonList);
-                                        document.addEventListener('keyup', compressButtonList);
-                                    }}
-                                    onMouseLeave={(e) => {
-                                        document.removeEventListener("keydown", expandButtonList);
-                                        document.removeEventListener('keyup', compressButtonList);
-                                    }}
-                                >
-
-                                    <div className="message-wrapper1" ref={buttonRef} tabIndex={-1}>
-
-                                        <div className="message-wrapper-contents">
-                                            {
-                                                props.dmMessageAuthor.photo === undefined ? (
-                                                    <img className={`chat-member-avatar-img color-${props.dmMessageAuthor.color_tag}`} src={DefaultPFPSVG3} alt=" "
-                                                        onClick={(e) => {
-                                                            setSelectedDmMsg(props.dmMessage)
-                                                            props.handleOpenSUOMM(e, props.dmMessageAuthor);
-                                                        }}
-                                                    />
-                                                ) : (
-                                                    <img className="chat-member-avatar-img" src={props.dmMessageAuthor.photo} alt=" "
-                                                        onClick={(e) => {
-                                                            setSelectedDmMsg(props.dmMessage)
-                                                            props.handleOpenSUOMM(e, props.dmMessageAuthor);
-                                                        }} />
-                                                )
-                                            }
-
-                                            <h3 className="chat-member-username-header">
-                                                <span className="chat-member-username-wrap">
-                                                    <span role="button" tabIndex={0} className="chat-member-username"
-                                                        onClick={(e) => {
-                                                            setSelectedDmMsg(props.dmMessage)
-                                                            props.handleOpenSUOMM(e, props.dmMessageAuthor, "userNameClicked");
-                                                        }}
-                                                    >
-                                                        {dmMessageAuthorName}
-                                                    </span>
-                                                </span>
-                                                <span className="chat-message-timestamp" data-tooltip-id="dmsgsTimeStamp-thread-tip" data-tooltip-place="top"
-                                                    data-tooltip-content={`${useToolTipFullTimeStamp(props.dmMessage.created_at)}`}>
-                                                    <time dateTime={`${props.dmMessage.created_at}`}>
-                                                        <i className="chat-message-timestamp-i"> — </i>
-                                                        {useFormatTimeStampMessageBody(props.dmMessage.created_at)}
-                                                    </time>
-                                                </span>
-                                            </h3>
-                                            {messageBody()}
-                                        </div>
-
-                                        <DmMessageSettingsContainer
-                                            openEdit={openEdit}
-                                            showDmEdit={showDmEdit}
-                                            dmMessageAuthor={props.dmMessageAuthor}
-                                            dmMembers={props.dmMembers}
-                                            dmMessage={props.dmMessage}
-                                            dmServerId={props.dmServerId}
-                                            key={`dm-message-controller-${props.dmMessage.id}`}
-                                            strifeBot={props.strifeBot}
-                                            setRenderDeleteDM={props.setRenderDeleteDM}
-                                            is_header={true}
-                                            setMsgCtrlTTHover={props.setMsgCtrlTTHover}
-                                            setMsgCtrlSRTTHover={props.setMsgCtrlSRTTHover}
-                                            _SHIFT={_SHIFT}
-                                        />
+                props.dmMsgIdx === 0 || props.dmMessage.seconds - props.DmMessages[props.dmMsgIdx - 1].seconds >= 300 || props.dmMessage.sender_id !== props.DmMessages[props.dmMsgIdx - 1].sender_id ?
+                    (
+                        <>
+                            {
+                                props.dmMsgIdx === 0 || props.dmMessage.date !== props.DmMessages[props.dmMsgIdx - 1].date ? (
+                                    <div className="chat-divider">
+                                        <span className="chat-divider-content">{`${props.dmMessage.date}`}</span>
                                     </div>
-                                </li>
-                            </>
-                        )
-                        : (
+                                ) : (null)
+                            }
                             <li id={`dmMessage-id-${props.dmMessage.id}`} className={`chat-message-item ${showDmEdit === true ? `selected` : ``}`} key={props.dmMessage.id} ref={popUpRef} tabIndex={-1}
                                 // onMouseEnter={(e) => { e.preventDefault(); popUpRef.current.focus({ preventScroll: true, focusVisible: false }); }}
                                 // onMouseLeave={(e) => { popUpRef.current.blur(); setShift(false); }}
@@ -258,19 +181,49 @@ const DMMessages = (props) => {
                                     document.removeEventListener('keyup', compressButtonList);
                                 }}
                             >
-                                <div className="message-wrapper1 follow-up-msg" ref={buttonRef} tabIndex={-1}>
+
+                                <div className="message-wrapper" ref={buttonRef} tabIndex={-1}>
+
                                     <div className="message-wrapper-contents">
-                                        <span className="chat-message-hover-timestamp" data-tooltip-id="dmsgsTimeStamp-thread-tip"
-                                            data-tooltip-place="top" data-tooltip-position-strategy="fixed"
-                                            data-tooltip-content={`${useToolTipFullTimeStamp(props.dmMessage.created_at)}`}>
-                                            <time dateTime={`${props.dmMessage.created_at}`}>
-                                                <i className="chat-message-timestamp-i"> [ </i>
-                                                {`${props.dmMessage.time}`}
-                                                <i className="chat-message-timestamp-i"> ] </i>
-                                            </time>
-                                        </span>
+                                        {
+                                            props.dmMessageAuthor.photo === undefined ? (
+                                                <img className={`chat-member-avatar-img color-${props.dmMessageAuthor.color_tag}`} src={DefaultPFPSVG3} alt=" "
+                                                    onClick={(e) => {
+                                                        setSelectedDmMsg(props.dmMessage)
+                                                        props.handleOpenSUOMM(e, props.dmMessageAuthor);
+                                                    }}
+                                                />
+                                            ) : (
+                                                <img className="chat-member-avatar-img" src={props.dmMessageAuthor.photo} alt=" "
+                                                    onClick={(e) => {
+                                                        setSelectedDmMsg(props.dmMessage)
+                                                        props.handleOpenSUOMM(e, props.dmMessageAuthor);
+                                                    }} />
+                                            )
+                                        }
+
+                                        <h3 className="chat-member-username-header">
+                                            <span className="chat-member-username-wrap">
+                                                <span role="button" tabIndex={0} className="chat-member-username"
+                                                    onClick={(e) => {
+                                                        setSelectedDmMsg(props.dmMessage)
+                                                        props.handleOpenSUOMM(e, props.dmMessageAuthor, "userNameClicked");
+                                                    }}
+                                                >
+                                                    {dmMessageAuthorName}
+                                                </span>
+                                            </span>
+                                            <span className="chat-message-timestamp" data-tooltip-id="dmsgsTimeStamp-thread-tip" data-tooltip-place="top"
+                                                data-tooltip-content={`${useToolTipFullTimeStamp(props.dmMessage.created_at)}`}>
+                                                <time dateTime={`${props.dmMessage.created_at}`}>
+                                                    <i className="chat-message-timestamp-i"> — </i>
+                                                    {useFormatTimeStampMessageBody(props.dmMessage.created_at)}
+                                                </time>
+                                            </span>
+                                        </h3>
                                         {messageBody()}
                                     </div>
+                                    <div className="message-accessories"></div>
                                     <DmMessageSettingsContainer
                                         openEdit={openEdit}
                                         showDmEdit={showDmEdit}
@@ -281,14 +234,62 @@ const DMMessages = (props) => {
                                         key={`dm-message-controller-${props.dmMessage.id}`}
                                         strifeBot={props.strifeBot}
                                         setRenderDeleteDM={props.setRenderDeleteDM}
-                                        is_header={false}
+                                        is_header={true}
                                         setMsgCtrlTTHover={props.setMsgCtrlTTHover}
                                         setMsgCtrlSRTTHover={props.setMsgCtrlSRTTHover}
                                         _SHIFT={_SHIFT}
                                     />
                                 </div>
                             </li>
-                        )
+                        </>
+                    )
+                    : (
+                        <li id={`dmMessage-id-${props.dmMessage.id}`} className={`chat-message-item ${showDmEdit === true ? `selected` : ``}`} key={props.dmMessage.id} ref={popUpRef} tabIndex={-1}
+                            // onMouseEnter={(e) => { e.preventDefault(); popUpRef.current.focus({ preventScroll: true, focusVisible: false }); }}
+                            // onMouseLeave={(e) => { popUpRef.current.blur(); setShift(false); }}
+                            // onKeyUp={(e) => { if (popUpRef?.current.focus && !e.shiftKey) { setShift(false); } }}
+                            // onKeyDown={(e) => { if (popUpRef?.current.focus && e.shiftKey) { setShift(true); } }}
+                            onMouseEnter={(e) => {
+                                document.addEventListener("keydown", expandButtonList);
+                                document.addEventListener('keyup', compressButtonList);
+                            }}
+                            onMouseLeave={(e) => {
+                                document.removeEventListener("keydown", expandButtonList);
+                                document.removeEventListener('keyup', compressButtonList);
+                            }}
+                        >
+                            <div className="message-wrapper follow-up-msg" ref={buttonRef} tabIndex={-1}>
+                                <div className="message-wrapper-contents">
+                                    <span className="chat-message-hover-timestamp" data-tooltip-id="dmsgsTimeStamp-thread-tip"
+                                        data-tooltip-place="top" data-tooltip-position-strategy="fixed"
+                                        data-tooltip-content={`${useToolTipFullTimeStamp(props.dmMessage.created_at)}`}>
+                                        <time dateTime={`${props.dmMessage.created_at}`}>
+                                            <i className="chat-message-timestamp-i"> [ </i>
+                                            {`${props.dmMessage.time}`}
+                                            <i className="chat-message-timestamp-i"> ] </i>
+                                        </time>
+                                    </span>
+                                    {messageBody()}
+                                </div>
+                                <div className="message-accessories"></div>
+                                <DmMessageSettingsContainer
+                                    openEdit={openEdit}
+                                    showDmEdit={showDmEdit}
+                                    dmMessageAuthor={props.dmMessageAuthor}
+                                    dmMembers={props.dmMembers}
+                                    dmMessage={props.dmMessage}
+                                    dmServerId={props.dmServerId}
+                                    key={`dm-message-controller-${props.dmMessage.id}`}
+                                    strifeBot={props.strifeBot}
+                                    setRenderDeleteDM={props.setRenderDeleteDM}
+                                    is_header={false}
+                                    setMsgCtrlTTHover={props.setMsgCtrlTTHover}
+                                    setMsgCtrlSRTTHover={props.setMsgCtrlSRTTHover}
+                                    _SHIFT={_SHIFT}
+                                />
+                            </div>
+                        </li>
+                    )
             }
         </>
     )
