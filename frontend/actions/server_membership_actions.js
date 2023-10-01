27a714885e5b981e4c4598c4ef6cf5a4c87c4b79
miker704 <miker704@gmail.com>
2023-10-01 16:35:11 -0400

    - Finalized Server MemberShip Actions.

--- frontend/actions/server_membership_actions.js
+++ frontend/actions/server_membership_actions.js
@@ -3,6 +3,7 @@ import * as ServerMembershipAPI from "../utils/server_membership_api_util.js"
 export const RECEIVE_SERVER_MEMBERSHIP = "RECEIVE_SERVER_MEMBERSHIP";
 export const REMOVE_SERVER_MEMBERSHIP = "REMOVE_SERVER_MEMBERSHIP";
 // export const INVITE_TO_SERVER = "INVITE_TO_SERVER";
+export const RECEIVE_SERVER_MEMBERSHIP_INJEXTION = "RECEIVE_SERVER_MEMBERSHIP_INJEXTION";
 
 
 export const receiveServerMembership = servermembership => {
@@ -11,6 +12,15 @@ export const receiveServerMembership = servermembership => {
         servermembership
     }
 }
+
+export const receiveServerMembershipViaInjextion = servermembership => {
+    return {
+        type: RECEIVE_SERVER_MEMBERSHIP_INJEXTION,
+        servermembership
+    }
+}
+
+
 export const removeServerMembership = (membershiphash) => {
     return {
         type: REMOVE_SERVER_MEMBERSHIP,
@@ -33,10 +43,21 @@ export const removeServerMembership = (membershiphash) => {
 // })
 
 
-export const createServerMembership = (servermembership) => (dispatch) => 
-ServerMembershipAPI.createServerMembership(servermembership).then((servermembership) => 
-dispatch(receiveServerMembership(servermembership)))
+export const createServerMembership = (servermembership) => (dispatch) =>
+    ServerMembershipAPI.createServerMembership(servermembership).then((servermembership) =>
+        dispatch(receiveServerMembership(servermembership)))
 
 
- export const deleteServerMembership = (servermembershipId,servermembership) => (dispatch) => 
- ServerMembershipAPI.deleteServerMembership(servermembershipId,servermembership).then((membershiphash) => dispatch(removeServerMembership(membershiphash)))
+// export const deleteServerMembership = (servermembershipId, servermembership) => (dispatch) =>
+//     ServerMembershipAPI.deleteServerMembership(servermembershipId, servermembership).then((membershiphash) => dispatch(removeServerMembership(membershiphash)))
+
+export const createServerMembershipViaInjectionOfDmMembers = (servermembership) => (dispatch) =>
+    ServerMembershipAPI.createServerMembershipViaInjectionOfDmMembers(servermembership).then((servermembership) =>
+        dispatch(receiveServerMembershipViaInjextion(servermembership)))
+
+
+export const deleteServerMembership = (servermembershipId, servermembership) => (dispatch, getState) =>
+    ServerMembershipAPI.deleteServerMembership(servermembershipId, servermembership).then(membershiphash => {
+        const state = getState();
+        return dispatch(removeServerMembership(Object.assign({}, membershiphash, { currentUserId: state.session.id })))
+    })
