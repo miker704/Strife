78d70573384693f233375a5cc01025aff8498249
miker704 <miker704@gmail.com>
2023-09-30 20:55:34 -0400

    - Upgraded rails action cable library file usage switched from action_cable to actioncable for rails 8.

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -1,13 +1,13 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the `rails generate channel` command.
 //
-//= require action_cable
+//= require actioncable
 //= require_self
 //= require_tree ./channels
 
 (function() {
   this.App || (this.App = {});
-
+  // ActionCable.logger.enabled = true;
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
