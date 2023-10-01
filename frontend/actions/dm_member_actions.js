cd84e549c160bd78d76b6c94ff223dfd1da7f3f3
miker704 <miker704@gmail.com>
2023-10-01 16:33:09 -0400

    - Finalized Dm Member Actions.

--- frontend/actions/dm_member_actions.js
+++ frontend/actions/dm_member_actions.js
@@ -1,15 +1,20 @@
 import * as DmMemberAPI from "../utils/dm_member_api_util.js";
-
-
 export const RECEIVE_DM_MEMBER = "RECEIVE_DM_MEMBER";
+export const FETCH_DM_MEMBERS = "FETCH_DM_MEMBERS";
 export const REMOVE_DM_MEMBER = "REMOVE_DM_MEMBER";
 export const RECEIVE_DM_MEMBERSHIP_STATUS = "RECEIVE_DM_MEMBERSHIP_STATUS";
-
 
 export const receiveDmMember = (dm_member) => {
   return {
     type: RECEIVE_DM_MEMBER,
     dm_member
+  }
+}
+
+export const fetchDmMembers = (dm_members) => {
+  return {
+    type: FETCH_DM_MEMBERS,
+    dm_members
   }
 }
 
@@ -27,11 +32,19 @@ export const fetchDmMSStatus = (dm_member) => {
   }
 }
 
+
 export const createDmMember = (dm_member) => (dispatch) =>
   DmMemberAPI.createDmMember(dm_member).then((dm_member) => dispatch(receiveDmMember(dm_member)))
 
-export const deleteDmMember = (dm_memberId, dm_member) => (dispatch) =>
-  DmMemberAPI.deleteDmMember(dm_memberId, dm_member).then((dm_member_hash) => dispatch(removeDmMember(dm_member_hash)))
+// export const deleteDmMember = (dm_memberId, dm_member) => (dispatch) =>
+//   DmMemberAPI.deleteDmMember(dm_memberId, dm_member).then((dm_member_hash) => dispatch(removeDmMember(dm_member_hash)))
 
-export const fetchDmMemberShipStatus = (dm_member) => (dispatch) =>
-  DmMemberAPI.fetchDmMemberShipStatus(dm_member).then(dm_member => dispatch(fetchDmMSStatus(dm_member)));
+export const fetchDmMemberShip = (dm_member) => (dispatch) =>
+  DmMemberAPI.fetchDmMemberShip(dm_member).then(dm_member => dispatch(fetchDmMSStatus(dm_member)));
+
+
+export const deleteDmMember = (dm_memberId, dm_member) => (dispatch, getState) =>
+  DmMemberAPI.deleteDmMember(dm_memberId, dm_member).then(dm_member_hash => {
+    const state = getState();
+    return dispatch(removeDmMember(Object.assign({}, dm_member_hash, { currentUserId: state.session.id })))
+  })
