b22e03216d57bc72f79549e9bb8c2f27248df772
miker704 <miker704@gmail.com>
2023-10-02 16:52:48 -0400

    - Finalized WebPackAnaylzer.js.

--- webpackAnaylzer.js
+++ webpackAnaylzer.js
@@ -1,45 +1,43 @@
-// // webpack.config.js
-// var path = require('path');
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-// module.exports = {
-//   entry: './frontend/strife.jsx',
+// webpack.config.js
+var path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+module.exports = {
+  entry: './frontend/strife.jsx',
 
-//   output: {
-//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-//     filename: './bundle.js',
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: [/\.jsx?$/],
-//         exclude: /(node_modules)/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/env', '@babel/react']
-//           }
-//         },
-//       },
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      },
 
-//       {
-//         test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-//         loader: 'url-loader' 
-//       }
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader'
+      }
 
 
-//     ]
-//   },
-//   devtool: 'source-map',
-//   resolve: {
-//     extensions: ['.js', '.jsx', '*'],
-//      alias: {
-//     '@mui/material': '@mui/material/modern',
-//     "@mui/styled-engine": "@mui/styled-engine-sc"
-
-//   }
-//   },
-//   plugins: [
-//     new BundleAnalyzerPlugin()
-//   ]
-// };
-
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*'],
+    alias: {
+      '@mui/material': '@mui/material/modern',
+      "@mui/styled-engine": "@mui/styled-engine-sc"
+    }
+  },
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ],
+};
