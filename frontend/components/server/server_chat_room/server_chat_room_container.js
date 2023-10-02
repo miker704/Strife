736dead2f79c0d2646b5b2c47e8f3fc485d0cfa2
miker704 <miker704@gmail.com>
2023-10-02 16:48:58 -0400

    - Finalized Server Chat Room Container.

--- frontend/components/server/server_chat_room/server_chat_room_container.js
+++ frontend/components/server/server_chat_room/server_chat_room_container.js
@@ -1,23 +1,28 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import { fetchChannel, deleteChannel, removeChannelErrors } from "../../../actions/channel_actions.js";
+import { fetchChannel, deleteChannel, removeChannelErrors, webSocketReceiveChannel } from "../../../actions/channel_actions.js";
 import { fetchServer, fetchServers, deleteServer, removeServerErrors, removeServer } from "../../../actions/server_actions.js";
-import { receiveMessage, createMessage, removeMessageErrors} from "../../../actions/message_actions.js";
+import { receiveMessage, createMessage, removeMessageErrors, removeMessage } from "../../../actions/message_actions.js";
 import { reSyncCurrentUser } from "../../../actions/session_actions.js";
 import ServerChatRoom from "./server_chat_room.jsx";
+// import ServerChatRoom from "./server_chat_room3.jsx";
 
 const mSTP = (state, ownProps) => {
 
+
+    const currentChannelId = ownProps.match.params.channelId;
+    const sortedChannelMessages = Object.values(state.entities.messages)
+        .filter(msg => msg.channel_id === parseInt(currentChannelId));
     //fails safe as switching to a different server crashes due to channel name being undefined
     // we can set this to null but it leaves the placeholder in message input of null when switch too fast 
     // so setting it to an empty string we can set placeholder with message # then the channel name
     let channelName = '';
-    if(state.entities.channels[ownProps.match.params.channelId]){
+    if (state.entities.channels[ownProps.match.params.channelId]) {
         channelName = state.entities.channels[ownProps.match.params.channelId].channel_name;
     }
 
     let channel_type = 1;
-    if(state.entities.channels[ownProps.match.params.channelId]){
+    if (state.entities.channels[ownProps.match.params.channelId]) {
         channel_type = state.entities.channels[ownProps.match.params.channelId].channel_type;
     }
 
@@ -29,8 +34,8 @@ const mSTP = (state, ownProps) => {
         },
         messages: Object.values(state.entities.messages),
         messageIds: Object.keys(state.entities.messages),
+        currentChannelMessages: sortedChannelMessages,
         currentUserId: state.session.id,
-        // currentUser: state.entities.users[state.session.id],
         currentUser: state.currentUser,
         serverId: ownProps.match.params.serverId,
         channelId: ownProps.match.params.channelId,
@@ -57,10 +62,13 @@ const mDTP = (dispatch, ownProps) => {
         fetchServer: () => { dispatch(fetchServer(ownProps.match.params.serverId)) },
         deleteServer: (serverId) => dispatch(deleteServer(serverId)),
         removeServer: (serverId) => dispatch(removeServer(serverId)),
-        fetchChannel: () => dispatch(fetchChannel(ownProps.match.params.channelId)),
+        // fetchChannel: () => dispatch(fetchChannel(ownProps.match.params.channelId)),
+        fetchChannel: (channelId) => dispatch(fetchChannel(channelId)),
         deleteChannel: (channelId) => dispatch(deleteChannel(channelId)),
+        webSocketReceiveChannel: (channelId) => dispatch(webSocketReceiveChannel(channelId)),
         createMessage: (message) => dispatch(createMessage(message)),
         receiveMessage: (message) => dispatch(receiveMessage(message)),
+        removeMessage: (messageId) => dispatch(removeMessage(messageId)),
         reSyncCurrentUser: (currentUser) => dispatch(reSyncCurrentUser(currentUser)),
         removeChannelErrors: () => dispatch(removeChannelErrors()),
         removeMessageErrors: () => dispatch(removeMessageErrors()),
