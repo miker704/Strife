96fabf2e14958fb19c1651926bac5ee8ac256fe4
miker704 <miker704@gmail.com>
2023-10-29 01:52:55 -0400

    comp set up

---
+++ frontend/components/shop/shop_header_nav_bar/shop_header_nav_bar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import { StrifeNitroBadgeIcon } from "../../front_end_svgs/Strife_svgs";
+
+
+const ShopHeaderNavBar = (props) => {
+    
+}
+
+export default ShopHeaderNavBar;
