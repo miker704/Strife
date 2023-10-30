69f9e43e32d8e0d354e72283c359d3525b96072d
miker704 <miker704@gmail.com>
2023-10-29 20:47:48 -0400

    added flex container

--- frontend/components/server/server_header_nav_bar/server_header_nav_bar.jsx
+++ frontend/components/server/server_header_nav_bar/server_header_nav_bar.jsx
@@ -49,129 +49,130 @@ const ServerHeaderNavBar = (props) => {
 
     return (
         <section className="server-header-bar">
-            <div className="server-bar-children">
-                <div id="normDm" className={`server-children-icon-wrapper`}>
+            <div className="server-header-nav-bar-upper-container">
+                <div className="server-bar-children">
+                    <div id="normDm" className={`server-children-icon-wrapper`}>
 
-                    {
-                        channelType === 1 ? (
-                            <HashIcon className={`icon-server-hashtag`} />
-                        ) : (
-                            <LoudSpeakerIcon className={`icon-server-mega-phone`} />
-                        )
-                    }
-                </div>
-
-                <div id="normal-chat" className={`server-hbar-name`}>
-
-                    <h3 className="server-hbar-name-header">{channelName}</h3>
-
-                </div>
-
-                <div className="shb-spacer"></div>
-
-            </div>
-
-            <div className="shb-tool-bar">
-
-                <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Threads'}
-                    onMouseEnter={(e) => setHovered(true)}
-                    onMouseLeave={(e) => setHovered(false)}>
-
-                    <ThreadsIcon className="icon-shnb-thread" />
-                </div>
-
-                <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Notification Settings'}
-                    onClick={(e) => setMuteNotifications(!muteNotifications)}
-                    onMouseEnter={(e) => setHovered(true)}
-                    onMouseLeave={(e) => setHovered(false)}
-                >
-                    {
-                        !muteNotifications ? (
-                            <NotificationBellIcon className="icon-shnb-ns" />
-                        ) : (
-
-                            strikethroughChance.current ? (
-                                <NotificationBellMutedRedIcon className="icon-shnb-ns" />
+                        {
+                            channelType === 1 ? (
+                                <HashIcon className={`icon-server-hashtag`} />
                             ) : (
-                                <NotificationBellMutedIcon className="icon-shnb-ns" />
+                                <LoudSpeakerIcon className={`icon-server-mega-phone`} />
                             )
-                        )
-                    }
+                        }
+                    </div>
+
+                    <div id="normal-chat" className={`server-hbar-name`}>
+
+                        <h3 className="server-hbar-name-header">{channelName}</h3>
+
+                    </div>
+
+                    <div className="shb-spacer"></div>
 
                 </div>
+                <div className="shb-tool-bar">
 
-                <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Pinned Messages'}
-                    onMouseEnter={(e) => setHovered(true)}
-                    onMouseLeave={(e) => setHovered(false)}>
-                    <PinnedIcon className="icon-shnb-pin-messages" />
-                </div>
+                    <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Threads'}
+                        onMouseEnter={(e) => setHovered(true)}
+                        onMouseLeave={(e) => setHovered(false)}>
 
-                <div
-                    className={`shb-tool-icon-wrapper`}
-                    data-tooltip-place="bottom"
-                    data-tooltip-id="thread-tip-shnb" data-tooltip-content={`${hideMembersToolTipMessage}`}
-                    onClick={() => props.isViz()}
-                    onMouseEnter={(e) => setHovered(true)}
-                    onMouseLeave={(e) => setHovered(false)}
-                >
-                    <HideMembersListIcon className={`icon-hide-members-list ${props._viz === false ? `selected` : ``}`} />
-                </div>
+                        <ThreadsIcon className="icon-shnb-thread" />
+                    </div>
 
-                <div className="shbar-search-bar-wrapper" >
-                    <div className="shbar-search-bar-inner-wrapper">
-                        <div className="shbar-search-bar">
-                            <div className="shbar-draft-edit">
-                                <div className="shbar-public-draft-edit">
-                                    <div className="shbar-public-draft-edit-placeholder">Search</div>
-                                </div>
-                                <div className="shbar-public-draft-editor-container">
-                                    <div className="shbar-public-draft-editor-container-placeholder">
-                                        <div className="shbar-data-contents">
-                                            <div className="shbar-data-block">
-                                                <div className="shbar-data-key-offset">
-                                                    <span className="shbar-data-key-offset2"></span>
+                    <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Notification Settings'}
+                        onClick={(e) => setMuteNotifications(!muteNotifications)}
+                        onMouseEnter={(e) => setHovered(true)}
+                        onMouseLeave={(e) => setHovered(false)}
+                    >
+                        {
+                            !muteNotifications ? (
+                                <NotificationBellIcon className="icon-shnb-ns" />
+                            ) : (
+
+                                strikethroughChance.current ? (
+                                    <NotificationBellMutedRedIcon className="icon-shnb-ns" />
+                                ) : (
+                                    <NotificationBellMutedIcon className="icon-shnb-ns" />
+                                )
+                            )
+                        }
+
+                    </div>
+
+                    <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Pinned Messages'}
+                        onMouseEnter={(e) => setHovered(true)}
+                        onMouseLeave={(e) => setHovered(false)}>
+                        <PinnedIcon className="icon-shnb-pin-messages" />
+                    </div>
+
+                    <div
+                        className={`shb-tool-icon-wrapper`}
+                        data-tooltip-place="bottom"
+                        data-tooltip-id="thread-tip-shnb" data-tooltip-content={`${hideMembersToolTipMessage}`}
+                        onClick={() => props.isViz()}
+                        onMouseEnter={(e) => setHovered(true)}
+                        onMouseLeave={(e) => setHovered(false)}
+                    >
+                        <HideMembersListIcon className={`icon-hide-members-list ${props._viz === false ? `selected` : ``}`} />
+                    </div>
+
+                    <div className="shbar-search-bar-wrapper" >
+                        <div className="shbar-search-bar-inner-wrapper">
+                            <div className="shbar-search-bar">
+                                <div className="shbar-draft-edit">
+                                    <div className="shbar-public-draft-edit">
+                                        <div className="shbar-public-draft-edit-placeholder">Search</div>
+                                    </div>
+                                    <div className="shbar-public-draft-editor-container">
+                                        <div className="shbar-public-draft-editor-container-placeholder">
+                                            <div className="shbar-data-contents">
+                                                <div className="shbar-data-block">
+                                                    <div className="shbar-data-key-offset">
+                                                        <span className="shbar-data-key-offset2"></span>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="shbar-mag-icon-container">
-                                <div className="shnb-mag-container">
-                                    <SearchMagIcon className="shnb-mag-icon-2-visible" />
-                                    <SearchXIcon className="shnb-mag-icon-2" />
+                                <div className="shbar-mag-icon-container">
+                                    <div className="shnb-mag-container">
+                                        <SearchMagIcon className="shnb-mag-icon-2-visible" />
+                                        <SearchXIcon className="shnb-mag-icon-2" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                {
-                    showUpdate && (
-                        <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Update Ready!'}
-                            onClick={(e) => handleRemoveUpdateIcon(e)}
-                            onMouseEnter={(e) => setHovered(true)}
-                            onMouseLeave={(e) => setHovered(false)}>
-                            <UpdateReadyIcon className="icon-shnb-update-ready" />
-                        </div>
-                    )
+                    {
+                        showUpdate && (
+                            <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Update Ready!'}
+                                onClick={(e) => handleRemoveUpdateIcon(e)}
+                                onMouseEnter={(e) => setHovered(true)}
+                                onMouseLeave={(e) => setHovered(false)}>
+                                <UpdateReadyIcon className="icon-shnb-update-ready" />
+                            </div>
+                        )
 
-                }
+                    }
 
-                <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Inbox'}
-                    onMouseEnter={(e) => setHovered(true)}
-                    onMouseLeave={(e) => setHovered(false)}>
-                    <InboxIcon className="icon-shnb-inbox" />
-                </div>
-
-                <a className="shnb-help-tool-bar" href="https://support.discord.com" target="_blank" rel="noreferrer noopener">
-                    <div className="shnb-help-tool-bar-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Help'}
+                    <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Inbox'}
                         onMouseEnter={(e) => setHovered(true)}
                         onMouseLeave={(e) => setHovered(false)}>
-                        <HelpIcon className="icon-shnb-help" />
+                        <InboxIcon className="icon-shnb-inbox" />
                     </div>
-                </a>
 
+                    <a className="shnb-help-tool-bar" href="https://support.discord.com" target="_blank" rel="noreferrer noopener">
+                        <div className="shnb-help-tool-bar-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Help'}
+                            onMouseEnter={(e) => setHovered(true)}
+                            onMouseLeave={(e) => setHovered(false)}>
+                            <HelpIcon className="icon-shnb-help" />
+                        </div>
+                    </a>
+
+                </div>
             </div>
             <Tooltip className="thread-tool-tip" id="thread-tip-shnb" place="bottom" closeOnResize={true} isOpen={hovered} />
         </section>
