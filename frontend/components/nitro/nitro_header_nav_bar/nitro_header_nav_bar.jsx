ef9b617efd227f2a7c5b2cacbef4fd880a8769b2
miker704 <miker704@gmail.com>
2023-10-29 20:48:01 -0400

    added flex container

--- frontend/components/nitro/nitro_header_nav_bar/nitro_header_nav_bar.jsx
+++ frontend/components/nitro/nitro_header_nav_bar/nitro_header_nav_bar.jsx
@@ -6,13 +6,15 @@ import { StrifeNitroBadgeIcon } from "../../front_end_svgs/Strife_svgs";
 const NitroHeaderNavBar = (props) => {
     return (
         <section className="nitro-header-nav-bar">
-            <div className="nitro-header-nav-bar-children">
-                <div className="nitro-header-nav-bar-icon-wrapper">
-                    <StrifeNitroBadgeIcon className="nhnbi-ball" height={24} width={24} />
-                </div>
-                <div className="nitro-header-nav-bar-title-wrapper">
-                    <div className="nitro-header-nav-bar-title-contents">
-                        Nitro
+            <div className="nitro-header-nav-bar-upper-container">
+                <div className="nitro-header-nav-bar-children">
+                    <div className="nitro-header-nav-bar-icon-wrapper">
+                        <StrifeNitroBadgeIcon className="nhnbi-ball" height={24} width={24} />
+                    </div>
+                    <div className="nitro-header-nav-bar-title-wrapper">
+                        <div className="nitro-header-nav-bar-title-contents">
+                            Nitro
+                        </div>
                     </div>
                 </div>
             </div>
