57559afedd89eb3776627dc9bda71aa22bc61dc8
miker704 <miker704@gmail.com>
2023-11-14 16:00:05 -0500

    - Started AvatarDecorationPreviewModal.

---
+++ frontend/components/strife_shop/avatar_decorations_preview_modal/avatar_decoration_preview_modal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import REACT_PORTAL from "../../../utils/ReactPortal_api_util";
+import { CloseXIcon, StrifeBotTagIcon, StrifeNitroBadgeIcon } from "../../front_end_svgs/Strife_svgs";
+import { returnUserOnlineActivityStatusBadgeMaskIMG } from "../../../utils/user_online_activity_status_badge_api_util";
+import { returnUserBadgeFillColor } from "../../../utils/user_status_badge_color_api_util";
+
+
+const AvatarDecorationPreviewModal = (props)=>{
+
+}
+
+export default AvatarDecorationPreviewModal;
