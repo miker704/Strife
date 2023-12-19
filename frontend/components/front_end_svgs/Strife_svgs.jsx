eb4c2826ad8bad2cb35414d72940011488688158
miker704 <miker704@gmail.com>
2023-12-19 18:11:20 -0500

    - Added new version of PenEditIcon, backed up old version under suffix old all components using it has been changed to the new version.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -449,6 +449,27 @@ export const PrivacyShieldIcon = (props) => (
 
 export const PenEditIcon = (props) => (
     <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={18}
+        height={18}
+        role="img"
+        fill="none"
+        aria-hidden="false"
+        aria-label="Edit Profile"
+        className=""
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <path
+            fill="currentColor"
+            d="m13.96 5.46 4.58 4.58a1 1 0 0 0 1.42 0l1.38-1.38a2 2 0 0 0 0-2.82l-3.18-3.18a2 2 0 0 0-2.82 0l-1.38 1.38a1 1 0 0 0 0 1.42ZM2.11 20.16l.73-4.22a3 3 0 0 1 .83-1.61l7.87-7.87a1 1 0 0 1 1.42 0l4.58 4.58a1 1 0 0 1 0 1.42l-7.87 7.87a3 3 0 0 1-1.6.83l-4.23.73a1.5 1.5 0 0 1-1.73-1.73Z"
+        />
+    </svg>
+)
+
+
+export const PenEditIconOld = (props) => (
+    <svg
         width={16}
         height={16}
         aria-hidden="true"
