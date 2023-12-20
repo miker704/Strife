9ca612088f1a3e3eddf946b3c25c2808e6236775
miker704 <miker704@gmail.com>
2023-12-19 22:45:30 -0500

    - Added new version of HeadPhoneIcon backed up old version under suffix old.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -1921,6 +1921,16 @@ export const MicroPhoneMutedIconOld = (props) => (
 )
 
 export const HeadPhoneIcon = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" width={20} height={20} aria-hidden="true" viewBox="0 0 24 24" className="svg-headphone" {...props}>
+        <path
+            fill="currentColor"
+            d="M12 3a9 9 0 0 0-8.95 10h1.87a5 5 0 0 1 4.1 2.13l1.37 1.97a3.1 3.1 0 0 1-.17 3.78 2.85 2.85 0 0 1-3.55.74 11 11 0 1 1 10.66 0c-1.27.71-2.73.23-3.55-.74a3.1 3.1 0 0 1-.17-3.78l1.38-1.97a5 5 0 0 1 4.1-2.13h1.86A9 9 0 0 0 12 3Z"
+        />
+    </svg>
+)
+
+
+export const HeadPhoneIconOld = (props) => (
     <svg width={20} height={20} aria-hidden="true" viewBox="0 0 24 24" className="svg-headphone" {...props}>
         <svg width={24} height={24} viewBox="0 0 24 24" {...props}>
             <path
