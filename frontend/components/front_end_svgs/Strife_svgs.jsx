4105f2534cdb8f541692ea0ed1e77e2f6f7ce669
miker704 <miker704@gmail.com>
2023-12-20 13:39:05 -0500

    - Added MessageRequestsEnvelopeIcon.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -3734,4 +3734,27 @@ export const PopOutWindowIcon = (props) => (
             d="M5 2a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3v-6a1 1 0 1 0-2 0v6a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h6a1 1 0 1 0 0-2H5Z"
         />
     </svg>
-) 
+)
+
+
+export const MessageRequestsEnvelopeIcon = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        fill="none"
+        aria-hidden="true"
+        className=""
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <path
+            fill="currentColor"
+            d="M1.16 5.02c-.1.28.04.58.29.74l10.27 6.85a.5.5 0 0 0 .56 0l10.27-6.85c.25-.16.38-.46.29-.74A3 3 0 0 0 20 3H4a3 3 0 0 0-2.84 2.02Z"
+        />
+        <path
+            fill="currentColor"
+            d="M23 8.8a.5.5 0 0 0-.78-.41l-9.53 6.35c-.42.28-.96.28-1.38 0L1.78 8.39A.5.5 0 0 0 1 8.8V18a3 3 0 0 0 3 3h16a3 3 0 0 0 3-3V8.8Z"
+        />
+    </svg>
+)
