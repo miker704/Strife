dc9bd25a06122bdd7d22bdc8f34ee1ed938223e2
miker704 <miker704@gmail.com>
2023-12-20 03:35:06 -0500

    - Added new version of SelectBoxCheckIcon backed up old version under suffix old.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -952,6 +952,18 @@ export const AddIcon = (props) => (
 
 
 export const SelectBoxCheckIcon = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} fill="none" role="img" className="selectRegionSvgCheckMark" aria-hidden="true" viewBox="0 0 24 24" {...props}>
+        <circle cx={12} cy={12} r={10} fill="#fff" />
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M12 23a11 11 0 1 0 0-22 11 11 0 0 0 0 22Zm5.7-13.3a1 1 0 0 0-1.4-1.4L10 14.58l-2.3-2.3a1 1 0 0 0-1.4 1.42l3 3a1 1 0 0 0 1.4 0l7-7Z"
+            clipRule="evenodd"
+        />
+    </svg>
+)
+
+export const SelectBoxCheckIconOld = (props) => (
     <svg width={20} height={20} role="img" className="selectRegionSvgCheckMark" aria-hidden="true" viewBox="0 0 24 24" {...props}>
         <circle cx={12} cy={12} r={8} fill="#fff" />
         <path
@@ -960,6 +972,9 @@ export const SelectBoxCheckIcon = (props) => (
         />
     </svg>
 )
+
+
+
 
 export const ESCICON = (props) => (
     <svg width={18} height={18} aria-hidden="true" viewBox="0 0 24 24" {...props}>
