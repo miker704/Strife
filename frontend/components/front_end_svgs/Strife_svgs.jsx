3fcc12bd31dc701554b7f89a5ece9f6d6e804a89
miker704 <miker704@gmail.com>
2023-12-20 10:22:27 -0500

    - Added new version of LeaveServerIcon backed up old version under suffix old.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -554,6 +554,8 @@ export const ReportRaidShieldIcon = (props) => (
 
 export const LeaveServerIcon = (props) => (
     <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
         width={18}
         height={18}
         aria-hidden="true"
@@ -564,7 +566,17 @@ export const LeaveServerIcon = (props) => (
     >
         <path
             fill="currentColor"
-            d="m10.418 13 2.29 2.294-1.416 1.412-4.706-4.715 4.708-4.699 1.413 1.416L10.41 11h11.539C21.446 5.955 17.177 2 12 2 6.486 2 2 6.487 2 12s4.486 10 10 10c5.177 0 9.446-3.954 9.949-9H10.418Z"
+            d="M9 12a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1Z"
+        />
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M2.75 3.02A3 3 0 0 1 5 2h10a3 3 0 0 1 3 3v7.64c0 .44-.55.7-.95.55a3 3 0 0 0-3.17 4.93l.02.03a.5.5 0 0 1-.35.85h-.05a.5.5 0 0 0-.5.5 2.5 2.5 0 0 1-3.68 2.2l-5.8-3.09A3 3 0 0 1 2 16V5a3 3 0 0 1 .76-1.98Zm1.3 1.95A.04.04 0 0 0 4 5v11c0 .36.2.68.49.86l5.77 3.08a.5.5 0 0 0 .74-.44V8.02a.5.5 0 0 0-.32-.46l-6.63-2.6Z"
+            clipRule="evenodd"
+        />
+        <path
+            fill="currentColor"
+            d="M15.3 16.7a1 1 0 0 1 1.4-1.4l4.3 4.29V16a1 1 0 1 1 2 0v6a1 1 0 0 1-1 1h-6a1 1 0 1 1 0-2h3.59l-4.3-4.3Z"
         />
     </svg>
 )
