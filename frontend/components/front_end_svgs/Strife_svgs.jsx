e566a52afa62b5830305774c50bc163f82cb651c
miker704 <miker704@gmail.com>
2023-12-20 07:54:55 -0500

    - Added new version of ChatAddFileIcon backed up old version under suffix old.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -2214,6 +2214,29 @@ export const ChatStickerIcon = (props) => (
 
 
 export const ChatAddFileIcon = (props) => (
+
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        className=""
+        width={24}
+        height={24}
+        fill="none"
+        aria-hidden="true"
+        {...props}
+    >
+        <circle cx={12} cy={12} r={10} fill="transparent" />
+        <path
+            fill="var(--interactive-normal)"
+            fillRule="evenodd"
+            d="M12 23a11 11 0 1 0 0-22 11 11 0 0 0 0 22Zm0-17a1 1 0 0 1 1 1v4h4a1 1 0 1 1 0 2h-4v4a1 1 0 1 1-2 0v-4H7a1 1 0 1 1 0-2h4V7a1 1 0 0 1 1-1Z"
+            className="attachButtonPlus"
+            clipRule="evenodd"
+        />
+    </svg>
+);
+
+export const ChatAddFileIconOld = (props) => (
     <svg width={24} height={24} viewBox="0 0 24 24" className="" {...props}>
         <path
             fill="currentColor"
@@ -2222,8 +2245,6 @@ export const ChatAddFileIcon = (props) => (
         />
     </svg>
 );
-
-
 
 export const ExploreCompassIcon = (props) => (
     <svg
