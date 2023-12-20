701dd1479f377150dc91c07a44b713a0d095caea
miker704 <miker704@gmail.com>
2023-12-20 09:17:31 -0500

    - Added new version of AddCircleIcon backed up old version under suffix old.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -118,6 +118,8 @@ export const SideBarTwitterIconOld = (props) => (
         </g>
     </svg>
 )
+
+
 export const SideBarTwitterIcon = (props) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -393,6 +395,28 @@ export const SettingsIcon_Medium_Old = (props) => (
 )
 
 export const AddCircleIcon = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={16}
+        height={16}
+        fill="none"
+        aria-hidden="true"
+        className=""
+        role="img"
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <circle cx={12} cy={12} r={10} fill="transparent" />
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M12 23a11 11 0 1 0 0-22 11 11 0 0 0 0 22Zm0-17a1 1 0 0 1 1 1v4h4a1 1 0 1 1 0 2h-4v4a1 1 0 1 1-2 0v-4H7a1 1 0 1 1 0-2h4V7a1 1 0 0 1 1-1Z"
+            clipRule="evenodd"
+        />
+    </svg>
+)
+
+export const AddCircleIconOld = (props) => (
     <svg
         width={16}
         height={16}
@@ -2240,7 +2264,7 @@ export const ChatStickerIcon = (props) => (
         height={24}
         aria-hidden="true"
         className=""
-        viewBox="0 0 20 20"
+        viewBox="0 0 24 24"
         {...props}
     >
         <path
