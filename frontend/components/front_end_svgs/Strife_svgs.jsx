5e7da1a32fb428fcdf5cc9c692fea38952b2d751
miker704 <miker704@gmail.com>
2023-12-20 08:17:49 -0500

    - Added new version of ChatStickerIcon backed up old version under suffix old.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -2234,6 +2234,30 @@ export const ChatGIFIconOld = (props) => (
 
 export const ChatStickerIcon = (props) => (
     <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        width={24}
+        height={24}
+        aria-hidden="true"
+        className=""
+        viewBox="0 0 20 20"
+        {...props}
+    >
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M6 2h12a4 4 0 0 1 4 4v7.5a.5.5 0 0 1-.5.5H19a5 5 0 0 0-5 5v2.5a.5.5 0 0 1-.5.5H6a4 4 0 0 1-4-4V6a4 4 0 0 1 4-4Zm.5 8a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3ZM19 8.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm-9.91 2.94a1 1 0 0 0-1.66 1.12 5.5 5.5 0 0 0 9.14 0 1 1 0 0 0-1.66-1.12 3.5 3.5 0 0 1-5.82 0Z"
+            clipRule="evenodd"
+        />
+        <path
+            fill="currentColor"
+            d="M21.66 16c.03 0 .05.03.04.06a3 3 0 0 1-.58.82l-4.24 4.24a3 3 0 0 1-.82.58.04.04 0 0 1-.06-.04V19a3 3 0 0 1 3-3h2.66Z"
+        />
+    </svg>
+);
+
+export const ChatStickerIconOld = (props) => (
+    <svg
         width={24}
         height={24}
         aria-hidden="true"
