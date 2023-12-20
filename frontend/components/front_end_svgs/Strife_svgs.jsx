fd6ffba9a502400da0690253420bb6361a43c027
miker704 <miker704@gmail.com>
2023-12-19 22:04:33 -0500

    - Added ExploreServersChevronIcon.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -3091,6 +3091,24 @@ export const ProfilePanelChevronIcon = (props) => (
     </svg>
 )
 
+export const ExploreServersChevronIcon = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={18}
+        height={18}
+        fill="none"
+        aria-hidden="true"
+        className=""
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <path
+            fill="currentColor"
+            d="M5.3 9.3a1 1 0 0 1 1.4 0l5.3 5.29 5.3-5.3a1 1 0 1 1 1.4 1.42l-6 6a1 1 0 0 1-1.4 0l-6-6a1 1 0 0 1 0-1.42Z"
+        />
+    </svg>
+)
+
 export const PersonShieldIcon = (props) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
