80d1c3cf43d57a4ffdae7b53d3e8e4abd592cdbf
miker704 <miker704@gmail.com>
2023-12-19 21:30:10 -0500

    - Added new version of GameControllerIcon backed up old version under suffix old.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -229,7 +229,6 @@ export const SideBarFaceBookIconOld = (props) => (
 )
 
 
-
 export const SideBarYouTubeIcon = (props) => (
     <svg width={16} height={16} aria-hidden="true" viewBox="0 0 24 24" {...props}>
         <path
@@ -243,7 +242,6 @@ export const SideBarYouTubeIcon = (props) => (
 )
 
 
-
 export const SideBarTikTokIcon = (props) => (
     <svg width={16} height={16} aria-hidden="true" viewBox="0 0 24 24" {...props}>
         <path
@@ -253,9 +251,6 @@ export const SideBarTikTokIcon = (props) => (
         />
     </svg>
 )
-
-
-
 
 
 // channel drop down icons
@@ -2052,6 +2047,26 @@ export const ExploreCompassIconOld = (props) => (
 
 
 export const GameControllerIcon = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        fill="none"
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+        className=""
+        {...props}
+    >
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M20.97 4.06c0 .18.08.35.24.43.55.28.9.82 1.04 1.42.3 1.24.75 3.7.75 7.09v4.91a3.09 3.09 0 0 1-5.85 1.38l-1.76-3.51a1.09 1.09 0 0 0-1.23-.55c-.57.13-1.36.27-2.16.27s-1.6-.14-2.16-.27c-.49-.11-1 .1-1.23.55l-1.76 3.51A3.09 3.09 0 0 1 1 17.91V13c0-3.38.46-5.85.75-7.1.15-.6.49-1.13 1.04-1.4a.47.47 0 0 0 .24-.44c0-.7.48-1.32 1.2-1.47l2.93-.62c.5-.1 1 .06 1.36.4.35.34.78.71 1.28.68a42.4 42.4 0 0 1 4.4 0c.5.03.93-.34 1.28-.69.35-.33.86-.5 1.36-.39l2.94.62c.7.15 1.19.78 1.19 1.47ZM20 7.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0ZM15.5 12a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3ZM5 7a1 1 0 0 1 2 0v1h1a1 1 0 0 1 0 2H7v1a1 1 0 1 1-2 0v-1H4a1 1 0 1 1 0-2h1V7Z"
+            clipRule="evenodd"
+        />
+    </svg>
+)
+
+export const GameControllerIconOld = (props) => (
     <svg width={24} height={24} aria-hidden="true" viewBox="0 0 24 24" className="" {...props}>
         <g fill="none" fillRule="evenodd">
             <path
