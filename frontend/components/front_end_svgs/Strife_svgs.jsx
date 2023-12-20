295e84d755ceedf1e0ac3b71f018c7b0cfdaff95
miker704 <miker704@gmail.com>
2023-12-20 07:08:58 -0500

    - Added new version of OwnerCrownIcon backed up old version under suffix old.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -1681,6 +1681,24 @@ export const AcceptFriendRequestIcon = (props) => (
 
 export const OwnerCrownIcon = (props) => (
     <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        aria-hidden="false"
+        className=""
+        fill="none"
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <path
+            fill="currentColor"
+            d="M5 18a1 1 0 0 0-1 1 3 3 0 0 0 3 3h10a3 3 0 0 0 3-3 1 1 0 0 0-1-1H5ZM3.04 7.76a1 1 0 0 0-1.52 1.15l2.25 6.42a1 1 0 0 0 .94.67h14.55a1 1 0 0 0 .95-.71l1.94-6.45a1 1 0 0 0-1.55-1.1l-4.11 3-3.55-5.33.82-.82a.83.83 0 0 0 0-1.18l-1.17-1.17a.83.83 0 0 0-1.18 0l-1.17 1.17a.83.83 0 0 0 0 1.18l.82.82-3.61 5.42-4.41-3.07Z"
+        />
+    </svg>
+)
+
+export const OwnerCrownIconOld = (props) => (
+    <svg
         width={24}
         height={24}
         aria-hidden="false"
@@ -1697,8 +1715,6 @@ export const OwnerCrownIcon = (props) => (
         />
     </svg>
 )
-
-
 
 export const StrifeBotTagIcon = (props) => (
     <svg
