fd00f2828d480fd4f56a140fbc9eba94fdf4268a
miker704 <miker704@gmail.com>
2023-12-20 03:41:27 -0500

    Added new version of HashIcon and LoudSpeakerIcon

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -721,6 +721,28 @@ export const CloseXIconOld = (props) => (
 
 export const HashIcon = (props) => (
     <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        width={24}
+        height={24}
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+        role="img"
+        className=""
+        {...props}
+    >
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M10.99 3.16A1 1 0 1 0 9 2.84L8.15 8H4a1 1 0 0 0 0 2h3.82l-.67 4H3a1 1 0 1 0 0 2h3.82l-.8 4.84a1 1 0 0 0 1.97.32L8.85 16h4.97l-.8 4.84a1 1 0 0 0 1.97.32l.86-5.16H20a1 1 0 1 0 0-2h-3.82l.67-4H21a1 1 0 1 0 0-2h-3.82l.8-4.84a1 1 0 1 0-1.97-.32L15.15 8h-4.97l.8-4.84ZM14.15 14l.67-4H9.85l-.67 4h4.97Z"
+            clipRule="evenodd"
+        />
+    </svg>
+
+)
+
+export const HashIconOld = (props) => (
+    <svg
         width={24}
         height={24}
         aria-hidden="true"
@@ -757,6 +779,30 @@ export const HashLockIcon = (props) => (
 
 export const LoudSpeakerIcon = (props) => (
     <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        fill="none"
+        aria-hidden="true"
+        className=""
+        viewBox="0 0 24 24"
+        role="img"
+        {...props}
+    >
+        <path
+            fill="currentColor"
+            d="M12 3a1 1 0 0 0-1-1h-.06a1 1 0 0 0-.74.32L5.92 7H3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h2.92l4.28 4.68a1 1 0 0 0 .74.32H11a1 1 0 0 0 1-1V3Zm3.1 17.75c-.58.14-1.1-.33-1.1-.92v-.03c0-.5.37-.92.85-1.05a7 7 0 0 0 0-13.5A1.11 1.11 0 0 1 14 4.2v-.03c0-.6.52-1.06 1.1-.92a9 9 0 0 1 0 17.5Z"
+        />
+        <path
+            fill="currentColor"
+            d="M15.16 16.51c-.57.28-1.16-.2-1.16-.83v-.14c0-.43.28-.8.63-1.02a3 3 0 0 0 0-5.04c-.35-.23-.63-.6-.63-1.02v-.14c0-.63.59-1.1 1.16-.83a5 5 0 0 1 0 9.02Z"
+        />
+    </svg>
+)
+
+
+export const LoudSpeakerIconOld = (props) => (
+    <svg
         width={24}
         height={24}
         aria-hidden="true"
@@ -775,7 +821,6 @@ export const LoudSpeakerIcon = (props) => (
         />
     </svg>
 )
-
 
 export const LoudSpeakerLockIcon = (props) => (
     <svg width={24} height={24} className="" viewBox="0 0 24 24" aria-hidden="true" role="img" {...props}>
