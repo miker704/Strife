849d04cc4db227adcc5c85ba8f3c1588cf106334
miker704 <miker704@gmail.com>
2023-12-19 21:26:48 -0500

    - Added new version of ExploreCompassIcon backed up old version under suffix old.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -2021,6 +2021,27 @@ export const ChatAddFileIcon = (props) => (
 
 
 export const ExploreCompassIcon = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        fill="none"
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+        className=""
+        {...props}
+    >
+        <path fill="currentColor" d="M12 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M23 12a11 11 0 1 1-22 0 11 11 0 0 1 22 0ZM7.74 9.3A2 2 0 0 1 9.3 7.75l7.22-1.45a1 1 0 0 1 1.18 1.18l-1.45 7.22a2 2 0 0 1-1.57 1.57l-7.22 1.45a1 1 0 0 1-1.18-1.18L7.74 9.3Z"
+            clipRule="evenodd"
+        />
+    </svg>
+)
+
+export const ExploreCompassIconOld = (props) => (
     <svg width={24} height={24} aria-hidden="true" viewBox="0 0 24 24" className="" {...props}>
         <path
             fill="currentColor"
