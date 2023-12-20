d07a954dbf8cb15dbb3005f7718d2504cc824340
miker704 <miker704@gmail.com>
2023-12-20 09:40:39 -0500

    - Added new version of AddEventIcon backed up old version under suffix old.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -484,6 +484,38 @@ export const AddEventIcon = (props) => (
         role="img"
         viewBox="0 0 24 24"
         {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        {...props}
+    >
+        <path
+            fill="currentColor"
+            d="M19 14a1 1 0 0 1 1 1v3h3a1 1 0 0 1 0 2h-3v3a1 1 0 0 1-2 0v-3h-3a1 1 0 1 1 0-2h3v-3a1 1 0 0 1 1-1Z"
+        />
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M22 13.67c0 .12-.33.17-.39.06A2.87 2.87 0 0 0 19 12a3 3 0 0 0-3 3v.5a.5.5 0 0 1-.5.5H15a3 3 0 0 0-3 3c0 1.2.7 2.1 1.73 2.61.11.06.06.39-.06.39H5a3 3 0 0 1-3-3v-9a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v3.67ZM5.5 12a.5.5 0 0 0-.5.5v3c0 .28.22.5.5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3Z"
+            clipRule="evenodd"
+        />
+        <path
+            fill="currentColor"
+            d="M7 1a1 1 0 0 1 1 1v.75c0 .14.11.25.25.25h7.5c.14 0 .25-.11.25-.25V2a1 1 0 1 1 2 0v.75c0 .14.11.25.25.25H19a3 3 0 0 1 3 3 1 1 0 0 1-1 1H3a1 1 0 0 1-1-1 3 3 0 0 1 3-3h.75c.14 0 .25-.11.25-.25V2a1 1 0 0 1 1-1Z"
+        />
+    </svg>
+
+)
+
+
+export const AddEventIconOld = (props) => (
+    <svg
+        width={24}
+        height={24}
+        aria-hidden="true"
+        className=""
+        role="img"
+        viewBox="0 0 24 24"
+        {...props}
     >
         <path fill="currentColor" d="M21 4V1h-2v3h-3v2h3v3h2V6h3V4h-3Z" />
         <path
@@ -493,6 +525,7 @@ export const AddEventIcon = (props) => (
         <path fill="currentColor" d="M7 11h5v5H7v-5Z" />
     </svg>
 )
+
 
 export const AppDirectoryBotIcon = (props) => (
     <svg
