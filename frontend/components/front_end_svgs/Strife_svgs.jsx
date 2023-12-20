7e8d1c42ccaa50bd5b01fb1f4a202e7fd4ceb323
miker704 <miker704@gmail.com>
2023-12-20 10:16:16 -0500

    - Added new version of ReportRaidShieldIcon backed up old version under suffix old.

--- frontend/components/front_end_svgs/Strife_svgs.jsx
+++ frontend/components/front_end_svgs/Strife_svgs.jsx
@@ -534,9 +534,11 @@ export const PenEditIcon = (props) => (
 
 export const ReportRaidShieldIcon = (props) => (
     <svg
+        xmlns="http://www.w3.org/2000/svg"
         width={24}
         height={24}
         aria-hidden="true"
+        fill="none"
         className=""
         role="img"
         viewBox="0 0 24 24"
@@ -544,7 +546,9 @@ export const ReportRaidShieldIcon = (props) => (
     >
         <path
             fill="currentColor"
-            d="M12 1 3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4Zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v8.8Z"
+            fillRule="evenodd"
+            d="M4.27 5.22A2.66 2.66 0 0 0 3 7.5v2.3c0 5.6 3.3 10.68 8.42 12.95.37.17.79.17 1.16 0A14.18 14.18 0 0 0 21 9.78V7.5c0-.93-.48-1.78-1.27-2.27l-6.17-3.76a3 3 0 0 0-3.12 0L4.27 5.22ZM6 7.68l6-3.66V12H6.22C6.08 11.28 6 10.54 6 9.78v-2.1Zm6 12.01V12h5.78A11.19 11.19 0 0 1 12 19.7Z"
+            clipRule="evenodd"
         />
     </svg>
 )
