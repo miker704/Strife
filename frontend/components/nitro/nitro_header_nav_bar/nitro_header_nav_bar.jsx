77ac39a43c32ffcbf39cec83d8b5bc961c73617d
miker704 <miker704@gmail.com>
2023-12-24 07:25:39 -0500

    - Added Warp To Update Loading Screen to nitro Header Nav Bar.

--- frontend/components/nitro/nitro_header_nav_bar/nitro_header_nav_bar.jsx
+++ frontend/components/nitro/nitro_header_nav_bar/nitro_header_nav_bar.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { useEffect, useState, useRef } from "react";
-import { StrifeNitroBadgeIcon } from "../../front_end_svgs/Strife_svgs";
-
+import { StrifeNitroBadgeIcon, UpdateReadyIcon } from "../../front_end_svgs/Strife_svgs";
+import { Tooltip } from "react-tooltip";
 
 const NitroHeaderNavBar = (props) => {
+    const [hovered, setHovered] = useState(false);
+    const [showUpdate, setShowUpdate] = useState(false);
+    useEffect(() => {
+        showUpdateProbability();
+    }, []);
+
+    const showUpdateProbability = () => {
+        let probability = Math.random() > 0.99;
+        probability === true ? setShowUpdate(true) : setShowUpdate(false);
+    }
+
+    const handleRemoveUpdateIcon = (e) => {
+        e.preventDefault();
+        setShowUpdate(false);
+        props.history.push(`/$/updating/`);
+    }
     return (
         <section className="nitro-header-nav-bar">
             <div className="nitro-header-nav-bar-upper-container">
@@ -17,7 +33,20 @@ const NitroHeaderNavBar = (props) => {
                         </div>
                     </div>
                 </div>
+                {
+                    showUpdate && (
+                        <div className="shb-tool-bar">
+                            <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Update Ready!'}
+                                onClick={(e) => handleRemoveUpdateIcon(e)}
+                                onMouseEnter={(e) => setHovered(true)}
+                                onMouseLeave={(e) => setHovered(false)}>
+                                <UpdateReadyIcon className="icon-shnb-update-ready" />
+                            </div>
+                        </div>
+                    )
+                }
             </div>
+            <Tooltip className="thread-tool-tip" id="thread-tip-shnb" place="bottom" closeOnResize={true} isOpen={hovered} />
         </section>
     )
 }
