0e96ee4b5f859438bb38e895a2da1f773e7bd33a
miker704 <miker704@gmail.com>
2023-12-24 07:26:00 -0500

    - Added Warp To Update Loading Screen to Nitro Header Nav Bar.

--- frontend/components/strife_shop/strife_shop_header_nav_bar/strife_shop_header_nav_bar.jsx
+++ frontend/components/strife_shop/strife_shop_header_nav_bar/strife_shop_header_nav_bar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { useEffect, useState, useRef } from "react";
-import { HelpIcon, InboxIcon, StrifeBannerLogo, StrifeNitroBadgeIcon } from "../../front_end_svgs/Strife_svgs";
+import { HelpIcon, InboxIcon, StrifeBannerLogo, UpdateReadyIcon } from "../../front_end_svgs/Strife_svgs";
 import { Tooltip } from "react-tooltip";
-
 
 const StrifeShopHeaderNavBar = (props) => {
     const [hovered, setHovered] = useState(false);
+    const [showUpdate, setShowUpdate] = useState(false);
+    useEffect(() => {
+        showUpdateProbability();
+    }, []);
+
+    const showUpdateProbability = () => {
+        let probability = Math.random() > 0.99;
+        probability === true ? setShowUpdate(true) : setShowUpdate(false);
+    }
+    const handleRemoveUpdateIcon = (e) => {
+        e.preventDefault();
+        setShowUpdate(false);
+        props.history.push(`/$/updating/`);
+    }
+
 
     return (
         <section className="shop-header-nav-bar">
@@ -17,6 +31,16 @@ const StrifeShopHeaderNavBar = (props) => {
                     </div>
                 </div>
                 <div className="shb-tool-bar">
+                    {
+                        showUpdate && (
+                            <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Update Ready!'}
+                                onClick={(e) => handleRemoveUpdateIcon(e)}
+                                onMouseEnter={(e) => setHovered(true)}
+                                onMouseLeave={(e) => setHovered(false)}>
+                                <UpdateReadyIcon className="icon-shnb-update-ready" />
+                            </div>
+                        )
+                    }
                     <div className="shb-tool-icon-wrapper" data-tooltip-place="bottom" data-tooltip-id="thread-tip-shnb" data-tooltip-content={'Inbox'}
                         onMouseEnter={(e) => setHovered(true)}
                         onMouseLeave={(e) => setHovered(false)}>
