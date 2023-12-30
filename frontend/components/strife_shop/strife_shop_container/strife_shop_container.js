8bd2ca5a5b15e98433d4f40f1c1d881e3e89fc2b
miker704 <miker704@gmail.com>
2023-12-30 15:42:14 -0500

    - Finished update for strife shop Page Container.

--- frontend/components/strife_shop/strife_shop_container/strife_shop_container.js
+++ frontend/components/strife_shop/strife_shop_container/strife_shop_container.js
@@ -1,14 +1,20 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { openModal, closeModal, openModalWithProps } from "../../../actions/modal_actions";
-import { reSyncCurrentUser } from "../../../actions/session_actions";
+import { reSyncCurrentUser, removeSessionErrors } from "../../../actions/session_actions";
+import { selectFriendsViaStatusType } from "../../../utils/selectors_api_util";
+import { removeFriendshipErrors, requestAllFriendships } from "../../../actions/friendship_actions";
 import StrifeShop from "./strife_shop";
 
 const mSTP = (state, ownProps) => {
 
     return {
         currentUserId: state.session.id,
+        friends: selectFriendsViaStatusType(state, 3),
+        friendErrors: state.errors.friendship,
+        errors: state.errors.session,
         currentUser: state.currentUser,
+        users: Object.values(state.entities.users),
         ui_modal: state.ui.modal,
         modalProps: state.ui.modalProps,
     }
@@ -18,6 +24,9 @@ const mSTP = (state, ownProps) => {
 const mDTP = (dispatch, ownProps) => {
     return {
         reSyncCurrentUser: (userId) => dispatch(reSyncCurrentUser(userId)),
+        removeFriendshipErrors: () => dispatch(removeFriendshipErrors()),
+        requestAllFriendships: () => dispatch(requestAllFriendships()),
+        removeSessionErrors: () => dispatch(removeSessionErrors()),
         // modal api functions
         openModal: modal => dispatch(openModal(modal)),
         closeModal: () => dispatch(closeModal()),
