f8a414ef06d97cda3659bd4fb8be6a8dfdce945f
miker704 <miker704@gmail.com>
2024-03-25 16:07:32 -0400

    adjusted fellowship profile effect

--- frontend/components/strife_shop/send_a_gift_modal/send_a_gift_modal.jsx
+++ frontend/components/strife_shop/send_a_gift_modal/send_a_gift_modal.jsx
@@ -820,7 +820,6 @@ const SendAGiftModal = (props) => {
         "springToonsFellowship": (
             <>
                 <img className="shop-item-spring-toons-fellowship-idle-critters" alt=" " style={{ top: `0px` }} />
-                <img className="shop-item-spring-toons-fellowship-idle-critter-vines" alt=" " style={{ top: `0px` }} />
                 <img className="shop-item-spring-toons-fellowship-intro" alt=" " style={{ top: `0px` }} />s
             </>
         ),
